4a775700fd8f7c9d4b58c17cba6d9565f62b83ef
Diane Wakim <diane.wakim@web-education.net>
2013-03-01 18:36:00 +0100

    started tracer application logger

--- src/main/java/edu/one/core/module/Tracer.java
+++ src/main/java/edu/one/core/module/Tracer.java
@@ -1,17 +1,61 @@
 package edu.one.core.module;
 
 
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import org.vertx.java.busmods.BusModBase;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
 
 /**
  *
  * @author rafik
  */
-public class Tracer extends BusModBase {
-	
+public class Tracer extends BusModBase implements Handler<Message<JsonObject>> {
+
+
+@Override
+public void start() {
+super.start();
+vertx.eventBus().registerHandler("one.address", this);
+Logger tracer = java.util.logging.Logger.getLogger("one.tracer");
+Formatter formatter = new SimpleFormatter();
+FileHandler handler = null;
+		try {
+			handler = new FileHandler("./data/dev/trace", true);
+		} catch (IOException | SecurityException ex) {
+			//Logger.getLogger(Tracer.class.getName()).log(Level.SEVERE, null, ex);
+		}
+handler.setFormatter(formatter);
+tracer.addHandler(handler);
+
+
+vertx.eventBus().publish("one.address", new JsonObject().putString("app", "Directory").putString("action", "blablablu"));
 }
+
+@Override
+public void stop() throws Exception {
+super.stop();
+}
+
+@Override
+public void handle(Message<JsonObject> m) {
+	Logger.getLogger("one.tracer").log(Level.OFF,JsonFormatter(m.body).toString());
+}
+
+public JsonObject JsonFormatter(JsonObject message){
+	JsonObject log = new JsonObject()
+			.putString("app", message.getField("app").toString())
+			.putString("date", Calendar.getInstance().getTime().toString())
+			.putString("message", message.getField("action").toString());
+	return log;
+}
+
+}
+
