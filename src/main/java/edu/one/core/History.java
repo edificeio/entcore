1a8a302aa6f2086be2ab82b982ae925e5eda98b8
Diane Wakim <diane.wakim@web-education.net>
2013-03-04 17:28:58 +0100

    [History] basic log display

--- src/main/java/edu/one/core/History.java
+++ src/main/java/edu/one/core/History.java
@@ -4,15 +4,44 @@
  */
 package edu.one.core;
 
-import edu.one.core.infra.Controller;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.platform.Verticle;
+>>>>>>> 7285ff9... [History] basic log display
 
 /**
  *
  * @author rafik
  */
-public class History extends Controller implements Handler<Message<String>> {
+public class History extends Verticle implements Handler<Message<String>> {
+	private Logger log;
+
+	@Override
+	public void start() throws Exception {
+		log = container.getLogger();
+		log.info(container.getConfig().getString("test"));
+		
+		
+		RouteMatcher rm = new RouteMatcher();
+		rm.get("/", new Handler<HttpServerRequest> () {
+			public void handle(HttpServerRequest req) {
+				req.response.sendFile("./data/dev/trace");
+			}
+		});
+		vertx.createHttpServer().requestHandler(rm).listen(container.getConfig().getInteger("port"));
+
+	}
 
 	@Override
 	public void handle(Message<String> event) {
