b0ce42e19d830c7d5fb63cf9bc81f917df1f9d60
Diane Wakim <diane.wakim@web-education.net>
2013-03-08 18:16:33 +0100

    [Fix] History adjustments
    - deleted logs view
    - History html form displays selected option

--- src/main/java/edu/one/core/History.java
+++ src/main/java/edu/one/core/History.java
@@ -4,7 +4,14 @@
  */
 package edu.one.core;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
@@ -30,14 +37,33 @@ public class History extends Verticle implements Handler<Message<String>> {
 		RouteMatcher rm = new RouteMatcher();
 		rm.get("/", new Handler<HttpServerRequest> () {
 			public void handle(HttpServerRequest req) {
-				try {
-					if (req.params().isEmpty()){
-						req.response.sendFile("view/logs/logs.html");
-					} else {
-						req.response.sendFile(new File(".").getCanonicalPath() + config.getString("log-path") + req.params().get("app").toLowerCase() + ".trace");
+				String html = "<!DOCTYPE html><html><head><title>One : Logs</title></head>"
+						+ "<body><form method=\"GET\" action=\"http://localhost:8006/\">"
+						+ "<select name=\"app\"><option>all</option><option>sync</option>"
+						+ "<option>history</option><option>directory</option></select>"
+						+ "<input type=\"submit\" value=\"see logs\"/></form>";
+				req.response.setChunked(true);
+				if (req.params().isEmpty()){
+					req.response.write(html);
+					req.response.end("</body></html>");
+				} else {
+					req.response.write(html.replace("<option>"+req.params().get("app").toLowerCase()
+							, "<option selected=\"selected\">"+req.params().get("app").toLowerCase()));
+					req.response.write("<p>");
+					try {
+						Path readFile = Paths.get(new File(".").getCanonicalPath() + config.getString("log-path") 
+								+ req.params().get("app").toLowerCase() + ".trace");
+						BufferedReader br = Files.newBufferedReader(readFile, Charset.forName("UTF-8"));
+						String thisLine;
+						while ((thisLine = br.readLine()) != null) {
+							req.response.write(thisLine + "\n");
+						}
+						br.close();
+					} catch (IOException ex) {
+						log.info(ex);
 					}
-				} catch (Exception ex) {
-					log.info(ex);
+					req.response.write("</p>");
+					req.response.end("</body></html>");
 				}
 			}
 		});
