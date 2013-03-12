48fe76b8db2c5816040ad45c1c6f0ea8e3546ffd
Diane Wakim <diane.wakim@web-education.net>
2013-03-12 19:10:39 +0100

    [Tmp] Bootstrap Directory app

--- src/main/java/edu/one/core/Directory.java
+++ src/main/java/edu/one/core/Directory.java
@@ -1,44 +1,31 @@
 package edu.one.core;
 
-import com.hazelcast.config.Config;
-import edu.one.core.infra.Neo;
+import edu.one.core.infra.Controller;
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.buffer.Buffer;
-import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.json.JsonObject;
-import org.vertx.java.core.logging.Logger;
-import org.vertx.java.core.net.NetSocket;
-import org.vertx.java.platform.Verticle;
 
-public class Directory extends Verticle implements Handler<Message<String>> {
-
-	private Logger log;
-	private JsonObject config;
+public class Directory extends Controller{
 
 	@Override
 	public void start() throws Exception {
-		log = container.getLogger();
-		config = container.getConfig();
-		log.info(config.getString("test"));
-
-		// Test
-		vertx.createNetServer().connectHandler(new Handler<NetSocket>() {
-			public void handle(final NetSocket socket) {
-				socket.dataHandler(new Handler<Buffer>() {
-					public void handle(Buffer b) {
-						JsonObject query = new JsonObject()
-								.putString("action", "execute")
-								.putString("query", b.toString());
-//					JsonObject result = new Neo(vertx.eventBus(),"wse.neo4j.persistor").send(query);
-//						log.info("REPLY " + result);
-					}
-				});
+		super.start();
+		final JsonObject dataMock = new JsonObject(vertx.fileSystem().readFileSync("directory-data-mock.json").toString());
+		
+		rm.get("/directory/admin", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(HttpServerRequest request) {
+				renderView(request, new JsonObject());
 			}
-		}).listen(1234);
+		});
+		
+		rm.get("/directory/api/ecole", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(HttpServerRequest request) {
+				renderJson(request.response, dataMock.getObject("ecole"));
+
+			}
+		});
 	}
 
-	@Override
-	public void handle(Message<String> event) {
-		log.info(event.body);
-	}
 }
