0e90e368eb5146bf825cffb5efc781d9acf163ef
Diane Wakim <diane.wakim@web-education.net>
2013-03-13 19:14:03 +0100

    [Tmp] Directory app and view

--- src/main/java/edu/one/core/Directory.java
+++ src/main/java/edu/one/core/Directory.java
@@ -1,8 +1,11 @@
 package edu.one.core;
 
 import edu.one.core.infra.Controller;
+import java.util.AbstractMap;
+import java.util.Map;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 
 public class Directory extends Controller{
@@ -23,7 +26,54 @@ public class Directory extends Controller{
 			@Override
 			public void handle(HttpServerRequest request) {
 				renderJson(request.response, dataMock.getObject("ecole"));
-
+				
+			}
+		});
+		
+		rm.get("/directory/api/classes", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(HttpServerRequest request) {
+				request.response.putHeader("content-type", "text/json");
+				request.response.end(dataMock.getArray("classes").encode());
+				//renderJson(request.response, dataMock.getObject("classes"));
+				
+			}
+		});
+		
+		rm.get("/directory/api/personnes", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(HttpServerRequest request) {
+				request.response.putHeader("content-type", "text/json");
+				request.response.end(dataMock.getArray("personnes").encode());
+				//renderJson(request.response, dataMock.getObject("personnes"));
+				
+			}
+		});
+		
+		rm.get("/directory/api/details", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(HttpServerRequest request) {
+				JsonArray people = dataMock.getArray("personnes");
+				for (Object object : people) {
+					JsonObject jo = (JsonObject)object;
+					if (jo.getInteger("id").equals(new Integer(request.params().get("id")))){
+						renderJson(request.response, jo);
+					}
+				}
+			}
+		});
+		
+		rm.post("/directory/api/edit", new Handler<HttpServerRequest>() {
+			@Override
+			public void handle(final HttpServerRequest request) {
+				bodyToParams(request, new Handler<Map<String, String>>() {
+					@Override
+					public void handle(Map<String, String> params) {
+						JsonObject jo = new JsonObject().putString("message", "success");
+						renderJson(request.response, new JsonObject());
+					}
+				});
+				
 			}
 		});
 	}
