55aafc89c22a15819c9ba8eebd168f3fc99fd979
Rafik Djedjig <rafik.djedjig@web-education.net>
2013-03-18 12:24:32 +0100

    [Fix] Refactor History admin
    * TODO : Modify logFormatter in Tracer Module
    * Trace file pseudo-Json Format is :
    - - -
    {"date":"<date>","appli":"<appName>","message":"<message>"},
    {"date":"<date1>","appli":"<appName1>","message":"<message1>"},
    {"date":"<date2>","appli":"<appName2>","message":"<message2>"},
    - - - -

--- src/main/java/edu/one/core/History.java
+++ src/main/java/edu/one/core/History.java
@@ -1,60 +1,47 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.one.core;
 
 import edu.one.core.infra.Controller;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 
-/**
- *
- * @author rafik
- */
 public class History extends Controller {
 
 	@Override
 	public void start() throws Exception {
 		super.start();
-		
+
 		rm.get("/history/admin", new Handler<HttpServerRequest>() {
 			@Override
 			public void handle(HttpServerRequest request) {
 				renderView(request, new JsonObject());
 			}
 		});
-		
+
 		rm.get("/history/admin/logs", new Handler<HttpServerRequest>() {
 			@Override
-			public void handle(HttpServerRequest request) {
-				String str = "";
+			public void handle(final HttpServerRequest request) {
 				try {
-					str = vertx.fileSystem().readFileSync(Paths.get(new File(".").getCanonicalPath() + "/data/dev/"+request.params().get("app")+".trace").toString()).toString();
+					String tracesFile = config.getString("log-path") + request.params().get("app") + ".trace";
+					renderJson(request, tracesToJson(tracesFile));
 				} catch (Exception ex) {
-					log.info(ex);
+					// TODO : manage end-user error message (i18n , human compatible message ...)
+					renderError(request);
 				}
-				JsonArray arr = new JsonArray();
-				for (String string : str.split("}")) {
-					arr.add(new JsonObject(string + "}"));
-				}
-				request.response.putHeader("content-type", "text/json");
-				request.response.end(arr.encode());
 			}
 		});
-		
+
+	}
+
+	// TODO : Try to writer a logFormatter in Trace Module that avoid this bloat operations
+	private JsonObject tracesToJson(String logFileName) throws Exception {
+		Buffer b = vertx.fileSystem().readFileSync(logFileName);
+		String traces = b.toString().trim();
+		String tracesArray = "[" + traces.substring(0, traces.length() -1) + "]";
+		JsonObject jo = new JsonObject().putArray("records", new JsonArray(tracesArray));
+		return jo;
 	}
 
 }
