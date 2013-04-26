4fb9509307e68f9ff94fda6016c3f057213f9911
Benjamin Perez <benjamin.perez@web-education.net>
2013-04-26 17:39:52 +0200

    [dataDictionnary] [Evo] Add a test for chargeEnseignement generator

--- dataDictionary/src/test/java/edu/one/core/datadictionary/test/GeneratorTest.java
+++ dataDictionary/src/test/java/edu/one/core/datadictionary/test/GeneratorTest.java
@@ -2,6 +2,7 @@ package edu.one.core.datadictionary.test;
 
 import edu.one.core.datadictionary.dictionary.DefaultDictionary;
 import edu.one.core.datadictionary.dictionary.Dictionary;
+import edu.one.core.datadictionary.generation.ChargeEnseignementGenerator;
 import edu.one.core.datadictionary.generation.DisplayNameGenerator;
 import edu.one.core.datadictionary.generation.Generator;
 import edu.one.core.datadictionary.generation.IdGenerator;
@@ -28,6 +29,7 @@ public class GeneratorTest extends TestVerticle {
 		assertEquals(6, new PasswordGenerator().generate().length());
 		assertEquals(23, new IdGenerator().generate().length());
 		assertEquals("H", new SexGenerator().generate("M"));
+		assertEquals("O", new ChargeEnseignementGenerator().generate("ENSEIGNANT"));
 		assertEquals("bob.eponge", new LoginGenerator().generate("Bob", "Éponge"));
 		assertEquals("Bob Éponge", new DisplayNameGenerator().generate("Bob", "Éponge"));
 		testComplete();
@@ -43,10 +45,14 @@ public class GeneratorTest extends TestVerticle {
 		attrs.put("ENTPersonPrenom", Arrays.asList("Bob"));
 		attrs.put("ENTPersonNom", Arrays.asList("L'Éponge"));
 		attrs.put("ENTPersonNomAffichage", null);
+		attrs.put("ENTPersonProfils", Arrays.asList("ENSEIGNANT"));
+		attrs.put("ENTPersonChargeEnseignement", null);
+		attrs.put("ENTPersonIdentifiant", null);
 		attrs = d.generateField(attrs);
 		
 		assertEquals("H", attrs.get("ENTPersonSexe").get(0));
 		assertEquals("Bob L'Éponge", attrs.get("ENTPersonNomAffichage").get(0));
+		assertEquals("O", attrs.get("ENTPersonChargeEnseignement").get(0));
 		testComplete();
 	}
 }
