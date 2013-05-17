825f59843ef401000fffc94e2a1321893724dc24
Cyril Moralès <cyril.morales@web-education.net>
2013-05-17 09:46:41 +0200

    [history] application uses one-app.js

--- history/src/main/resources/public/js/admin.js
+++ history/src/main/resources/public/js/admin.js
@@ -1,69 +1,60 @@
-	var admin = function(){
-		var getAndRender = function (pathUrl, templateName){
-			$.get(pathUrl)
-				.done(function(data) {
-					template.render(templateName, data);
-				})
-				.error(function() { // TODO: Manage error message
-					template.render("error");
-				});
-		};
-
-		var template = {
-			render : function (nom, data) {
-				template[nom](data);
-			},
-			error: function() {
-				$('#log').html("ERREUR !");
-			},
-			logs : function (data) {
-				var logs = data.records;
-				var htmlString = '<li><span class="history-level title">Type</span>'
-					+ '<span class="history-message title">Message</span>'
-					+ '<span class="history-date title">Date</span>'
-					+ '</li>';
-				for (i = 0; i < logs.length; i++){
-					htmlString +=
-						'<li>'
-						+ '<span class="history-level"> ' + logs[i].level + '</span>'
-						+ '<span class="history-message">'+ '<span class="history-app label history-badge-warning">' + logs[i].app + '</span>' + logs[i].message + '</span>'
-						+ '<span class="history-date">' + transformDate(logs[i].date) + '</span>'
-						+ '</li>';
+var admin = function() {
+	var app = Object.create(oneApp);
+	app.scope = "#main";
+	app.start = function() {
+		this.init();
+		window.addEventListener('message', this.action.styleApp, false);
+	};
+	app.define ({
+		template : {
+			list : '<li><span class="history-level title">Type</span><span class="history-message title">Message</span><span class="history-date title">Date</span></li>{{#list}}<li><span class="history-level">{{level}}</span><span class="history-message"><span class="history-app label history-badge-warning">{{app}}</span>{{message}}</span><span class="history-date">{{date}}</span></li>{{/list}}',
+			getAndRenderHistory : function (pathUrl, templateName, elem, dataExtractor){
+				var that = this;
+				if (_.isUndefined(dataExtractor)) {
+					dataExtractor = function (d) { return {list : _.values(d.records)}; };
 				}
-				$('#log').html(htmlString);
-			}
-		};
-
-		var transformDate = function(str) {
-			var tab = str.split(" ");
-			var mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
-			var jours = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
-			var dt = new Date(tab[5] + '-' + tab[2] + '-' + tab[1] + ' ' + tab[3]);
-			return (jours[dt.getDay()] + ' ' + dt.getDay() + ' ' + mois[dt.getMonth()] + ' ' + dt.getFullYear() + ' - ' + tab[3] + ' ' + tab[4]);
-		};
-
-		var receiver = function(event) {
-			if (event.origin == "http://localhost:8008") {
-				$("head").append("<link rel='stylesheet' href='" + event.data + "' media='all' />");
-			}
-		};
-
-		return {
-			init : function() {
-				$('body').delegate('#historique', 'click',function(event) {
-					event.preventDefault();
-					if (!event.target.getAttribute('call')) return;
-					var call = event.target.getAttribute('call');
-					admin[call]({url : event.target.getAttribute('href'), id: event.id});
+				$.get(pathUrl)
+				.done(function(data) {
+					$(elem).html(that.render(templateName, dataExtractor(data)));
+				})
+				.error(function(data) {
+					oneApp.notify.error(data);
 				});
-				window.addEventListener('message', receiver, false);
-			},
+			}
+		},
+		action : {
 			logs : function(o) {
-				getAndRender(o.url, "logs");
+				app.template.getAndRenderHistory(o.url, "list", "#log", function(data) {
+					var transformDate = function(str) {
+						var tab = str.split(" ");
+						var mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre"];
+						var jours = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
+						var dt = new Date(tab[5] + '-' + tab[2] + '-' + tab[1] + ' ' + tab[3]);
+						return (jours[dt.getDay()] + ' ' + dt.getDay() + ' ' + mois[dt.getMonth()] + ' ' + dt.getFullYear() + ' - ' + tab[3] + ' ' + tab[4]);
+					};
+					var list = {list: []};
+					for (var i = 0, nb = data.records.length; i < nb; i++) {
+						list.list.push({
+							level : data.records[i].level,
+							app : data.records[i].app,
+							message : data.records[i].message,
+							date : transformDate(data.records[i].date)
+						});
+					}
+					return list;
+				});
+
+			},
+			styleApp : function(e) {
+				if (event.origin == "http://localhost:8008") {
+					$("head").append("<link rel='stylesheet' href='" + e.data + "' media='all' />");
+				}
 			}
 		}
-	}();
-
-	$(document).ready(function(){
-		admin.init(); 
 	});
+	return app;
+}();
+
+$(document).ready(function(){
+	admin.start();
+});
