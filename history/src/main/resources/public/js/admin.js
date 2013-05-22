5cfb87f2eec01028944434615e1042da29645f4c
Rafik Djedjig <rafik.djedjig@web-education.net>
2013-05-22 10:16:07 +0200

    [history] Migrate admin to last version of one-app.js

--- history/src/main/resources/public/js/admin.js
+++ history/src/main/resources/public/js/admin.js
@@ -1,10 +1,6 @@
 var admin = function() {
 	var app = Object.create(oneApp);
 	app.scope = "#main";
-	app.start = function() {
-		this.init();
-		window.addEventListener('message', this.action.styleApp, false);
-	};
 	app.define ({
 		template : {
 			logs : '<li>\
@@ -21,24 +17,9 @@ var admin = function() {
 		},
 		action : {
 			logs : function(o) {
-				var formatDate = function() {
-						return function(str) {
-							var tab = Mustache.render(str, this).split(" ");
-							var dt = new Date(tab[5] + '-' + tab[2] + '-' + tab[1] + ' ' + tab[3]).toLocaleDateString();
-							return dt;
-						};
-					};
-
-				$.get(o.url)
-				.done(function(data) {
-					$('#log').html(app.template.render("logs", _.extend({"records" : data.records}, {"formatDate" : formatDate})));
-				})
-
-			},
-			styleApp : function(e) {
-				if (event.origin == "http://localhost:8008") {
-					$("head").append("<link rel='stylesheet' href='" + e.data + "' media='all' />");
-				}
+				$.get(o.url).done(function(data) {
+					$('#log').html(app.template.render("logs", _.extend({"records" : data.records})));
+				});
 			}
 		}
 	});
@@ -46,5 +27,5 @@ var admin = function() {
 }();
 
 $(document).ready(function(){
-	admin.start();
+	admin.init();
 });
