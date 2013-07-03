8af6ef30c6a4968f1747448539d9d9a3c7742ecb
Diane Wakim <diane.wakim@web-education.net>
2013-07-03 11:53:26 +0200

    [userBook][fix] fix myAccount view display

--- userBook/src/main/resources/public/js/account.js
+++ userBook/src/main/resources/public/js/account.js
@@ -1,16 +1,21 @@
 var account = function(){
 
 	var dataExtractor = function (d) { return {list : _.values(d.result)}; };
+	var personDataExtractor = function(d) {
+		return {"displayName":d.result[0]["displayName"],"address":d.result[0]["address"],
+			"health":d.result[0]["health"],"mood":d.result[0]["mood"],
+			"motto":d.result[0]["motto"],list:_.values(d.result) };
+	};
 
 	var app = Object.create(oneApp);
 	app.scope = "#userBook";
 	app.define ({
 		template : {
 			personne: '\
-				{{#list}}<img src="/public/img/no-avatar.jpg" alt="user" class="avatar"/>\
-				<span class="name">{{displayName}}</span>\
-				<span class="address">{{address}}</span>\
-				<span class="motto">{{motto}}</span>\
+				<img src="/public/img/no-avatar.jpg" alt="user" class="avatar"/>\
+				<p class="name">{{displayName}}</p>\
+				<p class="address">{{address}}</p>\
+				<p class="motto">{{#i18n}}userBook.profile.motto{{/i18n}} : {{motto}}</p>\
 				<img src="/public/img/reveur.png" alt="panda" class="mood"/>\
 				<div class="clear"></div>\
 				<span id="actions"><img src="/public/img/mailto.png" alt="mailto"/>\
@@ -18,14 +23,15 @@ var account = function(){
 				<img src="/public/img/carnet.png" alt="carnet"/>{{#i18n}}userBook.class.edit-notebook{{/i18n}}\
 				<div class="clear"></div><img src="/public/img/files.png" alt="files"/>\
 				{{#i18n}}userBook.class.see-portfolio{{/i18n}}\
-				</span><div>{{#i18n}}userBook.class.mood{{/i18n}} : {{mood}} \
-				{{#i18n}}userBook.class.health{{/i18n}} : {{health}}</div>{{/list}}'
+				<h3>{{#i18n}}userBook.profile.health{{/i18n}}</h3><p>{{health}}</p></div>\
+				<h2>{{#i18n}}userBook.interests{{/i18n}}</h2>\
+				{{#list}}<h3>{{category}}</h3>{{/list}}'
 		},
 		action : {
 			profile : function(url) {
 				$.get(url)
 				.done(function(data){
-					$('#person').html(app.template.render('personne', dataExtractor(data)));
+					$('#person').html(app.template.render('personne', personDataExtractor(data)));
 				})
 			}
 		}
@@ -35,5 +41,5 @@ var account = function(){
 
 $(document).ready(function(){
 	account.init();
-	account.action.profile("/api/person?id=Vlnny020130624161244366")
+	account.action.profile("/api/person?id=Vxxrg020130624161244358")
 });
