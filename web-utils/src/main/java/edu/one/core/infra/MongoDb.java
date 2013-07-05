db7035263c6b00a1139905ba11448decd5517352
Damien BOISSIN <damien.boissin@web-education.net>
2013-07-05 18:23:21 +0200

    [Evo][Core] #81 : mongodb persistor helper

---
+++ web-utils/src/main/java/edu/one/core/infra/MongoDb.java
@@ -0,0 +1,180 @@
+package edu.one.core.infra;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
+
+public class MongoDb {
+
+	private final EventBus eb;
+	private final String address;
+
+	public static enum WriteConcern {
+		NONE, NORMAL, SAFE, MAJORITY, FSYNC_SAFE, JOURNAL_SAFE, REPLICAS_SAFE;
+	}
+
+	public MongoDb(EventBus eb, String address) {
+		this.eb = eb;
+		this.address = address;
+	}
+
+	public void save(String collection, JsonObject document, WriteConcern writeConcern,
+			Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "save");
+		jo.putString("collection", collection);
+		jo.putObject("document", document);
+		if (writeConcern != null) {
+			jo.putString("write_concern", writeConcern.name());
+		}
+		eb.send(address, jo, callback);
+	}
+
+	public void save(String collection, JsonObject document,
+			Handler<Message<JsonObject>> callback) {
+		save(collection, document, null, callback);
+	}
+
+	public void save(String collection, JsonObject document) {
+		save(collection, document, null, null);
+	}
+
+	/**
+	 *
+	 * @param collection
+	 * @param criteria the query argument corresponds to the WHERE statement
+	 * @param objNew the update corresponds to the SET ... statement
+	 * @param upsert if true and document doesn't exist, save it
+	 * @param multi update all document matching query
+	 * @param writeConcern
+	 * @param callback
+	 */
+	public void update(String collection, JsonObject criteria, JsonObject objNew,
+			boolean upsert, boolean multi, WriteConcern writeConcern,
+			Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "update");
+		jo.putString("collection", collection);
+		jo.putObject("criteria", criteria);
+		jo.putObject("objNew", objNew);
+		jo.putBoolean("upsert", upsert);
+		jo.putBoolean("multi", multi);
+		if (writeConcern != null) {
+			jo.putString("write_concern", writeConcern.name());
+		}
+		eb.send(address, jo, callback);
+	}
+
+	public void update(String collection, JsonObject criteria, JsonObject objNew,
+			boolean upsert, boolean multi, Handler<Message<JsonObject>> callback) {
+		update(collection, criteria, objNew, upsert, multi, null, callback);
+	}
+
+	public void update(String collection, JsonObject criteria, JsonObject objNew,
+			boolean upsert, boolean multi) {
+		update(collection, criteria, objNew, upsert, multi, null, null);
+	}
+
+	public void update(String collection, JsonObject criteria, JsonObject objNew) {
+		update(collection, criteria, objNew, false, false, null, null);
+	}
+
+	public void update(String collection, JsonObject criteria, JsonObject objNew,
+			Handler<Message<JsonObject>> callback) {
+		update(collection, criteria, objNew, false, false, null, callback);
+	}
+
+	public void find(String collection, JsonObject matcher, JsonObject sort, JsonObject keys,
+			int skip, int limit, int batchSize, Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "find");
+		jo.putString("collection", collection);
+		jo.putObject("matcher", matcher);
+		jo.putObject("sort", sort);
+		jo.putObject("keys", keys);
+		jo.putNumber("skip", skip);
+		jo.putNumber("limit", limit);
+		eb.send(address, jo, callback);
+	}
+
+	public void find(String collection, JsonObject matcher, JsonObject sort, JsonObject keys,
+			Handler<Message<JsonObject>> callback) {
+		find(collection, matcher, sort, keys, -1, -1, 100, callback);
+	}
+
+	public void find(String collection, JsonObject matcher,
+			Handler<Message<JsonObject>> callback) {
+		find(collection, matcher, null, null, -1, -1, 100, callback);
+	}
+
+	public void findOne(String collection, JsonObject matcher, JsonObject keys,
+			Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "findone");
+		jo.putString("collection", collection);
+		jo.putObject("matcher", matcher);
+		jo.putObject("keys", keys);
+		eb.send(address, jo, callback);
+	}
+
+	public void findOne(String collection, JsonObject matcher,
+			Handler<Message<JsonObject>> callback) {
+		findOne(collection, matcher, null, callback);
+	}
+
+	public void count(String collection, JsonObject matcher,
+			Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "count");
+		jo.putString("collection", collection);
+		jo.putObject("matcher", matcher);
+		eb.send(address, jo, callback);
+	}
+
+	public void delete(String collection, JsonObject matcher, WriteConcern writeConcern,
+			Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "delete");
+		jo.putString("collection", collection);
+		jo.putObject("matcher", matcher);
+		if (writeConcern != null) {
+			jo.putString("write_concern", writeConcern.name());
+		}
+		eb.send(address, jo, callback);
+	}
+
+	public void delete(String collection, JsonObject matcher,
+			Handler<Message<JsonObject>> callback) {
+		delete(collection, matcher, null, callback);
+	}
+
+	public void delete(String collection, JsonObject matcher) {
+		delete(collection, matcher, null, null);
+	}
+
+	public void command(String command, Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "command");
+		jo.putString("command", command);
+		eb.send(address, jo, callback);
+	}
+
+	public void command(String command) {
+		command(command, null);
+	}
+
+	public void getCollections(Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "getCollections");
+		eb.send(address, jo, callback);
+	}
+
+	public void getCollectionStats(String collection, Handler<Message<JsonObject>> callback) {
+		JsonObject jo = new JsonObject();
+		jo.putString("action", "collectionStats");
+		jo.putString("collection", collection);
+		eb.send(address, jo, callback);
+	}
+
+}
