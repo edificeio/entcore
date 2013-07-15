21fb4e655627ab768381ad31201ba202d766fb7a
dboissin <damien.boissin@web-education.net>
2013-07-15 17:02:58 +0200

    [Evo][Processor] #101 : processor to writes files (compile time) with list of secured actions

---
+++ processor/src/main/java/edu/one/core/security/SecuredAction.java
@@ -0,0 +1,17 @@
+package edu.one.core.security;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.SOURCE)
+@Documented
+@Target(ElementType.METHOD)
+public @interface SecuredAction {
+
+	String value();
+
+}
