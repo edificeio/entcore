0f6fa71363b2e82e0a0e0fbbfc78c147dcacd1b8
Rafik Djedjig <rafik.djedjig@web-education.net>
2013-07-16 17:10:13 +0200

    [auth] #21 : dummy authenttication server

--- auth/src/main/java/edu/one/core/auth/Auth.java
+++ auth/src/main/java/edu/one/core/auth/Auth.java
@@ -1,7 +1,12 @@
 package edu.one.core.auth;
 
 import edu.one.core.infra.Controller;
+import edu.one.core.infra.request.CookieUtils;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.VoidHandler;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.json.JsonObject;
 
@@ -14,20 +19,43 @@ public class Auth extends Controller {
 		rm.get("/login", new Handler<HttpServerRequest>() {
 			@Override
 			public void handle(HttpServerRequest request) {
-				if ("admin".equals(request.params().get("email"))
-					&& "admin".equals(request.params().get("password"))) {
-					redirect(request, "http://localhost:8008", "/admin?oneID=1234");
-				} else if (request.params().get("email") != null){
-					renderView(request, new JsonObject().putString("error", "true"));
-				} else {
-					renderView(request, config);
+				JsonObject context = new JsonObject();
+				context.putString("callBack", request.params().get("callBack"));
+				if (request.params().get("error") != null) {
+					context.putObject("error", new JsonObject().putString("message", request.params().get("error")));
 				}
+				renderView(request, context);
 			}
 		});
 
 		rm.post("/login", new Handler<HttpServerRequest>() {
 			@Override
 			public void handle(final HttpServerRequest request) {
+				request.endHandler(new VoidHandler() {
+					@Override
+					public void handle() {
+						String callBack = config.getObject("authenticationServer").getString("loginCallback");
+						try {
+							if (request.formAttributes().get("callback") != null) {
+								callBack =  URLDecoder.decode(request.formAttributes().get("callback"),"UTF-8");
+							}
+						} catch (UnsupportedEncodingException ex) {
+							ex.printStackTrace();
+							renderError(request);
+						}
+						if ("admin".equals(request.formAttributes().get("email"))
+								&& "admin".equals(request.formAttributes().get("password"))) {
+							CookieUtils.set("oneSessionId", "1234", request.response());
+							redirect(request, callBack, "");
+						} else {
+							try {
+								redirect(request, "/login?"
+										+ "callback=" + URLEncoder.encode(callBack, "UTF-8")
+										+ "&error=auth.error.authenticationFailed");
+							} catch (UnsupportedEncodingException ex) {}
+						}
+					}
+				});
 
 			}
 		});
