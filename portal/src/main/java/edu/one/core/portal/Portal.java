0ac4e46524fee2c52d68c33b1d957768e62b44d4
Rafik Djedjig <rafik.djedjig@web-education.net>
2013-08-28 11:49:37 +0200

    [portal] Securize server

--- portal/src/main/java/edu/one/core/portal/Portal.java
+++ portal/src/main/java/edu/one/core/portal/Portal.java
@@ -1,52 +1,20 @@
 package edu.one.core.portal;
 
 import edu.one.core.infra.Server;
-import edu.one.core.infra.http.Renders;
-import edu.one.core.portal.mustache.AssetResourceTemplateFunction;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.http.HttpServerRequest;
+import edu.one.core.portal.service.PortalService;
 
 public class Portal extends Server {
 
 	@Override
 	public void start() {
 		super.start();
-		final Renders render = new Renders(container);
-		try {
-			render.putTemplateFunction("asset", new AssetResourceTemplateFunction(config.getString("skin")));
-		} catch (Exception ex) {
-			log.error(ex.getMessage());
-		}
 
-		rm.get("/portal", new Handler<HttpServerRequest>() {
-			@Override
-			public void handle(HttpServerRequest request) {
-				render.renderView(request);
-			}
-		});
-		
-		rm.get("/theme-documentation", new Handler<HttpServerRequest>() {
-                    @Override
-		    public void handle(HttpServerRequest request){
-			render.renderView(request);
-                    }
-                });
-		
-		rm.get("/apps", new Handler<HttpServerRequest>() {
-                    @Override
-		    public void handle(HttpServerRequest request){
-			render.renderView(request);
-                    }
-                });
+		PortalService service = new PortalService(vertx, container, rm, securedActions);
 
-		rm.getWithRegEx("\\/assets\\/.+", new Handler<HttpServerRequest>() {
-			public void handle(HttpServerRequest request) {
-				request.response().sendFile("." + request.path());
-			}
-		});
+		service.get("/portal", "portal")
+				.get("/theme-documentation", "themeDocumentation")
+				.get("/apps", "apps")
+				.getWithRegEx("\\/assets\\/.+", "assets");
 
 	}
 
---
+++ portal/src/main/java/edu/one/core/portal/service/PortalService.java
@@ -0,0 +1,54 @@
+package edu.one.core.portal.service;
+
+import edu.one.core.infra.Controller;
+import edu.one.core.infra.security.UserUtils;
+import edu.one.core.infra.security.resources.UserInfos;
+import edu.one.core.portal.mustache.AssetResourceTemplateFunction;
+import edu.one.core.security.ActionType;
+import edu.one.core.security.SecuredAction;
+import java.util.Map;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Container;
+
+public class PortalService extends Controller {
+
+	public PortalService(Vertx vertx, Container container, RouteMatcher rm,
+			Map<String, edu.one.core.infra.security.SecuredAction> securedActions) {
+		super(vertx, container, rm, securedActions);
+		try {
+			putTemplateFunction("asset", new AssetResourceTemplateFunction(container.config().getString("skin")));
+		} catch (Exception ex) {
+			log.error(ex.getMessage());
+		}
+	}
+
+	@SecuredAction(value = "portal.auth",type = ActionType.RESOURCE)
+	public void portal(final HttpServerRequest request) {
+		UserUtils.getUserInfos(eb, request, new Handler<UserInfos>() {
+			@Override
+			public void handle(UserInfos user) {
+				JsonObject jo = new JsonObject()
+					.putString("userFirstname", user.getFirstName())
+					.putString("userClass", user.getClassId());
+				renderView(request,jo);
+			}
+		});
+	}
+
+	public void themeDocumentation(HttpServerRequest request) {
+		renderView(request);
+	}
+
+	@SecuredAction(value = "portal.auth",type = ActionType.RESOURCE)
+	public void apps(HttpServerRequest request) {
+		renderView(request);
+	}
+
+	public void assets(HttpServerRequest request) {
+		request.response().sendFile("." + request.path());
+	}
+}
