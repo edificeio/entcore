6c790b1f1f2404ac7fe21aa1bbe45e365e9c2b4a
Camille Wintz <camille.wintz@web-education.net>
2013-09-09 12:56:22 +0200

    [timeline] added message when empty

--- timeline/src/main/resources/public/js/timeline.js
+++ timeline/src/main/resources/public/js/timeline.js
@@ -10,13 +10,17 @@ var timeline = function(){
 										<div class="cell three right-magnet"><em>{{#formatDateTime}}{{#date}}{{$date}}{{/date}}{{/formatDateTime}}</em></div>\
 										<div class="clear"></div>\
 									</div>\
-								{{/results}}'
+								{{/results}}\
+								{{#empty}}\
+									{{#i18n}}timeline.nothingnew{{/i18n}}\
+								{{/empty}}'
 
 		},
 		action : {
 
 			lastNotifications : function (o) {
 				One.get(o.url).done(function(response){
+					response.empty = response.results.length === 0;
 					$('#list').html(app.template.render("lastNotifications", response));
 				});
 			}
