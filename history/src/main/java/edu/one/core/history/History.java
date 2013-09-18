f2d72486a5ee6529104645d6a854387a82dfd49b
Diane Wakim <diane.wakim@web-education.net>
2013-09-18 14:08:54 +0200

    [history][evo] #309 server/controller migration
    [Fix][History] fix refactoring

--- history/src/main/java/edu/one/core/history/History.java
+++ history/src/main/java/edu/one/core/history/History.java
@@ -1,58 +1,21 @@
 package edu.one.core.history;
 
-import static edu.one.core.infra.http.Renders.*;
-
+import edu.one.core.history.controllers.HistoryController;
 import edu.one.core.infra.Server;
-import edu.one.core.infra.http.Renders;
-
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.buffer.Buffer;
-import org.vertx.java.core.http.HttpServerRequest;
-import org.vertx.java.core.json.JsonArray;
-import org.vertx.java.core.json.JsonObject;
+import edu.one.core.infra.request.filter.ActionFilter;
+import edu.one.core.infra.request.filter.SecurityHandler;
 
 public class History extends Server {
 
 	@Override
 	public void start() {
 		super.start();
-		final Renders render = new Renders(container);
+		HistoryController historyController = new HistoryController(vertx, container, rm, securedActions);
 
-		rm.get("/admin", new Handler<HttpServerRequest>() {
-			@Override
-			public void handle(HttpServerRequest request) {
-				JsonObject traceFiles = new JsonObject().putArray("traceFiles", new JsonArray());
-				for (String file : vertx.fileSystem().readDirSync(config.getString("log-path"))) {
-					if (!file.endsWith(".trace")) { continue; }
-					file = file.replace(config.getString("log-path"), "").replace(".trace", "");
-					traceFiles.getArray("traceFiles").addObject(new JsonObject().putString("name", file));
-				}
-				render.renderView(request, traceFiles);
-			}
-		});
+		historyController
+				.get("/admin", "history")
+				.get("/admin/logs","logs");
 
-		rm.get("/admin/logs", new Handler<HttpServerRequest>() {
-			@Override
-			public void handle(final HttpServerRequest request) {
-				try {
-					String tracesFile = config.getString("log-path") + request.params().get("app") + ".trace";
-					renderJson(request, tracesToJson(tracesFile));
-				} catch (Exception ex) {
-					// TODO : manage end-user error message (i18n , human compatible message ...)
-					renderError(request);
-				}
-			}
-		});
-
+		SecurityHandler.addFilter(new ActionFilter(historyController.securedUriBinding(), getEventBus(vertx)));
 	}
-
-	// TODO : Try to writer a logFormatter in Trace Module that avoid this bloat operations
-	private JsonObject tracesToJson(String logFileName) throws Exception {
-		Buffer b = vertx.fileSystem().readFileSync(logFileName);
-		String traces = b.toString().trim();
-		String tracesArray = "[" + traces.substring(0, traces.length() -1) + "]";
-		JsonObject jo = new JsonObject().putArray("records", new JsonArray(tracesArray));
-		return jo;
-	}
-
 }
---
+++ history/src/main/java/edu/one/core/history/controllers/HistoryController.java
@@ -0,0 +1,57 @@
+package edu.one.core.history.controllers;
+
+import edu.one.core.infra.Controller;
+import edu.one.core.security.SecuredAction;
+
+import java.util.Map;
+
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Container;
+
+
+public class HistoryController extends Controller {
+
+	private final String logPath;
+
+	public HistoryController(Vertx vertx, Container container,
+		RouteMatcher rm, Map<String, edu.one.core.infra.security.SecuredAction> securedActions) {
+			super(vertx, container, rm, securedActions);
+			this.logPath = container.config().getString("log-path");
+		}
+
+	@SecuredAction("history.authent")
+	public void history(HttpServerRequest request) {
+		JsonObject traceFiles = new JsonObject().putArray("traceFiles", new JsonArray());
+		for (String file : vertx.fileSystem().readDirSync(logPath)) {
+			if (!file.endsWith(".trace")) { continue; }
+			file = file.replace(logPath, "").replace(".trace", "");
+			traceFiles.getArray("traceFiles").addObject(new JsonObject().putString("name", file));
+		}
+		renderView(request, traceFiles);
+	}
+
+	@SecuredAction("history.authent")
+	public void logs(final HttpServerRequest request) {
+		try {
+			String tracesFile = logPath + request.params().get("app") + ".trace";
+			renderJson(request, tracesToJson(tracesFile));
+		} catch (Exception ex) {
+			// TODO : manage end-user error message (i18n , human compatible message ...)
+			renderError(request);
+		}
+	}
+
+	// TODO : Try to writer a logFormatter in Trace Module that avoid this bloat operations
+	private JsonObject tracesToJson(String logFileName) throws Exception {
+		Buffer b = vertx.fileSystem().readFileSync(logFileName);
+		String traces = b.toString().trim();
+		String tracesArray = "[" + traces.substring(0, traces.length() -1) + "]";
+		JsonObject jo = new JsonObject().putArray("records", new JsonArray(tracesArray));
+		return jo;
+	}
+}
