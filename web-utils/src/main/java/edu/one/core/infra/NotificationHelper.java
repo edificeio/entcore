31be187ee980fcc4dd0b5718043c3f1f008c1c14
dboissin <damien.boissin@web-education.net>
2013-09-19 18:19:53 +0200

    [Evo][Web-utils] email notification with template and i18n

--- web-utils/src/main/java/edu/one/core/infra/NotificationHelper.java
+++ web-utils/src/main/java/edu/one/core/infra/NotificationHelper.java
@@ -3,7 +3,9 @@ package edu.one.core.infra;
 import java.io.IOException;
 import java.util.List;
 
+import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
@@ -15,6 +17,7 @@ import edu.one.core.infra.security.resources.UserInfos;
 public class NotificationHelper {
 
 	private static final String TIMELINE_ADDRESS = "wse.timeline";
+	private static final String EMAIL_ADDRESS = "wse.email";
 	private final EventBus eb;
 	private final Renders render;
 
@@ -40,6 +43,24 @@ public class NotificationHelper {
 		.putString("action", "delete")
 		.putString("resource", resource);
 		eb.send(TIMELINE_ADDRESS, json);
+	}
+
+	public void sendEmail(HttpServerRequest request, String to, String from, String cc, String bcc,
+			String subject, String templateBody, JsonObject templateParams,
+			boolean translateSubject, Handler<Message<JsonObject>> handler) throws IOException {
+		JsonObject json = new JsonObject()
+		.putString("to", to)
+		.putString("from", from)
+		.putString("cc", cc)
+		.putString("bcc", bcc);
+		if (translateSubject) {
+			json.putString("subject", I18n.getInstance().translate(
+					subject, request.headers().get("Accept-Language")));
+		} else {
+			json.putString("subject", subject);
+		}
+		json.putString("body", render.processTemplate(request, templateBody, templateParams));
+		eb.send(EMAIL_ADDRESS, json, handler);
 	}
 
 }
