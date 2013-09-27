137c795a51d4d8d680435a565e0b49bc2d21a824
Camille Wintz <camille.wintz@web-education.net>
2013-09-27 12:04:47 +0200

    [portal] changed input style to make them more obvious

--- userBook/src/main/resources/public/js/account.js
+++ userBook/src/main/resources/public/js/account.js
@@ -113,7 +113,7 @@ var account = function(){
 						<div class="row line" data-category="{{category}}">\
 							<div class="two cell"><span>{{#i18n}}userBook.hobby.{{category}}{{/i18n}}</span></div>\
 							<div class="nine cell">\
-								<input type="text" class="twelve cell" value="{{values}}" /></div>\
+								<input type="text" class="twelve cell inline-editing" value="{{values}}" /></div>\
 							<div class="one cell"><i role="{{visibility}}" href="api/set-visibility?category={{category}}" call="changeVisibility" class="right-magnet"></i></div>\
 							<div class="clear"></div>\
 						</div>\
