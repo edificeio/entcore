07aedf095809736b033fdca085746d78d3ff1821
Camille Wintz <camille.wintz@web-education.net>
2013-10-04 18:45:39 +0200

    [infra] small fix for admin

--- infra/src/main/resources/public/js/one-app.js
+++ infra/src/main/resources/public/js/one-app.js
@@ -1,3 +1,127 @@
+var oneApp = {
+	scope : '#main',
+	init : function() {
+		var that = this;
+		that.i18n.load();
+		$('body').delegate(that.scope, 'click',function(event) {
+			if (!event.target.getAttribute('call')) return;
+			event.preventDefault();
+			if(event.target.getAttribute('disabled') !== null){
+				return;
+			}
+			var call = event.target.getAttribute('call');
+			that.action[call]({url : event.target.getAttribute('href'), target: event.target});
+			event.stopPropagation();
+		});
+		// TODO : refactor web message dispatch policiy
+		window.addEventListener('message', that.message.css, false);
+	},
+	action : {
+	},
+	template : {
+		getAndRender : function (pathUrl, templateName, elem, dataExtractor){
+			var that = this;
+			if (_.isUndefined(dataExtractor)) {
+				dataExtractor = function (d) { return {list : _.values(d.result)}; };
+			}
+			$.get(pathUrl)
+				.done(function(data) {
+					$(elem).html(that.render(templateName, dataExtractor(data)));
+				})
+				.error(function(data) {
+					oneApp.notify.error(data);
+				});
+		},
+		render : function (name, data) {
+			_.extend(data, {
+				'i18n' : oneApp.i18n.i18n,
+				'formatDate' : function() {
+					return function(str) {
+						var dt = new Date(Mustache.render(str, this).replace('CEST', 'EST')).toShortString();
+						return dt;
+					};
+				},
+				'formatDateTime' : function() {
+					return function(str) {
+						var dt = new Date(Mustache.render(str, this).replace('CEST', 'EST')).toShortString();
+						return dt;
+					};
+				},
+				//today, yesterday...
+				calendar: function(){
+					return function(date) {
+						return moment(Mustache.render(date, this).replace('CEST', 'EST')).calendar();
+					};
+				},
+				//0 month 0000
+				longDate: function(){
+					return function(date) {
+						var momentDate = moment(Mustache.render(date, this).replace('CEST', 'EST'));
+						if(momentDate !== null){
+							return momentDate.format('D MMMM YYYY');
+						}
+					};
+				},
+				//0 month
+				longDay: function(){
+					return function(date) {
+						var momentDate = moment(Mustache.render(date, this).replace('CEST', 'EST'));
+						if(momentDate !== null){
+							return momentDate.format('D MMMM');
+						}
+					};
+				}
+			});
+			return Mustache.render(this[name] === undefined ? name : this[name], data);
+		}
+	},
+	notify : {
+		done : function (msg) { this.instance('success')(msg);},
+		error : function (msg) { this.instance('error')(msg); },
+		warn : function (msg) {},
+		info : function (msg) { this.instance('info')(msg); },
+		instance : function(level) {
+			return humane.spawn({ addnCls: 'humane-original-' + level });
+		}
+	},
+	i18n : {
+		load : function () {
+			var that = this;
+			$.ajax({url: 'i18n', async: false})
+				.done(function(data){
+					that.bundle = data;
+				})
+		},
+		bundle : {},
+		i18n : function() {
+			return function(key) {
+				key = Mustache.render(key, this);
+				return oneApp.i18n.bundle[key] === undefined ? key : oneApp.i18n.bundle[key];
+			};
+		},
+		translate: function(key){
+			return this.i18n()(key);
+		}
+	},
+	message : {
+		// TODO : dispatch policiy and paraméter
+		css : function(e) {
+			if (event.origin == "http://localhost:8008") {
+				$("head").append("<link rel='stylesheet' href='" + e.data + "' media='all' />");
+			}
+		}
+	},
+	define : function (o) {
+		var props = { template : {}, action:{}};
+		for (prop in props) {
+			for (key in o[prop]) {
+				props[[prop]][key] = {'value' : o[[prop]][key]};
+			}
+			Object.defineProperties(this[prop], props[[prop]]);
+		}
+	}
+};
+
 var oneModule = angular.module('one', ['ngSanitize'], function($interpolateProvider) {
 		$interpolateProvider.startSymbol('[[');
 		$interpolateProvider.endSymbol(']]');
@@ -152,132 +276,3 @@ oneModule.directive('enhancedSelect', function($compile) {
 			</div>'
 	};
 });
-
-
-var oneApp = {
-	scope : '#main',
-	init : function() {
-		var that = this;
-		that.i18n.load();
-		$('body').delegate(that.scope, 'click',function(event) {
-			if (!event.target.getAttribute('call')) return;
-			event.preventDefault();
-			if(event.target.getAttribute('disabled') !== null){
-				return;
-			}
-			var call = event.target.getAttribute('call');
-			that.action[call]({url : event.target.getAttribute('href'), target: event.target});
-			event.stopPropagation();
-		});
-		// TODO : refactor web message dispatch policiy
-		window.addEventListener('message', that.message.css, false);
-	},
-	action : {
-	},
-	template : {
-		getAndRender : function (pathUrl, templateName, elem, dataExtractor){
-			var that = this;
-			if (_.isUndefined(dataExtractor)) {
-				dataExtractor = function (d) { return {list : _.values(d.result)}; };
-			}
-			$.get(pathUrl)
-			.done(function(data) {
-				$(elem).html(that.render(templateName, dataExtractor(data)));
-			})
-			.error(function(data) {
-				oneApp.notify.error(data);
-			});
-		},
-		render : function (name, data) {
-			_.extend(data, {
-				'i18n' : oneApp.i18n.i18n,
-				'formatDate' : function() {
-					return function(str) {
-						var dt = new Date(Mustache.render(str, this).replace('CEST', 'EST')).toShortString();
-						return dt;
-					};
-				},
-				'formatDateTime' : function() {
-					return function(str) {
-						var dt = new Date(Mustache.render(str, this).replace('CEST', 'EST')).toShortString();
-						return dt;
-					};
-				},
-				//today, yesterday...
-				calendar: function(){
-					return function(date) {
-						return moment(Mustache.render(date, this).replace('CEST', 'EST')).calendar();
-					};
-				},
-				//0 month 0000
-				longDate: function(){
-					return function(date) {
-						var momentDate = moment(Mustache.render(date, this).replace('CEST', 'EST'));
-						if(momentDate !== null){
-							return momentDate.format('D MMMM YYYY');
-						}
-					};
-				},
-				//0 month
-				longDay: function(){
-					return function(date) {
-						var momentDate = moment(Mustache.render(date, this).replace('CEST', 'EST'));
-						if(momentDate !== null){
-							return momentDate.format('D MMMM');
-						}
-					};
-				}
-			});
-			return Mustache.render(this[name] === undefined ? name : this[name], data);
-		}
-	},
-	notify : {
-		done : function (msg) { this.instance('success')(msg);},
-		error : function (msg) { this.instance('error')(msg); },
-		warn : function (msg) {},
-		info : function (msg) { this.instance('info')(msg); },
-		instance : function(level) {
-			return humane.spawn({ addnCls: 'humane-original-' + level });
-		}
-	},
-	i18n : {
-		load : function () {
-			var that = this;
-			$.ajax({url: 'i18n', async: false})
-				.done(function(data){
-					that.bundle = data;
-				})
-		},
-		bundle : {},
-		i18n : function() {
-			return function(key) {
-				key = Mustache.render(key, this);
-				return oneApp.i18n.bundle[key] === undefined ? key : oneApp.i18n.bundle[key];
-			};
-		},
-		translate: function(key){
-			return this.i18n()(key);
-		}
-	},
-	message : {
-		// TODO : dispatch policiy and paraméter
-		css : function(e) {
-			if (event.origin == "http://localhost:8008") {
-				$("head").append("<link rel='stylesheet' href='" + e.data + "' media='all' />");
-			}
-		}
-	},
-	define : function (o) {
-		var props = { template : {}, action:{}};
-		for (prop in props) {
-			for (key in o[prop]) {
-				props[[prop]][key] = {'value' : o[[prop]][key]};
-			}
-			Object.defineProperties(this[prop], props[[prop]]);
-		}
-	}
-};
-
-var event = document.createEvent("Event");
-event.initEvent("oneLoaded", true, true);
-window.dispatchEvent(event);
