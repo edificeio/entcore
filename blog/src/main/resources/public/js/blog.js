4a09add3cdb8041331ecc36f2cb593ef11276dc3
Diane Wakim <diane.wakim@web-education.net>
2013-10-07 18:58:15 +0200

    [blog][evo] views and creation forms

--- blog/src/main/resources/public/js/blog.js
+++ blog/src/main/resources/public/js/blog.js
@@ -1,8 +1,33 @@
+var views = {
+	"createBlog":{
+		"path":"public/template/create-blog.html",
+		"allow":true
+	},
+	"editBlog":{
+		"path":"public/template/create-edit-blog.html",
+		"allow":true
+	},
+	"createPost":{
+		"path":"public/template/create-post.html",
+		"allow":true
+	},
+	"editPost":{
+		"path":"public/template/create-edit-post.html",
+		"allow":true
+	},
+	"commentBlog":{
+		"path":"public/template/comment-post.html",
+		"allow":true
+	}
+}
+
 function Blog($scope, http, lang, date, notify){
 	$scope.blogs = [];
 	$scope.currentBlog = {};
 
-	$scope.createPostPath = '';
+	$scope.creationFormPath = '';
+	$scope.mainContentPath = '';
+	$scope.currentView = '';
 
 	http.get('public/mock/mock-blogs-list.json')
 		.done(function(data){
@@ -13,6 +38,7 @@ function Blog($scope, http, lang, date, notify){
 	http.get('public/mock/mock-last-posts.json')
 		.done(function(data){
 			$scope.currentBlog = data;
+			$scope.currentView= views.createPost;
 			$scope.$apply();
 		});
 
@@ -20,15 +46,27 @@ function Blog($scope, http, lang, date, notify){
 		http.get('public/mock/mock-blog-' + id + '.json')
 			.done(function(data){
 				$scope.currentBlog = data;
+				$scope.creationFormPath = '';
+				$scope.currentView= '';
 				$scope.$apply();
 			});
 	};
+
 	$scope.isSelected = function(id){
-		return (id === $scope.currentBlog.id) ? true : false;
+		return (id == $scope.currentBlog.id);
+	}
+	$scope.isDisabled = function(){
+		return ($scope.currentView === views.createPost);
 	}
 
 	$scope.showCreatePost = function(){
-		$scope.createPostPath = ($scope.createPostPath === '') ? 'public/template/create-post.html' : '';
+		$scope.currentView = views.createPost;
+		$scope.creationFormPath = $scope.currentView.path;
+	}
+	$scope.showCreateBlog= function(){
+		$scope.currentBlog = '';
+		$scope.currentView = views.createBlog,
+		$scope.creationFormPath = $scope.currentView.path;
 	}
 
 	$scope.createPost = function(){};
