dfbcbe7517f56be516d6634ae9d7db5dc118df17
Camille Wintz <camille.wintz@web-education.net>
2013-10-28 14:49:06 +0100

    [infra] removed new page button

--- infra/src/main/resources/public/ckeditor/config.js
+++ infra/src/main/resources/public/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function(config){
 	config.allowedContent = true;
 	config.extraPlugins = 'upload';
 	config.toolbar = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: ['NewPage', 'Templates' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: ['Templates' ] },
 		{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', '-', 'RemoveFormat' ] },
 		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
