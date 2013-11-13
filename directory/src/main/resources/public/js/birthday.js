fffda237ad65a47e8272ef4e804c5a9defc0020a
Camille Wintz <camille.wintz@web-education.net>
2013-11-13 12:07:57 +0100

    [directory] added birthdays widget

--- directory/src/main/resources/public/js/birthday.js
+++ directory/src/main/resources/public/js/birthday.js
@@ -1,9 +1,20 @@
 (function(){
 	var Birthday = LoadedWidgets.findWidget('birthday');
 
+	Birthday.getDay = function(date){
+		return moment(date).date();
+	}
+
+	Birthday.getMonth = function(date){
+		return moment(date).format('MMMM');
+	}
+
 	One.get('/userbook/person/birthday').done(function(birthdays){
-		Birthday.birthdays = birthdays;
+		Birthday.birthdays = _.filter(birthdays, function(birthday){
+			return moment(birthday.birthDate).month() === moment().month();
+		});
+		LoadedWidgets.apply();
 	});
 
-	LoadedWidgets.apply();
+
 }());
