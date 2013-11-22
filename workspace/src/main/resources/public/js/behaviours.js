4d073162de4479204ad3bdca1ca5c17bcfc93945
Camille Wintz <camille.wintz@web-education.net>
2013-11-22 16:37:03 +0100

    [workspace] behaviours added

--- workspace/src/main/resources/public/js/behaviours.js
+++ workspace/src/main/resources/public/js/behaviours.js
@@ -1,12 +1,50 @@
-Behaviours.addApplication('workspace', {
-	root: function(){
-		return {
+function hasRight(resource, name){
+	var currentSharedRights = _.filter(resource.shared, function(sharedRight){
+		return Model.me.profilGroupsIds.indexOf(sharedRight.groupId) !== -1
+			|| sharedRight.userId === Model.me.userId;
+	});
 
+	return _.find(currentSharedRights, function(right){
+		return right[name];
+	}) !== undefined;
+}
+
+var behaviours = {
+	resources: {
+		comment: {
+			right: 'edu-one-core-workspace-service-WorkspaceService|commentDocument',
+			apply: function(){
+
+			}
+		},
+		copy: {
+			right: 'edu-one-core-workspace-service-WorkspaceService|moveDocument'
+		},
+		move: {
+			right: 'edu-one-core-workspace-service-WorkspaceService|moveDocument'
+		},
+		moveTrash: {
+			right: 'edu-one-core-workspace-service-WorkspaceService|moveTrash'
 		}
 	},
-	resource: function(resource){
-		return {
+	root: {
+		create: function(){
 
 		}
 	}
-})
+}
+
+Behaviours.register('workspace', {
+	resource: function(resource){
+		for(var behaviour in behaviours.resources){
+			if(hasRight(resource, behaviours.resources[behaviour].right)){
+				resource[behaviour] = behaviours.resources[behaviour].right;
+			}
+		}
+
+		return resource;
+	},
+	root: function(){
+		return behaviours.root;
+	}
+});
