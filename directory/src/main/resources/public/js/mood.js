57d6ebc6fdf28b703b41d0485b3fcd8d06758fbe
Camille Wintz <camille.wintz@web-education.net>
2013-12-17 17:03:22 +0100

    [widgets] mood widget

--- directory/src/main/resources/public/js/mood.js
+++ directory/src/main/resources/public/js/mood.js
@@ -1,5 +1,36 @@
 (function(){
 	var Mood = Model.widgets.findWidget('mood');
 
-	Model.widgets.apply();
+	var availableMoods = ['happy','proud','dreamy','love','tired','angry','worried','sick','joker','sad'];
+	Mood.mood = 'default';
+
+	Mood.updateMood = function(){
+		http().get('/userbook/api/edit-userbook-info?prop=mood&value=' + Mood.mood.id);
+	};
+
+	lang.addBundle('/directory/i18n', function(){
+		Mood.moods = [];
+		availableMoods.forEach(function(mood){
+			Mood.moods.push({
+				icon: mood,
+				text: lang.translate('userBook.mood.' + mood),
+				id: mood
+			})
+		});
+		http().get('/userbook/api/person').done(function(data){
+			var currentMood = data.result[0].mood;
+			if(currentMood === 'default'){
+				Mood.mood = {
+					icon: 'default',
+					text: lang.translate('userBook.mood.default'),
+					id: 'default'
+				};
+			}
+			else{
+				Mood.mood = _(Mood.moods).where({id: currentMood})[0];
+			}
+
+			Model.widgets.apply();
+		});
+	});
 }());
