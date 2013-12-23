e161dd1cac1c6920035350ac30171b3af555acac
dboissin <damien.boissin@web-education.net>
2013-12-23 17:32:36 +0100

    [Evo][Common] string validation

---
+++ common/src/main/java/edu/one/core/common/validation/StringValidation.java
@@ -0,0 +1,13 @@
+package edu.one.core.common.validation;
+
+import java.util.regex.Pattern;
+
+public class StringValidation {
+
+	private static final Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$");
+
+	public static boolean isEmail(String email) {
+		return email != null && emailPattern.matcher(email).matches();
+	}
+
+}
