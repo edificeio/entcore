668880839d264f09ddbf0fe7498a3adc3a470781
Camille Wintz <camille.wintz@web-education.net>
2014-01-08 15:13:24 +0100

    [directory] #796 new lines for hobbies weren't working

--- directory/src/main/resources/public/js/account.js
+++ directory/src/main/resources/public/js/account.js
@@ -71,7 +71,7 @@ function MyAccount($scope, http, lang, date, notify, _){
 
 	$scope.saveHobby = function(hobby){
 		//new lines formatting
-		hobby.values = hobby.values.replace(/[\n]/g, '%0a');
+		//hobby.values = hobby.values.replace(/[\n]/g, '%0a');
 		http.get('api/edit-userbook-info', hobby);
 	};
 
