561a34dec9ca8cc0c3326c9d90d6b0f0c330ca08
Camille Wintz <camille.wintz@web-education.net>
2014-01-21 17:48:47 +0100

    [workspace] #730 added order by arrows

--- workspace/src/main/resources/public/js/model.js
+++ workspace/src/main/resources/public/js/model.js
@@ -6,14 +6,36 @@ function Document(){
 
 }
 
-function Folder(){
+function Folder(data){
+	this.updateData(data);
+	this.collection(Document,  {
 
+	})
 }
 
-function Rack(){
-
-}
 
 Model.build = function(){
+	this.collection(User, {
 
-}
+	});
+
+	this.collection(Folder, {
+	});
+
+	this.folders.rack = new Folder();
+	this.folders.myDocuments = new Folder({
+		children: [],
+		appendFolderToParent: function(folder){
+			var parentsFolders = folder.folder.split('_');
+			if(parentsFolders)
+			var parentName = parentsFolders[parentsFolders.length - 2];
+			this.children.forEach(function(folder){
+				if(parentsFolders.indexOf(folder.name) !== -1){
+
+				}
+			})
+		}
+	});
+	this.folders.shared = new Folder();
+	this.folders.trash = new Folder();
+};
