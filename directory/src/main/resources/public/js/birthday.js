e3482f39a9c658ad76edb3f3707e490d09b318d5
Camille Wintz <camille.wintz@web-education.net>
2014-02-17 11:11:35 +0100

    [directory] birthday widget temporary fix

--- directory/src/main/resources/public/js/birthday.js
+++ directory/src/main/resources/public/js/birthday.js
@@ -14,48 +14,49 @@
 
 	Birthday.birthdaysInClass = function(){
 		return _.filter(Birthday.birthdays, function(birthday){
-			return birthday.classes.indexOf(Birthday.currentClass) !== -1;
+			return _.find(birthday.classes, function(b){ return b[0] === Birthday.currentClass.id }) !== undefined;
 		});
 	};
 
 	Birthday.saveDefaultClass = function(){
-		http().get('/userbook/api/edit-userbook-info?prop=userPreferencesBirthdayClass&value=' + Birthday.currentClass);
+		http().get('/userbook/api/edit-userbook-info?prop=userPreferencesBirthdayClass&value=' + Birthday.currentClass.id);
 	};
 
 	http().get('/userbook/user-preferences').done(function(result){
-		Birthday.currentClass = result.result[0].userPreferencesBirthdayClass;
-		if(Birthday.classes.indexOf(Birthday.currentClass) === -1 && Birthday.currentClass !== ''){
-			Birthday.classes.push(Birthday.currentClass);
-		}
-		model.widgets.apply();
-	});
-
-	http().get('/userbook/person/birthday').done(function(birthdays){
-		lang.addBundle('/directory/i18n', function(){
-			Birthday.emptyList = lang.translate('nobirthday');
-			Birthday.birthdays = _.filter(birthdays, function(birthday){
-				return moment(birthday.birthDate).month() === moment().month();
-			});
-
-			Birthday.birthdays = Birthday.birthdays.sort(function(a, b){
-				return moment(a.birthDate).date() - moment(b.birthDate).date()
-			});
-
-			var classes = [];
-			classes = _.pluck(Birthday.birthdays, 'classes');
-			classes.forEach(function(classList){
-				classList.forEach(function(className){
-					if(Birthday.classes.indexOf(className) === -1){
-						Birthday.classes.push(className);
-					}
+		Birthday.currentClass = { id: result.result[0].userPreferencesBirthdayClass };
+		http().get('/userbook/person/birthday').done(function(birthdays){
+			lang.addBundle('/directory/i18n', function(){
+				Birthday.emptyList = lang.translate('nobirthday');
+				Birthday.birthdays = _.filter(birthdays, function(birthday){
+					return moment(birthday.birthDate).month() === moment().month();
 				});
+
+				Birthday.birthdays = Birthday.birthdays.sort(function(a, b){
+					return moment(a.birthDate).date() - moment(b.birthDate).date()
+				});
+
+				var classes = [];
+				classes = _.pluck(Birthday.birthdays, 'classes');
+				classes.forEach(function(classList){
+					classList.forEach(function(myClass){
+						if(!_.findWhere(Birthday.classes, {id :myClass[0] })){
+							Birthday.classes.push({
+								name: myClass[1],
+								id: myClass[0]
+							});
+						}
+					});
+				});
+
+				Birthday.currentClass = _.findWhere(Birthday.classes, { id: Birthday.currentClass.id })
+				if(!Birthday.currentClass){
+					Birthday.currentClass = Birthday.classes[0];
+				}
+
+				model.widgets.apply();
 			});
-
-			if(!Birthday.currentClass){
-				Birthday.currentClass = Birthday.classes[0];
-			}
-
-			model.widgets.apply();
 		});
 	});
+
+
 }());
