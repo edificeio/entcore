67b7a46bce97207cfa9cff10beed2681617d051e
Benjamin Perez <benjamin.perez@web-education.net>
2014-02-21 17:38:49 +0100

    [directory][i18n] Key mistake

--- directory/src/main/java/org/entcore/directory/services/impl/DefaultUserService.java
+++ directory/src/main/java/org/entcore/directory/services/impl/DefaultUserService.java
@@ -135,7 +135,7 @@ public class DefaultUserService implements UserService {
 							.putString("login", login);
 					logger.debug(json.encode());
 					notification.sendEmail(request, email, null, null,
-							"email.user.created", "email/userCreated.html", json, true,
+							"email.user.created.info", "email/userCreated.html", json, true,
 							new Handler<Message<JsonObject>>() {
 
 								@Override
