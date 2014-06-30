ef94ac5a2c608483db8ea20a6882a2d9ac46c3b1
dboissin <damien.boissin@web-education.net>
2014-06-30 17:54:57 +0200

    [Test][Feeder] import update test

--- feeder/src/test/java/org/entcore/feeder/test/integration/java/FeederTest.java
+++ feeder/src/test/java/org/entcore/feeder/test/integration/java/FeederTest.java
@@ -13,6 +13,7 @@ import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.testtools.TestVerticle;
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.vertx.testtools.VertxAssert.assertEquals;
@@ -23,22 +24,27 @@ public class FeederTest extends TestVerticle {
 
 	public static final String NEO4J_PERSISTOR = "neo4j.persistor";
 	private String neo4jDeploymentId;
-	private TemporaryFolder tmpFolder;
+	private TemporaryFolder neo4jTmpFolder;
+	private TemporaryFolder importTmpFolder;
 	private EventBus eb;
 	private Neo4j neo4j;
 
 	@Override
 	public void start() {
-		tmpFolder = new TemporaryFolder();
+		neo4jTmpFolder = new TemporaryFolder();
+		importTmpFolder = new TemporaryFolder();
 		try {
-			tmpFolder.create();
+			neo4jTmpFolder.create();
+			importTmpFolder.create();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
+		vertx.fileSystem().copySync(FeederTest.class.getClassLoader().getResource("aaf-test").getPath(),
+				importTmpFolder.getRoot().getAbsolutePath(), true);
 		JsonObject neo4jConfig = new JsonObject()
 			.putString("address", NEO4J_PERSISTOR)
-			.putString("datastore-path", tmpFolder.getRoot().getAbsolutePath())
+			.putString("datastore-path", neo4jTmpFolder.getRoot().getAbsolutePath())
 			.putObject("neo4j", new JsonObject()
 					.putString("node_keys_indexable", "externalId")
 					.putString("node_auto_indexing", "true"));
@@ -49,7 +55,11 @@ public class FeederTest extends TestVerticle {
 				JsonObject config = new JsonObject()
 						.putString("neo4j-address", NEO4J_PERSISTOR)
 						.putString("feeder", "AAF")
-						.putString("import-files", FeederTest.class.getClassLoader().getResource("aaf-test").getPath());
+						.putString("import-files", importTmpFolder.getRoot().getAbsolutePath())
+						.putNumber("delete-user-delay", 1000l)
+						.putNumber("pre-delete-user-delay", 1000l)
+						.putNumber("delete-task-delay", 3 * 60 * 1000l)
+						.putNumber("pre-delete-task-delay", 2 * 60 * 1000l);
 				container.deployModule(System.getProperty("vertx.modulename"), config, 1,
 						new AsyncResultHandler<String>() {
 							public void handle(AsyncResult<String> ar) {
@@ -57,7 +67,7 @@ public class FeederTest extends TestVerticle {
 									eb = vertx.eventBus();
 									neo4j = new Neo4j(eb, NEO4J_PERSISTOR);
 									neo4jDeploymentId = ar.result();
-									neo4j.execute("CREATE (n:System {externalId :'neo4j', name : 'neo4j'})", null,
+									neo4j.execute("CREATE (n:DeleteGroup {externalId :'DeleteGroup'})", null,
 											new Handler<Message<JsonObject>>() {
 										@Override
 										public void handle(Message<JsonObject> event) {
@@ -75,6 +85,9 @@ public class FeederTest extends TestVerticle {
 
 	@Override
 	public void stop() {
+		if (importTmpFolder != null) {
+			importTmpFolder.delete();
+		}
 		if (neo4jDeploymentId != null) {
 			container.undeployModule(neo4jDeploymentId, new Handler<AsyncResult<Void>>() {
 				@Override
@@ -82,8 +95,8 @@ public class FeederTest extends TestVerticle {
 					vertx.setTimer(1000, new Handler<Long>() {
 						@Override
 						public void handle(Long event) {
-							if (tmpFolder != null) {
-								tmpFolder.delete();
+							if (neo4jTmpFolder != null) {
+								neo4jTmpFolder.delete();
 							}
 							FeederTest.super.stop();
 						}
@@ -91,8 +104,8 @@ public class FeederTest extends TestVerticle {
 				}
 			});
 		} else {
-			if (tmpFolder != null) {
-				tmpFolder.delete();
+			if (neo4jTmpFolder != null) {
+				neo4jTmpFolder.delete();
 			}
 			super.stop();
 		}
@@ -106,11 +119,24 @@ public class FeederTest extends TestVerticle {
 				transition(new VoidHandler() {
 					@Override
 					protected void handle() {
-						testComplete();
+						deleteUsersInImport();
+						importUpdate(new VoidHandler() {
+							@Override
+							protected void handle() {
+								testComplete();
+							}
+						});
 					}
 				});
 			}
 		});
+	}
+
+	private void deleteUsersInImport() {
+		vertx.fileSystem().deleteSync(importTmpFolder.getRoot().getAbsolutePath() + File.separator +
+				"FULL_ENTTSSERVICES_Complet_20130117_Eleve_0000.xml");
+		vertx.fileSystem().deleteSync(importTmpFolder.getRoot().getAbsolutePath() + File.separator +
+				"FULL_ENTTSSERVICES_Complet_20130117_PersEducNat_0000.xml");
 	}
 
 	private void transition(final VoidHandler handler) {
@@ -141,6 +167,7 @@ public class FeederTest extends TestVerticle {
 								handler.handle(null);
 							}
 						});
+						eb.unregisterHandler("user.repository", this);
 						break;
 					default:
 						fail("invalid.action");
@@ -171,11 +198,11 @@ public class FeederTest extends TestVerticle {
 				assertEquals("ok", message.body().getString("status"));
 				String countQuery =
 						"MATCH (:User) WITH count(*) as nbUsers " +
-								"MATCH (:Structure) WITH count(*) as nbStructures, nbUsers " +
-								"MATCH (:Class) WITH nbUsers, nbStructures, count(*) as nbClasses " +
-								"MATCH (:FunctionalGroup) WITH nbUsers, nbStructures, nbClasses, count(*) as nbFunctionalGroups " +
-								"MATCH (:ProfileGroup) " +
-								"RETURN nbUsers, nbStructures, nbClasses, nbFunctionalGroups, count(*) as nbProfileGroups";
+						"MATCH (:Structure) WITH count(*) as nbStructures, nbUsers " +
+						"MATCH (:Class) WITH nbUsers, nbStructures, count(*) as nbClasses " +
+						"MATCH (:FunctionalGroup) WITH nbUsers, nbStructures, nbClasses, count(*) as nbFunctionalGroups " +
+						"MATCH (:ProfileGroup) " +
+						"RETURN nbUsers, nbStructures, nbClasses, nbFunctionalGroups, count(*) as nbProfileGroups";
 				neo4j.execute(countQuery, new JsonObject(), new Handler<Message<JsonObject>>() {
 					@Override
 					public void handle(Message<JsonObject> event) {
@@ -189,6 +216,60 @@ public class FeederTest extends TestVerticle {
 						handler.handle(null);
 					}
 				});
+			}
+		});
+	}
+
+	private void importUpdate(final VoidHandler handler) {
+		eb.registerHandler("user.repository", new Handler<Message<JsonObject>>() {
+			@Override
+			public void handle(Message<JsonObject> message) {
+				String action = message.body().getString("action", "");
+				switch (action) {
+					case "delete-users":
+						JsonArray users = message.body().getArray("old-users", new JsonArray());
+						assertEquals(3972 + 698, users.size());
+						String countQuery = "MATCH (:User) RETURN count(*) as nbUsers ";
+						neo4j.execute(countQuery, new JsonObject(), new Handler<Message<JsonObject>>() {
+							@Override
+							public void handle(Message<JsonObject> event) {
+								assertEquals("ok", event.body().getString("status"));
+								JsonObject r = event.body().getArray("result").get(0);
+								assertEquals(15290 - 3972 - 698, (int) r.getInteger("nbUsers", 0));
+								handler.handle(null);
+							}
+						});
+						break;
+					default:
+						fail("invalid.action : " + action);
+				}
+			}
+		}, new AsyncResultHandler<Void>() {
+			@Override
+			public void handle(AsyncResult<Void> event) {
+				if (event.succeeded()) {
+					eb.send("entcore.feeder", new JsonObject().putString("action", "import"),
+							new Handler<Message<JsonObject>>() {
+								@Override
+								public void handle(Message<JsonObject> message) {
+									assertEquals("ok", message.body().getString("status"));
+									String countQuery =
+											"MATCH (u:User) " +
+											"WHERE HAS(u.disappearanceDate) " +
+											"RETURN count(*) as nbUsers ";
+									neo4j.execute(countQuery, new JsonObject(), new Handler<Message<JsonObject>>() {
+										@Override
+										public void handle(Message<JsonObject> event) {
+											assertEquals("ok", event.body().getString("status"));
+											JsonObject r = event.body().getArray("result").get(0);
+											assertEquals(3972 + 698, (int) r.getInteger("nbUsers", 0));
+										}
+									});
+								}
+							});
+				} else {
+					fail();
+				}
 			}
 		});
 	}
