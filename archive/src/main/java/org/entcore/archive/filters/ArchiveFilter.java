f1cac738467cbfd962479e1b58d4ead4868c71dc
dboissin <damien.boissin@web-education.net>
2014-07-16 19:51:56 +0200

    [Fix][Archive] archive filter : remove useless code

--- archive/src/main/java/org/entcore/archive/filters/ArchiveFilter.java
+++ archive/src/main/java/org/entcore/archive/filters/ArchiveFilter.java
@@ -20,17 +20,12 @@
 package org.entcore.archive.filters;
 
 import fr.wseduc.webutils.http.Binding;
-import org.entcore.common.http.filter.BaseResourceProvider;
+import org.entcore.common.http.filter.ResourcesProvider;
 import org.entcore.common.user.UserInfos;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.http.HttpServerRequest;
 
-public class ArchiveFilter extends BaseResourceProvider {
-
-	@Override
-	protected String defaultFilter() {
-		return "downloadExport";
-	}
+public class ArchiveFilter implements ResourcesProvider {
 
 	public void authorize(HttpServerRequest resourceRequest, Binding binding,
 						  UserInfos user, Handler<Boolean> handler) {
