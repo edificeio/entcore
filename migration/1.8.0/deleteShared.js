4b4c9e9a0459fcb1f64b0d8e8c35975c6acb483c
dboissin <damien.boissin@web-education.net>
2014-08-11 09:14:08 +0200

    keep old shared in poor delete shares script

--- migration/1.8.0/deleteShared.js
+++ migration/1.8.0/deleteShared.js
@@ -1,2 +1,4 @@
+db.documents.update({}, {$unset : { "old_shared" : "" }}, { multi:true });
+db.documents.update({}, {$rename : { "shared" : "old_shared" }}, { multi:true });
 db.documents.update({}, {$set : { shared : []}}, { multi:true });
 db.blogs.update({}, {$set : { shared : []}}, { multi:true });
