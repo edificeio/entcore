0ea02fc4757f0108fac25ed84f4b0a50856eea86
Rafik Djedjig <camille.wintz@web-education.net>
2015-01-08 14:15:37 +0100

    [infra] fixed transclusion in wizard

--- infra/src/main/resources/public/js/angular-app.js
+++ infra/src/main/resources/public/js/angular-app.js
@@ -3715,58 +3715,65 @@ module.directive('wizard', function(){
 			onFinish: '&'
 		},
 		transclude: true,
-		link: function(scope, element, attributes){
-			$('div.steps').hide();
-			scope.currentStep = 0;
+		compile: function(element, attributes, transclude){
+			return function(scope, element, attributes){
+				element.find('div.steps').hide();
+				scope.currentStep = 0;
 
-			var currentStepContent;
-			var steps = element.find('div.steps step');
-			scope.nbSteps = steps.length;
+				var currentStepContent;
+				var steps = element.find('div.steps step');
+				scope.nbSteps = steps.length;
 
-			var nav = element.find('nav.steps');
-			nav.append('<ul></ul>');
-			steps.each(function(index, item){
-				nav.children('ul').append('<li>' + $(item).find('h1').html() + '</li>');
-			});
+				var nav = element.find('nav.steps');
+				nav.append('<ul></ul>');
+				steps.each(function(index, item){
+					nav.children('ul').append('<li>' + $(item).find('h1').html() + '</li>');
+				});
 
-			function displayCurrentStep(){
-				currentStepContent = $(steps[scope.currentStep]);
-				element
-					.find('.current-step .step-content')
-					.html('')
-					.append(currentStepContent.html());
-				$('nav.steps li').removeClass('active');
-				$(element.find('nav.steps li')[scope.currentStep]).addClass('active');
+				function displayCurrentStep(){
+					transclude(scope.$parent.$new(), function(content){
+						currentStepContent = _.filter(content, function(el){
+							return el.tagName === 'STEP';
+						})[scope.currentStep];
+						currentStepContent = $(currentStepContent);
+						element
+							.find('.current-step .step-content')
+							.html('')
+							.append(currentStepContent);
+					});
 
+					$('nav.steps li').removeClass('active');
+					$(element.find('nav.steps li')[scope.currentStep]).addClass('active');
+				}
+
+				scope.nextStep = function(){
+					var stepScope = angular.element(currentStepContent[0]).scope();
+					stepScope.onNext();
+					scope.currentStep++;
+					displayCurrentStep();
+				};
+
+				scope.previousStep = function(){
+					var stepScope = angular.element(currentStepContent[0]).scope();
+					stepScope.onPrevious();
+					scope.currentStep--;
+					displayCurrentStep();
+				};
+
+				scope.cancel = function(){
+					scope.currentStep = 0;
+					displayCurrentStep();
+					scope.onCancel();
+				};
+
+				scope.finish = function(){
+					scope.currentStep = 0;
+					displayCurrentStep();
+					scope.onFinish();
+				};
+
+				displayCurrentStep();
 			}
-
-			scope.nextStep = function(){
-				var stepScope = angular.element(currentStepContent[0]).scope();
-				stepScope.onNext();
-				scope.currentStep++;
-				displayCurrentStep();
-			};
-
-			scope.previousStep = function(){
-				var stepScope = angular.element(currentStepContent[0]).scope();
-				stepScope.onPrevious();
-				scope.currentStep--;
-				displayCurrentStep();
-			};
-
-			scope.cancel = function(){
-				scope.currentStep = 0;
-				displayCurrentStep();
-				scope.onCancel();
-			};
-
-			scope.finish = function(){
-				scope.currentStep = 0;
-				displayCurrentStep();
-				scope.onFinish();
-			};
-
-			displayCurrentStep();
 		}
 	}
 });
