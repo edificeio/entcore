19408c3577e2ead4bdc8e43fa93a9a7adb36188d
dboissin <damien.boissin@web-education.net>
2015-02-03 17:46:23 +0100

    expose internal userId to oauth2 client

--- auth/src/main/java/org/entcore/auth/adapter/UserInfoAdapterV1_0Json.java
+++ auth/src/main/java/org/entcore/auth/adapter/UserInfoAdapterV1_0Json.java
@@ -67,7 +67,6 @@ public class UserInfoAdapterV1_0Json implements UserInfoAdapter {
 			if (uai != null && uai.size() > 0) {
 				filteredInfos.putString("uai", uai.<String>get(0));
 			}
-			filteredInfos.removeField("userId");
 			filteredInfos.removeField("functions");
 			filteredInfos.removeField("groupsIds");
 			filteredInfos.removeField("structures");
