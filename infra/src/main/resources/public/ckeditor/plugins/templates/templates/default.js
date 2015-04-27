090635f9c518f9a79406ff64f38a332c66e8a5ec
Camille Wintz <camille.wintz@web-education.net>
2015-04-27 12:24:21 +0200

    [infra] #3752 added spacing between columns in ckeditor template

--- infra/src/main/resources/public/ckeditor/plugins/templates/templates/default.js
+++ infra/src/main/resources/public/ckeditor/plugins/templates/templates/default.js
@@ -20,10 +20,10 @@ CKEDITOR.addTemplates(
 			description:"Deux colonnes et un titre.",
 			html:'<h2>Titre</h2>' +
 				'<div class="row">' +
-				'<div class="six cell" style="text-align: justify">' +
+				'<div class="six cell reduce-block-eight" style="text-align: justify">' +
 				'<h3>Titre</h3>' +
 				'<p>Colonne de gauche</p></div>' +
-				'<div class="six cell" style="text-align: justify">' +
+				'<div class="six cell reduce-block-eight" style="text-align: justify">' +
 				'<h3>Titre</h3>' +
 				'<p>Colonne de droite</p>' +
 				'</div>' +
