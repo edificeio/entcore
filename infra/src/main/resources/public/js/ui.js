879f96398d06b09604e41bab1eac10ec3a1990d3
Camille Wintz <camille.wintz@web-education.net>
2015-09-18 13:08:01 +0200

    [infra] allow drag and drop with touch

--- infra/src/main/resources/public/js/ui.js
+++ infra/src/main/resources/public/js/ui.js
@@ -248,8 +248,196 @@ var ui = (function(){
 
 
 ui.extendElement = {
+	resizable: function(element, params){
+		if(!params){
+			params = {};
+		}
+		if(!params.lock){
+			params.lock = {};
+		}
+
+		if(element.length > 1){
+			element.each(function(index, item){
+				ui.extendElement.resizable($(item), params);
+			})
+		}
+
+		$('body').css({
+			'-webkit-user-select': 'none',
+			'-moz-user-select': 'none',
+			'user-select' : 'none'
+		});
+
+		//cursor styles to indicate resizing possibilities
+		element.on('mouseover', function(e){
+			element.on('mousemove', function(e){
+				if(element.data('resizing') || element.data('lock')){
+					return;
+				}
+				var mouse = { x: e.pageX, y: e.pageY };
+				var resizeLimits = {
+					horizontalRight:  element.offset().left + element.width() + 5 > mouse.x && mouse.x > element.offset().left + element.width() - 15 && element.attr('horizontal-resize-lock') === undefined,
+					horizontalLeft: element.offset().left + 5 > mouse.x && mouse.x > element.offset().left - 15 && element.attr('horizontal-resize-lock') === undefined,
+					verticalTop: element.offset().top + 5 > mouse.y && mouse.y > element.offset().top - 5 && element.attr('vertical-resize-lock') === undefined,
+					verticalBottom: element.offset().top + element.height() + 5 > mouse.y && mouse.y > element.offset().top + element.height() - 5 && element.attr('vertical-resize-lock') === undefined
+				};
+
+				var orientations = {
+					'ns': resizeLimits.verticalTop || resizeLimits.verticalBottom,
+					'ew': resizeLimits.horizontalLeft || resizeLimits.horizontalRight,
+					'nwse': (resizeLimits.verticalBottom && resizeLimits.horizontalRight) || (resizeLimits.verticalTop && resizeLimits.horizontalLeft),
+					'nesw': (resizeLimits.verticalBottom && resizeLimits.horizontalLeft) || (resizeLimits.verticalTop && resizeLimits.horizontalRight)
+
+				};
+
+				var cursor = '';
+				for(var orientation in orientations){
+					if(orientations[orientation]){
+						cursor = orientation;
+					}
+				}
+
+
+				if(cursor){
+					cursor = cursor + '-resize';
+				}
+				element.css({ cursor: cursor });
+				element.find('[contenteditable]').css({ cursor: cursor });
+			});
+			element.on('mouseout', function(e){
+				element.unbind('mousemove');
+			});
+		});
+
+		//actual resize
+		element.on('mousedown.resize touchstart.resize', function(e){
+			if(element.data('lock') === true || element.data('resizing') === true){
+				return;
+			}
+
+			var interrupt = false;
+			var mouse = {
+				y: f.clientY || f.originalEvent.touches[0].clientY,
+				x: f.clientX || f.originalEvent.touches[0].clientX
+			};
+			var resizeLimits = {
+				horizontalRight:  element.offset().left + element.width() + 15 > mouse.x && mouse.x > element.offset().left + element.width() - 15 && element.attr('horizontal-resize-lock') === undefined,
+				horizontalLeft: element.offset().left + 15 > mouse.x && mouse.x > element.offset().left - 15 && params.lock.horizontal === undefined,
+				verticalTop: element.offset().top + 5 > mouse.y && mouse.y > element.offset().top - 15 && element.attr('vertical-resize-lock') === undefined,
+				verticalBottom: element.offset().top + element.height() + 5 > mouse.y && mouse.y > element.offset().top + element.height() - 5 && element.attr('vertical-resize-lock') === undefined
+			};
+
+			var initial = {
+				pos: element.offset(),
+				size: {
+					width: element.width(),
+					height: element.height()
+				}
+			};
+			var parent = element.parents('.drawing-zone');
+			var parentData = {
+				pos: parent.offset(),
+				size: {
+					width: parent.width(),
+					height: parent.height()
+				}
+			};
+
+			if(resizeLimits.horizontalLeft || resizeLimits.horizontalRight ||resizeLimits.verticalTop || resizeLimits.verticalBottom){
+				element.trigger('startResize');
+				e.preventDefault();
+				element.data('resizing', true);
+				$('.main').css({
+					'cursor': element.css('cursor')
+				});
+				$(window).unbind('mousemove.drag touchmove.start');
+				$(window).on('mousemove.resize touchmove.resize', function(e){
+					element.unbind("click");
+					mouse = {
+						y: e.pageY || e.originalEvent.touches[0].pageY,
+						x: e.pageX || e.originalEvent.touches[0].pageX
+					};
+				});
+
+				//animation for resizing
+				var resize = function(){
+					var newWidth = 0; var newHeight = 0;
+					if(resizeLimits.horizontalLeft || resizeLimits.horizontalRight){
+						var p = element.offset();
+						if(resizeLimits.horizontalLeft){
+							var distance = initial.pos.left - mouse.x;
+							if(initial.pos.left - distance < parentData.pos.left){
+								distance = initial.pos.left - parentData.pos.left;
+							}
+							if(params.moveWithResize !== false){
+								element.offset({
+									left: initial.pos.left - distance,
+									top: p.top
+								});
+							}
+
+							newWidth = initial.size.width + distance;
+						}
+						else{
+							var distance = mouse.x - p.left;
+							if(element.offset().left + distance > parentData.pos.left + parentData.size.width){
+								distance = (parentData.pos.left + parentData.size.width) - element.offset().left - 2;
+							}
+							newWidth = distance;
+						}
+						if(newWidth > 0){
+							element.width(newWidth);
+						}
+					}
+					if(resizeLimits.verticalTop || resizeLimits.verticalBottom){
+						var p = element.offset();
+						if(resizeLimits.verticalTop){
+							var distance = initial.pos.top - mouse.y;
+							if(initial.pos.top - distance < parentData.pos.top){
+								distance = initial.pos.top - parentData.pos.top;
+							}
+							if(params.moveWithResize !== false){
+								element.offset({
+									left: p.left,
+									top: initial.pos.top - distance
+								});
+							}
+
+							newHeight = initial.size.height + distance;
+						}
+						else{
+							var distance = mouse.y - p.top;
+							if(element.offset().top + distance > parentData.pos.top + parent.height()){
+								distance = (parentData.pos.top + parentData.size.height) - element.offset().top - 2;
+							}
+							newHeight = distance;
+						}
+						if(newHeight > 0){
+							element.height(newHeight);
+						}
+					}
+					element.trigger('resizing');
+					if(!interrupt){
+						requestAnimationFrame(resize);
+					}
+				};
+				resize();
+
+				$(window).on('mouseup.resize touchleave.resize touchend.resize', function(){
+					interrupt = true;
+					setTimeout(function(){
+						element.data('resizing', false);
+						element.trigger('stopResize');
+					}, 100);
+					$(window).unbind('mousemove.resize touchmove.resize');
+					$('body').unbind('mouseup.resize touchleave.resize touchend.resize');
+					$('.main').css({'cursor': ''})
+				});
+			}
+		});
+	},
 	draggable: function(element, params){
-		element.on('mousedown', function(e){
+		element.on('touchstart mousedown', function(e){
 			if(element.data('lock') === true || (e.target.tagName === 'TEXTAREA' && $(e.target).is(':focus'))){
 				return;
 			}
@@ -257,7 +445,10 @@ ui.extendElement = {
 			var interrupt = false;
 			if(element.data('resizing') !== true){
 
-				var mouse = { y: e.clientY, x: e.clientX };
+				var mouse = mouse = {
+					y: e.clientY || e.originalEvent.touches[0].clientY,
+					x: e.clientX || e.originalEvent.touches[0].clientX
+				};;
 				var elementDistance = {
 					y: mouse.y - element.offset().top,
 					x: mouse.x - element.offset().left
@@ -318,10 +509,11 @@ ui.extendElement = {
 					}
 				};
 
-				$(window).on('mousemove.drag', function(f){
+				$(window).on('touchmove.drag mousemove.drag', function(f){
 					moved = true;
-					e.preventDefault();
-					if(f.clientX === mouse.x && f.clientY === mouse.y){
+					f.preventDefault();
+					if((f.clientX || f.originalEvent.touches[0].clientX) === mouse.x &&
+						(f.clientY || f.originalEvent.touches[0].clientY) === mouse.y){
 						return;
 					}
 					if(!element.data('dragging')){
@@ -348,12 +540,12 @@ ui.extendElement = {
 					element.unbind("click");
 					element.data('dragging', true);
 					mouse = {
-						y: f.clientY,
-						x: f.clientX
+						y: f.clientY || f.originalEvent.touches[0].clientY,
+						x: f.clientX || f.originalEvent.touches[0].clientX
 					};
 				});
 
-				$('body').on('mouseup.drag', function(e){
+				$('body').on('touchend.drag touchleave.drag mouseup.drag', function(e){
 					$('body').css({
 						'-webkit-user-select': 'initial',
 						'-moz-user-select': 'initial',
@@ -361,8 +553,8 @@ ui.extendElement = {
 					});
 					element.css('transition', '');
 					interrupt = true;
-					$('body').unbind('mouseup.drag');
-					$(window).unbind('mousemove.drag');
+					$('body').unbind('mouseup.drag touchend.drag touchleave.drag');
+					$(window).unbind('mousemove.drag touchmove.drag');
 
 					setTimeout(function(){
 						if(element.data('dragging')){
