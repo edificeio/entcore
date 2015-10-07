f0aac9a74fefbdec224a3377d31f8fd0176bc0f3
Julien Elbaz <julien.elbaz@web-education.net>
2015-10-07 10:19:26 +0200

    [Fix] Errors while parsing the KNE registered service infos.

--- cas/src/main/java/org/entcore/cas/services/KneRegisteredService.java
+++ cas/src/main/java/org/entcore/cas/services/KneRegisteredService.java
@@ -18,9 +18,28 @@ public class KneRegisteredService extends AbstractCas20ExtensionRegisteredServic
 	private static final Logger log = LoggerFactory.getLogger(KneRegisteredService.class);
 
 	private void addStringArray(String casLabel, String entLabel, JsonObject data, Document doc, List<Element> additionalAttributes){
-		Element root = createElement(entLabel+"s", doc);
-		for(Object item: data.getArray(entLabel)){
-			root.appendChild(createTextElement(casLabel, (String) item, doc));
+		Element root = createElement(casLabel+"s", doc);
+		if(data.containsField(entLabel)){
+			for(Object item: data.getArray(entLabel)){
+				root.appendChild(createTextElement(casLabel, (String) item, doc));
+			}
+		}
+		additionalAttributes.add(root);
+	}
+
+	private void addObjectArrayProp(String casLabel, String entLabel, String objProperty, JsonObject data,
+			Document doc, List<Element> additionalAttributes){
+		addObjectArrayProp(casLabel, entLabel, objProperty, objProperty, data, doc, additionalAttributes);
+	}
+	private void addObjectArrayProp(String casLabel, String entLabel, String objProperty, String conditionProperty,
+			JsonObject data, Document doc, List<Element> additionalAttributes){
+		Element root = createElement(casLabel+"s", doc);
+		if(data.containsField(entLabel)){
+			for(Object item: data.getArray(entLabel)){
+				JsonObject jsonItem = (JsonObject) item;
+				if(jsonItem.containsField(conditionProperty))
+					root.appendChild(createTextElement(casLabel, jsonItem.getString(objProperty), doc));
+			}
 		}
 		additionalAttributes.add(root);
 	}
@@ -58,42 +77,39 @@ public class KneRegisteredService extends AbstractCas20ExtensionRegisteredServic
 				}
 			}
 
-			/*
-			for (Object o : data.getArray("structures", new JsonArray()).toList()) {
-				Map<String, Object> structure = ((Map<String, Object>) o);
-				additionnalAttributes.add(createTextElement("ENTPersonStructRattachUAI", structure.get("UAI").toString(), doc));
-				additionnalAttributes.add(createTextElement("ENTStructureTypeStruct", structure.get("type").toString(), doc));
-			}
-			*/
-
+			Element rootProfiles;
 			switch(data.getString("type")) {
 				case "Student" :
-					additionalAttributes.add(createTextElement("ENTPersonProfil", "National_ELV", doc));
+					rootProfiles = createElement("ENTPersonProfils", doc);
+					rootProfiles.appendChild(createTextElement("ENTPersonProfil", "National_ELV", doc));
+					additionalAttributes.add(rootProfiles);
 					additionalAttributes.add(createTextElement("ENTEleveMEF", data.getString("module"), doc));
 					addStringArray("ENTEleveCodeEnseignement", "fieldOfStudy", data, doc, additionalAttributes);
-					addStringArray("ENTEleveClasse", "classes", data, doc, additionalAttributes);
-					addStringArray("ENTEleveGroupe", "groups", data, doc, additionalAttributes);
+					addObjectArrayProp("ENTEleveClasse", "classes", "name", data, doc, additionalAttributes);
+					addObjectArrayProp("ENTEleveGroupe", "groups", "name", "externalId",data, doc, additionalAttributes);
 					additionalAttributes.add(createTextElement("ENTAuxEnsClassesMatieres", "", doc));
 					additionalAttributes.add(createTextElement("ENTAuxEnsGroupes", "", doc));
 					additionalAttributes.add(createTextElement("ENTAuxEnsClasses", "", doc));
 					additionalAttributes.add(createTextElement("ENTAuxEnsMEFs", "", doc));
 					break;
 				case "Teacher" :
-					Element root = createElement("ENTPersonProfils", doc);
-					root.appendChild(createTextElement("ENTPersonProfil", "National_ENS", doc));
-					root.appendChild(createTextElement("ENTPersonProfil", "National_TUT", doc));
-					additionalAttributes.add(root);
+					rootProfiles = createElement("ENTPersonProfils", doc);
+					rootProfiles.appendChild(createTextElement("ENTPersonProfil", "National_ENS", doc));
+					rootProfiles.appendChild(createTextElement("ENTPersonProfil", "National_TUT", doc));
+					additionalAttributes.add(rootProfiles);
 					additionalAttributes.add(createTextElement("ENTEleveMEF", "", doc));
 					additionalAttributes.add(createTextElement("ENTEleveCodeEnseignements", "", doc));
 					additionalAttributes.add(createTextElement("ENTEleveClasses", "", doc));
 					additionalAttributes.add(createTextElement("ENTEleveGroupes", "", doc));
 					addStringArray("ENTAuxEnsClassesMatiere", "classesFieldOfStudy", data, doc, additionalAttributes);
-					addStringArray("ENTAuxEnsGroupe", "groups", data, doc, additionalAttributes);
-					addStringArray("ENTAuxEnsClasse", "classes", data, doc, additionalAttributes);
+					addObjectArrayProp("ENTAuxEnsGroupe", "groups", "name", "externalId", data, doc, additionalAttributes);
+					addObjectArrayProp("ENTAuxEnsClasse", "classes", "name", data, doc, additionalAttributes);
 					addStringArray("ENTAuxEnsMEF", "modules", data, doc, additionalAttributes);
 					break;
 				case "Relative" :
-					additionalAttributes.add(createTextElement("ENTPersonProfil", "National_TUT", doc));
+					rootProfiles = createElement("ENTPersonProfils", doc);
+					rootProfiles.appendChild(createTextElement("ENTPersonProfil", "National_TUT", doc));
+					additionalAttributes.add(rootProfiles);
 					additionalAttributes.add(createTextElement("ENTEleveMEF", "", doc));
 					additionalAttributes.add(createTextElement("ENTEleveCodeEnseignements", "", doc));
 					additionalAttributes.add(createTextElement("ENTEleveClasses", "", doc));
@@ -104,7 +120,9 @@ public class KneRegisteredService extends AbstractCas20ExtensionRegisteredServic
 					additionalAttributes.add(createTextElement("ENTAuxEnsMEFs", "", doc));
 					break;
 				case "Personnel" :
-					additionalAttributes.add(createTextElement("ENTPersonProfil", "National_DOC", doc));
+					rootProfiles = createElement("ENTPersonProfils", doc);
+					rootProfiles.appendChild(createTextElement("ENTPersonProfil", "National_DOC", doc));
+					additionalAttributes.add(rootProfiles);
 					additionalAttributes.add(createTextElement("ENTEleveMEF", "", doc));
 					additionalAttributes.add(createTextElement("ENTEleveCodeEnseignements", "", doc));
 					additionalAttributes.add(createTextElement("ENTEleveClasses", "", doc));
