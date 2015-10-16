b2c4601a446ffd7920cf1daddbc346cbe004e520
Camille Wintz <camille.wintz@web-education.net>
2015-10-16 15:42:45 +0200

    [infra] #6277 reordering placed block fixed

--- infra/src/main/resources/public/js/angular-app.js
+++ infra/src/main/resources/public/js/angular-app.js
@@ -3159,73 +3159,73 @@ module.directive('placedBlock', function($compile){
 			w: '='
 		},
 		template: '<article ng-transclude ng-style="{\'z-index\': z }"></article>',
-		link: function($scope, $element, $attributes){
-			$element.css({ 'position': 'absolute' });
-			$scope.$watch('x', function(newVal){
-				$element.offset({
-					top: $element.offset().top,
-					left: parseInt(newVal) + $element.parent().offset().left
+		link: function(scope, element){
+			element.css({ 'position': 'absolute' });
+			scope.$watch('x', function(newVal){
+				element.offset({
+					top: element.offset().top,
+					left: parseInt(newVal) + element.parent().offset().left
 				});
 			});
 
-			$scope.$watch('y', function(newVal){
-				$element.offset({
-					left: $element.offset().left,
-					top: parseInt(newVal) + $element.parent().offset().top
+			scope.$watch('y', function(newVal){
+				element.offset({
+					left: element.offset().left,
+					top: parseInt(newVal) + element.parent().offset().top
 				});
 			});
 
 			var toTop = function(){
 				$(':focus').blur();
-				if($scope.z === undefined){
+				if(scope.z === undefined){
 					return;
 				}
-				$element.parents('.drawing-zone').find('*').each(function(index, item){
+				element.parents('.drawing-zone').find('article[draggable]').each(function(index, item){
 					var zIndex = $(item).css('z-index');
-					if(!$scope.z){
-						$scope.z = 1;
+					if(!scope.z){
+						scope.z = 1;
 					}
-					if(parseInt(zIndex) && parseInt(zIndex) >= $scope.z){
-						$scope.z = parseInt(zIndex) + 1;
+					if(parseInt(zIndex) && parseInt(zIndex) >= scope.z){
+						scope.z = parseInt(zIndex) + 1;
 					}
 				});
-				if($scope.z){
-					$scope.$apply('z');
+				if(scope.z){
+					scope.$apply('z');
 				}
 			};
 
-			$element.on('startDrag', toTop);
-			$element.on('startResize', function(){
-				$scope.w = $element.width();
-				$scope.$apply('w');
-				$scope.h = $element.height();
-				$scope.$apply('h');
+			element.on('startDrag', toTop);
+			element.on('startResize', function(){
+				scope.w = element.width();
+				scope.$apply('w');
+				scope.h = element.height();
+				scope.$apply('h');
 				toTop();
 			});
 
-			$element.on('stopDrag', function(){
-				$scope.x = $element.position().left;
-				$scope.$apply('x');
-				$scope.y = $element.position().top;
-				$scope.$apply('y');
+			element.on('stopDrag', function(){
+				scope.x = element.position().left;
+				scope.$apply('x');
+				scope.y = element.position().top;
+				scope.$apply('y');
 			});
 
-			$scope.$watch('z', function(newVal){
-				$element.css({ 'z-index': $scope.z })
+			scope.$watch('z', function(newVal){
+				element.css({ 'z-index': scope.z })
 			});
 
-			$scope.$watch('w', function(newVal){
-				$element.width(newVal);
+			scope.$watch('w', function(newVal){
+				element.width(newVal);
 			});
-			$element.on('stopResize', function(){
-				$scope.w = $element.width();
-				$scope.$apply('w');
-				$scope.h = $element.height();
-				$scope.$apply('h');
+			element.on('stopResize', function(){
+				scope.w = element.width();
+				scope.$apply('w');
+				scope.h = element.height();
+				scope.$apply('h');
 			});
 
-			$scope.$watch('h', function(newVal){
-				$element.height(newVal);
+			scope.$watch('h', function(newVal){
+				element.height(newVal);
 			});
 		}
 	}
