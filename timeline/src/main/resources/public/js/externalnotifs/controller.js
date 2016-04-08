da586cec737866d989b88eace4f9afa29dfbce30
Lauren Ceccotti <lauren.ceccotti@web-education.net>
2016-04-08 19:08:11 +0200

    [evo] enable radio check override

--- timeline/src/main/resources/public/js/externalnotifs/controller.js
+++ timeline/src/main/resources/public/js/externalnotifs/controller.js
@@ -4,7 +4,44 @@ function MainController($rootScope, $scope, template, lang, model){
 
 	$scope.template = template;
 
-    $scope.appli = model.applis;
+	model.applis.list();
+    $scope.applis = model.applis;
+	$scope.preference = model.preference
+
+	$scope.lang = lang;
+
+	lang.addBundle('/timeline/i18nNotifications', function(){
+		$scope.$apply();
+	});
+
+	$scope.appliFreq = function(appli){
+		appli.appActions.each(function(appAction){
+			appAction.defaultFrequency = appli.freq;
+		})
+	}
+
+	$scope.removeAppliFreq = function(appli){
+		delete appli.freq
+	}
+
+	$scope.savePreferences = function(){
+
+		model.applis // liste des applis
+		appli.appActions // list les appActions qd t'es dns une appli
+
+		function(appAction){
+			var freq = appAction.defaultFrequency
+
+
+		}
+		// 1 - Pour chaque appli
+			// 2 - Pour chaque appAction
+				// 3 - On récupère la variable defaultFrequency
+				// 4 - On cherche dans les préférences (model.preference.preference.config.clef-de-la-notif)
+				// 5 - On affecte la defaultFrequency de l'appAction dans la preference
+		// 6 - On appelle preference.putinfo
+		// 7 - youpi
+	}
 }
 
 //(ng-repeat > applis.all)
