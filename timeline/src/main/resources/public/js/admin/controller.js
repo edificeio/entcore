60b38f28ab76a19a71b72c8bad17d6895b07ee83
Julien Elbaz <julien.elbaz@web-education.net>
2016-04-18 11:13:45 +0200

    [Evo] #9457 : Timeline admin types & notifs ordering.

--- timeline/src/main/resources/public/js/admin/controller.js
+++ timeline/src/main/resources/public/js/admin/controller.js
@@ -3,6 +3,7 @@ function TimelineAdminController($scope, model){
     $scope.config = model.configs
     $scope.notifs = model.notifications
     $scope.typesMap = {}
+    $scope.types = []
 
     $scope.constants = {
         "defaultFrequency": [{label: "NEVER"}, {label: "IMMEDIATE"}, {label: "DAILY"}, {label: "WEEKLY"}],
@@ -13,6 +14,14 @@ function TimelineAdminController($scope, model){
         notif.config.upsert()
     }
 
+    $scope.orderTypes = function(type){
+        return lang.translate(type.type.toLowerCase())
+    }
+
+    $scope.orderNotifs = function(notif){
+        return lang.translate(notif.key.toLowerCase())
+    }
+
     model.notifications.one('sync', function(){
         model.notifications.forEach(function(notif){
             if(!$scope.typesMap[notif.type]){
@@ -20,6 +29,12 @@ function TimelineAdminController($scope, model){
             }
             $scope.typesMap[notif.type].push(notif)
         })
+        for(var type in $scope.typesMap){
+            $scope.types.push({
+                type: type,
+                notifs: $scope.typesMap[type]
+            })
+        }
     })
 
     lang.addBundle('/timeline/i18nNotifications', function(){
