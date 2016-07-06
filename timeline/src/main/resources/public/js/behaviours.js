5a5cba68fa117fe072dec0349b804c7cbab47c7e
Benjamin Perez <benjamin.perez@web-education.net>
2016-07-06 15:36:56 +0200

    Revert "[evo]#9623 timeline banner about external notifs"
    This reverts commit a7610a8b972f9a224c148eae6ba2f1bf2f0d2977.

--- timeline/src/main/resources/public/js/behaviours.js
+++
@@ -1,9 +0,0 @@
-console.log('timeline behaviours loaded');
-
-Behaviours.register('timeline', {
-	rights:{
-		workflow: {
-			externalNotifications: "org.entcore.timeline.controllers.TimelineController|mixinConfig"
-		}
-	}
-});
