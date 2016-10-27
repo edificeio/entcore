31aa4d259840b42fa6c218b60cecbef7b92b15dc
dboissin <damien.boissin@web-education.net>
2016-10-27 11:22:51 +0200

    [Fix][Common] #12387: fix result id when copy document

--- migration/1.23.0/1.23.0-mongodb.js
+++ migration/1.23.0/1.23.0-mongodb.js
@@ -1,3 +1,7 @@
 db.flash_messages.createIndex( { "markedAsRead": 1 } );
 db.flash_messages.createIndex( { "modified": -1 } );
 db.documentsRevisions.createIndex({"owner":1});
+db.documents.find({"file" : {"$regex" : ".srv.*"}}, {"_id":1, "file":1}).forEach(function(doc) {
+  var fileId = doc.file.split("/").slice(-1)[0];
+  db.documents.update({"_id" : doc._id}, { $set : { "file" : fileId }});
+});
