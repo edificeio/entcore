1f2abf0c24f41fc73d918754532111a9a09291a5
Lauren Ceccotti <lauren.ceccotti@web-education.net>
2016-10-28 17:09:44 +0200

    [fix]#12439 resolve checktool lack of performance

--- infra/src/main/resources/public/js/angular-app.js
+++ infra/src/main/resources/public/js/angular-app.js
@@ -5145,11 +5145,12 @@ var checkToolDelay = (function(){
 			setTimeout(function(){
 				scope.$apply();
 				applyAllowed = true;
-			}, 200);
+			}, 500);
 		}
 
 	}
 }());
+
 
 module.directive('checkTool', function() {
     return {
