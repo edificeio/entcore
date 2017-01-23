b08d1c54cf0f7fc4790b1729d8ce849fafac64a5
dbreyton <david.breyton@gmail.com>
2017-01-23 09:40:29 +0100

    [CAS] add BRNE connector

---
+++ cas/src/main/java/org/entcore/cas/services/BRNERegisteredService.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright © WebServices pour l'Éducation, 2016
+ *
+ * This file is part of ENT Core. ENT Core is a versatile ENT engine based on the JVM.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * For the sake of explanation, any module that communicate over native
+ * Web protocols, such as HTTP, with ENT Core is outside the scope of this
+ * license and could be license under its own terms. This is merely considered
+ * normal use of ENT Core, and does not fall under the heading of "covered work".
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package org.entcore.cas.services;
+
+import fr.wseduc.cas.entities.User;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BRNERegisteredService extends DefaultRegisteredService {
+
+    protected static final String PROFIL = "PROFIL_NATIONAL";
+
+    @Override
+    public void configure(org.vertx.java.core.eventbus.EventBus eb, Map<String,Object> conf){
+        super.configure(eb, conf);
+    }
+
+    @Override
+    protected void prepareUser(User user, String userId, String service, JsonObject data) {
+        //return the first uai as principalAttributeName
+        for (Object s : data.getArray("structureNodes", new JsonArray())) {
+            if (s == null || !(s instanceof JsonObject)) continue;
+            JsonObject structure = (JsonObject) s;
+            String uai = structure.getString("UAI", "");
+            if (!uai.isEmpty()) {
+                user.setUser(uai);
+                break;
+            }
+        }
+
+        user.setAttributes(new HashMap<String, String>());
+
+        // Profile
+        JsonArray profiles = data.getArray("type", new JsonArray());
+        if (profiles.contains("Teacher")) {
+            user.getAttributes().put(PROFIL, "National_3");
+        } else if (profiles.contains("Student")) {
+            user.getAttributes().put(PROFIL, "National_1");
+        }
+    }
+}
