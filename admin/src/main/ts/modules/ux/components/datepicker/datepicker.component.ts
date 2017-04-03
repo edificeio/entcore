f0a74a1727cca63cb4bb993c7c9dc3a4d2ff89e6
david-cc <viddouille.cc@gmail.com>
2017-04-03 18:38:07 +0200

    Date HTML input embedded in Datepicker Component

--- admin/src/main/ts/modules/ux/components/datepicker/datepicker.component.ts
+++ admin/src/main/ts/modules/ux/components/datepicker/datepicker.component.ts
@@ -1,41 +1,84 @@
-import { Component, ViewChild, Input, ElementRef, OnDestroy, AfterViewInit } from '@angular/core'
+import { Component, forwardRef, ViewChild, ElementRef, Input, OnDestroy, AfterViewInit, Renderer } from '@angular/core'
+
+import { NgModel } from '@angular/forms';
+
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+const NOOP = () => {
+};
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => Datepicker),
+    multi: true
+};
 
 import Flatpickr from 'flatpickr';
 
 @Component({
-    selector: 'datepicker',
+    selector: 'date-picker',
     template: `
         <div class="flatpickr" #datePickerElement>
-            <ng-content></ng-content>
+            <input type="date" [(ngModel)]="value" [ngClass]="{ 'cursor-default': disabled }" placeholder="{{ placeholder }}" #inputRef>
             <a *ngIf="!disabled" data-toggle><i class="fa fa-calendar" aria-hidden="true"></i></a>
             <a *ngIf="!disabled" data-clear><i class="fa fa-times" aria-hidden="true"></i></a>
         </div>
     `,
-    styles: ['@import url("/admin/public/styles/flatpickr.min.css")']
+    styles: ['@import url("/admin/public/styles/flatpickr.min.css")'],
+    providers: [ CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR ]
 })
-export class Datepicker implements OnDestroy, AfterViewInit {
+export class Datepicker implements OnDestroy, AfterViewInit, ControlValueAccessor {
+
+    private innerValue: any = ''
+
+    @ViewChild("datePickerElement") 
+    datePickerElement: ElementRef
     
-    @ViewChild("datePickerElement") datePickerElement: ElementRef
+    @ViewChild("inputRef")
+    inputElement: ElementRef
+
+    // instance flatpickr
     private datePickerInst: Flatpickr
-    private disabled: boolean = false
 
-    @Input() enableTime: boolean = false
-    @Input() minDate
-    @Input() maxDate
+    @ViewChild(NgModel) 
+    model: NgModel
+    
+    @Input()
+    disabled: boolean = false
+    
+    @Input()
+    enableTime: boolean = false
+    
+    @Input()
+    placeholder: string = ''
+    
+    @Input()
+    minDate
+    
+    @Input()
+    maxDate
 
-    constructor() {}
+    constructor(private renderer: Renderer) {}
+
+    get value(): any {
+        return this.innerValue
+    }
+
+    set value(v: any) {
+        if (v !== this.innerValue) {
+            this.innerValue = v
+            this.onChangeCallback(v)
+        }
+    }
 
     ngAfterViewInit(): void {
-        let inputElem: HTMLElement = this.datePickerElement.nativeElement.querySelector("input")
+        // add attr data-input, mandatory for the picker to work in wrap mode
+        this.renderer.setElementAttribute(this.inputElement.nativeElement, 'data-input', '');
 
         // disabled case
-        if (inputElem.hasAttribute('disabled') === true) {
-            this.disabled = true
-            inputElem.style['cursor'] = 'default'
+        if (this.disabled === true) {
+            this.model.control.disable()
         }
-
-        // add attr data-input, mandatory for the picker to work in wrap mode
-        inputElem.setAttribute('data-input', '')
 
         // options for the flatpickr instance
         let options = {
@@ -56,4 +99,22 @@ export class Datepicker implements OnDestroy, AfterViewInit {
     ngOnDestroy(): void {
         this.datePickerInst.destroy()
     }
+
+    writeValue(value: any): void {
+        if (value !== this.innerValue) {
+            this.innerValue = value
+            this.datePickerInst.setDate(value)
+        }
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChangeCallback = fn
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouchedCallback = fn;
+    }
+
+    private onChangeCallback: (_: any) => void = NOOP
+    private onTouchedCallback: () => void = NOOP
 }
