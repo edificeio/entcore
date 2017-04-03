1ae1b92608cb5131bf81a12da45b0072b3e95926
david-cc <viddouille.cc@gmail.com>
2017-04-03 18:39:43 +0200

    Code cleaning

--- admin/src/main/ts/modules/users/components/user-create/user-create.component.ts
+++ admin/src/main/ts/modules/users/components/user-create/user-create.component.ts
@@ -18,11 +18,11 @@ import { LoadingService, NotifyService, UserListService } from '../../../../serv
 })
 export class UserCreate implements OnInit, OnDestroy {
 
-    private newUser: UserModel = new UserModel();
-    private noClasses : Array<any> = [];
-    private structureSubscriber : Subscription;
-    private searchChildrenTerm: string;
-
+    private newUser: UserModel = new UserModel()
+    private noClasses : Array<any> = []
+    private structureSubscriber : Subscription
+    private searchChildrenTerm: string
+    
     constructor(
         public usersStore: UsersStore,
         private ns: NotifyService,
@@ -32,53 +32,53 @@ export class UserCreate implements OnInit, OnDestroy {
         private userListService: UserListService) {}
 
     ngOnInit(): void {
-        this.newUser.classes = this.noClasses;
-        this.newUser.type = 'Personnel';
-        let {id, name} = this.usersStore.structure;
-        this.newUser.structures = [{id: id, name:name}];
+        this.newUser.classes = this.noClasses
+        this.newUser.type = 'Personnel'
+        let {id, name} = this.usersStore.structure
+        this.newUser.structures = [{id: id, name:name}]
 
         this.structureSubscriber = routing.observe(this.route, "data").subscribe((data: Data) => {
             if(data['structure']) {
-                this.newUser.structures = [data['structure']];
+                this.newUser.structures = [data['structure']]
             }
         })
-        this.newUser.userDetails.children = [];
+        this.newUser.userDetails.children = []
     }
 
     ngOnDestroy(): void {
-        this.structureSubscriber.unsubscribe();
+        this.structureSubscriber.unsubscribe()
     }
 
     createUser() {
         let payload = new window['URLSearchParams']();
 
-        payload.append('firstName', this.newUser.firstName);
-        payload.append('lastName', this.newUser.lastName);
-        payload.append('type', this.newUser.type);
+        payload.append('firstName', this.newUser.firstName)
+        payload.append('lastName', this.newUser.lastName)
+        payload.append('type', this.newUser.type)
         if (this.newUser.classes && this.newUser.classes.length > 0) {
-            payload.append('classId', this.newUser.classes[0].id);
+            payload.append('classId', this.newUser.classes[0].id)
         }
-        payload.append('structureId', this.usersStore.structure.id);
-        payload.append('birthDate', this.newUser.userDetails.birthDate);
-        this.newUser.userDetails.children.forEach(child => payload.append('childrenIds', child.id));
+        payload.append('structureId', this.usersStore.structure.id)
+        payload.append('birthDate', this.newUser.userDetails.birthDate)
+        this.newUser.userDetails.children.forEach(child => payload.append('childrenIds', child.id))
 
         this.ls.perform('portal-content', this.newUser.create(payload, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
             }
         })).then(res => {
-            this.newUser.id = res.data.id;
+            this.newUser.id = res.data.id
             this.ns.success(
                 {
                     key: 'notify.user.create.content',
                     parameters: {
                         user: this.newUser.firstName + ' ' + this.newUser.lastName}
                     }
-                , 'notify.user.create.title');
+                , 'notify.user.create.title')
 
-            this.usersStore.structure.users.data.push(this.newUser);
+            this.usersStore.structure.users.data.push(this.newUser)
 
-            this.router.navigate(['..', res.data.id], {relativeTo: this.route, replaceUrl: false});
+            this.router.navigate(['..', res.data.id], {relativeTo: this.route, replaceUrl: false})
         }).catch(err => {
             this.ns.error(
                 {
@@ -86,18 +86,18 @@ export class UserCreate implements OnInit, OnDestroy {
                     parameters: {
                         user: this.newUser.firstName + ' ' + this.newUser.lastName}
                     }
-                , 'notify.user.create.error.title', err);
+                , 'notify.user.create.error.title', err)
         });
     }
 
     addChild(child) {
         if (this.newUser.userDetails.children.indexOf(child) < 0) { 
-            this.newUser.userDetails.children.push(child); 
+            this.newUser.userDetails.children.push(child)
         }
     }
 
     removeChild(child) {
         const index = this.newUser.userDetails.children.indexOf(child);
-        this.newUser.userDetails.children.splice(index, 1);
+        this.newUser.userDetails.children.splice(index, 1)
     }
 }
