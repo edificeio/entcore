eafd4a3fa66b79fcb48fb48afdcbd9a856309e08
david-cc <viddouille.cc@gmail.com>
2017-05-04 18:10:10 +0200

    Styled groups tabs

--- admin/src/main/ts/modules/groups/components/groups-root/groups-root.component.ts
+++ admin/src/main/ts/modules/groups/components/groups-root/groups-root.component.ts
@@ -8,20 +8,18 @@ import { routing } from '../../../../routing/routing.utils'
 @Component({
     selector: 'groups-root',
     template: `
+        <div class="flex-header">
+            <h1><i class="fa fa-users"></i><s5l>groups</s5l></h1>
+            <button (click)="openCreationView()">
+                <s5l>create.group</s5l>
+            </button>
+        </div>
+        
         <div class="tabs">
             <button class="tab" *ngFor="let tab of tabs"
-                (click)="setSelectedType(tab.view)"
                 [routerLink]="tab.view"
                 routerLinkActive="active">
                 {{ tab.label | translate }}
-            </button>
-        </div>
-
-        <div class="flex-header">
-            <h1><i class="fa fa-users"></i><s5l>groups</s5l></h1>
-            <button (click)="openCreationView()"
-                [class.hidden]="hideCreateGroupButton()">
-                <s5l>create.group</s5l>
             </button>
         </div>
 
@@ -40,8 +38,6 @@ export class GroupsRoot implements OnInit, OnDestroy {
         { label: "profile.groups", view: "profile" },
         { label: "functional.groups", view: "functional" }
     ]
-
-    private selectedType: string
 
     private routerSubscriber : Subscription
     private error: Error
@@ -78,15 +74,7 @@ export class GroupsRoot implements OnInit, OnDestroy {
         this.error = error
     }
 
-    setSelectedType(t) {
-        this.selectedType = t
-    }
-
     openCreationView() {
-        this.router.navigate([this.selectedType, 'create'], { relativeTo: this.route })
-    }
-
-    private hideCreateGroupButton() {
-        return this.selectedType !== 'manual' 
+        this.router.navigate(['manual', 'create'], { relativeTo: this.route })
     }
 }
