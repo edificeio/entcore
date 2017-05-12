6a22912fbdb996476e788ebfdb9e5aaef930b29a
dboissin <damien.boissin@web-education.net>
2017-05-12 12:27:16 +0200

    prepare next development iteration

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker-compose up -d neo4j > /dev/null &
 sleep 10
-vertx runMod org.entcore~infra~1.29-SNAPSHOT > /dev/null &
+vertx runMod org.entcore~infra~2.0-SNAPSHOT > /dev/null &
