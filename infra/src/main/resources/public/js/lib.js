e55568b3a9e704e2cac62cabd5730f8001dc7a53
Camille Wintz <camille.wintz@web-education.net>
2017-06-26 11:27:17 +0200

    [infra][recorder] fixed notification when saving

--- infra/src/main/resources/public/js/lib.js
+++ infra/src/main/resources/public/js/lib.js
@@ -1855,7 +1855,7 @@ var recorder = (function(){
                 		console.log(event.data);
 						closeWs();
 						notify.info(event.data);
-                	} else if (event.data && event.data === "ok" && recorder.status === 'encoding') {
+                	} else if (event.data && event.data === "ok") {
                 		closeWs();
                 		notify.info("recorder.saved");
                 	}
