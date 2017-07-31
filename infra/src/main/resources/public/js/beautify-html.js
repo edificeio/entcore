2628acc868acb944e4a01206ec1a30c7c3121404
Camille Wintz <camille.wintz@web-education.net>
2017-07-31 15:06:59 +0200

    [infra][editor] fixed beautifier loading over webpack

--- infra/src/main/resources/public/js/beautify-html.js
+++ infra/src/main/resources/public/js/beautify-html.js
@@ -928,15 +928,6 @@
               }
             };
         });
-    } else if (typeof exports !== "undefined") {
-        // Add support for CommonJS. Just put this file somewhere on your require.paths
-        // and you will be able to `var html_beautify = require("beautify").html_beautify`.
-        var js_beautify = require('./beautify.js');
-        var css_beautify = require('./beautify-css.js');
-
-        exports.html_beautify = function(html_source, options) {
-            return style_html(html_source, options, js_beautify.js_beautify, css_beautify.css_beautify);
-        };
     } else if (typeof window !== "undefined") {
         // If we're running a web page and don't have either of the above, add our one global
         window.html_beautify = function(html_source, options) {
