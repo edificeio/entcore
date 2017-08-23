132c53077559fd9a4a766bab420f390cfd71ec66
Camille Wintz <camille.wintz@live.fr>
2017-08-23 16:27:51 +0200

    [workspace] image editor buttons

--- workspace/src/main/resources/public/ts/app.ts
+++ workspace/src/main/resources/public/ts/app.ts
@@ -1,4 +1,20 @@
-import { ng } from 'entcore';
+import { ng, routes } from 'entcore';
 import { workspaceController } from './controller';
+
+routes.define(function($routeProvider) {
+	$routeProvider
+		.when('/folder/:folderId', {
+			action: 'viewFolder'
+		})
+		.when('/shared/folder/:folderId', {
+	  		action: 'viewSharedFolder'
+		})
+		.when('/shared', {
+		  	action: 'openShared'
+		})
+		.otherwise({
+		  	redirectTo: '/'
+		})
+});
 
 ng.controllers.push(workspaceController);
