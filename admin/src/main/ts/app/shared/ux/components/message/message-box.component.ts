67fc8ca3ca8220f9d56991f293c1ea422d463ef9
Rafik Djedjig <rafik.djedjig@web-education.net>
2017-10-09 19:10:30 +0200

    [admin]fix MessageBox and MessageSticker component

--- admin/src/main/ts/app/shared/ux/components/message/message-box.component.ts
+++ admin/src/main/ts/app/shared/ux/components/message/message-box.component.ts
@@ -1,7 +1,13 @@
 import { Component, Input, Output, ChangeDetectorRef, ElementRef, EventEmitter, OnInit } from '@angular/core'
-import { BundlesService } from 'sijil'
 
 export type MessageType = 'info' | 'success' | 'warning' | 'danger';
+
+export const icons = {
+    info : 'fa-question-circle',
+    warning : 'fa-exclamation-circle',
+    danger : 'fa-ban',
+    success : 'fa-check-circle' 
+};
 
 @Component({
     selector: 'message-box',
@@ -10,20 +16,20 @@ export type MessageType = 'info' | 'success' | 'warning' | 'danger';
             <div *ngIf="header" class="message-header">
                 <p>
                 <i *ngIf="canHide()" (click)="hidden = true" class="fa fa-times-circle"></i>
-                    {{header}}
+                    {{header | translate}}
                 </p>
             </div>
             <div class="message-body">
             <i *ngIf="canHide() && !header" (click)="hidden = true" class="fa fa-times-circle"></i>
                 <p *ngFor="let message of messages"> 
-                {{message | translate}}
+                    <s5l>{{message}}</s5l>
                 </p>
             </div>
         </article>
         `,
     styles: [`
-        :host { font-size : initial; }
-        article.message { 
+        :host { 
+            font-size : initial;
             position: inherit;
             top: -5px;
             right: -5px; 
@@ -38,11 +44,8 @@ export type MessageType = 'info' | 'success' | 'warning' | 'danger';
 })
 export class MessageBoxComponent implements OnInit {
     constructor (
-        private bundles: BundlesService,
         private eltRef:ElementRef,
         private cdRef : ChangeDetectorRef)  {}
-
-    translate = (...args) => { return (<any> this.bundles.translate)(...args) }
 
     @Input() type: MessageType;
     @Input() header:string;
@@ -52,7 +55,6 @@ export class MessageBoxComponent implements OnInit {
     @Input() set position(value:'absolute' | 'inherit') {
         this._position = value;
     }
-
 
     hidden:boolean = false ;
     canHide():boolean {
@@ -66,6 +68,11 @@ export class MessageBoxComponent implements OnInit {
             throw new Error('MessageSticker : type\' property must be set');
         }
         this.eltRef.nativeElement.style.position = this._position;
+        if (this._position == 'absolute') {
+            this.eltRef.nativeElement.style.width = '450px';
+            // Maybe use a static counter to push on top the last open MessageBox ?
+            this.eltRef.nativeElement.style.zIndex = '10';
+        }
     }
 }
 
