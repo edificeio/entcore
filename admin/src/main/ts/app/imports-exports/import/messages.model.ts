7520e8b81d516d3241c5886ead92fab2fdf718f4
Rafik Djedjig <rafik.djedjig@web-education.net>
2017-10-10 12:57:23 +0200

    [wizard] Messages class to collect help messages

---
+++ admin/src/main/ts/app/imports-exports/import/messages.model.ts
@@ -0,0 +1,27 @@
+const MESSAGES = {
+    'import.info.file': 10,
+    'import.info.deleteAccount' : 1,
+    'import.info.transition' : 5
+};
+
+export class Messages {
+    constructor() {
+        this.dictionary = {};
+    }
+    private dictionary:{};
+
+     get(key:string):string[] {
+        if (this.dictionary[key] == undefined) {
+            if (MESSAGES[key] == undefined) {
+                throw new Error(`Messages : key : ${key} is note dedined`);
+            }
+            let value : string[] = [];
+            for (let index = 1; index < MESSAGES[key]+1; index++) {
+                value.push(key+ '.' + index)
+            }
+            this.dictionary[key] = value;
+        }
+        return this.dictionary[key];
+    }
+
+}
