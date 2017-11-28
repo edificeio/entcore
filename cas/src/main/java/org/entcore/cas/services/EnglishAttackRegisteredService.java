c3dd687499fc9687ab50a31086fb26a320d9c322
vogelmt <matthieu.vogel@cgi.com>
2017-11-28 16:33:54 +0100

    [CO-55] : CAS Response for English Attach Service : Function for discipline
    [CO-55] : CAS Response for English Attach Service : Function for discipline 2

--- cas/src/main/java/org/entcore/cas/services/EnglishAttackRegisteredService.java
+++ cas/src/main/java/org/entcore/cas/services/EnglishAttackRegisteredService.java
@@ -20,17 +20,23 @@
 package org.entcore.cas.services;
 
 import fr.wseduc.cas.entities.User;
+import fr.wseduc.webutils.Either;
+import org.entcore.common.neo4j.Neo4j;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.core.logging.impl.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.Handler;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import static org.entcore.common.neo4j.Neo4jResult.*;
 
 public class EnglishAttackRegisteredService extends AbstractCas20ExtensionRegisteredService {
 
@@ -45,8 +51,11 @@ public class EnglishAttackRegisteredService extends AbstractCas20ExtensionRegist
 	protected static final String EA_DISCIPLINE = "ENTDiscipline";
 	protected static final String EA_EMAIL = "ENTPersonMail";
 	protected static final String EA_LASTNAME = "Nom";
+
+
 	protected static final String EA_FIRSTNAME = "Prenoms";
-	private final Pattern classGroupDisciplinePattern = Pattern.compile(".*\\$(.*)");
+	private final Pattern classGroupPattern = Pattern.compile(".*\\$(.*)");
+	private final Neo4j neo = Neo4j.getInstance();
 
 	@Override
 	public void configure(org.vertx.java.core.eventbus.EventBus eb, Map<String,Object> conf) {
@@ -55,7 +64,7 @@ public class EnglishAttackRegisteredService extends AbstractCas20ExtensionRegist
 	};
 
 	@Override
-	protected void prepareUserCas20(User user, String userId, String service, JsonObject data, Document doc, List<Element> additionnalAttributes) {
+	protected void prepareUserCas20(User user,final String userId, String service,final JsonObject data, final Document doc,final List<Element> additionnalAttributes) {
 		user.setUser(data.getString(principalAttributeName));
 
 		if (log.isDebugEnabled()){
@@ -63,95 +72,125 @@ public class EnglishAttackRegisteredService extends AbstractCas20ExtensionRegist
 		}
 
 		try {
-
 			if (log.isDebugEnabled()){
 				log.debug("DATA : prepareUserCas20 data : " + data);
 			}
 
-			// Uid
-			if (data.containsField("externalId")) {
-				additionnalAttributes.add(createTextElement(EA_ID, data.getString("externalId"), doc));
-			}
+			String query = "MATCH (u:`User` { id : {id}}) return u";
+			JsonObject params = new JsonObject().putString("id", userId);
+			neo.execute(query, params, new Handler<Message<JsonObject>>() {
+				@Override
+				public void handle(Message<JsonObject> m) {
 
-			// Structures
-			Element rootStructures = createElement(EA_STRUCTURE+"s", doc);
-			for (Object o : data.getArray("structures", new JsonArray()).toList()) {
-				Element rootStructure = createElement(EA_STRUCTURE, doc);
-				Map<String, Object> structure = ((Map<String, Object>) o);
-				if (structure.containsKey("UAI")) {
-					rootStructure.appendChild(createTextElement(EA_STRUCTURE_UAI, structure.get("UAI").toString(), doc));
-				}
-				if (structure.containsKey("name")) {
-					rootStructure.appendChild(createTextElement(EA_STRUCTURE_NAME, structure.get("name").toString(), doc));
-				}
-				rootStructures.appendChild(rootStructure);
-			}
-			additionnalAttributes.add(rootStructures);
-
-			// Profile
-			switch(data.getString("type")) {
-			case "Student" :
-				additionnalAttributes.add(createTextElement(EA_PROFILES, "National_1", doc));
-				addStringArray(EA_CLASSE, "classes","name", data, doc, additionnalAttributes, new Mapper<String, String>(){
-					String map(String input) {
-						Matcher m = classGroupDisciplinePattern.matcher(input);
-						if(m.matches() && m.groupCount() >= 1){
-							return m.group(1);
-						}
-						return input;
+					// Uid
+					if (data.containsField("externalId")) {
+						additionnalAttributes.add(createTextElement(EA_ID, data.getString("externalId"), doc));
 					}
-				});
-				// Email
-				if (data.containsField("email")) {
-					additionnalAttributes.add(createTextElement(EA_EMAIL, data.getString("email"), doc));
-				}
-				break;
-			case "Teacher" :
-				additionnalAttributes.add(createTextElement(EA_PROFILES, "National_3", doc));
-				addStringArray(EA_CLASSE, "classes","name", data, doc, additionnalAttributes, new Mapper<String, String>(){
-					String map(String input) {
-						Matcher m = classGroupDisciplinePattern.matcher(input);
-						if(m.matches() && m.groupCount() >= 1){
-							return m.group(1);
+
+					// Structures
+					Element rootStructures = createElement(EA_STRUCTURE+"s", doc);
+					for (Object o : data.getArray("structures", new JsonArray()).toList()) {
+						Element rootStructure = createElement(EA_STRUCTURE, doc);
+						Map<String, Object> structure = ((Map<String, Object>) o);
+						if (structure.containsKey("UAI")) {
+							rootStructure.appendChild(createTextElement(EA_STRUCTURE_UAI, structure.get("UAI").toString(), doc));
 						}
-						return input;
-					}
-				});
-				addStringArray(EA_DISCIPLINE, "classCategories","name", data, doc, additionnalAttributes, new Mapper<String, String>(){
-					String map(String input) {
-						Matcher m = classGroupDisciplinePattern.matcher(input);
-						if(m.matches() && m.groupCount() >= 1){
-							return m.group(1);
+						if (structure.containsKey("name")) {
+							rootStructure.appendChild(createTextElement(EA_STRUCTURE_NAME, structure.get("name").toString(), doc));
 						}
-						return input;
+						rootStructures.appendChild(rootStructure);
 					}
-				});
-				// Email
-				if (data.containsField("emailAcademy")) {
-					additionnalAttributes.add(createTextElement(EA_EMAIL, data.getString("emailAcademy"), doc));
-				} else if (data.containsField("email")) {
-					additionnalAttributes.add(createTextElement(EA_EMAIL, data.getString("email"), doc));
+					additionnalAttributes.add(rootStructures);
 
+					// Profile
+					switch(data.getString("type")) {
+						case "Student" :
+							additionnalAttributes.add(createTextElement(EA_PROFILES, "National_1", doc));
+							addStringArray(EA_CLASSE, "classes","name", data, doc, additionnalAttributes, new Mapper<String, String>(){
+								String map(String input) {
+									Matcher m = classGroupPattern.matcher(input);
+									if(m.matches() && m.groupCount() >= 1){
+										return m.group(1);
+									}
+									return input;
+								}
+							});
+							// Email
+							if (data.containsField("email")) {
+								additionnalAttributes.add(createTextElement(EA_EMAIL, data.getString("email"), doc));
+							}
+							break;
+						case "Teacher" :
+							additionnalAttributes.add(createTextElement(EA_PROFILES, "National_3", doc));
+							addStringArray(EA_CLASSE, "classes","name", data, doc, additionnalAttributes, new Mapper<String, String>(){
+								String map(String input) {
+									Matcher m = classGroupPattern.matcher(input);
+									if(m.matches() && m.groupCount() >= 1){
+										return m.group(1);
+									}
+									return input;
+								}
+							});
+
+							//Discipline
+							Either<String, JsonObject> res = validUniqueResult(m);
+							if (res.isRight()) {
+								JsonObject j = res.right().getValue();
+								if(null != j.getObject("u")
+										&& null != j.getObject("u").getObject("data")
+										&& null != j.getObject("u").getObject("data").getArray("functions")){
+									JsonArray jsonArrayFunctions = j.getObject("u").getObject("data").getArray("functions");
+									if(jsonArrayFunctions.size() > 0){
+										Element rootDisciplines = createElement(EA_DISCIPLINE+"s", doc);
+										List<String> vTempListDiscipline = new ArrayList<>();
+										for (int i = 0; i < jsonArrayFunctions.size(); i++) {
+											String fonction = jsonArrayFunctions.get(i);
+											String[] elements = fonction.split("\\$");
+											if ( elements.length > 1 ){
+												String discipline = elements[elements.length - 2] + "$" + elements[elements.length - 1];
+												if(!vTempListDiscipline.contains(discipline)){
+													vTempListDiscipline.add(discipline);
+													rootDisciplines.appendChild(createTextElement(EA_DISCIPLINE, discipline, doc));
+												}
+											}else{
+												log.error("Failed to get User functions userID, : " + userId + " fonction : " + fonction);
+											}
+										}
+										additionnalAttributes.add(rootDisciplines);
+									}
+								}else{
+									log.error("Failed to get User functions userID, user empty : " + userId + " j : " + j);
+								}
+							} else {
+								log.error("Failed to get User functions userID : " + userId);
+							}
+							// Email
+							if (data.containsField("emailAcademy")) {
+								additionnalAttributes.add(createTextElement(EA_EMAIL, data.getString("emailAcademy"), doc));
+							} else if (data.containsField("email")) {
+								additionnalAttributes.add(createTextElement(EA_EMAIL, data.getString("email"), doc));
+
+							}
+							break;
+						case "Relative" :
+							additionnalAttributes.add(createTextElement(EA_PROFILES, "National_2", doc));
+							break;
+						case "Personnel" :
+							additionnalAttributes.add(createTextElement(EA_PROFILES, "National_4", doc));
+							break;
+					}
+
+					// Lastname
+					if (data.containsField("lastName")) {
+						additionnalAttributes.add(createTextElement(EA_LASTNAME, data.getString("lastName"), doc));
+					}
+
+					// Firstname
+					if (data.containsField("firstName")) {
+						additionnalAttributes.add(createTextElement(EA_FIRSTNAME, data.getString("firstName"), doc));
+					}
 				}
-				break;
-			case "Relative" :
-				additionnalAttributes.add(createTextElement(EA_PROFILES, "National_2", doc));
-				break;
-			case "Personnel" :
-				additionnalAttributes.add(createTextElement(EA_PROFILES, "National_4", doc));
-				break;
-			}
-
-			// Lastname
-			if (data.containsField("lastName")) {
-				additionnalAttributes.add(createTextElement(EA_LASTNAME, data.getString("lastName"), doc));
-			}
-
-			// Firstname
-			if (data.containsField("firstName")) {
-				additionnalAttributes.add(createTextElement(EA_FIRSTNAME, data.getString("firstName"), doc));
-			}
-
+			});
 
 		} catch (Exception e) {
 			log.error("Failed to transform User for EnglishAttack", e);
@@ -175,6 +214,7 @@ public class EnglishAttackRegisteredService extends AbstractCas20ExtensionRegist
 		}
 		additionalAttributes.add(root);
 	}
+
 
 	private abstract class Mapper<IN, OUT>{
 		abstract OUT map(IN input);
