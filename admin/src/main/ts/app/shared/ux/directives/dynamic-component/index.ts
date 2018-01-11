307a412ceee6f5f292bd14560f8a4aa08c204af2
Rafik Djedjig <rafik.djedjig@web-education.net>
2018-01-11 16:13:24 +0100

    [wizard] refactor DynamicComponentDirective declaration

--- admin/src/main/ts/app/shared/ux/directives/dynamic-component/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './dynamic-component.directive'
-export * from './component-descriptor.model'
--- admin/src/main/ts/app/shared/ux/directives/index.ts
+++ admin/src/main/ts/app/shared/ux/directives/index.ts
@@ -1,2 +1,4 @@
 export { AnchorDirective } from './anchor.directive'
 export { DynamicTemplateDirective } from './dynamictemplate.directive'
+export { DynamicComponentDirective } from './dynamic-component/dynamic-component.directive'
+export { ComponentDescriptor } from './dynamic-component/component-descriptor.model'
