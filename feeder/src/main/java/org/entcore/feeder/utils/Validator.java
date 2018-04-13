1bfe5fe03fb2fd1f29f21a0c8822fbc1d660e8f8
dboissin <damien.boissin@web-education.net>
2018-04-13 13:45:31 +0200

    Revert "Init login map in blocking context"

--- feeder/src/main/java/org/entcore/feeder/utils/Validator.java
+++ feeder/src/main/java/org/entcore/feeder/utils/Validator.java
@@ -416,48 +416,43 @@ public class Validator {
 	}
 
 	public static void initLogin(Neo4j neo4j, Vertx vertx) {
-			final long startInit = System.currentTimeMillis();
-			if (logins == null) {
-				logins = MapFactory.getSyncClusterMap("usedLogins", vertx);
-				initLogins(neo4j, startInit, false, vertx);
-			} else {
-				initLogins(neo4j, startInit, true, vertx);
-			}
+		final long startInit = System.currentTimeMillis();
+		if (logins == null) {
+			logins = MapFactory.getSyncClusterMap("usedLogins", vertx);
+			initLogins(neo4j, startInit, false);
+		} else {
+			initLogins(neo4j, startInit, true);
+		}
 
-			if (invalidEmails == null) {
-				invalidEmails = MapFactory.getSyncClusterMap("invalidEmails", vertx);
-			}
+		if (invalidEmails == null) {
+			invalidEmails = MapFactory.getSyncClusterMap("invalidEmails", vertx);
+		}
 	}
 
-	protected static void initLogins(Neo4j neo4j, final long startInit, final boolean remove, final Vertx vertx) {
+	protected static void initLogins(Neo4j neo4j, final long startInit, final boolean remove) {
 		String query = "MATCH (u:User) RETURN COLLECT(DISTINCT u.login) as logins";
 		neo4j.execute(query, new JsonObject(), new Handler<Message<JsonObject>>() {
 			@Override
 			public void handle(Message<JsonObject> message) {
 				JsonArray r = message.body().getJsonArray("result");
 				if ("ok".equals(message.body().getString("status")) && r != null && r.size() == 1) {
-					vertx.executeBlocking(future -> {
-						JsonArray l = (r.getJsonObject(0)).getJsonArray("logins");
-						if (l != null) {
-							final Set<Object> tmp = new HashSet<>(l.getList());
-							if (remove) {
-								for (Object key : logins.keySet()) {
-									if (!tmp.contains(key)) {
-										logins.remove(key, null);
-									} else {
-										tmp.remove(key);
-									}
+					JsonArray l = (r.getJsonObject(0)).getJsonArray("logins");
+
+					if (l != null) {
+						final Set<Object> tmp = new HashSet<>(l.getList());
+						if (remove) {
+							for (Object key : logins.keySet()) {
+								if (!tmp.contains(key)) {
+									logins.remove(key, null);
+								} else {
+									tmp.remove(key);
 								}
-								putLogin(tmp);
-							} else {
-								putLogin(tmp);
 							}
+							putLogin(tmp);
+						} else {
+							putLogin(tmp);
 						}
-					}, ar -> {
-						if (ar.failed()) {
-							log.error("Error loading logins.", ar.cause());
-						}
-					});
+					}
 				}
 			}
 
