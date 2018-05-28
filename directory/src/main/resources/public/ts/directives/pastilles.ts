8d65059e0c0e5217da852860cfc1acc50fbd7eb5
Marie Laporte <mmarie.laporte@gmail.com>
2018-05-28 13:40:02 +0200

    [Evo][Groups] Adding attribute images for pastilles

--- directory/src/main/resources/public/ts/directives/pastilles.ts
+++ directory/src/main/resources/public/ts/directives/pastilles.ts
@@ -10,15 +10,9 @@ export const pastilles = ng.directive('pastilles', () => {
     return {
         restrict: 'E',
         template: `
-            <div class="spacer-medium">
-                <a class="absolute-w round square-large img-shadow high-index inactive">
-                    <img skin-src="/img/illustrations/group-avatar.svg"/>
-                </a>
-                <a class="absolute-w round square-large img-shadow high-index inactive">
-                    <img skin-src="/img/illustrations/group-avatar.svg"/>
-                </a>
-                <a class="absolute-w round square-large img-shadow high-index active">
-                    <img skin-src="/img/illustrations/group-avatar.svg"/>
+            <div class="spacer-medium invisible-content">
+                <a class="absolute-w round square-large img-shadow high-index inactive" ng-repeat="image in images track by $index">
+                    <img skin-src="[[ image ]]"/>
                 </a>
             </div>
         `,
@@ -28,44 +22,56 @@ export const pastilles = ng.directive('pastilles', () => {
         },
 
         link: (scope, element, attributes) => {
-            var pastilles = element.find(".spacer-medium").children();
-            var i, nbPastilles = pastilles.length;
-            var totalWidth, pastilleWidth, leftOffset, offset, pastilleOffset;
-
-            totalWidth = element.width();
-            pastilleWidth = pastilles[0].offsetWidth;
-            offset = pastilleWidth * 3 / 4;
-            leftOffset = (totalWidth - (pastilleWidth + ((nbPastilles - 1) * offset))) / 2;
-
-            for (i = nbPastilles - 1; i >= 0; i--) {
-                pastilles[nbPastilles - 1 - i].originalLeft = leftOffset + (i * offset);
-                pastilles.eq(nbPastilles - 1 - i).css("left", pastilles[nbPastilles - 1 - i].originalLeft + "px");
-            }
-
-            // Centering when hovering (padding+absolute position)
-            element.find('.round:not(.active)').on('mouseenter', function() {
-                scope.setActive(this);
-            });
-
-            element.find('.round:not(.active)').on('mouseleave', function() {
-                this.style.left = this.originalLeft + "px";
-                this.style.top = "0px";
-                this.style.marginTop = "0px";
-            });
-
-            scope.setActive = (e) => {
-                e.style.left = this.originalLeft - 3 + "px";
-                e.style.top = "-3px";
-                e.style.marginTop = "-3px";
-            };
-
-            // Activate the first pastille
-            scope.setActive(pastilles[nbPastilles - 1]);
+            scope.images = JSON.parse(attributes.images);
             
+            // Waiting for pastilles to be created
             setTimeout(function () {
-                for (i = 0; i < nbPastilles; i++) {
-                    pastilles.eq(i).addClass("animated");
+                var i, l;
+                var pastilles = element.find("div").children();
+                var totalWidth, pastilleWidth, leftOffset, offset, pastilleOffset, nbPastilles = pastilles.length;
+
+                totalWidth = element.width();
+                pastilleWidth = pastilles[0].offsetWidth;
+                offset = pastilleWidth * 3 / 4;
+                leftOffset = (totalWidth - (pastilleWidth + ((nbPastilles - 1) * offset))) / 2;
+
+                for (i = nbPastilles - 1; i >= 0; i--) {
+                    pastilles[nbPastilles - 1 - i].originalLeft = leftOffset + (i * offset);
+                    pastilles.eq(nbPastilles - 1 - i).css("left", pastilles[nbPastilles - 1 - i].originalLeft + "px");
                 }
+
+                // Centering when hovering (padding+absolute position)
+                element.find('.round').on('mouseenter', function() {
+                    if (this.classList.contains("inactive"))
+                        scope.setActive(this);
+                });
+
+                element.find('.round').on('mouseleave', function() {
+                    if (this.classList.contains("inactive")) {
+                        this.style.left = this.originalLeft + "px";
+                        this.style.top = "0px";
+                        this.style.marginTop = "0px";
+                    }
+                });
+
+                scope.setActive = (e) => {
+                    e.style.left = this.originalLeft - 3 + "px";
+                    e.style.top = "-3px";
+                    e.style.marginTop = "-3px";
+                };
+
+                // Activate the first pastille
+                scope.setActive(pastilles[nbPastilles - 1]);
+                pastilles.eq(nbPastilles - 1).removeClass("inactive");
+                pastilles.eq(nbPastilles - 1).addClass("active");
+
+                // Animated class added after fix positioned
+                setTimeout(function () {
+                    for (i = 0; i < nbPastilles; i++)
+                        pastilles.eq(i).addClass("animated");
+                }, 0);
+
+                element.find("div").removeClass("invisible-content");
             }, 0);
         }
     };
