21971610d3544ea29a48195fa43abca12ca63295
Marie Laporte <mmarie.laporte@gmail.com>
2018-06-11 15:43:13 +0200

    [Evo][Groups] Adding circles design for transfer and some info messages for empty states

--- directory/src/main/resources/public/ts/directives/transferColumns.ts
+++ directory/src/main/resources/public/ts/directives/transferColumns.ts
@@ -10,9 +10,23 @@ export const transferColumns = ng.directive('transferColumns', () => {
     return {
         restrict: 'E',
         template: `
+            <div class="flex-row bottom-spacing">
+                <div class="six">
+                    <div class="circle square-normal green right-magnet right-spacing-three" ng-click="search()" ng-if="searchedItems.length > 0">
+                        <i class="right-arrow white-text centered-text block"></i>
+                    </div>
+                </div>
+                <div class="six">
+                    <span class="medium-importance left-spacing-twice">[[ textTitle ]]</span>
+                    <div class="circle square-normal red right-magnet right-spacing" ng-click="search()" ng-if="ngModel.length > 0">
+                        <i class="close white-text centered-text block"></i>
+                    </div>
+                </div>
+            </div>
             <div class="scroll-nine-chips">
                 <div class="flex-row">
                     <div class="six">
+                        <div class="row info" ng-if="searchedItems.length === 0">[[ textLeftInfo ]]</div>
                         <label class="block row twelve chip movable low-importance" ng-repeat="item in searchedItems">
                             <span class="cell round square-small group">
                                 <img skin-src="/img/illustrations/group-avatar.svg"/>
@@ -24,7 +38,14 @@ export const transferColumns = ng.directive('transferColumns', () => {
                     </div>
                     <div class="horizontal-margin-twice divider-border"></div>
                     <div class="six">
-                        <label class="block row twelve chip removable low-importance">
+                        <div class="flex-row warning" ng-if="ngModel.length === 0">
+                            <div><i class="warning"></i></div>
+                            <div>
+                                <div>[[ textRightWarningTitle ]]</div>
+                                <div>[[ textRightWarningDescription ]]</div>
+                            </div>
+                        </div>
+                        <label class="block row twelve chip removable low-importance" ng-repeat="item in ngModel">
                             <span class="cell round square-small group">
                                 <img skin-src="/img/illustrations/group-avatar.svg"/>
                             </span>
@@ -43,7 +64,10 @@ export const transferColumns = ng.directive('transferColumns', () => {
         },
 
         link: (scope, element, attributes) => {
-            
+            scope.textTitle = attributes.textTitle;
+            scope.textLeftInfo = attributes.textLeftInfo;
+            scope.textRightWarningTitle = attributes.textRightWarningTitle;
+            scope.textRightWarningDescription = attributes.textRightWarningDescription;
         }
     };
 });
