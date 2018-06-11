09a52bc37d4e1e8afb62da032320c1aa2b932d11
Marie Laporte <mmarie.laporte@gmail.com>
2018-06-11 13:57:10 +0200

    [Evo][Groups] Creation favorite form search

--- directory/src/main/resources/public/ts/directives/transferColumns.ts
+++ directory/src/main/resources/public/ts/directives/transferColumns.ts
@@ -10,33 +10,36 @@ export const transferColumns = ng.directive('transferColumns', () => {
     return {
         restrict: 'E',
         template: `
-            <div class="flex-row">
-                <div class="six">
-                    <label ng-model="ngModel" ng-change="ngChange" class="block row twelve chip movable low-importance">
-                        <span class="cell round square-small group">
-                            <img skin-src="/img/illustrations/group-avatar.svg"/>
-                        </span>
-                        <span class="cell circle square-mini purple"></span>
-                        <span class="cell-ellipsis block left-text">Tous les élèves du groupe 4TRIS</span>
-                        <i class="right-arrow absolute-magnet"></i>
-                    </label>
-                </div>
-                <div class="horizontal-margin-twice divider-border"></div>
-                <div class="six">
-                    <label ng-model="ngModel" ng-change="ngChange" class="block row twelve chip removable low-importance">
-                        <span class="cell round square-small group">
-                            <img skin-src="/img/illustrations/group-avatar.svg"/>
-                        </span>
-                        <span class="cell circle square-mini purple"></span>
-                        <span class="cell-ellipsis block left-text">Tous les élèves du groupe 4TRIS</span>
-                        <i class="close absolute-magnet"></i>
-                    </label>
+            <div class="scroll-nine-chips">
+                <div class="flex-row">
+                    <div class="six">
+                        <label class="block row twelve chip movable low-importance" ng-repeat="item in searchedItems">
+                            <span class="cell round square-small group">
+                                <img skin-src="/img/illustrations/group-avatar.svg"/>
+                            </span>
+                            <span class="cell circle square-mini purple"></span>
+                            <span class="cell-ellipsis block left-text">Tous les élèves du groupe 4TRIS</span>
+                            <i class="right-arrow absolute-magnet"></i>
+                        </label>
+                    </div>
+                    <div class="horizontal-margin-twice divider-border"></div>
+                    <div class="six">
+                        <label class="block row twelve chip removable low-importance">
+                            <span class="cell round square-small group">
+                                <img skin-src="/img/illustrations/group-avatar.svg"/>
+                            </span>
+                            <span class="cell circle square-mini purple"></span>
+                            <span class="cell-ellipsis block left-text">Tous les élèves du groupe 4TRIS</span>
+                            <i class="close absolute-magnet"></i>
+                        </label>
+                    </div>
                 </div>
             </div>
         `,
 
         scope: {
-
+            ngModel: '=',
+            searchedItems: '='
         },
 
         link: (scope, element, attributes) => {
