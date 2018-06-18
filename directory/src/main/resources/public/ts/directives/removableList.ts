d7e0ab51942895eddf7b01dc83a94c3f6f77f1d5
Marie Laporte <mmarie.laporte@gmail.com>
2018-06-18 13:37:55 +0200

    [Evo][Groups] removable-list search + empty search

--- directory/src/main/resources/public/ts/directives/removableList.ts
+++ directory/src/main/resources/public/ts/directives/removableList.ts
@@ -10,21 +10,21 @@ export const removableList = ng.directive('removableList', () => {
     return {
         restrict: 'E',
         template: `
-            <div class="row info" ng-if="ngModel.length === 0"><i18n>directory.nofavorite</i18n></div>
-            <div ng-if="ngModel.length > 0">
-                <input type="search"
-                    ng-model="search"
+            <div class="row info" ng-if="ngModel.length === 0"><i18n>[[ noitems ]]</i18n></div>
+            <div ng-show="ngModel.length > 0">
+                <input type="text"
+                    ng-model="searchText"
                     placeholder="[[ placeholder ]]"
-                    class="text-flow twelve"
-                    required ng-minlength="1"/>
-                <nav class="removable-list wrapper left-text">
-                    <div class="row big-block-container" ng-repeat="item in ngModel" ng-click="selectItem({item: item})">
+                    class="text-flow twelve"/>
+                <nav class="removable-list wrapper left-text" ng-show="filteredItems.length > 0">
+                    <div class="row big-block-container" ng-repeat="item in filteredItems = (ngModel | filter:filterByName)" ng-click="selectItem({item: item})">
                         <span class="block cell-ellipsis right-spacing-twice">[[ item.name ]]</span>
                         <i class="trash right-spacing-twice vertical-spacing-four absolute-magnet only-desktop" 
                             ng-click="deleteItem({item: item}); $event.stopPropagation();" 
                             ng-if="removable"/>
                     </div>
                 </nav>
+                <div ng-show="filteredItems.length === 0"><span><i18n>[[ noresult ]]</i18n></span></div>
             </div>
         `,
 
@@ -36,8 +36,17 @@ export const removableList = ng.directive('removableList', () => {
 
         link: (scope, element, attributes) => {
             scope.placeholder = attributes.placeholder;
-            scope.search = '';
+            scope.noitems = attributes.noitems;
+            scope.noresult = attributes.noresult;
             scope.removable = !angular.isUndefined(attributes.deleteItem);
+            scope.searchText = '';
+
+            // Filter names in search field
+            scope.filterByName = (item) => {
+                if (!item.name)
+                    item.name = '';
+                return item.name.toLowerCase().includes(scope.searchText.toLowerCase());
+            };
         }
     };
 });
