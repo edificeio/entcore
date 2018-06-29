017f1f97e46b9ed650170a6eecf05db0830d17a3
Rafik Djedjig <rafik.djedjig@web-education.net>
2018-06-29 17:34:42 +0200

    [shared/ux] Add ObjectURLDirective to create and download blob in file

--- admin/src/main/ts/app/shared/ux/directives/index.ts
+++ admin/src/main/ts/app/shared/ux/directives/index.ts
@@ -3,3 +3,4 @@ export { DynamicTemplateDirective } from './dynamictemplate.directive';
 export { DynamicComponentDirective } from './dynamic-component/dynamic-component.directive';
 export { ComponentDescriptor } from './dynamic-component/component-descriptor.model';
 export { DragAndDropFilesDirective } from './drag-and-drop-files.directive';
+export { ObjectURLDirective } from './object-url.directive';
