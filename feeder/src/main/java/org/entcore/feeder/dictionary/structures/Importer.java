8a2905dac7d9e3786b2ab901c8b6a4777f446802
dboissin <damien.boissin@web-education.net>
2018-07-04 19:00:21 +0200

    [Fix][Feeder] fix attachements in some groups

--- feeder/src/main/java/org/entcore/feeder/dictionary/structures/Importer.java
+++ feeder/src/main/java/org/entcore/feeder/dictionary/structures/Importer.java
@@ -696,9 +696,9 @@ public class Importer {
 	}
 
 	public void removeOldFunctionalGroup() {
-		transactionHelper.add("MATCH (g:Group) WHERE labels(g) IN ['FunctionalGroup', 'FunctionGroup', 'HTGroup'] set g.notEmptyGroup = false;", null);
-		transactionHelper.add("MATCH (g:Group)<-[:IN]-(:User) WHERE labels(g) IN ['FunctionalGroup', 'FunctionGroup', 'HTGroup'] set g.notEmptyGroup = true;", null);
-		transactionHelper.add("MATCH (g:Group {notEmptyGroup:false}) WHERE labels(g) IN ['FunctionalGroup', 'FunctionGroup', 'HTGroup'] detach delete g;", null);
+		transactionHelper.add("MATCH (g:Group) WHERE g:FunctionalGroup OR g:FunctionGroup OR g:HTGroup set g.notEmptyGroup = false;", null);
+		transactionHelper.add("MATCH (g:Group)<-[:IN]-(:User) WHERE g:FunctionalGroup OR g:FunctionGroup OR g:HTGroup set g.notEmptyGroup = true;", null);
+		transactionHelper.add("MATCH (g:Group {notEmptyGroup:false}) WHERE g:FunctionalGroup OR g:FunctionGroup OR g:HTGroup detach delete g;", null);
 		// prevent difference between relationships and properties
 		String query2 =
 				"MATCH (u:User) " +
