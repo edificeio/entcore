b0b1470e40694a7191ef5b92ec039e037cca0c96
Alexandre Hascour <alexandre.hascour@web-education.net>
2018-08-17 12:11:28 +0200

    [Fix][Wizard] #19795 Added event support to destroy component ref when closing

--- admin/src/main/ts/app/shared/ux/components/message/message-box.component.ts
+++ admin/src/main/ts/app/shared/ux/components/message/message-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, ChangeDetectorRef, ElementRef, EventEmitter, OnInit } from '@angular/core'
+import { Component, Input, Output, ChangeDetectorRef, ElementRef, EventEmitter, OnInit, OnDestroy } from '@angular/core'
 
 export type MessageType = 'info' | 'success' | 'warning' | 'danger';
 
@@ -15,12 +15,12 @@ export const icons = {
         <article class="message is-{{type}}" [hidden]="hidden">
             <div *ngIf="header" class="message-header">
                 <p>
-                <i *ngIf="canHide()" (click)="hidden = true" class="fa fa-times-circle"></i>
+                <i *ngIf="canHide()" (click)="hide()" class="fa fa-times-circle"></i>
                     {{header | translate}}
                 </p>
             </div>
             <div class="message-body">
-            <i *ngIf="canHide() && !header" (click)="hidden = true" class="fa fa-times-circle"></i>
+                <i *ngIf="canHide() && !header" (click)="hide()" class="fa fa-times-circle"></i>
                 <p *ngFor="let message of messages"> 
                     <s5l [s5l-params]="message[1]">{{message[0]}}</s5l>
                 </p>
@@ -59,8 +59,6 @@ export class MessageBoxComponent implements OnInit {
                 this._messages.push([m,{}]);
             else if (typeof m == "object") {
                 this._messages.push(m);
-                console.log(m[0] +' '+m[1]);
-                console.log(typeof m);
             }
         }
     };
@@ -73,11 +71,16 @@ export class MessageBoxComponent implements OnInit {
         this._position = value;
     }
 
-    hidden:boolean = false ;
+    hidden:boolean = false;
+    @Output('onHide') hideEvent: EventEmitter<void>;
     canHide():boolean {
         if (this._position == 'absolute')
             return true; 
         return false;
+    }
+    hide(): void {
+        this.hidden = true;
+        this.hideEvent.emit();
     }
 
     ngOnInit():void { 
