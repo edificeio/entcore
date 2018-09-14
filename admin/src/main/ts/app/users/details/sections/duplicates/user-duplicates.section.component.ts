b2731fab64044989f9d46642c324122a183a595c
Alexandre Hascour <alexandre.hascour@web-education.net>
2018-09-14 12:38:49 +0200

    [Fix][Admin v2] #20481 Fixed user duplicate link

--- admin/src/main/ts/app/users/details/sections/duplicates/user-duplicates.section.component.ts
+++ admin/src/main/ts/app/users/details/sections/duplicates/user-duplicates.section.component.ts
@@ -17,7 +17,7 @@ import { UsersStore } from '../../../users.store'
                 <li *ngFor="let duplicate of user?.duplicates">
                     <span *ngIf="findVisibleStruct(duplicate.structures)">
                         <a class="action" 
-                            [routerLink]="['/admin', findVisibleStruct(duplicate.structures), 'users', duplicate.id]">
+                            [routerLink]="['/admin', findVisibleStruct(duplicate.structures).id, 'users', duplicate.id]">
                             {{ duplicate.lastName | uppercase }} {{ duplicate.firstName }} {{ formatStructures(duplicate.structures) }}
                         </a>
                     </span>
