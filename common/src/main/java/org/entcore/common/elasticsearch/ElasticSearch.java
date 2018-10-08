e3787e2b51885a4e184439c789bde6274ca85bda
dboissin <damien.boissin@web-education.net>
2018-10-08 14:11:42 +0200

    [Evo][Common] support multi data nodes in elasticsearch client

--- common/src/main/java/org/entcore/common/elasticsearch/ElasticSearch.java
+++ common/src/main/java/org/entcore/common/elasticsearch/ElasticSearch.java
@@ -30,15 +30,42 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.entcore.common.neo4j.Neo4jRest;
 
 import java.net.URI;
+import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ElasticSearch {
 
+	private static class ElasticSearchClient {
+
+		private final int index;
+		private final HttpClient client;
+		private AtomicInteger errorsCount = new AtomicInteger(0);
+
+		private ElasticSearchClient(int index, HttpClient client) {
+			this.index = index;
+			this.client = client;
+		}
+
+		private boolean checkError() {
+			return errorsCount.incrementAndGet() > 3;
+		}
+
+		private void checkSuccess() {
+			if (errorsCount.get() > 0) {
+				errorsCount.set(0);
+			}
+		}
+	}
+
 	private static final Logger log = LoggerFactory.getLogger(ElasticSearch.class);
-	private HttpClient httpClient;
+	private ElasticSearchClient[] clients;
+	private final CopyOnWriteArrayList<Integer> availableNodes = new CopyOnWriteArrayList<>();
+	private final Random rnd = new Random();
 	private String defaultIndex;
+	private Vertx vertx;
 
 	private ElasticSearch() {}
 
@@ -51,6 +78,7 @@ public class ElasticSearch {
 	}
 
 	public void init(Vertx vertx, JsonObject config) {
+		this.vertx = vertx;
 		JsonArray serverUris = config.getJsonArray("server-uris");
 		String serverUri = config.getString("server-uri");
 		if (serverUris == null && serverUri != null) {
@@ -64,7 +92,6 @@ public class ElasticSearch {
 					uris[i] = new URI(serverUris.getString(i));
 				}
 				init(uris, vertx,
-						config.getLong("checkDelay", 3000l),
 						config.getInteger("poolSize", 16),
 						config.getBoolean("keepAlive", true),
 						config);
@@ -76,16 +103,20 @@ public class ElasticSearch {
 		}
 	}
 
-	public void init(URI[] uris, Vertx vertx, long checkDelay, int poolSize,
+	public void init(URI[] uris, Vertx vertx, int poolSize,
 						 boolean keepAlive, JsonObject elasticsearchConfig) {
 		defaultIndex = elasticsearchConfig.getString("index");
-		// TODO manage cluster nodes
-		HttpClientOptions httpClientOptions = new HttpClientOptions()
-				.setKeepAlive(keepAlive)
-				.setMaxPoolSize(poolSize)
-				.setDefaultHost(uris[0].getHost())
-				.setDefaultPort(uris[0].getPort());
-		httpClient = vertx.createHttpClient(httpClientOptions);
+		clients = new ElasticSearchClient[uris.length];
+		for (int i = 0; i < uris.length; i++) {
+			HttpClientOptions httpClientOptions = new HttpClientOptions()
+					.setKeepAlive(keepAlive)
+					.setMaxPoolSize(poolSize)
+					.setDefaultHost(uris[i].getHost())
+					.setDefaultPort(uris[i].getPort())
+					.setConnectTimeout(20000);
+			clients[i] = new ElasticSearchClient(i, vertx.createHttpClient(httpClientOptions));
+			availableNodes.addIfAbsent(i);
+		}
 	}
 
 	public void search(String type, JsonObject query, Handler<AsyncResult<JsonObject>> handler) {
@@ -97,30 +128,48 @@ public class ElasticSearch {
 	}
 
 	private void postInternal(String path, int expectedStatus, JsonObject payload, Handler<AsyncResult<JsonObject>> handler) {
-		HttpClientRequest req = httpClient.post(path, event -> {
+		final ElasticSearchClient esc = getClient();
+		final HttpClientRequest req = esc.client.post(path, event -> {
 			if (event.statusCode() == expectedStatus) {
 				event.bodyHandler(respBody -> handler.handle(new DefaultAsyncResult<>(new JsonObject(respBody))));
 			} else {
 				handler.handle(new DefaultAsyncResult<>(new ElasticSearchException(event.statusMessage())));
 			}
+			esc.checkSuccess();
 		});
+		req.exceptionHandler(e -> checkDisableClientAfterError(esc, e));
 		req.putHeader("Content-Type", "application/json");
 		req.putHeader("Accept", "application/json; charset=UTF-8");
 		req.end(payload.encode());
 	}
 
 	public BulkRequest bulk(String type, Handler<AsyncResult<JsonObject>> handler) {
-		HttpClientRequest req = httpClient.post(defaultIndex + "/" + type + "/_bulk", event -> {
+		final ElasticSearchClient esc = getClient();
+		final HttpClientRequest req = esc.client.post(defaultIndex + "/" + type + "/_bulk", event -> {
 			if (event.statusCode() == 200) {
 				event.bodyHandler(respBody -> handler.handle(new DefaultAsyncResult<>(new JsonObject(respBody))));
 			} else {
 				handler.handle(new DefaultAsyncResult<>(new ElasticSearchException(event.statusMessage())));
 			}
+			esc.checkSuccess();
 		});
+		req.exceptionHandler(e -> checkDisableClientAfterError(esc, e));
 		req.putHeader("Content-Type", "application/x-ndjson");
 		req.putHeader("Accept", "application/json; charset=UTF-8");
 		req.setChunked(true);
 		return new BulkRequest(req);
+	}
+
+	private void checkDisableClientAfterError(ElasticSearchClient esc, Throwable e) {
+		log.error("Error with ElasticSearchClient : " + esc.index, e);
+		if (esc.checkError()) {
+			availableNodes.remove(Integer.valueOf(esc.index));
+			vertx.setTimer(60000L, h -> availableNodes.addIfAbsent(esc.index));
+		}
+	}
+
+	private ElasticSearchClient getClient() {
+		return clients[rnd.nextInt(availableNodes.size())];
 	}
 
 }
