7c8678704db450dfaf0d1db0c243662dbd2bc5eb
dboissin <damien.boissin@web-education.net>
2018-10-12 14:39:28 +0200

    update license

--- communication/src/test/java/org/entcore/communication/test/integration/java/CommunicationTest.java
+++ communication/src/test/java/org/entcore/communication/test/integration/java/CommunicationTest.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright © "Open Digital Education", 2018
+ *
+ * This program is published by "Open Digital Education".
+ * You must indicate the name of the software and the company in any production /contribution
+ * using the software and indicate on the home page of the software industry in question,
+ * "powered by Open Digital Education" with a reference to the website: https://opendigitaleducation.com/.
+ *
+ * This program is free software, licensed under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, version 3 of the License.
+ *
+ * You can redistribute this application and/or modify it since you respect the terms of the GNU Affero General Public License.
+ * If you modify the source code and then use this modified source code in your creation, you must make available the source code of your modifications.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with the software.
+ * If not, please see : <http://www.gnu.org/licenses/>. Full compliance requires reading the terms of this license and following its directives.
+ */
+
 ///*
-// * Copyright © WebServices pour l'Éducation, 2014
+// * Copyright © "Open Digital Education", 2014
 // *
 // * This file is part of ENT Core. ENT Core is a versatile ENT engine based on the JVM.
 // *
