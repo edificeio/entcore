a78ed95216e47d38e6b87f0c35c6f46085418283
Simon Basset <simon.basset@gmail.com>
2018-11-23 15:56:42 +0100

    feat(adminV2): CAV2-369, m group are created with com rule both + switch
    - add a switch button to set the internal com rule of a manual group
    - reformat + removed unused deps in GroupUsersList
    - refactor UserListService, GroupDetails
    - adds tests on GroupUsersList, GroupDetails
    - also fix an issue with webpack versions

---
+++ admin/src/main/ts/app/groups/details/group-internal-communication-rule.resolver.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+
+export type InternalCommunicationRule = 'BOTH' | 'INCOMING' | 'OUTGOING' | 'NONE';
+
+export interface CommunicationGroupResponse {
+    groupDisplayName: string;
+    displayNameSearchField: string;
+    name: string;
+    id: string;
+    users: InternalCommunicationRule | undefined;
+    communiqueWith: Array<string>;
+}
+
+export interface GroupIdAndInternalCommunicationRule {
+    groupId: string;
+    internalCommunicationRule: InternalCommunicationRule;
+}
+
+@Injectable()
+export class GroupInternalCommunicationRuleResolver implements Resolve<GroupIdAndInternalCommunicationRule> {
+    constructor(private http: HttpClient) {
+    }
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<GroupIdAndInternalCommunicationRule> {
+        if(!route.paramMap.has('groupId')) {
+            return Observable.throw(new Error('no groupId'));
+        }
+
+        return this.http.get<CommunicationGroupResponse>(`/communication/group/${route.paramMap.get('groupId')}`)
+            .map(response => ({
+                groupId: response.id,
+                internalCommunicationRule: response.users? response.users : 'NONE'
+            }));
+    }
+}
