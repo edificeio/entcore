caabcea8b1d63cae0a2cf5e33ede4450284f5af2
Simon Basset <simon.basset@gmail.com>
2018-11-23 15:41:08 +0100

    refactor(adminV2): use HttpClientModule to adds a group

--- admin/src/main/ts/app/app.module.ts
+++ admin/src/main/ts/app/app.module.ts
@@ -1,25 +1,28 @@
-import { NgModule, Provider } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { RouterModule } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { SijilModule } from "sijil";
 
-import { CoreModule } from './core/core.module'
-import { AppComponent } from './app.component'
-import { AppHomeComponent } from './app-home.component'
+import { CoreModule } from './core/core.module';
+import { AppComponent } from './app.component';
+import { AppHomeComponent } from './app-home.component';
 
-import { routes } from './app.routing'
+import { routes } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     imports: [
         BrowserModule,
         RouterModule.forRoot(routes),
         CoreModule,
+        HttpClientModule,
         SijilModule.forRoot()
     ],
     declarations: [
         AppComponent,
         AppHomeComponent
     ],
-    bootstrap: [ AppComponent ]
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
