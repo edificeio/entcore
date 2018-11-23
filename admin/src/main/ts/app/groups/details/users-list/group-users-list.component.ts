af461268c7ce47d81b77baca809d11c5b16db857
Simon Basset <simon.basset@gmail.com>
2018-11-23 17:24:44 +0100

    refactor(adminV2): reformat + removed unused deps in GroupUsersList

--- admin/src/main/ts/app/groups/details/users-list/group-users-list.component.ts
+++ admin/src/main/ts/app/groups/details/users-list/group-users-list.component.ts
@@ -1,47 +1,45 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core'
-import { Router } from '@angular/router'
-import { BundlesService } from 'sijil'
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { GroupModel, UserModel } from '../../../core/store'
-import { UserListService } from '../../../core/services'
+import { UserModel } from '../../../core/store';
+import { UserListService } from '../../../core/services';
 
 @Component({
     selector: 'group-users-list',
     template: `
-        <list
-            [model]="users"
-            [inputFilter]="userLS.filterByInput"
-            [sort]="userLS.sorts"
-            searchPlaceholder="search.user"
-            (inputChange)="userLS.inputFilter = $event"
-            (onSelect)="selectUser($event)"
-            noResultsLabel="list.results.no.users">
+        <list [model]="users"
+              [inputFilter]="userLS.filterByInput"
+              [sort]="userLS.sorts"
+              searchPlaceholder="search.user"
+              (inputChange)="userLS.inputFilter = $event"
+              (onSelect)="selectUser($event)"
+              noResultsLabel="list.results.no.users">
             <div toolbar>
                 <i class="fa" aria-hidden="true"
-                [ngClass]="{
+                   [ngClass]="{
                     'fa-sort-alpha-asc': userLS.sortsMap.alphabetical.sort === '+',
                     'fa-sort-alpha-desc': userLS.sortsMap.alphabetical.sort === '-',
                     'selected': userLS.sortsMap.alphabetical.selected
                 }"
-                [tooltip]="'sort.alphabetical' | translate" position="top"
-                (click)="userLS.changeSorts('alphabetical')"></i>
+                   [tooltip]="'sort.alphabetical' | translate" position="top"
+                   (click)="userLS.changeSorts('alphabetical')"></i>
                 <i class="fa" aria-hidden="true"
-                    [ngClass]="{
+                   [ngClass]="{
                         'fa-sort-amount-asc': userLS.sortsMap.profile.sort === '+',
                         'fa-sort-amount-desc': userLS.sortsMap.profile.sort === '-',
                         'selected': userLS.sortsMap.profile.selected
                     }"
-                    [tooltip]="'sort.profile' | translate" position="top"
-                    (click)="userLS.changeSorts('profile')"></i>
+                   [tooltip]="'sort.profile' | translate" position="top"
+                   (click)="userLS.changeSorts('profile')"></i>
                 <strong class="badge">
                     {{ users.length }}
-                    {{ 'members' | translate:{ count: users.length } | lowercase }}
+                    {{ 'members' | translate:{count: users.length} | lowercase }}
                 </strong>
             </div>
 
             <ng-template let-item>
                 <span class="display-name">{{item?.lastName.toUpperCase()}} {{item?.firstName}}</span>
-                
+
                 <i class="profile" [ngClass]="item.type">{{item.type | translate}}</i>
                 <span class="structures">
                     <ul>
@@ -50,25 +48,20 @@ import { UserListService } from '../../../core/services'
                 </span>
             </ng-template>
         </list>
-    ` ,
-    styles: [``],
-    providers: [ UserListService ],
+    `,
+    providers: [UserListService],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GroupUsersList {
+    constructor(private router: Router, public userLS: UserListService) {
+    }
 
-    constructor(
-            private router: Router,
-            private bundles: BundlesService,
-            public userLS: UserListService){}
-
-    // Model
     @Input()
-    users : UserModel[]
+    users: UserModel[];
 
     selectUser(user: UserModel) {
-        if(user.structures.length > 0){
-            this.router.navigate(['admin', user.structures[0].id, 'users', user.id])
+        if (user.structures.length > 0) {
+            this.router.navigate(['admin', user.structures[0].id, 'users', user.id]);
         }
     }
 }
