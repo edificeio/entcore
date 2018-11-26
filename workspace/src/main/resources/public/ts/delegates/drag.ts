9e7b3c7a17ab6096ba27890f8f2bbbaffa19f062
nabil <nabil.mansouri@opendigitaleducation.com>
2018-11-26 09:57:20 +0100

    [Evo][ContribFolder] add css optimisation + move css to lib

--- workspace/src/main/resources/public/ts/delegates/drag.ts
+++ workspace/src/main/resources/public/ts/delegates/drag.ts
@@ -4,51 +4,72 @@ import { models, workspaceService } from "../services";
 export interface DragDelegateScope {
     //from others
     currentTree: models.Tree
+    openedFolder: models.FolderContext
     onInit(cab: () => void);
     safeApply()
+    isDraggingElement: boolean
+    selectedItems(): models.Element[];
     //
     //drag and drop
+    countDragItems(): number
     lockDropzone: boolean
-    dragMove(origin: models.Element, target: models.Element)
+    canDropOnElement(el: models.Element): boolean
+    dropMove(origin: models.Element[], target: models.Element)
     drag(item: models.Element, event?: any)
     dragEnd(item: models.Element, event?: any)
     dragCondition(item: models.Element)
-    dragToTrash(item: models.Element)
-    dropCondition(item: models.Element)
+    dropTrash(item: models.Element[])
+    dropCondition(item: models.Element): (event) => boolean
     dropTo(item: models.Element, event?: any)
-    dropTrash(item: models.Element)
-    dropMove(source: models.Element, dest: models.Element)
     //
 }
 declare var jQuery;
 export function DragDelegate($scope: DragDelegateScope) {
+    let draggingItems: models.Element[] = []
     $scope.onInit(function () {
         //INIT
+        $scope.isDraggingElement = false;
     });
     $scope.lockDropzone = false;
+    $scope.countDragItems = function () {
+        return draggingItems.length;
+    }
     $scope.dragEnd = function (el, event: Event) {
         //reset
         $scope.lockDropzone = false;
-        jQuery(event.target).removeClass("zoomout-2x")
+        $scope.isDraggingElement = false;
         $scope.safeApply()
     }
-    $scope.dragMove = async function (origin, target) {
+    $scope.dropMove = async function (origin, target) {
         template.close('lightbox');
         if ($scope.currentTree.filter == "shared") {
-            await workspaceService.moveAllForShared([origin], target)
+            await workspaceService.moveAllForShared(origin, target)
         } else {
-            await workspaceService.moveAll([origin], target)
+            await workspaceService.moveAll(origin, target)
         }
     };
 
     $scope.drag = function (item, $originalEvent: DragEvent) {
-        jQuery($originalEvent.target).addClass("zoomout-2x")
+        //clean null values and keep unique values
+        draggingItems = [...$scope.selectedItems(), item].filter(item => !!item).filter((elem, pos, arr) => {
+            return arr.indexOf(elem) == pos;
+        });
         $scope.lockDropzone = true;
+        try {
+            const original = jQuery($originalEvent.target);
+            const copy = original.clone().css({ position: "absolute", top: -10000, left: -1000, zoom: 0.5 }).addClass("zoomout-2x").insertAfter(original)
+            $originalEvent.dataTransfer.setDragImage(copy[0], undefined, undefined);
+        } catch (e) {
+            console.warn(e)
+        }
+        //
         try {
             $originalEvent.dataTransfer.setData('application/json', JSON.stringify(item));
         } catch (e) {
             $originalEvent.dataTransfer.setData('Text', JSON.stringify(item));
         }
+        //
+        $scope.isDraggingElement = true;
     };
 
     $scope.dragCondition = function (item) {
@@ -56,42 +77,61 @@ export function DragDelegate($scope: DragDelegateScope) {
     }
 
     $scope.dropCondition = function (targetItem) {
-        return function (event) {
-            const { types } = event.dataTransfer;
-            const dataField = types.indexOf && types.indexOf("application/json") > -1 ? "application/json" : //Chrome & Safari
-                types.contains && types.contains("application/json") ? "application/json" : //Firefox
-                    types.contains && types.contains("Text") ? "Text" : //IE
-                        undefined
-            const tree = (targetItem as models.Tree);
-            if (!dataField || tree.filter === 'shared' || tree.filter === 'protected')
+        return function (event): boolean {
+            if (!targetItem) {
+                return false;
+            }
+            if (draggingItems.length == 0)
                 return false
-
-            return dataField
+            //cannot drag on shared or protected tree
+            const tree = (targetItem as models.Tree);
+            const isTree = !!tree.filter;
+            if (isTree && (tree.filter === 'shared' || tree.filter === 'protected'))
+                return false
+            //get real folder
+            if (!isTree) {
+                targetItem = $scope.openedFolder.folders.find(folder => folder._id == targetItem._id);
+            }
+            //check after search
+            if (!targetItem) {
+                return false;
+            }
+            //cannot drag on himself
+            const targetIndex = draggingItems.filter(item => item === targetItem || (item && targetItem && item._id == targetItem._id))
+            if (targetIndex.length > 0) {
+                return false;
+            }
+            //can drop only on folder
+            if (!isTree && targetItem.eType != models.FOLDER_TYPE) {
+                return false;
+            }
+            //can drop on shared
+            const fileIds = draggingItems.map(item => item._id);
+            if (!isTree && targetItem.isShared && !targetItem.canCopyFileIdsInto(fileIds)) {
+                return false;
+            }
+            //else accept
+            return true
         }
+    }
+    $scope.canDropOnElement = function (el) {
+        return $scope.isDraggingElement && $scope.dropCondition(el)(null);
     }
 
     $scope.dropTo = function (targetItem, $originalEvent) {
-        const dataField = $scope.dropCondition(targetItem)($originalEvent);
-        const originalItem = JSON.parse($originalEvent.dataTransfer.getData(dataField));
-
-        if (originalItem._id === targetItem._id)
+        const can = $scope.dropCondition(targetItem)($originalEvent);
+        if (!can)
             return;
 
         if ((targetItem as models.Tree).filter === 'trash')
-            $scope.dropTrash(originalItem);
+            $scope.dropTrash(draggingItems);
         else
-            $scope.dropMove(originalItem, targetItem);
+            $scope.dropMove(draggingItems, targetItem);
+        draggingItems = [];
     };
 
-    $scope.dropMove = function (originalItem, targetItem) {
-        $scope.dragMove(originalItem, targetItem)
-    }
-    $scope.dropTrash = function (originalItem) {
-        $scope.dragToTrash(originalItem)
-    }
-
-    $scope.dragToTrash = async function (item) {
-        const res = workspaceService.trashAll([item]);
+    $scope.dropTrash = async function (item) {
+        const res = workspaceService.trashAll(item);
         notify.info('workspace.removed.message');
         await res;
     };
