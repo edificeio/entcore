e964c4437a01824ab8267d439f7ef38fb5091682
Simon Basset <simon.basset@gmail.com>
2018-11-29 09:47:35 +0100

    feat(adminV2): CAV2-369, add a switch button to set the internal com rule of a manual group

--- admin/src/main/ts/app/groups/details/group-details.component.spec.ts
+++ admin/src/main/ts/app/groups/details/group-details.component.spec.ts
@@ -1,8 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
 import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
 import { SijilModule } from 'sijil';
-import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/observable/of';
 
 import { UxModule } from '../../shared/ux/ux.module';
@@ -10,6 +11,7 @@ import { GroupsStore } from '../groups.store';
 import { GroupDetails } from './group-details.component';
 import { GroupModel, StructureModel } from '../../core/store/models';
 import { GroupCollection } from '../../core/store/collections';
+import { GroupIdAndInternalCommunicationRule } from './group-internal-communication-rule.resolver';
 
 @Component({
     selector: 'group-manage-users',
@@ -40,17 +42,27 @@ describe('GroupDetails', () => {
     let mockGroupStore: GroupsStore;
     let mockStructure: StructureModel;
     let mockChangeDetectorRef: ChangeDetectorRef;
+    let httpController: HttpTestingController;
+    let paramsController: BehaviorSubject<{ [key: string]: string }>;
+    let dataController: BehaviorSubject<{ rule: GroupIdAndInternalCommunicationRule }>;
 
     beforeEach(() => {
         mockStructure = {} as StructureModel;
         mockStructure.groups = {
             data: [
-                generateMockGroupModel('groupId1', 'Manual'),
-                generateMockGroupModel('groupId2', 'Other')
+                generateMockGroupModel('groupId1', 'ManualGroup'),
+                generateMockGroupModel('groupId2', 'OtherGroup')
             ]
         } as GroupCollection;
         mockGroupStore = {structure: mockStructure} as GroupsStore;
         mockChangeDetectorRef = jasmine.createSpyObj('ChangeDetectorRef', ['markForCheck']);
+        paramsController = new BehaviorSubject({groupId: 'groupId1'});
+        dataController = new BehaviorSubject<{ rule: GroupIdAndInternalCommunicationRule }>({
+            rule: {
+                groupId: 'groupId1',
+                internalCommunicationRule: 'BOTH'
+            }
+        });
     });
 
     beforeEach(async(() => {
@@ -63,14 +75,21 @@ describe('GroupDetails', () => {
             providers: [
                 {provide: GroupsStore, useValue: mockGroupStore},
                 {provide: ChangeDetectorRef, useValue: mockChangeDetectorRef},
-                {provide: ActivatedRoute, useValue: {params: Observable.of({groupId: 'groupId1'})}}
+                {
+                    provide: ActivatedRoute, useValue: {
+                        data: dataController.asObservable(),
+                        params: paramsController.asObservable()
+                    }
+                }
             ],
             imports: [
+                HttpClientTestingModule,
                 SijilModule.forRoot(),
                 UxModule.forRoot(null)
             ]
         }).compileComponents();
         fixture = TestBed.createComponent(GroupDetails);
+        httpController = TestBed.get(HttpTestingController);
         component = fixture.debugElement.componentInstance;
         fixture.detectChanges();
     }));
@@ -80,6 +99,94 @@ describe('GroupDetails', () => {
     }));
 
     it('should set the current group based on the groupId route params', () => {
-        expect(component.groupsStore.group).toEqual(generateMockGroupModel('groupId1', 'Manual'));
+        expect(component.groupsStore.group).toEqual(generateMockGroupModel('groupId1', 'ManualGroup'));
+    });
+
+    it('should display the internal communication rule when the current group type is ManualGroup', () => {
+        expect(fixture.nativeElement.querySelectorAll('.lct-communication-rule').length).toBe(1);
+    });
+
+    it('should not display the internal communication rule when the current group type is not ManualGroup', () => {
+        paramsController.next({groupId: 'groupId2'});
+        fixture.detectChanges();
+        expect(fixture.nativeElement.querySelectorAll('.lct-communication-rule').length).toBe(0);
+    });
+
+    it(`should display the 'can-communicate' element when the group type is ManualGroup and internal communication rule is 'BOTH'`, () => {
+        expect(fixture.nativeElement.querySelectorAll('.lct-communication-rule__can-communicate').length).toBe(1);
+        expect(fixture.nativeElement.querySelectorAll('.lct-communication-rule__cannot-communicate').length).toBe(0);
+    });
+
+    it(`should display the 'cannot-communicate' element when the group type is ManualGroup and internal communication rule is 'INCOMING'`, () => {
+        dataController.next({rule: {groupId: 'groupId1', internalCommunicationRule: 'INCOMING'}});
+        fixture.detectChanges();
+        expect(fixture.nativeElement.querySelectorAll('.lct-communication-rule__cannot-communicate').length).toBe(1);
+        expect(fixture.nativeElement.querySelectorAll('.lct-communication-rule__can-communicate').length).toBe(0);
+    });
+
+    describe('toggleCommunicationBetweenMembers', () => {
+        it(`should call the backend DELETE /communication/group/myGroupId with direction BOTH given group id 'myGroupId' and internal communication rule 'BOTH'`, () => {
+            component.toggleCommunicationBetweenMembers('myGroupId', 'BOTH').subscribe();
+            const communicationGroupRequest = httpController.expectOne('/communication/group/myGroupId');
+            expect(communicationGroupRequest.request.method).toBe('DELETE');
+            expect(communicationGroupRequest.request.body).toEqual({
+                direction: 'BOTH'
+            });
+        });
+
+        it(`should call the backend /communication/group/myGroupId with direction BOTH given group id 'myGroupId' and internal communication rule 'NONE'`, () => {
+            component.toggleCommunicationBetweenMembers('myGroupId', 'NONE').subscribe();
+            expect(httpController.expectOne('/communication/group/myGroupId').request.body).toEqual({
+                direction: 'BOTH'
+            });
+        });
+
+        it(`should call the backend /communication/group/myGroupId with direction BOTH given group id 'myGroupId' and internal communication rule 'INCOMING'`, () => {
+            component.toggleCommunicationBetweenMembers('myGroupId', 'INCOMING').subscribe();
+            expect(httpController.expectOne('/communication/group/myGroupId').request.body).toEqual({
+                direction: 'BOTH'
+            });
+        });
+
+        it(`should call the backend /communication/group/myGroupId with direction BOTH given group id 'myGroupId' and internal communication rule 'OUTGOING'`, () => {
+            component.toggleCommunicationBetweenMembers('myGroupId', 'OUTGOING').subscribe();
+            expect(httpController.expectOne('/communication/group/myGroupId').request.body).toEqual({
+                direction: 'BOTH'
+            });
+        });
+    });
+
+    it(`should set the internalCommunicationRule of the group to 'NONE'
+        when clicking on the communication rule toggle button and given the current group has the 'BOTH' rule`, () => {
+        fixture.nativeElement.querySelector('.lct-communication-rule').click();
+        httpController.expectOne('/communication/group/groupId1').flush({number: 1});
+        expect(component.internalCommunicationRule).toBe('NONE');
+    });
+
+    it(`should set the internalCommunicationRule of the group to 'BOTH'
+        when clicking on the communication rule toggle button and given the current group has the 'NONE' rule`, () => {
+        dataController.next({rule: {groupId: 'groupId1', internalCommunicationRule: 'NONE'}});
+        fixture.detectChanges();
+        fixture.nativeElement.querySelector('.lct-communication-rule').click();
+        httpController.expectOne('/communication/group/groupId1').flush({number: 1});
+        expect(component.internalCommunicationRule).toBe('BOTH');
+    });
+
+    it(`should set the internalCommunicationRule of the group to 'BOTH'
+        when clicking on the communication rule toggle button and given the current group has the 'INCOMING' rule`, () => {
+        dataController.next({rule: {groupId: 'groupId1', internalCommunicationRule: 'INCOMING'}});
+        fixture.detectChanges();
+        fixture.nativeElement.querySelector('.lct-communication-rule').click();
+        httpController.expectOne('/communication/group/groupId1').flush({number: 1});
+        expect(component.internalCommunicationRule).toBe('BOTH');
+    });
+
+    it(`should set the internalCommunicationRule of the group to 'BOTH'
+        when clicking on the communication rule toggle button and given the current group has the 'OUTGOING' rule`, () => {
+        dataController.next({rule: {groupId: 'groupId1', internalCommunicationRule: 'OUTGOING'}});
+        fixture.detectChanges();
+        fixture.nativeElement.querySelector('.lct-communication-rule').click();
+        httpController.expectOne('/communication/group/groupId1').flush({number: 1});
+        expect(component.internalCommunicationRule).toBe('BOTH');
     });
 });
--- admin/src/main/ts/app/groups/details/group-details.component.ts
+++ admin/src/main/ts/app/groups/details/group-details.component.ts
@@ -1,8 +1,20 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/merge';
 
 import { GroupsStore } from '../groups.store';
+import {
+    GroupIdAndInternalCommunicationRule,
+    InternalCommunicationRule
+} from './group-internal-communication-rule.resolver';
 
 @Component({
     selector: 'group-detail',
@@ -21,33 +33,76 @@ import { GroupsStore } from '../groups.store';
                 <group-manage-users (close)="closeLightBox()"></group-manage-users>
             </lightbox>
 
+            <span class="lct-communication-rule"
+                  *ngIf="internalCommunicationRule && groupsStore.group?.type === 'ManualGroup'"
+                  (click)="toggleCommunicationRuleClicked.next({groupId: groupsStore.group.id, internalCommunicationRule: internalCommunicationRule})">
+                <span class="lct-communication-rule__can-communicate"
+                      *ngIf="internalCommunicationRule === 'BOTH'; else cannotCommunicateTogether;">
+                    <s5l>group.details.members.can.communicate</s5l> <i class="fa fa-toggle-on"></i>
+                </span>
+                <ng-template #cannotCommunicateTogether>
+                    <span class="lct-communication-rule__cannot-communicate">
+                        <s5l>group.details.members.cannot.communicate</s5l> <i class="fa fa-toggle-off"></i>
+                    </span>
+                </ng-template>
+            </span>
+
             <group-users-list [users]="groupsStore.group?.users">
             </group-users-list>
         </div>
     `,
+    styles: [
+        '.lct-communication-rule {cursor: pointer;}',
+        '.lct-communication-rule__can-communicate {color: green;}',
+        '.lct-communication-rule__cannot-communicate {color: red;}'
+    ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GroupDetails implements OnInit, OnDestroy {
-    showAddUsersLightBox = false;
-    private groupSubscriber: Subscription;
+    public internalCommunicationRule: InternalCommunicationRule | undefined;
+    public showAddUsersLightBox = false;
+    public toggleCommunicationRuleClicked: Subject<GroupIdAndInternalCommunicationRule> = new Subject();
+
+    private changesSubscription: Subscription;
 
     constructor(public groupsStore: GroupsStore,
+                private http: HttpClient,
                 private route: ActivatedRoute,
                 private cdRef: ChangeDetectorRef) {
     }
 
     ngOnInit(): void {
-        this.groupSubscriber = this.route.params.subscribe(params => {
-            if (params['groupId']) {
+        const rulesChangesObserver = Observable.merge(
+            this.route.data
+                .do(() => console.log('resolver emitted'))
+                .map((data: {rule: GroupIdAndInternalCommunicationRule}) => data.rule),
+            this.toggleCommunicationRuleClicked
+                .switchMap(data => this.toggleCommunicationBetweenMembers(data.groupId, data.internalCommunicationRule))
+                .do(() => console.log('toggleCommunicationRuleClicked start'))
+                .filter(res => res.groupId === this.groupsStore.group.id) // don't affect the UI if we changed the current group
+                .do(res => console.log('toggleCommunicationRuleClicked changed', res.groupId, this.groupsStore.group.id, this.internalCommunicationRule))
+        )
+            .map((data: GroupIdAndInternalCommunicationRule) => data.internalCommunicationRule)
+            .do(rule => this.internalCommunicationRule = rule)
+            .do(() => console.log('rule changed', this.internalCommunicationRule));
+
+        const groupChangesObserver = this.route.params
+            .filter(params => params['groupId'])
+            .do(params =>
                 this.groupsStore.group = this.groupsStore.structure.groups.data
-                    .find(g => g.id === params['groupId']);
-                this.cdRef.markForCheck();
-            }
-        });
+                    .find(g => g.id === params['groupId'])
+            )
+            .do(() => console.log('group changed', this.internalCommunicationRule));
+
+        this.changesSubscription = Observable
+            .merge(rulesChangesObserver, groupChangesObserver)
+            .do(() => console.log('marking for check', this.internalCommunicationRule))
+            .subscribe(() => this.cdRef.markForCheck());
+
     }
 
     ngOnDestroy(): void {
-        this.groupSubscriber.unsubscribe();
+        this.changesSubscription.unsubscribe();
     }
 
     showLightBox() {
@@ -58,5 +113,18 @@ export class GroupDetails implements OnInit, OnDestroy {
     closeLightBox() {
         this.showAddUsersLightBox = false;
         document.body.style.overflowY = 'auto';
+    }
+
+    toggleCommunicationBetweenMembers(groupId: string, internalCommunicationRule: InternalCommunicationRule): Observable<GroupIdAndInternalCommunicationRule> {
+        let request: Observable<{ number: number }>;
+        const direction: InternalCommunicationRule = internalCommunicationRule === 'BOTH' ? 'NONE' : 'BOTH';
+
+        if (direction === 'BOTH') {
+            request = this.http.post<{ number: number }>(`/communication/group/${groupId}`, {direction});
+        } else {
+            request = this.http.request<{ number: number }>('delete', `/communication/group/${groupId}`, {body: {direction: 'BOTH'}});
+        }
+
+        return request.map(() => ({groupId, internalCommunicationRule: direction}));
     }
 }
