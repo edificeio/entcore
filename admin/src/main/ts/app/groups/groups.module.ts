d24378040b2f10210da072bd0775402bf5b99842
Simon Basset <simon.basset@gmail.com>
2018-11-29 09:47:35 +0100

    feat(adminV2): CAV2-369, add a switch button to set the internal com rule of a manual group

--- admin/src/main/ts/app/groups/groups.module.ts
+++ admin/src/main/ts/app/groups/groups.module.ts
@@ -1,25 +1,26 @@
-import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
-import { RouterModule } from '@angular/router'
-import { NgModule } from '@angular/core'
-import { SijilModule } from 'sijil'
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { SijilModule } from 'sijil';
 
-import { UxModule } from '../shared/ux/ux.module'
-import { routes } from './groups.routing'
-import { GroupsResolver } from './groups.resolver'
-import { GroupDetailsResolver } from './details/group-details.resolver'
-import { GroupsStore } from './groups.store'
-import { UserlistFiltersService } from '../core/services'
+import { UxModule } from '../shared/ux/ux.module';
+import { routes } from './groups.routing';
+import { GroupsResolver } from './groups.resolver';
+import { GroupDetailsResolver } from './details/group-details.resolver';
+import { GroupInternalCommunicationRuleResolver } from './details/group-internal-communication-rule.resolver';
+import { GroupsStore } from './groups.store';
+import { UserlistFiltersService } from '../core/services';
 
-import { GroupsComponent } from './groups.component'
-import { GroupCreate } from "./create/group-create.component";
-import { GroupDetails } from './details/group-details.component'
-import { GroupManageUsers } from './details/manage-users/group-manage-users.component'
-import { GroupInputUsers } from './details/manage-users/input/group-input-users.component'
-import { GroupInputFilters } from './details/manage-users/input/group-input-filters.component'
-import { GroupOutputUsers } from './details/manage-users/output/group-output-users.component'
-import { GroupUsersList} from './details/users-list/group-users-list.component'
-import { GroupsTypeView} from './type-view/groups-type-view.component'
+import { GroupsComponent } from './groups.component';
+import { GroupCreate } from './create/group-create.component';
+import { GroupDetails } from './details/group-details.component';
+import { GroupManageUsers } from './details/manage-users/group-manage-users.component';
+import { GroupInputUsers } from './details/manage-users/input/group-input-users.component';
+import { GroupInputFilters } from './details/manage-users/input/group-input-filters.component';
+import { GroupOutputUsers } from './details/manage-users/output/group-output-users.component';
+import { GroupUsersList } from './details/users-list/group-users-list.component';
+import { GroupsTypeView } from './type-view/groups-type-view.component';
 
 @NgModule({
     imports: [
@@ -43,6 +44,7 @@ import { GroupsTypeView} from './type-view/groups-type-view.component'
     providers: [
         GroupsResolver,
         GroupDetailsResolver,
+        GroupInternalCommunicationRuleResolver,
         GroupsStore,
         UserlistFiltersService
     ],
@@ -50,4 +52,5 @@ import { GroupsTypeView} from './type-view/groups-type-view.component'
         RouterModule
     ]
 })
-export class GroupsModule {}
+export class GroupsModule {
+}
