aff611daabdd2298aeb661f8363965897077536c
nabil <nabil.mansouri@opendigitaleducation.com>
2018-12-04 10:22:43 +0100

    [Evo][ContribFolder] add missing mongo index + remove old indexes

--- migration/3.3.0/workspaceIndex.js
+++ migration/3.3.0/workspaceIndex.js
@@ -1,2 +1,9 @@
 db.documents.createIndex({ name: "text", ownerName: "text" }, { background: true });
 db.documentsRevisions.createIndex({ name: "text", ownerName: "text" }, { background: true });
+db.documents.createIndex({eParent:1},{name:"idx_eparent"})
+db.documents.createIndex({"inheritedShares.userId":1},{name:"idx_inherited_userid"})
+db.documents.createIndex({"inheritedShares.groupId":1},{name:"idx_inherited_groupid"})
+db.documents.dropIndex("shared.userId_1");
+db.documents.dropIndex("shared.groupId_1");
+db.documents.dropIndex("old-folder_1");
+db.documents.dropIndex("folder_1");
