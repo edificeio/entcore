14388959a733f9edc45fbad861d7ae1b6678b2f5
nabil <nabil.mansouri@opendigitaleducation.com>
2018-12-11 09:35:45 +0100

    [Evo][ContribFolder] add lazyloading on icons view

---
+++ workspace/src/main/resources/public/ts/directives/lazyLoad.ts
@@ -0,0 +1,42 @@
+import { ng, angular } from 'entcore';
+declare var window: any
+let logged = false;
+function lazyLoadImgFunc() {
+    return {
+        restrict: 'A',
+        scope: {
+            lazyLoadImg: "="
+        },
+        link: function (scope, element, attrs) {
+            const win = window as any;
+            if (!win.IntersectionObserver ||
+                !win.IntersectionObserverEntry ||
+                !win.IntersectionObserverEntry.prototype) {
+                // load polyfill 
+                if (!logged){
+                    console.warn("lazy load is disabled")
+                }
+                logged = true;
+            } else {
+                if (!logged){
+                    console.info("lazy load is enabled")
+                }
+                logged = true;
+                //
+                const loadImg = (changes) => {
+                    changes.forEach(change => {
+                        if (change.intersectionRatio > 0) {
+                            if(!change.target.src || change.target.src.indexOf(scope.lazyLoadImg)==-1){
+                                change.target.src = scope.lazyLoadImg;
+                            }
+                        }
+                    })
+                }
+                const observer = new IntersectionObserver(loadImg)
+                const img = angular.element(element)[0];
+                observer.observe(img)
+            }
+        }
+    }
+}
+export const lazyLoadImg = ng.directive('lazyLoadImg', lazyLoadImgFunc)
