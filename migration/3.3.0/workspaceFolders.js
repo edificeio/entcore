183a2b13bacfe61fcf2167614371dd07fdba22a5
nabil <nabil.mansouri@opendigitaleducation.com>
2018-12-11 15:25:15 +0100

    [Evo][ContribFolder] force owner to be visible on sharepanel + fix manager right on migration script

--- migration/3.3.0/workspaceFolders.js
+++ migration/3.3.0/workspaceFolders.js
@@ -52,7 +52,7 @@ function replaceShared(doc) {
                 item[replaced] = item[j];
                 delete item[j]
                 //ADD NEW RIGHT
-                if (j.indexOf("deleteComment")) {//if manager
+                if (j.indexOf("deleteDocument")) {//if manager
                     item[NEW_RIGHT + "|bulkDelete"] = true;
                 }
             }
