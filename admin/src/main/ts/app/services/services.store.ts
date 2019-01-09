520a92c5dfe3a99bf10dce34b8143708274a2f3e
Simon Basset <simon.basset@gmail.com>
2019-01-09 10:33:51 +0100

    refactor(adminv2): reformat some 'services' and lightbox files
    Related to CAV2-374 and CAV2-363

--- admin/src/main/ts/app/services/services.store.ts
+++ admin/src/main/ts/app/services/services.store.ts
@@ -1,9 +1,11 @@
-import { StructureModel, ApplicationModel, ConnectorModel, AbstractStore } from '../core/store';
+import { AbstractStore, ApplicationModel, ConnectorModel, StructureModel } from '../core/store';
 
 export class ServicesStore extends AbstractStore {
 
-    constructor(){ super(['structure', 'application']) }
-    
+    constructor() {
+        super(['structure', 'application']);
+    }
+
     structure: StructureModel;
     application: ApplicationModel;
     connector: ConnectorModel;
