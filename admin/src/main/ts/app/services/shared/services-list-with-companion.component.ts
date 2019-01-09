e51a733f0ad4b6169e233d10611f054c818a6320
Simon Basset <simon.basset@gmail.com>
2019-01-09 10:33:51 +0100

    refactor(adminv2): reformat some 'services' file

--- admin/src/main/ts/app/services/shared/services-list-with-companion.component.ts
+++ admin/src/main/ts/app/services/shared/services-list-with-companion.component.ts
@@ -1,12 +1,11 @@
-import { Component, ChangeDetectorRef, Input, Output,
-    ContentChild, EventEmitter, AfterViewInit } from "@angular/core";
+import { AfterViewInit, ChangeDetectorRef, Component, Input } from '@angular/core';
 
-import { ActivatedRoute, Router,  Data } from '@angular/router';
-import { SpinnerService, routing } from '../../core/services';
+import { ActivatedRoute, Data, Router } from '@angular/router';
+import { routing } from '../../core/services';
 import { Subscription } from 'rxjs/Subscription';
 
 import { ServicesStore } from '../services.store';
-import { SessionModel } from '../../core/store'
+import { SessionModel } from '../../core/store';
 
 @Component({
     selector: 'services-list-with-companion',
@@ -14,14 +13,14 @@ import { SessionModel } from '../../core/store'
         <side-layout (closeCompanion)="closePanel()" [showCompanion]="showCompanion">
             <div side-card>
                 <list
-                    [model]="collectionRef[serviceName].collection"
-                    sort="name"
-                    [inputFilter]="filterByInput"
-                    [searchPlaceholder]="collectionRef[serviceName].searchPlaceholder"
-                    [noResultsLabel]="collectionRef[serviceName].noResultsLabel"
-                    [isSelected]="isSelected"
-                    (inputChange)="itemInputFilter = $event"
-                    (onSelect)="selectedItem = $event; router.navigate([$event.id], {relativeTo: route})">
+                        [model]="collectionRef[serviceName].collection"
+                        sort="name"
+                        [inputFilter]="filterByInput"
+                        [searchPlaceholder]="collectionRef[serviceName].searchPlaceholder"
+                        [noResultsLabel]="collectionRef[serviceName].noResultsLabel"
+                        [isSelected]="isSelected"
+                        (inputChange)="itemInputFilter = $event"
+                        (onSelect)="selectedItem = $event; router.navigate([$event.id], {relativeTo: route})">
                     <ng-template let-item>
                         <div class="service-icon">
                             <img src="{{ item.icon }}" *ngIf="isIconWorkspaceImg(item.icon)" />
@@ -43,35 +42,36 @@ import { SessionModel } from '../../core/store'
     `]
 })
 export class ServicesListWithCompanionComponent implements AfterViewInit {
-    
+
     /* Store pipe */
     self = this;
     _storedElements = [];
 
     private filteredApps = [
-        "Auth",
-        "Application Mobile",
-        "AppRegistry",
-        "Cas",
-        "Communication",
-        "Directory",
-        "Eliot",
-        "FakeSSO",
-        "Portal",
-        "Rss",
-        "Starter",
-        "Timeline",
-        "Xiti",
-        "Searchengine",
-        "Signets",
-        "Supportpivot"
+        'Auth',
+        'Application Mobile',
+        'AppRegistry',
+        'Cas',
+        'Communication',
+        'Directory',
+        'Eliot',
+        'FakeSSO',
+        'Portal',
+        'Rss',
+        'Searchengine',
+        'Signets',
+        'Starter',
+        'Supportpivot',
+        'Timeline',
+        'Xiti'
     ];
 
     constructor(
         private cdRef: ChangeDetectorRef,
         public router: Router,
         public route: ActivatedRoute,
-        private servicesStore: ServicesStore){}
+        private servicesStore: ServicesStore) {
+    }
 
     ngAfterViewInit() {
         this.cdRef.markForCheck();
@@ -82,42 +82,42 @@ export class ServicesListWithCompanionComponent implements AfterViewInit {
     @Input() serviceName: 'applications' | 'connectors' | 'widgets';
 
     collectionRef = {
-        applications : {
-            collection: this.servicesStore.structure.applications.data, 
-            model: this.servicesStore.application, 
+        applications: {
+            collection: this.servicesStore.structure.applications.data,
+            model: this.servicesStore.application,
             routeData: 'apps',
             searchPlaceholder: 'services.application.search',
             noResultsLabel: 'services.application.list.empty'
         },
-        connectors : {
-            collection: this.servicesStore.structure.connectors.data, 
-            model:this.servicesStore.connector, 
-            routeData:'connectors',
-            searchPlaceholder:'services.connector.search',
-            noResultsLabel:'services.connector.list.empty'
+        connectors: {
+            collection: this.servicesStore.structure.connectors.data,
+            model: this.servicesStore.connector,
+            routeData: 'connectors',
+            searchPlaceholder: 'services.connector.search',
+            noResultsLabel: 'services.connector.list.empty'
         }
+    };
+
+    @Input() selectedItem;
+
+    closePanel(): void {
+        this.router.navigate(['..'], {relativeTo: this.route});
     }
 
-    @Input() selectedItem
-
-    closePanel() {
-        this.router.navigate(['..'], { relativeTo: this.route });
-    }
-
-    private routeSubscriber:Subscription;
+    private routeSubscriber: Subscription;
 
     ngOnInit(): void {
         // HAck : display app just if user is ADMC => TODO implement a directive or a component
         // Session is already fetched in nav.component and must be shared instead of being requested again
-        SessionModel.getSession().then(session => { 
+        SessionModel.getSession().then(session => {
             if (!session.functions['SUPER_ADMIN']) {
-                this.filteredApps.push('Admin', 'Administration', 'ABSENCES','NOTES','SCOLARITE','TEXTES','AGENDA');
+                this.filteredApps.push('Admin', 'Administration', 'ABSENCES', 'NOTES', 'SCOLARITE', 'TEXTES', 'AGENDA');
             }
             if (!this.serviceName) {
                 throw new Error('Input property serviceName is undefined. It must be set with one of "applications" | "connectors" | "widgets"')
             }
             this.routeSubscriber = routing.observe(this.route, "data").subscribe((data: Data) => {
-                if(data[this.collectionRef[this.serviceName].routeData]) {
+                if (data[this.collectionRef[this.serviceName].routeData]) {
                     this.collectionRef[this.serviceName].collection = data[this.collectionRef[this.serviceName].routeData]
                         .filter(app => this.filteredApps.indexOf(app.name) < 0);
                     this.cdRef.markForCheck();
@@ -131,24 +131,24 @@ export class ServicesListWithCompanionComponent implements AfterViewInit {
     }
 
 
-    itemInputFilter:string;
-    filterByInput = (item: any) => {
-        if(!this.itemInputFilter) return true;
-        return item.name.toLowerCase()
-            .indexOf(this.itemInputFilter.toLowerCase()) >= 0;
-    }
+    itemInputFilter: string;
+    filterByInput = (item: any): boolean => {
+        return !!this.itemInputFilter ?
+            item.name.toLowerCase().indexOf(this.itemInputFilter.toLowerCase()) >= 0 : true;
+    };
 
-    isSelected = (item) => {
+    isSelected = (item): boolean => {
         return this.selectedItem && item && this.selectedItem.id === item.id;
-    }
+    };
 
     showCompanion = (): boolean => {
         let basePath = `/admin/${this.servicesStore.structure.id}/services/${this.serviceName}`;
-        if (this.collectionRef[this.serviceName].model)
-            return this.router.isActive(`${basePath}\\${this.collectionRef[this.serviceName].model.id}`  , true);
-        else 
+        if (this.collectionRef[this.serviceName].model) {
+            return this.router.isActive(`${basePath}\\${this.collectionRef[this.serviceName].model.id}`, true);
+        } else {
             return false;
-    }
+        }
+    };
 
     private isIconWorkspaceImg(src: String) {
         return src.startsWith('/workspace');
