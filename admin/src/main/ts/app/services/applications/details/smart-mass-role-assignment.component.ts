b76bab36508d9284528710f8287cf0b80391d2df
Simon Basset <simon.basset@gmail.com>
2019-01-18 10:00:53 +0100

    feat(adminV2): CAV2-366, mass app attribution
    - adds a tab on application view if the current structure has children
    to do mass role assignment on user profiles
    - adds a new ux component MultiSelectComponent (similar to
    MultiComboComponent)
    - also reformat some service files

---
+++ admin/src/main/ts/app/services/applications/details/smart-mass-role-assignment.component.ts
@@ -0,0 +1,95 @@
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Assignment, Profile, Role, Structure } from './mass-role-assignment.component';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { RoleModel } from '../../../core/store/models';
+import { ServicesStore } from '../../services.store';
+import { NotifyService } from '../../../core/services';
+
+@Component({
+    selector: 'smart-mass-role-assignment',
+    template: `
+        <mass-role-assignment
+                [structure]="structure"
+                [profiles]="profiles"
+                [roles]="roles"
+                (submitAssignment)="assign($event)"
+                (submitUnassignment)="unassign($event)"
+        ></mass-role-assignment>`
+})
+export class SmartMassRoleAssignment implements OnInit, OnDestroy {
+    public structure: Structure;
+    public profiles: Array<Profile> = ['Guest', 'Personnel', 'Relative', 'Student', 'Teacher'];
+    public roles: Array<Role> = [];
+    private routeDataSubscription: Subscription;
+
+    @Output()
+    public massAssignment: EventEmitter<void> = new EventEmitter<void>();
+
+    constructor(private http: HttpClient, private route: ActivatedRoute, private servicesStore: ServicesStore, private notifyService: NotifyService) {
+    }
+
+    ngOnInit(): void {
+        this.structure = {id: this.servicesStore.structure.id, name: this.servicesStore.structure.name};
+        this.routeDataSubscription = this.route.data.subscribe(data => {
+            if (data['roles']) {
+                this.roles = data['roles']
+                    .filter((r: RoleModel) => r.transverse == false)
+                    .map((r: RoleModel) => ({id: r.id, name: r.name}));
+            }
+        });
+    }
+
+    public assign(assignment: Assignment): void {
+        this.http.put<void>(`/appregistry/structures/${assignment.structure.id}/roles`, {
+            roles: assignment.roles.map(role => role.id),
+            profiles: assignment.profiles
+        }).subscribe(
+            () => {
+                this.notifyService.success(
+                    'services.mass-assignment.assign-success.content',
+                    'services.mass-assignment.assign-success.title'
+                );
+                this.massAssignment.emit();
+            },
+            () => {
+                () => this.notifyService.error(
+                    'services.mass-assignment.assign-error.title',
+                    'services.mass-assignment.assign-error.title'
+                )
+            });
+    }
+
+    public unassign(assignment: Assignment): void {
+        const responseType: 'text' = 'text';
+        const options = {
+            responseType,
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+            }),
+            body: {
+                roles: assignment.roles.map(role => role.id),
+                profiles: assignment.profiles
+            }
+        };
+        this.http.delete(`/appregistry/structures/${assignment.structure.id}/roles`, options).subscribe(
+            () => {
+                this.notifyService.success(
+                    'services.mass-assignment.unassign-success.content',
+                    'services.mass-assignment.unassign-success.title'
+                );
+                this.massAssignment.emit();
+            },
+            () => {
+                () => this.notifyService.error(
+                    'services.mass-assignment.unassign-error.title',
+                    'services.mass-assignment.unassign-error.title'
+                )
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.routeDataSubscription.unsubscribe();
+    }
+}
