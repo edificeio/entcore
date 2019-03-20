46507b7fa0ff4f4bfedc357e8c79e6ed88a68851
Simon Basset <simon.basset.pro@gmail.com>
2019-03-20 16:20:50 +0100

    feat(adminv2): #CAV2-379, show the user groups in the communication view (#123)
    - adds internal communication rule to the frontend GroupModel model and
    restyle code of this file
    - expose an backend endpoint to get the user groups and display them on
    the frontend
    
    How it works:
    - before landing on the communication view the resolvers retrieve the user
    and its groups
    - smart component loads the groups in the communication rules
    service (= store), subscribe to changes in the communication rules store
    and make communication rules flow in its children components
    - user communication component display the view
    - communication rules component display a communication rule table
    - group card component display a single group

---
+++ admin/src/main/ts/app/users/communication/user-groups.resolver.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { GroupModel } from '../../core/store/models';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class UserGroupsResolver implements Resolve<GroupModel[]> {
+
+    constructor(private http: HttpClient) {
+    }
+
+    resolve(route: ActivatedRouteSnapshot): Observable<GroupModel[]> {
+        const userId = route.paramMap.get('userId');
+        return this.http.get<GroupModel[]>(`/directory/user/${userId}/groups`);
+    }
+}
