71ceea7bbb072cd5f134fb339b849ca978335316
Simon Basset <simon.basset@gmail.com>
2019-03-28 16:43:07 +0100

    FIXUP fix(adminv2): #CAV2-379, don't show CommunityGroup

--- admin/src/main/ts/app/users/communication/user-groups.resolver.spec.ts
+++ admin/src/main/ts/app/users/communication/user-groups.resolver.spec.ts
@@ -2,6 +2,8 @@ import { UserGroupsResolver } from './user-groups.resolver';
 import { ActivatedRouteSnapshot, convertToParamMap } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { generateGroup } from "./communication-test-utils";
+import { GroupModel } from "../../core/store/models";
 
 describe('UserGroupsResolver', () => {
     let userGroupsResolver: UserGroupsResolver;
@@ -21,8 +23,22 @@ describe('UserGroupsResolver', () => {
     });
 
     it('should call the backend API for listing groups of a user', () => {
-        userGroupsResolver.resolve(generateActivatedRouteSnapshot('myUser')).subscribe();
+        userGroupsResolver.resolve(generateActivatedRouteSnapshot('myUser'))
+            .subscribe();
         httpController.expectOne('/directory/user/myUser/groups');
+    });
+
+    it('should filter user groups to remove CommunityGroup', () => {
+        let groups: GroupModel[] = [];
+
+        userGroupsResolver.resolve(generateActivatedRouteSnapshot('myUser'))
+            .subscribe(g => groups = g);
+        httpController.expectOne('/directory/user/myUser/groups').flush([
+            generateGroup('manualGroup', 'BOTH', 'ManualGroup'),
+            generateGroup('communityGroup', 'BOTH', 'CommunityGroup'),
+            generateGroup('functionGroup', 'BOTH', 'FunctionGroup')
+        ]);
+        expect(groups.map(g => g.id)).toEqual(['manualGroup', 'functionGroup']);
     });
 });
 
--- admin/src/main/ts/app/users/communication/user-groups.resolver.ts
+++ admin/src/main/ts/app/users/communication/user-groups.resolver.ts
@@ -3,6 +3,7 @@ import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { GroupModel } from '../../core/store/models';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UserGroupsResolver implements Resolve<GroupModel[]> {
@@ -12,6 +13,9 @@ export class UserGroupsResolver implements Resolve<GroupModel[]> {
 
     resolve(route: ActivatedRouteSnapshot): Observable<GroupModel[]> {
         const userId = route.paramMap.get('userId');
-        return this.http.get<GroupModel[]>(`/directory/user/${userId}/groups`);
+        return this.http.get<GroupModel[]>(`/directory/user/${userId}/groups`)
+            .map(groups => groups.filter(group => unwantedGroupTypes.indexOf(group.type) < 0));
     }
 }
+
+const unwantedGroupTypes = ['CommunityGroup'];
