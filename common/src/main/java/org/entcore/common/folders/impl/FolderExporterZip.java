39d6d3d92c795dfa4cd221dea0260ff5f25b350d
Nabil Mansouri <nabil.mansouri."@gmail.com>
2019-05-20 18:51:16 +0200

    fix(workspace): #24872, Clean zipFiles after download

--- common/src/main/java/org/entcore/common/folders/impl/FolderExporterZip.java
+++ common/src/main/java/org/entcore/common/folders/impl/FolderExporterZip.java
@@ -21,9 +21,11 @@ class FolderExporterZip extends FolderExporter {
 		final String zipFullPath;
 		final String zipName;
 		final String baseName;
+		final String rootBase;
 
-		public ZipContext(String basePath, String baseName) {
+		public ZipContext(String rootBase, String basePath, String baseName) {
 			super(basePath);
+			this.rootBase = rootBase;
 			this.baseName = baseName;
 			this.zipName = this.baseName + ".zip";
 			// basePath is the root folder => so up and set filename
@@ -54,9 +56,10 @@ class FolderExporterZip extends FolderExporter {
 	public Future<ZipContext> exportToZip(Optional<JsonObject> root, List<JsonObject> rows) {
 		UUID uuid = UUID.randomUUID();
 		String baseName = root.isPresent() ? root.get().getString("name", "archive") : "archive";
+		String rootBase = Paths.get(System.getProperty("java.io.tmpdir"), uuid.toString()).normalize().toString();
 		String basePath = Paths.get(System.getProperty("java.io.tmpdir"), uuid.toString(), baseName).normalize()
 				.toString();
-		ZipContext context = new ZipContext(basePath, baseName);
+		ZipContext context = new ZipContext(rootBase, basePath, baseName);
 		return this.export(context, rows).compose(res -> {
 			return this.createZip(context);
 		}).map(res -> context);
@@ -73,15 +76,35 @@ class FolderExporterZip extends FolderExporter {
 		return future;
 	}
 
-	public Future<Void> exportAndSendZip(JsonObject root, List<JsonObject> rows, HttpServerRequest req) {
+	public Future<ZipContext> exportAndSendZip(JsonObject root, List<JsonObject> rows, HttpServerRequest req, boolean clean) {
 		return this.exportToZip(Optional.ofNullable(root), rows).compose(res -> {
-			return this.sendZip(req, res);
+			return this.sendZip(req, res).map((r)->res);
+		}).compose(res->{
+			if(clean){
+				return removeZip(res);
+			}else{
+				return Future.succeededFuture(res);
+			}
 		});
 	}
 
-	public Future<Void> exportAndSendZip(List<JsonObject> rows, HttpServerRequest req) {
+	public Future<ZipContext> exportAndSendZip(List<JsonObject> rows, HttpServerRequest req, boolean clean) {
 		return this.exportToZip(Optional.empty(), rows).compose(res -> {
-			return this.sendZip(req, res);
+			return this.sendZip(req, res).map((r)->res);
+		}).compose(res->{
+			if(clean){
+				return removeZip(res);
+			}else{
+				return Future.succeededFuture(res);
+			}
 		});
+	}
+
+	public Future<ZipContext> removeZip(ZipContext context){
+		Future<ZipContext> future = Future.future();
+		this.fs.deleteRecursive(context.rootBase,true, resRmDir->{
+			future.complete();
+		});
+		return future;
 	}
 }
