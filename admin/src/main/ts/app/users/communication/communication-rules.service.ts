f4d0d8a880611cd2752ec879561f77f9d5508fc8
Simon Basset <simon.basset@gmail.com>
2019-05-27 11:32:37 +0200

    feat(adminv2): #CAV2-63, show 'groups => user groups' communication rules

--- admin/src/main/ts/app/users/communication/communication-rules.service.ts
+++ admin/src/main/ts/app/users/communication/communication-rules.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CommunicationRule } from './communication-rules.component';
+import { Column, CommunicationRule } from './communication-rules.component';
 import { GroupModel, InternalCommunicationRule } from '../../core/store/models';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
@@ -26,21 +26,29 @@ function isNoDirectionChangeResponse(response: CreateCommunicationResponse): res
 @Injectable()
 export class CommunicationRulesService {
 
-    private rulesSubject: Subject<CommunicationRule[]> = new Subject<CommunicationRule[]>();
-    private rulesObservable: Observable<CommunicationRule[]> = this.rulesSubject.asObservable();
-    private currentRules: CommunicationRule[];
+    private rulesSubject: Subject<BidirectionalCommunicationRules> = new Subject<BidirectionalCommunicationRules>();
+    private rulesObservable: Observable<BidirectionalCommunicationRules> = this.rulesSubject.asObservable();
+    private currentRules: BidirectionalCommunicationRules;
 
     constructor(private http: HttpClient) {
         this.rulesObservable.subscribe(cr => this.currentRules = cr);
     }
 
-    public changes(): Observable<CommunicationRule[]> {
+    public changes(): Observable<BidirectionalCommunicationRules> {
         return this.rulesObservable;
     }
 
     public setGroups(groups: GroupModel[]) {
-        Observable.forkJoin(...groups.map(group => this.getCommunicationRulesOfGroup(group)))
-            .subscribe((communicationRules: CommunicationRule[]) => this.rulesSubject.next(communicationRules));
+        Observable.forkJoin(
+            Observable.forkJoin(...groups.map(group => this.getSendingCommunicationRulesOfGroup(group))),
+            Observable.forkJoin(...groups.map(group => this.getReceivingCommunicationRulesOfGroup(group)))
+                .map(arr => arr.reduce((prev, current) => {
+                    prev.push(...current);
+                    return prev;
+                }, []))
+        )
+            .map(arr => ({sending: arr[0], receiving: arr[1]}))
+            .subscribe((communicationRules: BidirectionalCommunicationRules) => this.rulesSubject.next(communicationRules));
     }
 
     public toggleInternalCommunicationRule(group: GroupModel): Observable<InternalCommunicationRule> {
@@ -57,7 +65,9 @@ export class CommunicationRulesService {
             .map(resp => resp.users ? resp.users : 'NONE')
             .do((internalCommunicationRule) => {
                 if (this.currentRules) {
-                    const groupsInCommunicationRules = this.findGroups(this.currentRules, group.id);
+                    const groupsInCommunicationRules = []
+                        .concat(this.findGroupsInCommunicationsRule(this.currentRules.sending, group.id))
+                        .concat(this.findGroupsInCommunicationsRule(this.currentRules.receiving, group.id));
                     if (groupsInCommunicationRules.length > 0) {
                         groupsInCommunicationRules.forEach(group => group.internalCommunicationRule = internalCommunicationRule);
                         this.rulesSubject.next(this.clone(this.currentRules));
@@ -66,16 +76,26 @@ export class CommunicationRulesService {
             });
     }
 
-    public removeCommunication(sender: GroupModel, receiver: GroupModel): Observable<void> {
+    public removeCommunication(sender: GroupModel, receiver: GroupModel, positionOfUserGroup: Column): Observable<void> {
         return this.http.delete<void>(`/communication/group/${sender.id}/relations/${receiver.id}`)
             .do(() => {
                 if (this.currentRules) {
-                    const communicationRuleOfSender = this.currentRules.find(cr => cr.sender.id === sender.id);
-                    if (!!communicationRuleOfSender) {
-                        communicationRuleOfSender.receivers = communicationRuleOfSender.receivers
-                            .filter(r => r.id !== receiver.id);
-                        this.rulesSubject.next(this.clone(this.currentRules));
+                    if (positionOfUserGroup === 'sending') {
+                        const communicationRuleOfSender = this.currentRules.sending.find(cr => cr.sender.id === sender.id);
+                        if (!!communicationRuleOfSender) {
+                            communicationRuleOfSender.receivers = communicationRuleOfSender.receivers
+                                .filter(r => r.id !== receiver.id);
+                        }
+                    } else {
+                        const communicationRulesOfReceiver = this.currentRules.receiving.filter(cr => cr.receivers.some(r => r.id === receiver.id));
+                        const communicationRulesToRemove = communicationRulesOfReceiver.find(cr => cr.sender.id === sender.id);
+                        if (communicationRulesOfReceiver.length > 1) {
+                            this.currentRules.receiving.splice(this.currentRules.receiving.indexOf(communicationRulesToRemove), 1)
+                        } else {
+                            communicationRulesToRemove.sender = null;
+                        }
                     }
+                    this.rulesSubject.next(this.clone(this.currentRules));
                 }
             });
     }
@@ -84,40 +104,80 @@ export class CommunicationRulesService {
         return this.http.get<{ warning: string }>(`/communication/v2/group/${sender.id}/communique/${receiver.id}/check`);
     }
 
-    public createCommunication(sender: GroupModel, receiver: GroupModel): Observable<{ groupId: number, internalCommunicationRule: string }> {
+    public createCommunication(sender: GroupModel, receiver: GroupModel, positionOfUserGroup: Column): Observable<{ groupId: number, internalCommunicationRule: string }> {
         return this.http.post<CreateCommunicationResponse>(`/communication/v2/group/${sender.id}/communique/${receiver.id}`, {})
             .do((createCommunicationResponse: CreateCommunicationResponse) => {
                 if (!isNoDirectionChangeResponse(createCommunicationResponse)) {
-                    sender.internalCommunicationRule = createCommunicationResponse[sender.id];
-                    receiver.internalCommunicationRule = createCommunicationResponse[receiver.id];
+                    sender.internalCommunicationRule = createCommunicationResponse[sender.id] || sender.internalCommunicationRule;
+                    receiver.internalCommunicationRule = createCommunicationResponse[receiver.id] || receiver.internalCommunicationRule;
+                    if (positionOfUserGroup === 'sending') {
+                        this.findGroupsInCommunicationsRule(this.currentRules.receiving, sender.id).forEach(g => g.internalCommunicationRule = sender.internalCommunicationRule);
+                        this.findGroupsInCommunicationsRule(this.currentRules.receiving, receiver.id).forEach(g => g.internalCommunicationRule = receiver.internalCommunicationRule);
+                    } else {
+                        this.findGroupsInCommunicationsRule(this.currentRules.sending, receiver.id).forEach(g => g.internalCommunicationRule = receiver.internalCommunicationRule);
+                        this.findGroupsInCommunicationsRule(this.currentRules.sending, sender.id).forEach(g => g.internalCommunicationRule = sender.internalCommunicationRule);
+                    }
                 }
                 if (this.currentRules) {
-                    const communicationRuleOfSender = this.currentRules.find(cr => cr.sender.id === sender.id);
-                    if (!!communicationRuleOfSender) {
-                        communicationRuleOfSender.receivers.push(receiver);
-                        this.rulesSubject.next(this.clone(this.currentRules));
+                    if (positionOfUserGroup === 'sending') {
+                        const communicationRuleOfSender = this.currentRules.sending.find(cr => cr.sender.id === sender.id);
+                        if (!!communicationRuleOfSender) {
+                            communicationRuleOfSender.receivers.push(receiver);
+                        }
+                    } else {
+                        const communicationRulesOfReceiver = this.currentRules.receiving.filter(cr => cr.receivers.some(r => r.id === receiver.id));
+                        const emptyCommunicationRule = communicationRulesOfReceiver.find(cr => !cr.sender);
+                        if (!!emptyCommunicationRule) {
+                            emptyCommunicationRule.sender = sender;
+                        } else {
+                            this.currentRules.receiving.push({sender: sender, receivers: [receiver]});
+                        }
                     }
+                    this.rulesSubject.next(this.clone(this.currentRules));
                 }
             });
     }
 
-    private getCommunicationRulesOfGroup(sender: GroupModel): Observable<CommunicationRule> {
+    private getSendingCommunicationRulesOfGroup(sender: GroupModel): Observable<CommunicationRule> {
         return this.http.get<GroupModel[]>(`/communication/group/${sender.id}/outgoing`)
             .map(receivers => ({sender, receivers}));
     }
 
-    private clone(communicationRules: CommunicationRule[]): CommunicationRule[] {
+    private getReceivingCommunicationRulesOfGroup(receiver: GroupModel): Observable<CommunicationRule[]> {
+        return this.http.get<GroupModel[]>(`/communication/group/${receiver.id}/incoming`)
+            .map((senders: GroupModel[]): CommunicationRule[] => senders.map(sender => ({
+                sender,
+                receivers: [receiver]
+            })))
+            .map(communicationRules => communicationRules.length === 0 ?
+                [{sender: null, receivers: [receiver]}] : communicationRules
+            )
+    }
+
+    private clone(bidirectionalCommunicationRules: BidirectionalCommunicationRules): BidirectionalCommunicationRules {
+        return {
+            sending: this.cloneCommunicationRules(bidirectionalCommunicationRules.sending),
+            receiving: this.cloneCommunicationRules(bidirectionalCommunicationRules.receiving)
+        }
+    }
+
+    private cloneCommunicationRules(communicationRules: CommunicationRule[]): CommunicationRule[] {
         return communicationRules.map(cr => ({
-            sender: Object.assign({}, cr.sender),
+            sender: cr.sender ? Object.assign({}, cr.sender) : null,
             receivers: cr.receivers.map(re => Object.assign({}, re))
         }));
     }
 
-    private findGroups(communicationRules: CommunicationRule[], groupId: string): GroupModel[] {
+    private findGroupsInCommunicationsRule(communicationRules: CommunicationRule[], groupId: string): GroupModel[] {
         return communicationRules.reduce(
             (arrayOfGroups: GroupModel[], communicationRule: CommunicationRule): GroupModel[] =>
                 [...arrayOfGroups, communicationRule.sender, ...communicationRule.receivers], [])
+            .filter(group => !!group)
             .filter(group => group.id === groupId);
     }
 }
 
+export interface BidirectionalCommunicationRules {
+    sending: CommunicationRule[],
+    receiving: CommunicationRule[]
+}
