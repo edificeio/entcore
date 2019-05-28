90adcf8efb81c2c9c9a8c515679b253f1ec47265
Simon Basset <simon.basset@gmail.com>
2019-05-28 16:05:53 +0200

    feat(adminv2): #CAV2-63, show 'groups => user groups' communication rules (#134)

--- communication/src/main/java/org/entcore/communication/services/CommunicationService.java
+++ communication/src/main/java/org/entcore/communication/services/CommunicationService.java
@@ -91,7 +91,9 @@ public interface CommunicationService {
 	void visibleManualGroups(String userId, String customReturn, JsonObject additionnalParams,
 			Handler<Either<String, JsonArray>> handler);
 
-	void getGroupsReachableByGroup(String id, Handler<Either<String, JsonArray>> results);
+	void getOutgoingRelations(String id, Handler<Either<String, JsonArray>> results);
+
+	void getIncomingRelations(String id, Handler<Either<String, JsonArray>> results);
 
 	void safelyRemoveLinkWithUsers(String groupId, Handler<Either<String, JsonObject>> handler);
 
