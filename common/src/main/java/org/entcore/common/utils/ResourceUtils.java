fadad2bcbe773b9b0092a2d4bb951714ef33a6be
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2019-05-31 18:28:16 +0200

    fix : #24768, Resource matching pattern fail on null

--- common/src/main/java/org/entcore/common/utils/ResourceUtils.java
+++ common/src/main/java/org/entcore/common/utils/ResourceUtils.java
@@ -10,6 +10,9 @@ public final class ResourceUtils {
     private static final Pattern workspaceDoc = Pattern.compile("/workspace/document/(?<id>([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}))");
 
     public static List<String> extractIds(String resource) {
+        if(resource==null){
+            return new ArrayList<>();
+        }
         Matcher matcher = workspaceDoc.matcher(resource);
         List<String> documentsIds = new ArrayList<>();
         while (matcher.find()) {
