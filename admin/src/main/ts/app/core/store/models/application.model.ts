70dcc8c8c0b254301a29f9fb691f1c7ed183b6a4
Simon Basset <simon.basset@gmail.com>
2019-06-05 10:25:48 +0200

    FIXUP refactor: removes useless getter/setter in application.model

--- admin/src/main/ts/app/core/store/models/application.model.ts
+++ admin/src/main/ts/app/core/store/models/application.model.ts
@@ -6,41 +6,25 @@ export type AppType = 'END_USER' | 'SYSTEM' | 'WIDGET';
 
 export class ApplicationModel extends Model<ApplicationModel> {
 
-    constructor() {
-        super({});
-        this.roles = [];
-    }
-
-    private _id: string;
-
-    get id() {
-        return this._id
-    };
-
-    set id(id) {
-        this._id = id
-    };
-
-    syncRoles = (structureId: string): Promise<void> => {
-        return this.http.get(`/appregistry/structure/${structureId}/application/${this._id}/groups/roles`)
-            .then(res => {
-                    this.roles = Mix.castArrayAs(RoleModel, res.data);
-                }
-            );
-    };
-
-    private _name: string;
-    get name() {
-        return this._name;
-    }
-    set name(name) {
-        this._name = name;
-    }
-
+    id: string;
+    name: string;
     displayName: string;
     roles: RoleModel[];
     levelsOfEducation: number[];
     appType: AppType;
     icon: string;
     isExternal: boolean;
+    
+    constructor() {
+        super({});
+        this.roles = [];
+    }
+    
+    syncRoles = (structureId: string): Promise<void> => {
+        return this.http.get(`/appregistry/structure/${structureId}/application/${this.id}/groups/roles`)
+            .then(res => {
+                    this.roles = Mix.castArrayAs(RoleModel, res.data);
+                }
+            );
+    };
 }
