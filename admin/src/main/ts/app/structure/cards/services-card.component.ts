db05954f5802f8371a79426744b07a0d37b1c62d
david-cc <david.caudeli@web-education.net>
2019-06-11 17:31:52 +0200

    feat(adminv2): #CAV2-454, enable/disable quick actions (#145)

--- admin/src/main/ts/app/structure/cards/services-card.component.ts
+++ admin/src/main/ts/app/structure/cards/services-card.component.ts
@@ -10,17 +10,13 @@ import { Component, ChangeDetectionStrategy } from '@angular/core'
             </span>
         </div>
         <div class="card-body">
-            <button routerLink="services/connectors">
-                <i class="fa fa-plug"></i>
-                <s5l>services.card.connectors</s5l>
-            </button>
-            <button disabled title="En construction">
-                <i class="fa fa-window-maximize"></i>
-                <s5l>services.card.widgets</s5l>
-            </button>
             <button routerLink="services/applications">
                 <i class="fa fa-th"></i>
                 <s5l>services.card.applications</s5l>
+            </button>
+            <button routerLink="services/connectors">
+                <i class="fa fa-plug"></i>
+                <s5l>services.card.connectors</s5l>
             </button>
         </div>
     `,
