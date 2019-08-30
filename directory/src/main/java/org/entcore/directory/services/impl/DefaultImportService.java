rdjedjig/entcore-mirror

[Evo][Feeder] CAV2-203 : ignore empty lines and block if line doesn't have the correct columns number.

directory/src/main/java/org/entcore/directory/services/impl/DefaultImportService.java
@@ -182,7 +182,7 @@ public class DefaultImportService implements ImportService {
 			@Override
 			public void handle(Message<JsonObject> event) {
 				if ("ok".equals(event.body().getString("status"))) {
-					JsonObject r = event.body();
+					JsonObject r = event.body().getObject("result", new JsonObject());
 					r.removeField("status");
 					if (r.getObject("errors", new JsonObject()).size() > 0) {
 						handler.handle(new Either.Left<JsonObject, JsonObject>(r));

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -401,7 +401,8 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 							.putObject("mappings", v.getMappings())
 							.putObject("availableFields", v.getColumnsMapper().availableFields()));
 				} else {
-					sendError(message, "column.mapping.error");
+//					sendError(message, "column.mapping.error");
+					sendOK(message, new JsonObject().putObject("result", v.getResult()));
 				}
 			}
 		});

edificeio/entcore

directory/src/main/java/org/entcore/directory/services/impl/DefaultImportService.java
@@ -206,7 +206,7 @@ public class DefaultImportService implements ImportService {
 			@Override
 			public void handle(Message<JsonObject> event) {
 				if ("ok".equals(event.body().getString("status"))) {
-					JsonObject r = event.body();
+					JsonObject r = event.body().getJsonObject("result", new JsonObject());
 					r.remove("status");
 					if (r.getJsonObject("errors", new JsonObject()).size() > 0) {
 						handler.handle(new Either.Left<JsonObject, JsonObject>(r));

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -421,7 +421,8 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 							.put("mappings", v.getMappings())
 							.put("availableFields", v.getColumnsMapper().availableFields()));
 				} else {
-					sendError(message, "column.mapping.error");
+//					sendError(message, "column.mapping.error");
+					sendOK(message, new JsonObject().put("result", v.getResult()));
 				}
 			}
 		});