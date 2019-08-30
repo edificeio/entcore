rdjedjig/entcore-mirror

[Evo] CAV2-191 : columns mapping

directory/src/main/java/org/entcore/directory/services/impl/DefaultImportService.java
@@ -105,4 +105,33 @@ public class DefaultImportService implements ImportService {
 		}
 	}
 
+	@Override
+	public void columnsMapping(ImportInfos importInfos, final Handler<Either<JsonObject, JsonObject>> handler) {
+		try {
+			JsonObject action = new JsonObject(mapper.writeValueAsString(importInfos))
+					.putString("action", "columnsMapping");
+			eb.send("entcore.feeder", action, new Handler<Message<JsonObject>>() {
+				@Override
+				public void handle(Message<JsonObject> event) {
+					if ("ok".equals(event.body().getString("status"))) {
+						JsonObject r = event.body();
+						r.removeField("status");
+						if (r.getObject("errors", new JsonObject()).size() > 0) {
+							handler.handle(new Either.Left<JsonObject, JsonObject>(r));
+						} else {
+							handler.handle(new Either.Right<JsonObject, JsonObject>(r));
+						}
+					} else {
+						handler.handle(new Either.Left<JsonObject, JsonObject>(new JsonObject().putArray("global",
+								new JsonArray().addString(event.body().getString("message", "")))));
+					}
+				}
+			});
+		} catch (JsonProcessingException e) {
+			handler.handle(new Either.Left<JsonObject, JsonObject>(new JsonObject()
+					.putArray("global", new JsonArray().addString("unexpected.error"))));
+			log.error(e.getMessage(), e);
+		}
+	}
+
 }

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -87,7 +87,7 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 		feeds.put("AAF", new AafFeeder(vertx, getFilesDirectory("AAF")));
 		feeds.put("AAF1D", new Aaf1dFeeder(vertx, getFilesDirectory("AAF1D")));
 //		feeds.put("BE1D", new Be1dFeeder(vertx, getFilesDirectory("BE1D")));
-		feeds.put("CSV", new CsvFeeder(vertx, container.config().getObject("csvMappings", new JsonObject())));
+		feeds.put("CSV", new CsvFeeder(vertx));
 		final long deleteUserDelay = container.config().getLong("delete-user-delay", 90 * 24 * 3600 * 1000l);
 		final long preDeleteUserDelay = container.config().getLong("pre-delete-user-delay", 90 * 24 * 3600 * 1000l);
 		final String deleteCron = container.config().getString("delete-cron", "0 0 2 * * ? *");
@@ -277,6 +277,8 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 				break;
 			case "validate" : launchImportValidation(message, null);
 				break;
+			case "columnsMapping" : csvColumnMapping(message);
+				break;
 			case "ignore-duplicate" :
 				duplicateUsers.ignoreDuplicate(message);
 				break;
@@ -315,6 +317,25 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 		checkEventQueue();
 	}
 
+	private void csvColumnMapping(final Message<JsonObject> message) {
+		final String acceptLanguage = message.body().getString("language", "fr");
+		final CsvValidator v = new CsvValidator(vertx, acceptLanguage,
+				container.config().getObject("csvMappings", new JsonObject()));
+		String path = message.body().getString("path");
+		v.columnsMapping(path, new Handler<JsonObject>() {
+			@Override
+			public void handle(JsonObject event) {
+				if (!v.containsErrors()) {
+					sendOK(message, new JsonObject()
+							.putObject("mappings", v.getMappings())
+							.putObject("availableFields", v.getColumnsMapper().availableFields()));
+				} else {
+					sendError(message, "column.mapping.error");
+				}
+			}
+		});
+	}
+
 	private void launchImportValidation(final Message<JsonObject> message, final Handler<Report> handler) {
 		logger.info(message.body().encodePrettily());
 		final String acceptLanguage = message.body().getString("language", "fr");
@@ -324,8 +345,7 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 		final ImportValidator v;
 		switch (source) {
 			case "CSV":
-				v = new CsvValidator(vertx, acceptLanguage,
-						container.config().getObject("csvMappings", new JsonObject()));
+				v = new CsvValidator(vertx, acceptLanguage, message.body().getObject("columnsMapping"));
 				break;
 //			case "BE1D":
 //				v = new Be1dValidator(vertx, container.config().getString("uai-separator", "_"),

edificeio/entcore

directory/src/main/java/org/entcore/directory/services/impl/DefaultImportService.java
@@ -113,4 +113,33 @@ public class DefaultImportService implements ImportService {
 		}
 	}
 
+	@Override
+	public void columnsMapping(ImportInfos importInfos, final Handler<Either<JsonObject, JsonObject>> handler) {
+		try {
+			JsonObject action = new JsonObject(mapper.writeValueAsString(importInfos))
+					.put("action", "columnsMapping");
+			eb.send("entcore.feeder", action, handlerToAsyncHandler(new Handler<Message<JsonObject>>() {
+				@Override
+				public void handle(Message<JsonObject> event) {
+					if ("ok".equals(event.body().getString("status"))) {
+						JsonObject r = event.body();
+						r.remove("status");
+						if (r.getJsonObject("errors", new JsonObject()).size() > 0) {
+							handler.handle(new Either.Left<JsonObject, JsonObject>(r));
+						} else {
+							handler.handle(new Either.Right<JsonObject, JsonObject>(r));
+						}
+					} else {
+						handler.handle(new Either.Left<JsonObject, JsonObject>(new JsonObject().put("global",
+								new JsonArray().add(event.body().getString("message", "")))));
+					}
+				}
+			}));
+		} catch (JsonProcessingException e) {
+			handler.handle(new Either.Left<JsonObject, JsonObject>(new JsonObject()
+					.put("global", new JsonArray().add("unexpected.error"))));
+			log.error(e.getMessage(), e);
+		}
+	}
+
 }

feeder/src/main/java/org/entcore/feeder/Feeder.java
@@ -94,9 +94,8 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 		defaultFeed = config.getString("feeder", "AAF");
 		feeds.put("AAF", new AafFeeder(vertx, getFilesDirectory("AAF")));
 		feeds.put("AAF1D", new Aaf1dFeeder(vertx, getFilesDirectory("AAF1D")));
-		feeds.put("CSV", new CsvFeeder(vertx, config.getJsonObject("csvMappings", new JsonObject())));
-		final long deleteUserDelay = config.getLong("delete-user-delay", defaultDeleteUserDelay);
-		final long preDeleteUserDelay = config.getLong("pre-delete-user-delay", defaultPreDeleteUserDelay);
+		final long deleteUserDelay = config.getLong("delete-user-delay", 90 * 24 * 3600 * 1000l);
+		final long preDeleteUserDelay = config.getLong("pre-delete-user-delay", 90 * 24 * 3600 * 1000l);
 		final String deleteCron = config.getString("delete-cron", "0 0 2 * * ? *");
 		final String preDeleteCron = config.getString("pre-delete-cron", "0 0 3 * * ? *");
 		final String importCron = config.getString("import-cron");
@@ -292,6 +291,8 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 				break;
 			case "validate" : launchImportValidation(message, null);
 				break;
+			case "columnsMapping" : csvColumnMapping(message);
+				break;
 			case "ignore-duplicate" :
 				duplicateUsers.ignoreDuplicate(message);
 				break;
@@ -333,6 +334,25 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 		checkEventQueue();
 	}
 
+	private void csvColumnMapping(final Message<JsonObject> message) {
+		final String acceptLanguage = message.body().getString("language", "fr");
+		final CsvValidator v = new CsvValidator(vertx, acceptLanguage,
+				this.config.getJsonObject("csvMappings", new JsonObject()));
+		String path = message.body().getString("path");
+		v.columnsMapping(path, new Handler<JsonObject>() {
+			@Override
+			public void handle(JsonObject event) {
+				if (!v.containsErrors()) {
+					sendOK(message, new JsonObject()
+							.put("mappings", v.getMappings())
+							.put("availableFields", v.getColumnsMapper().availableFields()));
+				} else {
+					sendError(message, "column.mapping.error");
+				}
+			}
+		});
+	}
+
 	private void launchImportValidation(final Message<JsonObject> message, final Handler<Report> handler) {
 		logger.info(message.body().encodePrettily());
 		final String acceptLanguage = getOrElse(message.body().getString("language"), "fr");
@@ -342,8 +362,7 @@ public class Feeder extends BusModBase implements Handler<Message<JsonObject>> {
 		final ImportValidator v;
 		switch (source) {
 			case "CSV":
-				v = new CsvValidator(vertx, acceptLanguage,
-						config.getJsonObject("csvMappings", new JsonObject()));
+				v = new CsvValidator(vertx, acceptLanguage, message.body().getJsonObject("columnsMapping"));
 				break;
 			case "AAF":
 			case "AAF1D":