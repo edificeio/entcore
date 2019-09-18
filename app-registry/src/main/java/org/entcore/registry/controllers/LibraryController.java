4599b34d4c5bf942c311fb9f46d98bf94c1fb654
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2019-09-18 17:43:06 +0200

    fix(appregistry): Enable publication of ressources to BPR

--- app-registry/src/main/java/org/entcore/registry/controllers/LibraryController.java
+++ app-registry/src/main/java/org/entcore/registry/controllers/LibraryController.java
@@ -19,25 +19,96 @@
 
 package org.entcore.registry.controllers;
 
-import fr.wseduc.bus.BusAddress;
+import fr.wseduc.rs.Get;
+import fr.wseduc.rs.Post;
+import fr.wseduc.security.ActionType;
+import fr.wseduc.security.SecuredAction;
 import fr.wseduc.webutils.http.BaseController;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
+import io.vertx.core.MultiMap;
+import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.eventbus.Message;
-import org.entcore.common.appregistry.LibraryBusObject;
-import org.entcore.common.appregistry.LibraryBusObjectCodec;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.JsonObject;
+import org.entcore.common.http.filter.AdminFilter;
+import org.entcore.common.http.filter.ResourceFilter;
+import org.entcore.common.user.UserInfos;
+import org.entcore.common.user.UserUtils;
 import org.entcore.registry.services.LibraryService;
 import org.entcore.registry.services.impl.DefaultLibraryService;
 
-import static org.entcore.common.appregistry.LibraryUtils.LIBRARY_BUS_ADDRESS;
-
 public class LibraryController extends BaseController {
+    final LibraryService service;
 
-    @BusAddress(LIBRARY_BUS_ADDRESS)
-    public void publish(final Message<Buffer> message) {
-        LibraryBusObjectCodec libraryBusObjectCodec = new LibraryBusObjectCodec();
-        LibraryService service = new DefaultLibraryService(vertx.createHttpClient(), config);
-        Buffer body = message.body();
-        LibraryBusObject libraryBusObject = libraryBusObjectCodec.decodeFromWire(0, body);
-        service.publish(libraryBusObject.getForm(), libraryBusObject.getCover()).thenAccept(message::reply);
+    public LibraryController(Vertx vertx, JsonObject config) throws Exception {
+        service = new DefaultLibraryService(vertx, config);
+    }
+
+    @Post("/library/resource")
+    @SecuredAction(value = "", type = ActionType.AUTHENTICATED)
+    public void publishToLibrary(final HttpServerRequest request) {
+        request.setExpectMultipart(true);
+        final Future<UserInfos> futureUser = getUser(request);
+        CompositeFuture.all(getCover(request), getAttributes(request, futureUser), futureUser).compose(res -> {
+            Buffer cover = res.resultAt(0);
+            MultiMap attributes = res.resultAt(1);
+            UserInfos user = res.resultAt(2);
+            return service.publish(user, attributes, cover);
+        }).setHandler(res -> {
+            if (res.succeeded()) {
+                final JsonObject json = res.result();
+                if (json.getBoolean("success", false)) {
+                    renderJson(request, res.result());
+                } else {
+                    renderError(request, res.result());
+                }
+            } else {
+                final JsonObject body = new JsonObject().put("success", false).put("reason", "unknown").put("message", res.cause().getMessage());
+                renderError(request, body);
+            }
+        });
+    }
+
+    private Future<Buffer> getCover(HttpServerRequest request) {
+        Future<Buffer> coverFuture = Future.future();
+        request.uploadHandler(upload -> {
+            final Buffer buffer = Buffer.buffer();
+            upload.handler(b -> buffer.appendBuffer(b));
+            upload.endHandler(v -> coverFuture.complete(buffer));
+            upload.exceptionHandler(v -> coverFuture.fail(v));
+        });
+        return coverFuture;
+    }
+
+    private Future<UserInfos> getUser(HttpServerRequest request) {
+        Future<UserInfos> futureUser = Future.future();
+        UserUtils.getUserInfos(eb, request, res -> {
+            if (res == null) {
+                futureUser.fail("Could not found user info");
+            } else {
+                futureUser.complete(res);
+            }
+        });
+        return futureUser;
+    }
+
+    private Future<MultiMap> getAttributes(HttpServerRequest request, Future<UserInfos> futureUser) {
+        Future<MultiMap> future = Future.future();
+        request.endHandler(res -> {
+            futureUser.setHandler(resuser -> {
+                if (resuser.succeeded()) {
+                    final UserInfos user = resuser.result();
+                    MultiMap form = request.formAttributes();
+                    form
+                            .add("teacherFullName", user.getFirstName() + ' ' + user.getLastName())
+                            .add("teacherSchool", user.getStructureNames().stream().findFirst().orElse(""));
+                    future.complete(form);
+                } else {
+                    future.fail(resuser.cause());
+                }
+            });
+        });
+        return future;
     }
 }
--- app-registry/src/main/java/org/entcore/registry/services/impl/DefaultLibraryService.java
+++ app-registry/src/main/java/org/entcore/registry/services/impl/DefaultLibraryService.java
@@ -1,68 +1,107 @@
 package org.entcore.registry.services.impl;
 
+import fr.wseduc.webutils.data.FileResolver;
+import io.vertx.core.Future;
 import io.vertx.core.MultiMap;
+import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.FileSystem;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.entcore.common.pdf.Pdf;
+import org.entcore.common.pdf.PdfFactory;
+import org.entcore.common.pdf.PdfGenerator;
+import org.entcore.common.user.UserInfos;
+import org.entcore.common.utils.StringUtils;
 import org.entcore.registry.services.LibraryService;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.Optional;
+import java.util.UUID;
+
 
 enum REASON {BAD_CONFIGURATION, LIBRARY, OK}
 
 enum MESSAGE {API_URL_NOT_SET, DISABLED, WRONG_TOKEN, LIBRARY_KO, OK}
 
 public class DefaultLibraryService implements LibraryService {
-    JsonObject config;
-    HttpClient http;
-    private static final String RESSOURCE_ENDPOINT = "api/ressources";
+    private static final String RESSOURCE_ENDPOINT = "api/documents";
     private static final String CONFIG_LIBRARY_ENABLED = "library-enabled";
     private static final String CONFIG_LIBRARY_API_URL = "library-api-url";
     private static final String CONFIG_LIBRARY_TOKEN = "library-token";
+    private static final Logger log = LoggerFactory.getLogger(DefaultLibraryService.class);
+    private final HttpClient http;
+    private final JsonObject config;
+    private final PdfGenerator pdfGenerator;
+    private final FileSystem fileSystem;
 
-    public DefaultLibraryService(HttpClient http, JsonObject config) {
+    public DefaultLibraryService(Vertx vertx, JsonObject config) throws Exception {
         this.config = config;
-        this.http = http;
+        this.fileSystem = vertx.fileSystem();
+        this.http = vertx.createHttpClient();
+        this.pdfGenerator = new PdfFactory(vertx, config).getPdfGenerator();
+    }
+
+    private Future<Buffer> getArchive() {
+        //TODO call real bus method to generate archive
+        Future<Buffer> archive = Future.future();
+        this.fileSystem.readFile(FileResolver.absolutePath("document.zip"), archive.completer());
+        return archive;
     }
 
     @Override
-    public CompletableFuture<JsonObject> publish(MultiMap form, Buffer cover) {
-        boolean isLibraryEnabled = config.getBoolean(CONFIG_LIBRARY_ENABLED, false);
-        String libraryApiUrl = config.getString(CONFIG_LIBRARY_API_URL);
-        String libraryToken = config.getString(CONFIG_LIBRARY_TOKEN);
+    public Future<JsonObject> publish(UserInfos user, MultiMap form, Buffer cover) {
+        final boolean isLibraryEnabled = config.getBoolean(CONFIG_LIBRARY_ENABLED, false);
+        final String libraryApiUrl = config.getString(CONFIG_LIBRARY_API_URL);
+        final String libraryToken = config.getString(CONFIG_LIBRARY_TOKEN);
 
         if (!isLibraryEnabled) {
-            return CompletableFuture.completedFuture(generateJsonResponse(false, REASON.BAD_CONFIGURATION, MESSAGE.DISABLED));
+            return Future.succeededFuture(generateJsonResponse(false, REASON.BAD_CONFIGURATION, MESSAGE.DISABLED));
         }
         if (!(libraryApiUrl != null && libraryApiUrl.length() > 0)) {
-            return CompletableFuture.completedFuture(generateJsonResponse(false, REASON.BAD_CONFIGURATION, MESSAGE.API_URL_NOT_SET));
+            return Future.succeededFuture(generateJsonResponse(false, REASON.BAD_CONFIGURATION, MESSAGE.API_URL_NOT_SET));
         }
         if (!(libraryToken != null && libraryToken.length() > 0)) {
-            return CompletableFuture.completedFuture(generateJsonResponse(false, REASON.BAD_CONFIGURATION, MESSAGE.WRONG_TOKEN));
+            return Future.succeededFuture(generateJsonResponse(false, REASON.BAD_CONFIGURATION, MESSAGE.WRONG_TOKEN));
         }
-
-        CompletableFuture<JsonObject> future = new CompletableFuture<>();
-
-        String boundary = "myBoundary";
-        Buffer multipartBody = generateMultipartBody(boundary, form, cover);
-        HttpClientRequest request = http.postAbs(libraryApiUrl.concat(RESSOURCE_ENDPOINT));
-        request.handler(response -> {
-            if (response.statusCode() == 401) {
-                future.complete(generateJsonResponse(false, REASON.LIBRARY, MESSAGE.WRONG_TOKEN));
-            } else if (response.statusCode() == 200) {
-                future.complete(generateJsonResponse(true, REASON.OK, MESSAGE.OK));
-            } else {
-                future.complete(generateJsonResponse(false, REASON.LIBRARY, MESSAGE.LIBRARY_KO));
-            }
+        Future<Buffer> archive = getArchive();
+        return archive.compose(resArchive -> generatePdf(user, form)).compose(resPdf -> {
+            final Buffer exportPdf = resPdf.getContent();
+            final Future<JsonObject> future = Future.future();
+            final String boundary = UUID.randomUUID().toString();
+            final Buffer multipartBody = createMultipartBody(form, cover, exportPdf, archive.result(), boundary);
+            HttpClientRequest request = http.postAbs(libraryApiUrl.concat(RESSOURCE_ENDPOINT));
+            request.handler(response -> {
+                if (response.statusCode() == 200 || response.statusCode() == 201) {
+                    response.bodyHandler(body -> {
+                        future.complete(generateJsonResponse(true, REASON.OK, MESSAGE.OK).put("details", new JsonObject(body)));
+                    });
+                    response.exceptionHandler(e -> {
+                        future.fail(e);
+                        log.error("Ressource publication succeed but get exception : " + response.statusCode(), e);
+                    });
+                } else {
+                    if (response.statusCode() == 401) {
+                        future.complete(generateJsonResponse(false, REASON.LIBRARY, MESSAGE.WRONG_TOKEN));
+                    } else {
+                        future.complete(generateJsonResponse(false, REASON.LIBRARY, MESSAGE.LIBRARY_KO));
+                    }
+                    response.bodyHandler(body -> {
+                        log.error("Ressource publication failed : " + response.statusCode() + " - " + body);
+                    });
+                }
+            });
+            request.setChunked(true);
+            request.putHeader("Authorization", "Bearer ".concat(libraryToken));
+            request.putHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
+            request.putHeader("Content-Length", String.valueOf(multipartBody.length()));
+            request.end(multipartBody);
+            return future;
         });
-        request.putHeader("Authorization", "Bearer ".concat(libraryToken));
-        request.putHeader("Content-Type", "multipart/form-data; boundary=" + boundary);
-        request.putHeader("Content-Length", String.valueOf(multipartBody.length()));
-        request.write(multipartBody);
-        request.end();
-        return future;
     }
+
 
     private JsonObject generateJsonResponse(boolean success, REASON reason, MESSAGE message) {
         return new JsonObject()
@@ -71,37 +110,53 @@ public class DefaultLibraryService implements LibraryService {
                 .put("message", message.name());
     }
 
-    private Buffer generateMultipartBody(String boundary, MultiMap form, Buffer cover) {
-        Buffer buffer = Buffer.buffer();
-        buffer.appendBuffer(generateMultipartBodyMultiMap(boundary, form));
-        buffer.appendBuffer(generateMultipartBodyCover(boundary, cover));
-        return buffer;
+    private Future<Pdf> generatePdf(UserInfos user, MultiMap form) {
+        final String pdfUri = form.get("pdfUri");
+        form.remove("pdfUri");
+        Future<Pdf> future = Future.future();
+        if (StringUtils.isEmpty(pdfUri)) {
+            future.fail("Pdf URI should not be empty");
+        } else {
+            this.pdfGenerator.generatePdfFromUrl(user, "pdfExport.pdf", pdfUri, future.completer());
+        }
+        return future;
     }
 
-    private Buffer generateMultipartBodyCover(String boundary, Buffer cover) {
-        Buffer buffer = Buffer.buffer();
-        String body = "--" + boundary + "\r\n" +
-                "Content-Disposition: form-data; name=\"cover\"; filename=\"cover.png\"\r\n" +
-                "Content-Type: image/png\r\n" +
-                "\r\n" +
-                cover.toString() + "\r\n";
-
-        buffer.appendString(body);
-        return buffer;
-    }
-    private Buffer generateMultipartBodyMultiMap(String boundary, MultiMap form) {
-        Buffer buffer = Buffer.buffer();
-        form.forEach(entry -> buffer.appendBuffer(generateMultipartBodyAttribute(boundary, entry.getKey(), entry.getValue())));
-        return buffer;
-    }
-    private Buffer generateMultipartBodyAttribute(String boundary, String attribute, String value) {
-        Buffer buffer = Buffer.buffer();
-        String body = "--" + boundary + "\r\n" +
-                "Content-Disposition: form-data; name=\""+attribute+"\"\r\n" +
-                "\r\n" +
-                value + "\r\n";
-
-        buffer.appendString(body);
+    private Buffer createMultipartBody(MultiMap form, Buffer cover, Buffer exportPdf, Buffer archive, String boundary) {
+        final Buffer buffer = Buffer.buffer();
+        final Optional<String> coverName = Optional.ofNullable(form.get("coverName"));
+        final Optional<String> coverType = Optional.ofNullable(form.get("coverType"));
+        form.remove("coverName").remove("coverType");
+        form.forEach(entry -> {
+            final String attribute = entry.getKey();
+            final String value = entry.getValue();
+            buffer.appendString("--" + boundary + "\r\n");
+            buffer.appendString(String.format("Content-Disposition: form-data; name=\"%s\"", attribute));
+            buffer.appendString("\r\n\r\n");
+            buffer.appendString(value + "\r\n");
+        });
+        //TODO dynamic name/filetype
+        buffer.appendString("--" + boundary + "\r\n");
+        buffer.appendString(String.format("Content-Disposition: form-data; name=\"cover\"; filename=\"%s\"\r\n", coverName.orElse("cover.png")));
+        buffer.appendString(String.format("Content-Type: %s\r\n", coverType.orElse("image/png")));
+        buffer.appendString("\r\n");
+        buffer.appendBuffer(cover);
+        buffer.appendString("\r\n");
+        //TODO put real archive (archive is mandatory in BPR side) and remove documents.zip in ressources
+        buffer.appendString("--" + boundary + "\r\n");
+        buffer.appendString("Content-Disposition: form-data; name=\"archive\"; filename=\"archive.zip\"\r\n");
+        buffer.appendString("Content-Type: application/zip\r\n");
+        buffer.appendString("\r\n");
+        buffer.appendBuffer(archive);
+        buffer.appendString("\r\n");
+        //pdf
+        buffer.appendString("--" + boundary + "\r\n");
+        buffer.appendString("Content-Disposition: form-data; name=\"pdfExport\"; filename=\"pdfExport.pdf\"\r\n");
+        buffer.appendString("Content-Type: application/pdf\r\n");
+        buffer.appendString("\r\n");
+        buffer.appendBuffer(exportPdf);
+        buffer.appendString("\r\n");
+        buffer.appendString("--" + boundary + "--\r\n");
         return buffer;
     }
 }
