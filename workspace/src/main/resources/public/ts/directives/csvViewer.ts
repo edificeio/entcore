b4a4224ed2dbba7b86d8857fde15acb9481791ff
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2019-10-14 12:06:26 +0200

    feat: Add multi sheet support using pagination

--- workspace/src/main/resources/public/ts/directives/csvViewer.ts
+++ workspace/src/main/resources/public/ts/directives/csvViewer.ts
@@ -10,13 +10,27 @@ export interface CsvController {
 export interface CsvDelegate {
     onInit(ctrl: CsvController): void;
 }
+interface Tab {
+    id: number;
+    name: string;
+    rows: any[][];
+    colIndexes: number[];
+    separator: string;
+}
 interface CsvViewerScope {
     csvContent: CsvDelegate;
-    rows: any[][];
     controller: CsvController;
     csvDelegate: CsvDelegate;
-    colIndexes: number[];
     loading: boolean;
+    tabs: Array<Tab>;
+    currentTab: Tab;
+    pageIndex: number;
+    showTabs(): boolean;
+    showContent(): boolean;
+    nameTab(): string;
+    numPages(): number;
+    previousPage(e): void;
+    nextPage(e): void;
     getValue(row: any[], index: number): string;
     $apply: any
 }
@@ -27,12 +41,19 @@ export const csvViewer = ng.directive('csvViewer', ['$sce', ($sce) => {
             csvDelegate: '='
         },
         template: `
+            <div ng-if="showTabs()" class="pagination__area flex-row align-center justify-center">
+                <div class="file-controls left"><i class="left" ng-click="previousPage(e)"></i></div>
+                <div class="pagination">
+                [[nameTab()]] <input type="text" ng-model="pageIndex" /> / [[numPages()]]
+                </div>
+                <div class="file-controls right"><i class="right" ng-click="nextPage(e)"></i></div>
+            </div>
             <div class="render">
 			    <p ng-if="loading" class="top-spacing-four flex-row align-start justify-center centered-text"><i18n>workspace.preview.loading</i18n>&nbsp;<i class="loading"></i></p>
-                <table>
+                <table ng-if="showContent()">
                     <tbody>
-                        <tr ng-repeat="row in rows">
-                            <td ng-repeat="colIndex in colIndexes">[[getValue(row, colIndex)]]</td>
+                        <tr ng-repeat="row in currentTab.rows">
+                            <td ng-repeat="colIndex in currentTab.colIndexes">[[getValue(row, colIndex)]]</td>
                         </tr>
                     </tbody>
                 </table>
@@ -41,12 +62,11 @@ export const csvViewer = ng.directive('csvViewer', ['$sce', ($sce) => {
         link: function (scope: CsvViewerScope, element, attributes, ctrl) {
             let _ngmodel = "";
             let _lastId = null;
-            scope.rows = [];
+            scope.tabs = [];
             scope.loading = true;
-            scope.colIndexes = [];
             const separator = [",", ";", "|"];
             const countOcurrences = (str: string, value: string) => {
-                if(value=="|") value = "\\|";
+                if (value == "|") value = "\\|";
                 const regExp = new RegExp(value, "gi");
                 return (str.match(regExp) || []).length;
             }
@@ -66,18 +86,86 @@ export const csvViewer = ng.directive('csvViewer', ['$sce', ($sce) => {
                 if (_ngmodel) {
                     const rows = _ngmodel.split("\n");
                     if (rows.length == 0) {
-                        scope.colIndexes = [];
+                        scope.tabs = [];
+                        scope.currentTab = null;
                         return;
                     }
-                    let sep = findSeparator(rows[0]);
-                    scope.rows = [];
-                    let countCol = 0;
-                    for (let row of rows) {
-                        const cols = row.split(sep);
-                        countCol = Math.max(countCol, cols.length);
-                        scope.rows.push(cols);
+                    let currentTab: Tab = null;
+                    const createTab = (name?: string) => {
+                        const tab = {
+                            id: scope.tabs.length,
+                            name: name ? name : `Feuille ${scope.tabs.length + 1}`,
+                            colIndexes: [],
+                            rows: [],
+                            separator: null
+                        } as Tab;
+                        scope.tabs.push(tab);
+                        return tab;
                     }
-                    scope.colIndexes = Array.from(Array(countCol).keys())
+                    for (const row of rows) {
+                        if (row.startsWith("\f")) {
+                            currentTab = createTab(row.trim());
+                        } else {
+                            if (currentTab == null) currentTab = createTab();
+                            if (currentTab.separator == null) {
+                                currentTab.separator = findSeparator(rows[0]);
+                            }
+                            const cols = row.split(currentTab.separator);
+                            const countCol = cols.length;
+                            currentTab.rows.push(cols);
+                            if (countCol > currentTab.colIndexes.length) {
+                                currentTab.colIndexes = Array.from(Array(countCol).keys());
+                            }
+                        }
+                    }
+                    scope.currentTab = scope.tabs[0];
+                }
+            }
+            Object.defineProperty(scope, "pageIndex", {
+                get() {
+                    return scope.currentTab ? scope.currentTab.id + 1 : 1
+                },
+                set(index) {
+                    const id = index - 1;
+                    if (id < 0) {
+                        scope.currentTab = scope.tabs[0]
+                    } else if (id >= scope.tabs.length) {
+                        scope.currentTab = scope.tabs[scope.tabs.length - 1];
+                    } else {
+                        scope.currentTab = scope.tabs[id];
+                    }
+                }
+            })
+            scope.showContent = () => !scope.loading && !!scope.currentTab;
+            scope.showTabs = () => !scope.loading && scope.tabs.length > 1;
+            scope.nameTab = () => {
+                return scope.currentTab && scope.currentTab.name;
+            }
+            scope.numPages = () => {
+                return scope.tabs.length;
+            }
+            scope.previousPage = (e) => {
+                e && e.stopPropagation();
+                if (scope.currentTab) {
+                    if (scope.currentTab.id > 0) {
+                        scope.currentTab = scope.tabs[scope.currentTab.id - 1];
+                    } else {
+                        //do nothing
+                    }
+                } else {
+                    scope.currentTab = scope.tabs[0];
+                }
+            }
+            scope.nextPage = (e) => {
+                e && e.stopPropagation();
+                if (scope.currentTab) {
+                    if (scope.currentTab.id < scope.tabs.length - 1) {
+                        scope.currentTab = scope.tabs[scope.currentTab.id + 1];
+                    } else {
+                        //do nothing
+                    }
+                } else {
+                    scope.currentTab = scope.tabs[0];
                 }
             }
             scope.getValue = (row, index) => {
