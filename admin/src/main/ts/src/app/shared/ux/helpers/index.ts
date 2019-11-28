a8bd4e225061b1e06dd00a98b03c59f5b4c6b87c
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2019-11-28 15:13:26 +0100

    feat(admin-v2): #28302, Add searchboxes to communication rules lists

--- admin/src/main/ts/src/app/shared/ux/helpers/index.ts
+++ admin/src/main/ts/src/app/shared/ux/helpers/index.ts
@@ -1,1 +1,7 @@
+import { removeAccents } from './accents.helper';
 export { removeAccents } from './accents.helper';
+
+export function standardise(str: string): string
+{
+  return removeAccents(str != null ? str : "").toLowerCase().replace("\\s+", " ");
+}
