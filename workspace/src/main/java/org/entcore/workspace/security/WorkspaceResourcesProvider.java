5f882e849a837956ccc0ea2bcb58fd9b9bc84f12
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2019-12-09 10:27:16 +0100

    fix(workspace): #28631, Display parent folder and managers. And add missing action check

--- workspace/src/main/java/org/entcore/workspace/security/WorkspaceResourcesProvider.java
+++ workspace/src/main/java/org/entcore/workspace/security/WorkspaceResourcesProvider.java
@@ -121,6 +121,7 @@ public class WorkspaceResourcesProvider implements ResourcesProvider {
 			case "bulkDelete":
 				authorizeDocuments(request, user, binding.getServiceMethod(), handler);
 				break;
+			case "getParentInfos":
 			case "renameDocument":
 			case "renameFolder":
 				authorizeDocument(request, user, binding.getServiceMethod(), handler);
@@ -241,12 +242,13 @@ public class WorkspaceResourcesProvider implements ResourcesProvider {
 
 	@SuppressWarnings("unchecked")
 	private void authorizeDocuments(HttpServerRequest request, UserInfos user, String serviceMethod,
-			Handler<Boolean> handler) {
+                       Handler<Boolean> handler) {
 		bodyToJson(request, body -> {
 			JsonArray idsArray = body.getJsonArray("ids");
 			if (idsArray != null && !idsArray.isEmpty()) {
 				ElementQuery query = new ElementQuery(true);
 				query.setIds(idsArray.getList());
+				query.setActionExists(serviceMethod);
 				executeCountQuery(request, user, query, idsArray.size(), handler);
 			} else {
 				handler.handle(false);
@@ -255,7 +257,7 @@ public class WorkspaceResourcesProvider implements ResourcesProvider {
 	}
 
 	private void authorizeGetDocument(HttpServerRequest request, UserInfos user, String serviceMethod,
-			Handler<Boolean> handler) {
+                       Handler<Boolean> handler) {
 		String id = request.params().get("id");
 		if (id != null && !id.trim().isEmpty()) {
 			ElementQuery query = new ElementQuery(true);
@@ -263,6 +265,7 @@ public class WorkspaceResourcesProvider implements ResourcesProvider {
 			query.getVisibilitiesOr().add("public");
 			query.getVisibilitiesOr().add("protected");
 			query.setId(id);
+			query.setActionExists(serviceMethod);
 			executeCountQuery(request, user, query, 1, handler);
 		} else {
 			handler.handle(false);
@@ -270,11 +273,12 @@ public class WorkspaceResourcesProvider implements ResourcesProvider {
 	}
 
 	private void authorizeDocument(HttpServerRequest request, UserInfos user, String serviceMethod,
-			Handler<Boolean> handler) {
+                       Handler<Boolean> handler) {
 		String id = request.params().get("id");
 		if (id != null && !id.trim().isEmpty()) {
 			ElementQuery query = new ElementQuery(true);
 			query.setId(id);
+			query.setActionExists(serviceMethod);
 			executeCountQuery(request, user, query, 1, handler);
 
 		} else {
--- workspace/src/main/resources/public/ts/delegates/actions/rename.ts
+++ workspace/src/main/resources/public/ts/delegates/actions/rename.ts
@@ -1,6 +1,8 @@
 import { notify, template } from "entcore";
 import { models, workspaceService } from "../../services";
+import { ElementWithVisible } from "entcore/types/src/ts/workspace/services";
 
+const RENAME_RIGHTS = ["org-entcore-workspace-controllers-WorkspaceController|renameDocument","org-entcore-workspace-controllers-WorkspaceController|renameFolder"]
 
 export interface RenameDelegateScope {
     //from others
@@ -9,14 +11,18 @@ export interface RenameDelegateScope {
     selectedDocuments(): models.Element[]
     selectedItems(): models.Element[]
     //
+    renameManagers:Array<{id:string, name:string, uri:string}>
+    renameParent: ElementWithVisible;
     renameTarget: models.Element
     canRenameFolder(): boolean
     canRenameDocument(): boolean
     openRenameView(document: models.Element)
     rename(item: models.Element, newName: string)
+    getRenameManagers():Array<{id:string, name:string}>
 }
 
 export function ActionRenameDelegate($scope: RenameDelegateScope) {
+    $scope.renameManagers=[];
     /**
       * Rename Action
       */
@@ -32,15 +38,51 @@ export function ActionRenameDelegate($scope: RenameDelegateScope) {
         return totalFold == 1 && totalDoc == 0 && $scope.currentTree.filter != "trash";
     }
 
-    $scope.openRenameView = function () {
-        const document = $scope.selectedItems()[0];
-        document.newName = document.newProperties ? document.newProperties.name : document.name;
-        $scope.renameTarget = document;
-        template.open('lightbox', 'rename');
+    $scope.openRenameView = async function () {
+        $scope.renameManagers=[];
+        try {
+            const document = $scope.selectedItems()[0];
+            const ownerId = typeof document.owner == "string"? document.owner: document.owner.userId;
+            if (document.eParent) $scope.renameParent = await workspaceService.fetchParentInfo(document._id);
+            else $scope.renameParent = undefined;
+            //fetch managers
+            $scope.renameManagers.push({id:ownerId, name:document.ownerName, uri:`/userbook/annuaire#/${ownerId}`});
+            if($scope.renameParent && $scope.renameParent.inheritedShares){
+                for(const share of $scope.renameParent.inheritedShares){
+                    let addToManager = false;
+                    for(const right of RENAME_RIGHTS){
+                        if(share[right]){
+                            addToManager = true;
+                        }
+                    }
+                    if(addToManager){
+                        const isUser = !!share.userId;
+                        if(isUser){
+                            if(share.userId != ownerId){
+                                const found = $scope.renameParent.visibleUsers.find(u=>u.id==share.userId)||{} as any;
+                                $scope.renameManagers.push({id:share.userId, name:found.username, uri:`/userbook/annuaire#${found.id}`});
+                            }
+                        } else {
+                            const found = $scope.renameParent.visibleGroups.find(u=>u.id==share.groupId)||{} as any;
+                            $scope.renameManagers.push({id:share.groupId, name:found.name, uri:`/userbook/annuaire#/group-view/${found.id}`});
+                        }
+                    }
+                }
+            }
+            document.newName = document.newProperties ? document.newProperties.name : document.name;
+            $scope.renameTarget = document;
+            template.open('lightbox', 'rename');
+        } catch(e){
+            e.error && notify.error(e.error);
+        }
     };
 
     $scope.rename = async function (item, newName) {
         template.close('lightbox');
         await workspaceService.rename(item, newName);
+    }
+
+    $scope.getRenameManagers = () => {
+        return $scope.renameManagers;
     }
 }
