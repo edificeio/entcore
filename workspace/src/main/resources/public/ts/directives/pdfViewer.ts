d53fe580ec4661de765af0f78f8227633d7bc403
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2019-12-18 16:29:34 +0100

    fix(vile-viewer): #27608, Improve file-viewer navigation

--- workspace/src/main/resources/public/ts/directives/pdfViewer.ts
+++ workspace/src/main/resources/public/ts/directives/pdfViewer.ts
@@ -1,5 +1,6 @@
 import { ng, http, $ } from 'entcore';
 import { Subject, Observable } from "rxjs";
+import moment = require('moment');
 
 interface ScopePdfViewer {
 	pageIndex: number | any;
@@ -11,6 +12,7 @@ interface ScopePdfViewer {
 	nextPage(): void;
 	openPage(): void;
 	previousPage(): void;
+	scroller: string;
 	$apply: any;
 }
 let _loadedPdfJs = false;
@@ -27,14 +29,17 @@ export const pdfViewer = ng.directive('pdfViewer', function () {
 		restrict: 'E',
 		template: `
 			<div class="pagination__area flex-row align-center justify-center">
-				<div class="file-controls left"><i class="left" ng-click="previousPage()"></i></div>
+				<div class="file-controls left" ng-click="previousPage()"><i class="left"></i></div>
 				<div class="pagination">
 					<input type="text" ng-model="pageIndex" ng-change="openPage()" /> / [[numPages]]
 				</div>
-				<div class="file-controls right"><i class="right" ng-click="nextPage()"></i></div>
+				<div class="file-controls right" ng-click="nextPage()"><i class="right"></i></div>
 			</div>
 			<p ng-if="loading" class="top-spacing-four flex-row align-start justify-center centered-text"><i18n>workspace.preview.loading</i18n>&nbsp;<i class="loading"></i></p>
 			`,
+		scope: {
+			scroller: '=?',
+		},
 		link: function (scope: ScopePdfViewer, element, attributes) {
 			let pdf:any;
 			scope.loading = true;
@@ -46,11 +51,27 @@ export const pdfViewer = ng.directive('pdfViewer', function () {
 				}
 			};
 			scope.previousPage = function () {
-				if (scope.pageIndex > 0) {
+				if (scope.pageIndex > 1) {
 					scope.pageIndex--;
 					scope.openPage();
 				}
 			};
+
+			let keyNav = function(e)
+			{
+				switch(e.keyCode)
+				{
+					case 37: scope.previousPage(); break;
+					case 39: scope.nextPage(); break;
+				}
+				scope.$apply();
+			};
+			document.addEventListener("keydown", keyNav);
+			(scope as any).$on("$destroy", function()
+			{
+				document.removeEventListener("keydown", keyNav);
+			});
+
 			const reloadPdf = new Subject<number>();
 			(reloadPdf as Observable<number>).debounceTime(100).subscribe(pageNumber=>{
 				pdf.getPage(pageNumber).then(function (page) {
@@ -108,6 +129,38 @@ export const pdfViewer = ng.directive('pdfViewer', function () {
 						scope.$apply('loading');
 					})
 			});
+
+			if(scope.scroller != null && scope.scroller != "")
+			{
+				let scrollerEl = document.querySelector(scope.scroller);
+				let lastTick = moment(0).valueOf();
+				if(scrollerEl != null)
+				{
+					scrollerEl.addEventListener("wheel", function(e: WheelEvent)
+					{
+						if(moment().valueOf() - lastTick <= 250)
+							return;
+
+						var pageNumber = parseInt(scope.pageIndex);
+						if(e.deltaY > 0 && scrollerEl.scrollHeight - scrollerEl.scrollTop === scrollerEl.clientHeight && pageNumber < scope.numPages)
+						{
+							scope.nextPage();
+							lastTick = moment().valueOf();
+							scrollerEl.scrollTop = 0;
+							e.preventDefault();
+							scope.$apply();
+						}
+						else if(e.deltaY < 0 && scrollerEl.scrollTop == 0 && pageNumber > 1)
+						{
+							scope.previousPage();
+							lastTick = moment().valueOf();
+							scrollerEl.scrollTop = scrollerEl.scrollHeight - scrollerEl.clientHeight;
+							e.preventDefault();
+							scope.$apply();
+						}
+					});
+				}
+			}
 		}
 	}
 });
