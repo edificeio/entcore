ea8146ca60ab2274861ad78c8b97f13c643725d5
david-cc <david.caudeli@web-education.net>
2020-01-13 10:58:01 +0100

    build(adminv2): add npm update for ngx-ode-libs

--- build.sh
+++ build.sh
@@ -82,10 +82,10 @@ buildNode () {
 buildAdminNode() {
   case `uname -s` in
     MINGW*)
-      docker-compose run --rm -u "$USER_UID:$GROUP_GID" node12 sh -c "npm --no-bin-links install && npm run build-docker-prod"
+      docker-compose run --rm -u "$USER_UID:$GROUP_GID" node12 sh -c "npm --no-bin-links install && npm update ngx-ode-core ngx-ode-sijil ngx-ode-ui && npm run build-docker-prod"
       ;;
     *)
-      docker-compose run --rm -u "$USER_UID:$GROUP_GID" node12 sh -c "npm install && npm run build-docker-prod"
+      docker-compose run --rm -u "$USER_UID:$GROUP_GID" node12 sh -c "npm install && npm update ngx-ode-core ngx-ode-sijil ngx-ode-ui && npm run build-docker-prod"
   esac
 }
 
