6f481bf314b6e0d3eb4dd2fde7cff8f5f809c87f
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2020-01-23 10:06:31 +0100

    feat(event): #30218, added route to create events for mobile access to modules

--- infra/src/main/java/org/entcore/infra/services/impl/MongoDbEventStore.java
+++ infra/src/main/java/org/entcore/infra/services/impl/MongoDbEventStore.java
@@ -21,6 +21,9 @@ package org.entcore.infra.services.impl;
 
 import fr.wseduc.mongodb.MongoDb;
 import fr.wseduc.webutils.Either;
+import fr.wseduc.webutils.request.CookieHelper;
+import io.vertx.core.http.HttpServerRequest;
+import org.entcore.common.user.UserInfos;
 import org.entcore.infra.services.EventStoreService;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.Message;
@@ -44,6 +47,35 @@ public class MongoDbEventStore implements EventStoreService {
 				}
 			}
 		});
+	}
+
+	@Override
+	public void generateMobileEvent(String eventType, UserInfos user, HttpServerRequest request,
+									 String module, final Handler<Either<String, Void>> handler) {
+		JsonObject event = new JsonObject();
+		event.put("event-type", eventType)
+				.put("module", module)
+				.put("date", System.currentTimeMillis());
+		if (user != null) {
+			event.put("userId", user.getUserId());
+			if (user.getType() != null) {
+				event.put("profil", user.getType());
+			}
+			if (user.getStructures() != null) {
+				event.put("structures", new fr.wseduc.webutils.collections.JsonArray(user.getStructures()));
+			}
+			if (user.getClasses() != null) {
+				event.put("classes", new fr.wseduc.webutils.collections.JsonArray(user.getClasses()));
+			}
+			if (user.getGroupsIds() != null) {
+				event.put("groups", new fr.wseduc.webutils.collections.JsonArray(user.getGroupsIds()));
+			}
+		}
+		if (request != null) {
+			event.put("referer", request.headers().get("Referer"));
+			event.put("sessionId", CookieHelper.getInstance().getSigned("oneSessionId", request));
+		}
+		store(event, handler);
 	}
 
 }
