2a698b7621b78212b7d77e136355619581d2d19e
dboissin <damien.boissin@web-education.net>
2020-04-18 19:48:35 +0200

    feat(conversation): #35186 optimize threads list query

--- migration/3.8.25/threads.sql
+++ migration/3.8.25/threads.sql
@@ -11,7 +11,6 @@ INSERT INTO conversation.userthreads (
 SELECT um.user_id as user_id, m.thread_id as thread_id, SUM(CASE WHEN um.unread THEN 1 ELSE 0 END) as unread
 FROM conversation.usermessages um
 JOIN conversation.messages m on um.message_id = m.id
-WHERE m.state = 'SENT'
+WHERE um.trashed = false AND m.state = 'SENT'
 GROUP BY user_id, m.thread_id) ON CONFLICT (user_id,thread_id) DO NOTHING;
 COMMIT;
-
