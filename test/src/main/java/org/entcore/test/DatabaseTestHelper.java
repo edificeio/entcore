022afb830dc365e66b116b839377f288dfdc44a4
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2020-06-16 09:47:35 +0200

    feat: Add test module

---
+++ test/src/main/java/org/entcore/test/DatabaseTestHelper.java
@@ -0,0 +1,59 @@
+package org.entcore.test;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import org.entcore.common.neo4j.Neo4j;
+import org.entcore.common.sql.DB;
+import org.entcore.common.sql.Sql;
+import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+public class DatabaseTestHelper {
+    private final Vertx vertx;
+
+    public DatabaseTestHelper(Vertx v){
+        this.vertx = v;
+    }
+
+    public Async initPostgreSQL(TestContext context, PostgreSQLContainer postgreSQLContainer, String schema) {
+        final Async async = context.async();
+        final JsonObject postgresConfig = new JsonObject().put("address", "sql.persistor")
+                .put("url", postgreSQLContainer.getJdbcUrl()).put("username", postgreSQLContainer.getUsername())
+                .put("password", postgreSQLContainer.getPassword());
+
+        final DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(postgresConfig).setWorker(true)
+                .setInstances(1).setMultiThreaded(true);
+
+        vertx.deployVerticle(fr.wseduc.sql.SqlPersistor.class.getName(), deploymentOptions, ar -> {
+            if (ar.succeeded()) {
+                Sql sql = Sql.getInstance();
+                sql.init(vertx.eventBus(), "sql.persistor");
+                DB.loadScripts(schema, vertx, "sql");
+                vertx.setTimer(2000L, t -> async.complete());
+            } else {
+                context.fail();
+            }
+        });
+        return async;
+    }
+
+    public void initNeo4j(TestContext context, Neo4jContainer neo4jContainer) {
+        final String base = neo4jContainer.getHttpUrl() + "/db/data/";
+        final JsonObject config = new JsonObject().put("server-uri", base).put("poolSize", 1);
+        final Neo4j neo4j = Neo4j.getInstance();
+        neo4j.init(vertx, config);
+    }
+
+    public PostgreSQLContainer createPostgreSQLContainer() {
+        return new PostgreSQLContainer("postgres:9.5");
+    }
+
+    public Neo4jContainer createNeo4jContainer() {
+        return new Neo4jContainer("neo4j:3.1").withoutAuthentication()//
+                .withNeo4jConfig("cypher.default_language_version", "2.3");
+    }
+}
---
+++ test/src/main/java/org/entcore/test/ShareTestHelper.java
@@ -0,0 +1,54 @@
+package org.entcore.test;
+
+import fr.wseduc.webutils.StartupUtils;
+import fr.wseduc.webutils.data.FileResolver;
+import fr.wseduc.webutils.security.SecuredAction;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import org.entcore.common.share.ShareService;
+import org.entcore.common.share.impl.SqlShareService;
+
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class ShareTestHelper {
+    private final Vertx vertx;
+
+    public ShareTestHelper(Vertx v) {
+        this.vertx = v;
+    }
+
+    public JsonObject createShareForUser(String userId, List<String> actions) {
+        return new JsonObject().put("users", new JsonObject().put(userId, new JsonArray(actions)));
+    }
+
+    public JsonObject createShareForGroup(String userId, List<String> actions) {
+        return new JsonObject().put("groups", new JsonObject().put(userId, new JsonArray(actions)));
+    }
+    //
+
+    public ShareService createSqlShareService(TestContext context, String schema, String table) throws Exception {
+        try {
+            // delegate IDE build to gradle in order to have annotation processor running
+            // before tests
+            final URL uri = getClass().getClassLoader().getResource("securedaction");
+            final String parent = Paths.get(uri.toURI()).getParent().toAbsolutePath().toString();
+            final String oldValue = FileResolver.absolutePath("");
+            try {
+
+                FileResolver.getInstance().setBasePath(parent);
+                final JsonArray actions = StartupUtils.loadSecuredActions(vertx);
+                final Map<String, SecuredAction> mapActions = StartupUtils.securedActionsToMap(actions);
+                return new SqlShareService(schema, table, vertx.eventBus(), mapActions, null);
+            } finally {
+                FileResolver.getInstance().setBasePath(oldValue);
+            }
+        } catch (Exception e) {
+            context.fail(e);
+            throw e;
+        }
+    }
+}
