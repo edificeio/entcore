9b34b165b7c14ca32590f44cb088efc062bc66d7
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2020-06-17 19:15:12 +0200

    chore: Add jenkins test

--- build.sh
+++ build.sh
@@ -113,6 +113,10 @@ buildGradle () {
   docker-compose run --rm -u "$USER_UID:$GROUP_GID" gradle gradle shadowJar install publishToMavenLocal
 }
 
+testGradle () {
+  ./gradlew test
+}
+
 watch () {
   docker-compose run --rm -u "$USER_UID:$GROUP_GID" node sh -c "node_modules/gulp/bin/gulp.js watch-$MODULE --springboard=/home/node/$SPRINGBOARD"
 }
@@ -152,6 +156,9 @@ do
       ;;
     watch)
       watch
+      ;;
+    test)
+      testGradle
       ;;
     infra)
       infra
