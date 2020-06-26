01372251af475df29e1dae9366ef7070e9192a16
Simon LEDUNOIS <simon.ledunois@gmail.com>
2020-06-26 16:32:29 +0200

    fix(Matrix) Fix Matrix SAML uid

--- auth/src/main/java/org/entcore/auth/services/impl/SSOMatrix.java
+++ auth/src/main/java/org/entcore/auth/services/impl/SSOMatrix.java
@@ -2,11 +2,9 @@ package org.entcore.auth.services.impl;
 
 import fr.wseduc.webutils.Either;
 import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
 import org.entcore.common.neo4j.Neo4j;
 import org.entcore.common.neo4j.Neo4jResult;
-import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.opensaml.saml2.core.Assertion;
@@ -14,7 +12,7 @@ import org.opensaml.saml2.core.Assertion;
 public class SSOMatrix extends AbstractSSOProvider {
     @Override
     public void generate(EventBus eb, String userId, String host, Handler<Either<String, JsonArray>> handler) {
-        String query = "MATCH (u:User {id:{userId}}) RETURN u.id as id, u.displayName as displayName";
+        String query = "MATCH (u:User {id:{userId}}) RETURN u.id as id, u.login as login, u.displayName as displayName";
         Neo4j.getInstance().execute(query, new JsonObject().put("userId", userId), Neo4jResult.validUniqueResultHandler(evt -> {
             if (evt.isLeft()) {
                 handler.handle(new Either.Left(evt.left().getValue()));
@@ -23,14 +21,19 @@ public class SSOMatrix extends AbstractSSOProvider {
 
             JsonArray result = new JsonArray();
             JsonObject user = evt.right().getValue();
-            result.add(new JsonObject().put("uid", user.getString("id", "")));
+            result.add(new JsonObject().put("uid", user.getString("id", "id")));
+            result.add(new JsonObject().put("mxid", mxid(user.getString("id", ""), user.getString("login", ""))));
             result.add(new JsonObject().put("displayName", user.getString("displayName", "")));
             handler.handle(new Either.Right<>(result));
         }));
     }
 
+    private String mxid(String id, String login) {
+        return String.format("%s-%s", login, id.substring(0, 8));
+    }
+
     @Override
     public void execute(Assertion assertion, Handler<Either<String, Object>> handler) {
-        handler.handle(new Either.Left<String, Object>("execute function ot avalable on SSO Gar Implementation"));
+        handler.handle(new Either.Left<String, Object>("execute function ot available on SSO Matrix Implementation"));
     }
 }
