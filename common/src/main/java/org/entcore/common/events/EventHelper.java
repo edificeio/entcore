3e75c9ef7e7d5ac48195270e729f798b8934833b
nabil <nabil.mansouri.3@gmail.com>
2020-10-01 09:40:25 +0200

    feat(admin): #29682, Add stat access on admin

---
+++ common/src/main/java/org/entcore/common/events/EventHelper.java
@@ -0,0 +1,57 @@
+package org.entcore.common.events;
+
+import fr.wseduc.webutils.Either;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+public class EventHelper {
+    private static final Logger logger = LoggerFactory.getLogger(EventHelper.class);
+    public static final String ACCESS_EVENT = "ACCESS";
+    public static final String CREATE_EVENT = "CREATE";
+    private final EventStore store;
+    public EventHelper(final EventStore aStore){
+        this.store = aStore;
+    }
+
+    public void onAccess(final HttpServerRequest request){
+        try{
+            store.createAndStoreEvent(ACCESS_EVENT, request);
+        }catch(Exception e){
+            logger.error("ACCESS event failed", e);
+        }
+    }
+
+    public void onCreateResource(final HttpServerRequest request, final String resourceType){
+        try{
+            store.createAndStoreEvent(CREATE_EVENT, request, new JsonObject().put("resource-type", resourceType));
+        }catch(Exception e){
+            logger.error("CREATE event failed", e);
+        }
+    }
+
+    public <T> Handler<T> onCreateResource(final HttpServerRequest request, final String resourceType, final Handler<T> handler){
+        final Handler<T> h = (r) -> {
+            handler.handle(r);
+            if(r instanceof AsyncResult){
+                final AsyncResult<?> tmp = (AsyncResult<?>)r;
+                if(tmp.succeeded()){
+                    this.onCreateResource(request, resourceType);
+                }
+            } else if(r instanceof Either){
+                final Either<?,?> tmp = (Either<?,?>)r;
+                if(tmp.isRight()){
+                    this.onCreateResource(request, resourceType);
+                }
+            }
+        };
+        return h;
+    }
+
+    public EventStore getStore() {
+        return store;
+    }
+}
