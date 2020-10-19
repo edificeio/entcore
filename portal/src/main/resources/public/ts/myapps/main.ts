c2d1d0b67551e78f27a1ef6d80b5f8834c664810
david-cc <david.caudeli@web-education.net>
2020-10-19 12:54:52 +0200

    feat(myapps): #37742, new my apps feature

---
+++ portal/src/main/resources/public/ts/myapps/main.ts
@@ -0,0 +1,13 @@
+import { ng, template, idiom as lang } from 'entcore';
+import { appController } from './app';
+
+const mainController = ng.controller('MainController', ['$scope', ($scope) => {
+    template.open('main', 'applications');
+    $scope.template = template;
+    $scope.translatedDisplayName = function(app){
+        return lang.translate(app.displayName);
+    }
+}]);
+
+ng.controllers.push(mainController);
+ng.controllers.push(appController);
