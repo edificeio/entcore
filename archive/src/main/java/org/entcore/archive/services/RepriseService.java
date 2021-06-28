31eb83a12e0d02056cf573300741a3cef70b997c
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2021-06-28 19:54:12 +0200

    feat(archive): #46571, Register events when exporting and importing

--- archive/src/main/java/org/entcore/archive/services/RepriseService.java
+++ archive/src/main/java/org/entcore/archive/services/RepriseService.java
@@ -4,8 +4,17 @@ import fr.wseduc.webutils.Either;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
+import org.entcore.common.events.EventStore;
+import org.entcore.common.events.EventStoreFactory;
 
 public interface RepriseService {
+
+    public enum RepriseEvent
+    {
+        EXPORT_OK, EXPORT_ERROR, IMPORT_OK, IMPORT_ERROR
+    }
+
+	public EventStore eventStore = EventStoreFactory.getFactory().getEventStore(RepriseService.class.getSimpleName());
 
     void launchExportForUsersFromOldPlatform(boolean relativePersonnelFirst);
     void launchImportForUsersFromOldPlatform();
