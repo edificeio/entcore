55d6199ea29b1c637909712911e3c16fa38a913f
david-cc <david.caudeli@web-education.net>
2021-06-29 11:36:22 +0200

    feat(adminv2): #45900, delete hotjar

--- admin/src/main/ts/src/app/app.component.ts
+++ admin/src/main/ts/src/app/app.component.ts
@@ -17,29 +17,5 @@ export class AppComponent extends OdeComponent implements AfterViewInit {
 
     ngAfterViewInit() {
         super.ngAfterViewInit();
-        this.appendHotjarScript();
-    }
-
-    private appendHotjarScript() {
-        http.get('admin/conf/public').then(res => {
-            if (res.data.hotjarId) {
-                const s = document.createElement('script');
-                s.type = 'text/javascript';
-                s.innerHTML = `
-                    (function(h,o,t,j,a,r){
-                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                        h._hjSettings={hjid:${res.data.hotjarId},hjsv:6};
-                        a=o.getElementsByTagName('head')[0];
-                        r=o.createElement('script');r.async=1;
-                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                        a.appendChild(r);
-                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-                `;
-                this.elementRef.nativeElement.appendChild(s);
-            } else {
-                // tslint:disable-next-line: no-console
-                console.info('hotjar not configured on this plateform');
-            }
-        });
     }
 }
