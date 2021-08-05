a7d9d170be610f3f1b0e5f6a01138a16483b6756
dboissin <damien.boissin@web-education.net>
2021-08-05 09:29:34 +0200

    update script schema mongo

--- scripts/schema.js
+++ scripts/schema.js
@@ -1,6 +1,9 @@
-db.documents.ensureIndex({ "owner" : 1 });
-db.documents.ensureIndex({ "folder" : 1 });
-db.documents.ensureIndex({ "old-folder" : 1 });
+db.documents.createIndex({ "owner" : 1 }, { background: true });
+db.documents.createIndex({ name: "text", ownerName: "text", nameSearch:"text" }, { background: true,name:"idx_document_text" });
+db.documentsRevisions.createIndex({ name: "text", ownerName: "text" }, { background: true,name:"idx_revision_text" });
+db.documents.createIndex({eParent:1},{background: true,name:"idx_eparent"});
+db.documents.createIndex({"inheritedShares.userId":1},{background: true,name:"idx_inherited_userid"});
+db.documents.createIndex({"inheritedShares.groupId":1},{background: true,name:"idx_inherited_groupid"});
 db.events.ensureIndex({ "resource" : 1 });
 db.events.ensureIndex({ "event-type" : 1 });
 db.events.ensureIndex({ "module" : 1 });
