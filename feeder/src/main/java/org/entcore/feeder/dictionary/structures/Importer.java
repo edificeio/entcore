36e622f88c564807293f0dea12787ec8c334c4e8
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2021-09-21 18:26:08 +0200

    fix(edt, feeder): #48072, Mark missing users in EDT feeder

--- feeder/src/main/java/org/entcore/feeder/dictionary/structures/Importer.java
+++ feeder/src/main/java/org/entcore/feeder/dictionary/structures/Importer.java
@@ -909,6 +909,10 @@ public class Importer {
 		markMissingUsers(structureExternalId, currentSource, userImportedExternalId, transactionHelper, null, handler);
 	}
 
+	public void markMissingUsers(String structureExternalId, String currentSource, String prefix, final Handler<Void> handler) {
+		markMissingUsers(structureExternalId, currentSource, userImportedExternalId, transactionHelper, prefix, handler);
+	}
+
 	public static void markMissingUsers(String structureExternalId, String currentSource,
 			final Set<String> userImportedExternalId, final TransactionHelper transactionHelper,
 			final Handler<Void> handler) {
