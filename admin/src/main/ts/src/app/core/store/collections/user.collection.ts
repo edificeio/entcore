1bf1e491c86cfa7cbb30e82debef60bb31dbb246
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2021-10-06 16:15:01 +0200

    fix(admin): #38573, remove duplicated users from list

--- admin/src/main/ts/src/app/core/store/collections/user.collection.ts
+++ admin/src/main/ts/src/app/core/store/collections/user.collection.ts
@@ -1,5 +1,5 @@
 import {UserModel} from '../models/user.model';
-import {Collection} from 'entcore-toolkit';
+import {Collection, HttpResponse} from 'entcore-toolkit';
 
 export class UserCollection extends Collection<UserModel> {
 
@@ -7,6 +7,24 @@ export class UserCollection extends Collection<UserModel> {
         super({
             sync: syncUrl != null ? syncUrl : '/directory/structure/:structureId/users'
         }, UserModel);
+    }
+
+    sync(opts?: {}): Promise<HttpResponse> {
+        const res = super.sync(opts);
+        const removeDuplicates = (users: UserModel[]): UserModel[] => {
+            const res: UserModel[] = [];
+            for (let user of users) {
+                let exists = res.find(u => u.id == user.id);
+                if (exists) {
+                    exists.duplicates = exists.duplicates.concat(user.duplicates);
+                } else {
+                    res.push(user);
+                }
+            }
+            return res;
+        }
+        res.then(event => { this.data = removeDuplicates(this.data); });
+        return res;
     }
 
     public structureId: string;
