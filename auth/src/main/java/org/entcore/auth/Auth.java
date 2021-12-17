89a52f79996d005629d757d9a119f7c60bcabc0d
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2021-12-17 18:14:42 +0100

    feat(auth): #51894, Remove user's device info when they are deleted

--- auth/src/main/java/org/entcore/auth/Auth.java
+++ auth/src/main/java/org/entcore/auth/Auth.java
@@ -34,6 +34,7 @@ import org.entcore.auth.services.impl.*;
 import org.entcore.auth.users.DefaultUserAuthAccount;
 import org.entcore.auth.users.UserAuthAccount;
 import org.entcore.auth.users.NewDeviceWarningTask;
+import org.entcore.auth.users.AuthRepositoryEvents;
 import org.entcore.common.email.EmailFactory;
 import org.entcore.common.events.EventStore;
 import org.entcore.common.events.EventStoreFactory;
@@ -148,6 +149,7 @@ public class Auth extends BaseServer {
 		}
 
 		final JsonObject NDWConf = config.getJsonObject("new-device-warning");
+		NewDeviceWarningTask NDWTask = null;
 		if(NDWConf != null)
 		{
 			String cron = NDWConf.getString("cron");
@@ -159,11 +161,13 @@ public class Auth extends BaseServer {
 				boolean warnUsers = NDWConf.getBoolean("warn-users", false);
 				int scoreThreshold = NDWConf.getInteger("score-threshold", 2).intValue();
 				int batchLimit = NDWConf.getInteger("batch-limit", 4000).intValue();
-				NewDeviceWarningTask NDW = new NewDeviceWarningTask(vertx, emailFactory.getSender(), config.getString("email"),
-																	warnADMC, warnADML, warnUsers, scoreThreshold, batchLimit);
-				new CronTrigger(vertx, cron).schedule(NDW);
+				NDWTask = new NewDeviceWarningTask(vertx, emailFactory.getSender(), config.getString("email"),
+													warnADMC, warnADML, warnUsers, scoreThreshold, batchLimit);
+				new CronTrigger(vertx, cron).schedule(NDWTask);
 			}
 		}
+
+		setRepositoryEvents(new AuthRepositoryEvents(NDWTask));
 	}
 
 }
