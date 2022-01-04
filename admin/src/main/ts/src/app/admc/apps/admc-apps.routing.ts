d362639d45062b9eed85f2b2c68a6fb2cd3ea37d
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2022-01-04 11:44:54 +0100

    feat(admin): #WB-62, applicative roles

--- admin/src/main/ts/src/app/admc/apps/admc-apps.routing.ts
+++ admin/src/main/ts/src/app/admc/apps/admc-apps.routing.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { AdmcAppsComponent } from './admc-apps.component';
-import { AdmcAppsRolesComponent } from './roles/admc-apps-roles.component';
 
 export let routes: Routes = [
     {
@@ -8,8 +7,7 @@ export let routes: Routes = [
         component: AdmcAppsComponent,
         children: [
             {
-                path: 'roles',
-                component: AdmcAppsRolesComponent
+                path: 'roles', loadChildren: () => import('./roles/admc-apps-roles.module').then(m => m.AdmcAppsRolesModule)
             }
         ]
     }
