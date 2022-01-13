8dde36d40f722dd49da59b18c3bf97486ac24624
Clément Creusat <clement.creusat@gmail.com>
2022-01-13 11:30:44 +0100

    feat(admin2): #WB-114, broadcastlist tab and info component

--- admin/src/main/ts/src/app/groups/groups.routing.ts
+++ admin/src/main/ts/src/app/groups/groups.routing.ts
@@ -1,41 +1,50 @@
-import {Routes} from '@angular/router';
+import { Routes } from "@angular/router";
 
-import {GroupsComponent} from './groups/groups.component';
-import {GroupCreateComponent} from './create/group-create/group-create.component';
-import {GroupDetailsComponent} from './details/group-details/group-details.component';
-import {GroupsTypeViewComponent} from './type-view/groups-type-view.component';
-import {GroupsResolver} from './groups.resolver';
-import {GroupDetailsResolver} from './details/group-details.resolver';
-import {GroupInternalCommunicationRuleResolver} from './details/group-internal-communication-rule.resolver';
-import {SmartGroupCommunicationComponent} from './communication/smart-group-communication/smart-group-communication.component';
+import { GroupsComponent } from "./groups/groups.component";
+import { GroupCreateComponent } from "./create/group-create/group-create.component";
+import { GroupDetailsComponent } from "./details/group-details/group-details.component";
+import { GroupsTypeViewComponent } from "./type-view/groups-type-view.component";
+import { GroupsResolver } from "./groups.resolver";
+import { GroupDetailsResolver } from "./details/group-details.resolver";
+import { GroupInternalCommunicationRuleResolver } from "./details/group-internal-communication-rule.resolver";
+import { SmartGroupCommunicationComponent } from "./communication/smart-group-communication/smart-group-communication.component";
+import { GroupInfoComponent } from "./info/group-info.component";
 
 export let routes: Routes = [
-    {
-        path: '', component: GroupsComponent, resolve: {grouplist: GroupsResolver},
+  {
+    path: "",
+    component: GroupsComponent,
+    resolve: { grouplist: GroupsResolver },
+    children: [
+      {
+        path: ":groupType",
+        component: GroupsTypeViewComponent,
         children: [
-            {
-                path: ':groupType', component: GroupsTypeViewComponent,
-                children: [
-                    {
-                        path: 'create',
-                        component: GroupCreateComponent
-                    },
-                    {
-                        path: ':groupId/details',
-                        component: GroupDetailsComponent,
-                        resolve: {
-                            group: GroupDetailsResolver,
-                            rule: GroupInternalCommunicationRuleResolver
-                        }
-                    },
-                    {
-                        path: ':groupId/communication',
-                        component: SmartGroupCommunicationComponent,
-                        resolve: {
-                            group: GroupDetailsResolver
-                        }
-                    }
-                ]
-            }]
-    }
+          {
+            path: "create",
+            component: GroupCreateComponent,
+          },
+          {
+            path: "info",
+            component: GroupInfoComponent,
+          },
+          {
+            path: ":groupId/details",
+            component: GroupDetailsComponent,
+            resolve: {
+              group: GroupDetailsResolver,
+              rule: GroupInternalCommunicationRuleResolver,
+            },
+          },
+          {
+            path: ":groupId/communication",
+            component: SmartGroupCommunicationComponent,
+            resolve: {
+              group: GroupDetailsResolver,
+            },
+          },
+        ],
+      },
+    ],
+  },
 ];
