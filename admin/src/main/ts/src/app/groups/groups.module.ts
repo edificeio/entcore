0455e70520fa3e38f8b97ce4cb254f850702237c
Clément Creusat <clement.creusat@gmail.com>
2022-01-13 11:30:44 +0100

    feat(admin2): #WB-114, broadcastlist tab and info component

--- admin/src/main/ts/src/app/groups/groups.module.ts
+++ admin/src/main/ts/src/app/groups/groups.module.ts
@@ -1,66 +1,65 @@
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {NgModule} from '@angular/core';
-import {NgxOdeSijilModule} from 'ngx-ode-sijil';
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { NgxOdeSijilModule } from "ngx-ode-sijil";
 
-import {NgxOdeUiModule} from 'ngx-ode-ui';
-import {routes} from './groups.routing';
-import {GroupsResolver} from './groups.resolver';
-import {GroupDetailsResolver} from './details/group-details.resolver';
-import {GroupInternalCommunicationRuleResolver} from './details/group-internal-communication-rule.resolver';
-import {GroupsStore} from './groups.store';
-import {GroupNameService} from '../core/services/group-name.service';
-import {UserlistFiltersService} from '../core/services/userlist.filters.service';
+import { NgxOdeUiModule } from "ngx-ode-ui";
+import { routes } from "./groups.routing";
+import { GroupsResolver } from "./groups.resolver";
+import { GroupDetailsResolver } from "./details/group-details.resolver";
+import { GroupInternalCommunicationRuleResolver } from "./details/group-internal-communication-rule.resolver";
+import { GroupsStore } from "./groups.store";
+import { GroupNameService } from "../core/services/group-name.service";
+import { UserlistFiltersService } from "../core/services/userlist.filters.service";
 
-import {GroupsComponent} from './groups/groups.component';
-import {GroupCreateComponent} from './create/group-create/group-create.component';
-import {GroupDetailsComponent} from './details/group-details/group-details.component';
-import {GroupManageUsersComponent} from './details/manage-users/group-manage-users/group-manage-users.component';
-import {GroupInputUsersComponent} from './details/manage-users/input/group-input-users/group-input-users.component';
-import {GroupInputFiltersComponent} from './details/manage-users/input/group-input-filters/group-input-filters.component';
-import {GroupOutputUsersComponent} from './details/manage-users/output/group-output-users/group-output-users.component';
-import {GroupUsersListComponent} from './details/users-list/group-users-list.component';
-import {GroupsTypeViewComponent} from './type-view/groups-type-view.component';
-import {GroupsService} from './groups.service';
-import {CommunicationModule} from '../communication/communication.module';
-import {SmartGroupCommunicationComponent} from './communication/smart-group-communication/smart-group-communication.component';
-import {globalStoreProvider} from '../core/store/global.store';
+import { GroupsComponent } from "./groups/groups.component";
+import { GroupCreateComponent } from "./create/group-create/group-create.component";
+import { GroupDetailsComponent } from "./details/group-details/group-details.component";
+import { GroupManageUsersComponent } from "./details/manage-users/group-manage-users/group-manage-users.component";
+import { GroupInputUsersComponent } from "./details/manage-users/input/group-input-users/group-input-users.component";
+import { GroupInputFiltersComponent } from "./details/manage-users/input/group-input-filters/group-input-filters.component";
+import { GroupOutputUsersComponent } from "./details/manage-users/output/group-output-users/group-output-users.component";
+import { GroupUsersListComponent } from "./details/users-list/group-users-list.component";
+import { GroupsTypeViewComponent } from "./type-view/groups-type-view.component";
+import { GroupsService } from "./groups.service";
+import { CommunicationModule } from "../communication/communication.module";
+import { SmartGroupCommunicationComponent } from "./communication/smart-group-communication/smart-group-communication.component";
+import { globalStoreProvider } from "../core/store/global.store";
+import { GroupInfoComponent } from "./info/group-info.component";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        NgxOdeUiModule,
-        CommunicationModule,
-        NgxOdeSijilModule.forChild(),
-        RouterModule.forChild(routes)
-    ],
-    declarations: [
-        GroupsComponent,
-        GroupCreateComponent,
-        GroupDetailsComponent,
-        GroupManageUsersComponent,
-        GroupInputUsersComponent,
-        GroupInputFiltersComponent,
-        GroupOutputUsersComponent,
-        GroupUsersListComponent,
-        GroupsTypeViewComponent,
-        SmartGroupCommunicationComponent
-    ],
-    providers: [
-        GroupsResolver,
-        GroupDetailsResolver,
-        GroupInternalCommunicationRuleResolver,
-        GroupsStore,
-        UserlistFiltersService,
-        GroupNameService,
-        GroupsService,
-        globalStoreProvider
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgxOdeUiModule,
+    CommunicationModule,
+    NgxOdeSijilModule.forChild(),
+    RouterModule.forChild(routes),
+  ],
+  declarations: [
+    GroupsComponent,
+    GroupCreateComponent,
+    GroupDetailsComponent,
+    GroupManageUsersComponent,
+    GroupInputUsersComponent,
+    GroupInputFiltersComponent,
+    GroupOutputUsersComponent,
+    GroupUsersListComponent,
+    GroupsTypeViewComponent,
+    GroupInfoComponent,
+    SmartGroupCommunicationComponent,
+  ],
+  providers: [
+    GroupsResolver,
+    GroupDetailsResolver,
+    GroupInternalCommunicationRuleResolver,
+    GroupsStore,
+    UserlistFiltersService,
+    GroupNameService,
+    GroupsService,
+    globalStoreProvider,
+  ],
+  exports: [RouterModule],
 })
-export class GroupsModule {
-}
+export class GroupsModule {}
--- admin/src/main/ts/src/app/groups/type-view/groups-type-view.component.ts
+++ admin/src/main/ts/src/app/groups/type-view/groups-type-view.component.ts
@@ -1,81 +1,122 @@
-import { ChangeDetectionStrategy, Component, Injector, OnDestroy, OnInit } from '@angular/core';
-import { OdeComponent } from 'ngx-ode-core';
-import { GroupModel } from '../../core/store/models/group.model';
-import { GroupsStore } from '../groups.store';
-
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Injector,
+  OnDestroy,
+  OnInit,
+} from "@angular/core";
+import { OdeComponent } from "ngx-ode-core";
+import { GroupModel } from "../../core/store/models/group.model";
+import { GroupsStore } from "../groups.store";
 
 @Component({
-    selector: 'ode-groups-type-view',
-    templateUrl: './groups-type-view.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "ode-groups-type-view",
+  templateUrl: "./groups-type-view.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GroupsTypeViewComponent extends OdeComponent implements OnInit, OnDestroy {
+export class GroupsTypeViewComponent
+  extends OdeComponent
+  implements OnInit, OnDestroy
+{
+  groupType: string;
+  groupInputFilter: string;
+  selectedGroup: GroupModel;
 
-    groupType: string;
-    groupInputFilter: string;
-    selectedGroup: GroupModel;
+  constructor(public groupsStore: GroupsStore, injector: Injector) {
+    super(injector);
+  }
 
-    constructor(
-        public groupsStore: GroupsStore,
-        injector: Injector) {
-            super(injector);
-    }
-
-    ngOnInit() {
-        super.ngOnInit();
-        this.subscriptions.add(this.route.params.subscribe(params => {
-            this.groupsStore.group = null;
-            const type = params.groupType;
-            const allowedTypes = ['manualGroup', 'profileGroup', 'functionalGroup', 'functionGroup'];
-            if (type && allowedTypes.indexOf(type) >= 0) {
-                this.groupType = params.groupType;
-                this.changeDetector.markForCheck();
-            } else {
-                this.router.navigate(['..'], {relativeTo: this.route});
-            }
-        }));
-        this.subscriptions.add(this.groupsStore.$onchange.subscribe(field => {
-            if (field === 'structure') {
-                this.changeDetector.markForCheck();
-                this.changeDetector.detectChanges();
-            }
-        }));
-
-        // handle change detection from create button click of group-root.component
-        this.subscriptions.add(this.route.url.subscribe(path => {
-            this.changeDetector.markForCheck();
-        }));
-    }
-
-    isSelected = (group: GroupModel) => {
-        return this.selectedGroup && group && this.selectedGroup.id === group.id;
-    }
-
-    filterByInput = (group: GroupModel) => {
-        if (!this.groupInputFilter) { return true; }
-        return group.name.toLowerCase()
-            .indexOf(this.groupInputFilter.toLowerCase()) >= 0;
-    }
-
-    showCompanion(): boolean {
-        const groupTypeRoute = '/admin/' +
-            (this.groupsStore.structure ? this.groupsStore.structure.id : '') +
-            '/groups/' + this.groupType;
-
-        let res: boolean = this.router.isActive(groupTypeRoute + '/create', true);
-        if (this.groupsStore.group) {
-            res = res || this.router.isActive(groupTypeRoute + '/' + this.groupsStore.group.id + '/details', true)
-                || this.router.isActive(groupTypeRoute + '/' + this.groupsStore.group.id + '/communication', true);
+  ngOnInit() {
+    super.ngOnInit();
+    this.subscriptions.add(
+      this.route.params.subscribe(params => {
+        this.groupsStore.group = null;
+        const type = params.groupType;
+        const allowedTypes = [
+          "manualGroup",
+          "profileGroup",
+          "functionalGroup",
+          "functionGroup",
+          "broadcastGroup",
+        ];
+        if (type && allowedTypes.indexOf(type) >= 0) {
+          this.groupType = params.groupType;
+          this.changeDetector.markForCheck();
+        } else {
+          this.router.navigate([".."], { relativeTo: this.route });
         }
-        return res;
-    }
+        if (type === "broadcastGroup") {
+          this.router.navigate(["./info"], { relativeTo: this.route });
+        }
+      })
+    );
+    this.subscriptions.add(
+      this.groupsStore.$onchange.subscribe(field => {
+        if (field === "structure") {
+          this.changeDetector.markForCheck();
+          this.changeDetector.detectChanges();
+        }
+      })
+    );
 
-    closePanel() {
-        this.router.navigateByUrl('/admin/' + (this.groupsStore.structure ? this.groupsStore.structure.id : '') +
-            '/groups/' + this.groupType);
-    }
+    // handle change detection from create button click of group-root.component
+    this.subscriptions.add(
+      this.route.url.subscribe(path => {
+        this.changeDetector.markForCheck();
+      })
+    );
+  }
 
-    routeToGroup(g: GroupModel) {
-        this.router.navigate([g.id, 'details'], {relativeTo: this.route});
+  isSelected = (group: GroupModel) => {
+    console.log(group.subType);
+
+    return this.selectedGroup && group && this.selectedGroup.id === group.id;
+  };
+
+  filterByInput = (group: GroupModel) => {
+    if (!this.groupInputFilter) {
+      return true;
     }
+    return (
+      group.name.toLowerCase().indexOf(this.groupInputFilter.toLowerCase()) >= 0
+    );
+  };
+
+  showCompanion(): boolean {
+    const groupTypeRoute =
+      "/admin/" +
+      (this.groupsStore.structure ? this.groupsStore.structure.id : "") +
+      "/groups/" +
+      this.groupType;
+
+    let res: boolean =
+      this.router.isActive(groupTypeRoute + "/create", true) ||
+      this.router.isActive(groupTypeRoute + "/info", true);
+    if (this.groupsStore.group) {
+      res =
+        res ||
+        this.router.isActive(
+          groupTypeRoute + "/" + this.groupsStore.group.id + "/details",
+          true
+        ) ||
+        this.router.isActive(
+          groupTypeRoute + "/" + this.groupsStore.group.id + "/communication",
+          true
+        );
+    }
+    return res;
+  }
+
+  closePanel() {
+    this.router.navigateByUrl(
+      "/admin/" +
+        (this.groupsStore.structure ? this.groupsStore.structure.id : "") +
+        "/groups/" +
+        this.groupType
+    );
+  }
+
+  routeToGroup(g: GroupModel) {
+    this.router.navigate([g.id, "details"], { relativeTo: this.route });
+  }
 }
