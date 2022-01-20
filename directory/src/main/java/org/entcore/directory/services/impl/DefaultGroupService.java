9423a89128a42b98b9a68311684b4b7a1b64470a
Clément Creusat <clement.creusat@gmail.com>
2022-01-20 17:27:01 +0100

    feat(admin2): #WB-201 ccreate broadcast list

--- directory/src/main/java/org/entcore/directory/services/impl/DefaultGroupService.java
+++ directory/src/main/java/org/entcore/directory/services/impl/DefaultGroupService.java
@@ -99,7 +99,7 @@ public class DefaultGroupService implements GroupService {
 				"g.lockDelete AS lockDelete, " +
 				"CASE WHEN any(x in classes where x <> {name: null, id: null}) THEN classes END as classes," +
 				"CASE WHEN any(x in structures where x <> {name: null, id: null}) THEN structures END as structures, " +
-				"CASE WHEN (g: ProfileGroup)-[:DEPENDS]-(:Structure) THEN 'StructureGroup' END as subType";
+				"CASE WHEN (g: ProfileGroup)-[:DEPENDS]-(:Structure) THEN 'StructureGroup' ELSE CASE WHEN HAS(g.subType) THEN g.subType END END as subType";
 		neo.execute(query, params, validResultHandler(results));
 	}
 
