91b02b13483daccc2ee188b6895e09b5121aff1c
richard.bunel@opendigitaleducation.com <richard.bunel@opendigitaleducation.com>
2022-01-24 11:17:21 +0100

    fix(xiti): #52553, prevent connector's opening being blocked by firefox

--- infra/src/main/resources/public/js/angular-app.js
+++ infra/src/main/resources/public/js/angular-app.js
@@ -6491,9 +6491,26 @@ module.directive('assistant', function(){
                     scope.display.showAuthenticatedConnectorLightbox = false;
                     _MUTEX = false;
                 }
-                async function open(address, target){
-                    if (_app.isExternal && window.xiti && window.xiti.click) await window.xiti.click(_app.name, _app.element[0]);
-                    window.open(address, target);
+                function open(address, target){
+                    if (_app.isExternal && window.xiti && window.xiti.click) {
+                        if (target == '_self') {
+                            /* If target is the same page, we must ensure 'window.xiti.click' method has terminated
+                               before opening the connector
+                            */
+                            window.xiti.click(_app.name, _app.element[0])
+                            .then(() => { window.open(address, target); })
+                            .catch(err => { window.open(address, target); });
+                        } else {
+                            /* If target is a new page, Firefox would block the connector as a popup, hence we don't
+                               wait for 'window.xiti.click' response, which is ok because its execution cannot be
+                               interrupted as the connector opens in a new page
+                            */
+                            window.xiti.click(_app.name, _app.element[0]);
+                            window.open(address, target);
+                        }
+                    } else {
+                        window.open(address, target);
+                    }
                 }
                 scope.onConfirm = function () {
                     scope.onClose();
