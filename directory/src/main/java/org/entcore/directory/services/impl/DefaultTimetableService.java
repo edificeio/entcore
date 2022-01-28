317512863286f969ae720f1bc243ad03cb31676b
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2022-01-28 16:09:46 +0100

    fix(edt, udt): #52696, Fix timetable only imports

--- directory/src/main/java/org/entcore/directory/services/impl/DefaultTimetableService.java
+++ directory/src/main/java/org/entcore/directory/services/impl/DefaultTimetableService.java
@@ -391,7 +391,7 @@ public class DefaultTimetableService implements TimetableService {
 
 	@Override
 	public void importTimetable(String structureId, final String path, final String domain,
-			final String acceptLanguage, boolean uai, String timetableType, boolean groupsOnly, boolean setReportAsAutomatic,
+			final String acceptLanguage, boolean uai, String timetableType, boolean groupsOnly, boolean automaticMode,
 			final Handler<Either<JsonObject, JsonObject>> handler) {
 		final String  structureAttr = uai ? "UAI" : "id";
 		final String setPunctualTimetable = timetableType == null ? "REMOVE s.punctualTimetable" : "SET s.punctualTimetable = {punctualTT}";
@@ -415,8 +415,8 @@ public class DefaultTimetableService implements TimetableService {
 						handler.handle(new Either.Left<JsonObject, JsonObject>(ge));
 						return;
 					}
-					callTimetableImport(event.right().getValue().getString("UAI"), ttType, timetableType != null && (timetableType.equals(dbTimetable) == false),
-										groupsOnly, setReportAsAutomatic, path, acceptLanguage, handler);
+					callTimetableImport(event.right().getValue().getString("UAI"), ttType, timetableType != null && (automaticMode == false && timetableType.equals(dbTimetable) == false),
+										groupsOnly, automaticMode, path, acceptLanguage, handler);
 				} else {
 					errors.add(I18n.getInstance().translate("invalid.structure", domain, acceptLanguage));
 					handler.handle(new Either.Left<JsonObject, JsonObject>(ge));
@@ -427,7 +427,7 @@ public class DefaultTimetableService implements TimetableService {
 
 	@Override
 	public void feederPronote(String structureId, final String path, final String domain,
-		final String acceptLanguage, boolean uai, boolean setReportAsAutomatic, final Handler<Either<JsonObject, JsonObject>> handler)
+		final String acceptLanguage, boolean uai, boolean automaticMode, final Handler<Either<JsonObject, JsonObject>> handler)
 	{
 		final String  structureAttr = uai ? "UAI" : "id";
 		final String query = "MATCH (s:Structure {" + structureAttr + ":{id}}) RETURN s.externalId AS externalId";
@@ -481,13 +481,13 @@ public class DefaultTimetableService implements TimetableService {
 		}));
 	}
 
-	private void callTimetableImport(String UAI, String timetableType, boolean isPunctual, boolean groupsOnly, boolean setReportAsAutomatic,
+	private void callTimetableImport(String UAI, String timetableType, boolean isPunctual, boolean groupsOnly, boolean automaticMode,
 										String path, String acceptLanguage, Handler<Either<JsonObject, JsonObject>> handler)
 	{
 		JsonObject action = new JsonObject().put("action", "manual-" + timetableType.toLowerCase())
 				.put("path", path)
 				.put("UAI", UAI)
-				.put("isManualImport", setReportAsAutomatic == false)
+				.put("isManualImport", automaticMode == false)
 				.put("updateGroups", isPunctual == false)
 				.put("updateTimetable", groupsOnly == false)
 				.put("language", acceptLanguage);
