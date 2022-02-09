5fc823eab6c7dccfe0bb9769406face0a0387b3d
Clément Creusat <clement.creusat@gmail.com>
2022-02-09 16:25:29 +0100

    feat(admin2): WB-257 lightbox recherche transverse

--- admin/src/main/ts/src/app/groups/details/manage-users/input/group-input-users/group-input-users.component.ts
+++ admin/src/main/ts/src/app/groups/details/manage-users/input/group-input-users/group-input-users.component.ts
@@ -20,6 +20,7 @@ import {
   UserlistFiltersService,
 } from "../../../../../core/services/userlist.filters.service";
 import { SpinnerService } from "ngx-ode-ui";
+import { BundlesService } from "ngx-ode-sijil";
 
 @Component({
   selector: "ode-group-input-users",
@@ -51,8 +52,11 @@ export class GroupInputUsersComponent
 
   structureOptions: SelectOption<StructureModel>[] = [];
 
+  allUsersChecked:boolean = false;
+
   constructor(
-    public userLS: UserListService,
+    private bundles: BundlesService,
+    public userListService: UserListService,
     public listFilters: UserlistFiltersService,
     public usersService: UsersService,
     public spinner: SpinnerService,
@@ -65,7 +69,7 @@ export class GroupInputUsersComponent
 
   ngOnInit(): void {
     super.ngOnInit();
-    
+
     this.subscriptions.add(
       this.listFilters.$updateSubject.subscribe(() => {
         this.changeDetector.markForCheck();
@@ -93,13 +97,17 @@ export class GroupInputUsersComponent
     return this.selectedUsers.indexOf(user) > -1;
   };
 
-  selectAll(): void {
-    this.selectedUsers = this.storedElements;
-    this.selectUsers.emit(this.selectedUsers);
-  }
+  toggleSelectedUsers(): void {
+    const selectedUsersLength = this.selectedUsers.length;
+    const storedElementsLength = this.storedElements.length;
 
-  deselectAll(): void {
-    this.selectedUsers = [];
+    if (selectedUsersLength !== storedElementsLength) {
+      this.selectedUsers = this.storedElements;
+      this.allUsersChecked = true;
+    } else {
+      this.selectedUsers = [];
+      this.allUsersChecked = false;
+    }
     this.selectUsers.emit(this.selectedUsers);
   }
 
@@ -107,18 +115,32 @@ export class GroupInputUsersComponent
     this.nbUser = list.length;
   }
 
+  userStructures(item: UserModel) {
+    if(item.structures && item.structures.length > 0) {
+        let result = item.structures[0].name;
+        if (item.structures.length > 1) {
+            result += ` + ${item.structures.length-1} ${this.bundles.translate("others")}`;
+        }
+        return result;
+    }
+    return "";
+}
+
   search = (): void => {
-    console.log(this.userLS.inputFilter);
-    
+    console.log(this.userListService.inputFilter);
+
     this.spinner.perform(
       "portal-content",
-      this.usersService
-        .search(this.userLS.inputFilter)
-        .then(data => {
-          this.model = data;
-          this.refreshListCount(data);
-          this.changeDetector.markForCheck();
-        })
+      this.usersService.search(this.userListService.inputFilter).then(data => {
+        console.table(data);
+        
+        // const uniqueObjects = [...new Map(data.map(item => [item.displayName, item])).values()]
+        // let setObject = new Set(data);
+        this.model = data;
+
+        this.refreshListCount(data);
+        this.changeDetector.markForCheck();
+      })
     );
   };
 }
--- admin/src/main/ts/src/app/groups/details/manage-users/output/group-output-users/group-output-users.component.ts
+++ admin/src/main/ts/src/app/groups/details/manage-users/output/group-output-users/group-output-users.component.ts
@@ -1,50 +1,61 @@
-import { Component, EventEmitter, Injector, Input, Output } from '@angular/core';
-import { OdeComponent } from 'ngx-ode-core';
-import { UserListService } from 'src/app/core/services/userlist.service';
-import { UserModel } from '../../../../../core/store/models/user.model';
+import {
+  Component,
+  EventEmitter,
+  Injector,
+  Input,
+  Output,
+} from "@angular/core";
+import { OdeComponent } from "ngx-ode-core";
+import { UserListService } from "src/app/core/services/userlist.service";
+import { UserModel } from "../../../../../core/store/models/user.model";
 
 @Component({
-    selector: 'ode-group-output-users',
-    templateUrl: './group-output-users.component.html',
-    styleUrls: ['./group-output-users.component.scss'],
-    providers: [ UserListService ]
+  selector: "ode-group-output-users",
+  templateUrl: "./group-output-users.component.html",
+  styleUrls: ["./group-output-users.component.scss"],
+  providers: [UserListService],
 })
 export class GroupOutputUsersComponent extends OdeComponent {
-    @Input() model: UserModel[] = [];
-    @Output() selectUsers: EventEmitter<UserModel[]> = new EventEmitter();
+  @Input() model: UserModel[] = [];
+  @Output() selectUsers: EventEmitter<UserModel[]> = new EventEmitter();
 
-    // list elements stored by store pipe in list component
-    // (takes filters in consideration)
-    storedElements: UserModel[] = [];
+  // list elements stored by store pipe in list component
+  // (takes filters in consideration)
+  storedElements: UserModel[] = [];
 
-    // Users selected by enduser
-    selectedUsers: UserModel[] = [];
+  // Users selected by enduser
+  selectedUsers: UserModel[] = [];
 
-    constructor(injector: Injector,
-                public userLS: UserListService) {
-        super(injector);
+  allUsersChecked:boolean = false;
+
+  constructor(injector: Injector, public userLS: UserListService) {
+    super(injector);
+  }
+
+  selectUser(u): void {
+    if (this.selectedUsers.indexOf(u) === -1) {
+      this.selectedUsers.push(u);
+    } else {
+      this.selectedUsers = this.selectedUsers.filter(su => su.id !== u.id);
     }
+    this.selectUsers.emit(this.selectedUsers);
+  }
 
-    selectUser(u): void {
-        if (this.selectedUsers.indexOf(u) === -1) {
-            this.selectedUsers.push(u);
-        } else {
-            this.selectedUsers = this.selectedUsers.filter(su => su.id !== u.id);
-        }
-        this.selectUsers.emit(this.selectedUsers);
-    }
+  isSelected = (user: UserModel) => {
+    return this.selectedUsers.indexOf(user) > -1;
+  };
 
-    isSelected = (user: UserModel) => {
-        return this.selectedUsers.indexOf(user) > -1;
-    }
+  toggleSelectedUsers(): void {
+    const selectedUsersLength = this.selectedUsers.length;
+    const storedElementsLength = this.storedElements.length;
 
-    selectAll(): void {
-        this.selectedUsers = this.storedElements;
-        this.selectUsers.emit(this.selectedUsers);
+    if (selectedUsersLength !== storedElementsLength) {
+      this.selectedUsers = this.storedElements;
+      this.allUsersChecked = true;
+    } else {
+      this.selectedUsers = [];
+      this.allUsersChecked = false;
     }
-
-    deselectAll(): void {
-        this.selectedUsers = [];
-        this.selectUsers.emit(this.selectedUsers);
-    }
+    this.selectUsers.emit(this.selectedUsers);
+  }
 }
