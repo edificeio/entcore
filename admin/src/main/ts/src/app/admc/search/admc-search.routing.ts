9c12a5d402e5df27a8efaee695a919e2bef18cf7
david-cc <david.caudeli@web-education.net>
2022-02-28 19:26:22 +0100

    feat(adminv2): #WB-76, open user details in admc transverse search

--- admin/src/main/ts/src/app/admc/search/admc-search.routing.ts
+++ admin/src/main/ts/src/app/admc/search/admc-search.routing.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { ConfigResolver } from 'src/app/core/resolvers/config.resolver';
 import { SimpleUserDetailsComponent } from 'src/app/users/simple-details/simple-user-details.component';
-import { UserDetailsResolver as userDetailsResolverFromUsersModule } from "src/app/users/details/user-details.resolver";
 import { AdmcSearchComponent } from './admc-search.component';
 import { AdmcSearchTransverseComponent } from './transverse/admc-search-transverse.component';
 import { AdmcSearchUnlinkedComponent } from './unlinked/admc-search-unlinked.component';
 import { AdmcSearchUnlinkedResolver } from './unlinked/admc-search-unlinked.resolver';
 import { UnlinkedUserDetailsComponent } from './unlinked/details/user-details.component';
 import { UserDetailsResolver } from './unlinked/details/user-details.resolver';
+import { UserDetailsResolver as userDetailsResolverFromUsersModule } from "src/app/users/details/user-details.resolver";
 
 export let routes: Routes = [
     {
