6d622f8bda38a594a16cef921fe883eb8731859f
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2022-06-09 10:26:03 +0200

    feat(auth, commen): #WB-34, Use attribute for validation terms

--- common/src/main/java/org/entcore/common/http/filter/TermsRevalidationFilter.java
+++ common/src/main/java/org/entcore/common/http/filter/TermsRevalidationFilter.java
@@ -5,8 +5,8 @@ import fr.wseduc.webutils.request.filter.Filter;
 import io.vertx.core.Handler;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpServerRequest;
-import org.entcore.common.user.UserInfos;
 import org.entcore.common.user.UserUtils;
+import static org.entcore.common.user.SessionAttributes.*;
 
 import java.util.Map;
 
@@ -23,14 +23,16 @@ public class TermsRevalidationFilter implements Filter {
                 && !request.headers().contains("Authorization")) {
             UserUtils.getUserInfos(this.eventBus, request, userInfos -> {
                 if (userInfos != null) {
-                    Object needRevalidateTermsFromSession = userInfos.getAttribute("needRevalidateTerms");
+                    //check whether user has validate terms in current session
+                    final Object needRevalidateTermsFromSession = userInfos.getAttribute(NEED_REVALIDATE_TERMS);
                     if (needRevalidateTermsFromSession != null) {
-                        Boolean needRevalidateTerms = Boolean.valueOf((String) needRevalidateTermsFromSession);
+                        final Boolean needRevalidateTerms = Boolean.valueOf(needRevalidateTermsFromSession.toString());
                         handler.handle(!needRevalidateTerms);
                     } else {
-                        Map<String, Object> otherProperties = userInfos.getOtherProperties();
-                        if (otherProperties != null && otherProperties.get("needRevalidateTerms") != null) {
-                            handler.handle(!(Boolean) otherProperties.get("needRevalidateTerms"));
+                        //check whether he has validated previously
+                        final Map<String, Object> otherProperties = userInfos.getOtherProperties();
+                        if (otherProperties != null && otherProperties.get(NEED_REVALIDATE_TERMS) != null) {
+                            handler.handle(!(Boolean) otherProperties.get(NEED_REVALIDATE_TERMS));
                         } else {
                             handler.handle(false);
                         }
