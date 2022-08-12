8339b54bfc07597e7d8133f8d263f7ef2e150202
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2022-08-12 10:37:08 +0200

    feat(communication): #WB-644, unit tests restored and completed with some tests on BroadcastGroup

--- communication/src/test/java/org/entcore/communication/services/impl/DefaultCommunicationServiceTest.java
+++ communication/src/test/java/org/entcore/communication/services/impl/DefaultCommunicationServiceTest.java
@@ -1,19 +1,49 @@
 package org.entcore.communication.services.impl;
 
+import org.entcore.common.user.UserInfos;
 import org.entcore.communication.services.CommunicationService;
+import org.entcore.communication.services.CommunicationService.Direction;
+import org.entcore.test.TestHelper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import fr.wseduc.webutils.collections.JsonObject;
+import io.vertx.core.Future;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RunWith(VertxUnitRunner.class)
 public class DefaultCommunicationServiceTest {
+    protected static final TestHelper test = TestHelper.helper();
     DefaultCommunicationService service;
+    private UserInfos userNoAdmin;
+
+    private io.vertx.core.json.JsonObject mockGroupWithDirection( String id, String type, String subType, CommunicationService.Direction direction ) {
+        return new io.vertx.core.json.JsonObject()
+            .put("id", id).put("internalCommunicationRule", direction.name())
+            .put("type", type).put("subType", subType).put("filter", (String) null);
+    }
+    private io.vertx.core.json.JsonObject mockManualGroupWithDirection( String id, CommunicationService.Direction direction ) {
+        return mockGroupWithDirection(id, "ManualGroup", null, direction);
+    }
+    private io.vertx.core.json.JsonObject mockBroadcastGroupWithDirection( String id, CommunicationService.Direction direction ) {
+        return mockGroupWithDirection(id, "ManualGroup", "BroadcastGroup", direction);
+    }
+    private UserInfos mockUserNoAdmin() {
+        return this.userNoAdmin;
+    }
 
     @Before
     public void prepare() {
         this.service = new DefaultCommunicationService();
+        this.userNoAdmin = test.directory().generateUser("notused");
+        this.userNoAdmin.setFunctions(new HashMap<String, UserInfos.Function>(0));
     }
 
     @Test
@@ -65,151 +95,152 @@ public class DefaultCommunicationServiceTest {
     }
 
     //-----------------------------------------------------------------------------------------------------
-    // NOTE JCBE 2022-02-01 : #WB-115, the new "broadcast list" feature has made the tests below OBSOLETE.
-    // These are more functional than technical tests. They cannot be THAT SIMPLE anymore.
-    //-----------------------------------------------------------------------------------------------------
+    private void computeWarningMessageForAddLinkCheckManualGroups(
+        TestContext context, Direction fromDir, Direction toDir, String expected
+        ) {
+        final Async later = context.async();
+        io.vertx.core.json.JsonObject from = mockManualGroupWithDirection( "1", fromDir );
+        io.vertx.core.json.JsonObject to   = mockManualGroupWithDirection( "2", toDir );
+        service.computeWarningMessageForAddLinkCheck( mockUserNoAdmin(), 
+            from, service.computeDirectionForAddLinkCheck(from, true),
+			to,   service.computeDirectionForAddLinkCheck(to, false))
+        .onSuccess( msg -> { Assert.assertEquals(expected, msg); later.complete(); } )
+        .onFailure( err -> { Assert.fail(err.getMessage()); context.fail(); } );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderNoneAndReceiverNone() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.NONE, CommunicationService.Direction.NONE));
-    // }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderBothAndReceiverBoth() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.BOTH, CommunicationService.Direction.BOTH));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderNoneAndReceiverNone(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.NONE, Direction.NONE, null );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderBothAndReceiverOutgoing() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.BOTH, CommunicationService.Direction.OUTGOING));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderBothAndReceiverBoth(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.BOTH, Direction.BOTH, null );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnEndgroupUsersCanCommunicate_GivenSenderBothAndReceiverIncoming() {
-    //     Assert.assertEquals(CommunicationService.WARNING_ENDGROUP_USERS_CAN_COMMUNICATE,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.BOTH, CommunicationService.Direction.INCOMING));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderBothAndReceiverOutgoing(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.BOTH, Direction.OUTGOING, null );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderBothAndReceiverNone() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.BOTH, CommunicationService.Direction.NONE));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnEndgroupUsersCanCommunicate_GivenSenderBothAndReceiverIncoming(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.BOTH, Direction.INCOMING, CommunicationService.WARNING_ENDGROUP_USERS_CAN_COMMUNICATE );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnStartgroupUsersCanCommunicate_GivenSenderOutgoingAndReceiverNone() {
-    //     Assert.assertEquals(CommunicationService.WARNING_STARTGROUP_USERS_CAN_COMMUNICATE,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.OUTGOING, CommunicationService.Direction.NONE));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderBothAndReceiverNone(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.BOTH, Direction.NONE, null );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnStartgroupUsersCanCommunicate_GivenSenderOutgoingAndReceiverBoth() {
-    //     Assert.assertEquals(CommunicationService.WARNING_STARTGROUP_USERS_CAN_COMMUNICATE,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.OUTGOING, CommunicationService.Direction.BOTH));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnStartgroupUsersCanCommunicate_GivenSenderOutgoingAndReceiverNone(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.OUTGOING, Direction.NONE, CommunicationService.WARNING_STARTGROUP_USERS_CAN_COMMUNICATE );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnStartgroupUsersCanCommunicate_GivenSenderOutgoingAndReceiverOutgoing() {
-    //     Assert.assertEquals(CommunicationService.WARNING_STARTGROUP_USERS_CAN_COMMUNICATE,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.OUTGOING, CommunicationService.Direction.OUTGOING));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnStartgroupUsersCanCommunicate_GivenSenderOutgoingAndReceiverBoth(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.OUTGOING, Direction.BOTH, CommunicationService.WARNING_STARTGROUP_USERS_CAN_COMMUNICATE );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnBothgroupUsersCanCommunicate_GivenSenderOutgoingAndReceiverIncoming() {
-    //     Assert.assertEquals(CommunicationService.WARNING_BOTH_GROUPS_USERS_CAN_COMMUNICATE,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.OUTGOING, CommunicationService.Direction.INCOMING));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnStartgroupUsersCanCommunicate_GivenSenderOutgoingAndReceiverOutgoing(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.OUTGOING, Direction.OUTGOING, CommunicationService.WARNING_STARTGROUP_USERS_CAN_COMMUNICATE );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderIncomingAndReceiverNone() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.INCOMING, CommunicationService.Direction.NONE));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnBothgroupUsersCanCommunicate_GivenSenderOutgoingAndReceiverIncoming(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.OUTGOING, Direction.INCOMING, CommunicationService.WARNING_BOTH_GROUPS_USERS_CAN_COMMUNICATE );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderIncomingAndReceiverBoth() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.INCOMING, CommunicationService.Direction.BOTH));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderIncomingAndReceiverNone(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.INCOMING, Direction.NONE, null );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderIncomingAndReceiverOutgoing() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.INCOMING, CommunicationService.Direction.OUTGOING));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderIncomingAndReceiverBoth(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.INCOMING, Direction.BOTH, null );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnEndgroupUsersCanCommunicate_GivenSenderIncomingAndReceiverIncoming() {
-    //     Assert.assertEquals(CommunicationService.WARNING_ENDGROUP_USERS_CAN_COMMUNICATE,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.INCOMING, CommunicationService.Direction.INCOMING));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderIncomingAndReceiverOutgoing(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.INCOMING, Direction.OUTGOING, null );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderNoneAndReceiverOutgoing() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.NONE, CommunicationService.Direction.OUTGOING));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnEndgroupUsersCanCommunicate_GivenSenderIncomingAndReceiverIncoming(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.INCOMING, Direction.INCOMING, CommunicationService.WARNING_ENDGROUP_USERS_CAN_COMMUNICATE );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnEndgroupUsersCanCommunicate_GivenSenderNoneAndReceiverIncoming() {
-    //     Assert.assertEquals(CommunicationService.WARNING_ENDGROUP_USERS_CAN_COMMUNICATE,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.NONE, CommunicationService.Direction.INCOMING));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderNoneAndReceiverOutgoing(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.NONE, Direction.OUTGOING, null );
+    }
 
-    // @Test
-    // public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderNoneAndReceiverBoth() {
-    //     Assert.assertEquals(null,
-    //             this.service.computeWarningMessageForAddLinkCheck(CommunicationService.Direction.NONE, CommunicationService.Direction.BOTH));
-    // }
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnEndgroupUsersCanCommunicate_GivenSenderNoneAndReceiverIncoming(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.NONE, Direction.INCOMING, CommunicationService.WARNING_ENDGROUP_USERS_CAN_COMMUNICATE );
+    }
 
-    // @Test
-    // public void computeNewDirectionAfterAddingLink_shouldReturnSenderIncomingAndReceiverOutgoing_GivenSenderNoneAndReceiverNone() {
-    //     Map<String, CommunicationService.Direction> expectedMap = new HashMap<>();
-    //     expectedMap.put("1", CommunicationService.Direction.INCOMING);
-    //     expectedMap.put("2", CommunicationService.Direction.OUTGOING);
+    @Test
+    public void computeWarningMessageForAddLinkCheck_shouldReturnNull_GivenSenderNoneAndReceiverBoth(TestContext context) {
+        computeWarningMessageForAddLinkCheckManualGroups( context, Direction.NONE, Direction.BOTH, null );
+    }
 
-    //     Assert.assertEquals(expectedMap, this.service.computeNewDirectionAfterAddingLink("1"
-    //             , CommunicationService.Direction.NONE
-    //             , "2"
-    //             , CommunicationService.Direction.NONE));
-    // }
 
-    // @Test
-    // public void computeNewDirectionAfterAddingLink_shouldReturnSenderIncomingAndReceiverBoth_GivenSenderNoneAndReceiverIncoming() {
-    //     Map<String, CommunicationService.Direction> expectedMap = new HashMap<>();
-    //     expectedMap.put("1", CommunicationService.Direction.INCOMING);
-    //     expectedMap.put("2", CommunicationService.Direction.BOTH);
+    @Test
+    public void computeNewDirectionAfterAddingLink_shouldReturnSenderIncomingAndReceiverOutgoing_GivenSenderNoneAndReceiverNone() {
+        io.vertx.core.json.JsonObject from = mockManualGroupWithDirection( "1", Direction.NONE );
+        io.vertx.core.json.JsonObject to   = mockManualGroupWithDirection( "2", Direction.NONE );
 
-    //     Assert.assertEquals(expectedMap, this.service.computeNewDirectionAfterAddingLink("1"
-    //             , CommunicationService.Direction.NONE
-    //             , "2"
-    //             , CommunicationService.Direction.INCOMING));
-    // }
+        Assert.assertEquals(Direction.INCOMING, this.service.computeDirectionForAddLinkCheck(from, true));
+        Assert.assertEquals(Direction.OUTGOING, this.service.computeDirectionForAddLinkCheck(to,   false));
+    }
 
-    // @Test
-    // public void computeNewDirectionAfterAddingLink_shouldReturnSenderBothAndReceiverOutgoing_GivenSenderOutgoingAndReceiverNone() {
-    //     Map<String, CommunicationService.Direction> expectedMap = new HashMap<>();
-    //     expectedMap.put("1", CommunicationService.Direction.BOTH);
-    //     expectedMap.put("2", CommunicationService.Direction.OUTGOING);
+    @Test
+    public void computeNewDirectionAfterAddingLink_shouldReturnSenderIncomingAndReceiverBoth_GivenSenderNoneAndReceiverIncoming() {
+        io.vertx.core.json.JsonObject from = mockManualGroupWithDirection( "1", Direction.NONE );
+        io.vertx.core.json.JsonObject to   = mockManualGroupWithDirection( "2", Direction.INCOMING );
 
-    //     Assert.assertEquals(expectedMap, this.service.computeNewDirectionAfterAddingLink("1"
-    //             , CommunicationService.Direction.OUTGOING
-    //             , "2"
-    //             , CommunicationService.Direction.NONE));
-    // }
+        Assert.assertEquals(Direction.INCOMING, this.service.computeDirectionForAddLinkCheck(from, true));
+        Assert.assertEquals(Direction.BOTH,     this.service.computeDirectionForAddLinkCheck(to,   false));
+    }
 
-    // @Test
-    // public void computeNewDirectionAfterAddingLink_shouldReturnSenderBothAndReceiverBoth_GivenSenderOutgoingAndReceiverIncoming() {
-    //     Map<String, CommunicationService.Direction> expectedMap = new HashMap<>();
-    //     expectedMap.put("1", CommunicationService.Direction.BOTH);
-    //     expectedMap.put("2", CommunicationService.Direction.BOTH);
+    @Test
+    public void computeNewDirectionAfterAddingLink_shouldReturnSenderBothAndReceiverOutgoing_GivenSenderOutgoingAndReceiverNone() {
+        io.vertx.core.json.JsonObject from = mockManualGroupWithDirection( "1", Direction.OUTGOING );
+        io.vertx.core.json.JsonObject to   = mockManualGroupWithDirection( "2", Direction.NONE );
 
-    //     Assert.assertEquals(expectedMap, this.service.computeNewDirectionAfterAddingLink("1"
-    //             , CommunicationService.Direction.OUTGOING
-    //             , "2"
-    //             , CommunicationService.Direction.INCOMING));
-    // }
+        Assert.assertEquals(Direction.BOTH,     this.service.computeDirectionForAddLinkCheck(from, true));
+        Assert.assertEquals(Direction.OUTGOING, this.service.computeDirectionForAddLinkCheck(to,   false));
+    }
+
+    @Test
+    public void computeNewDirectionAfterAddingLink_shouldReturnSenderBothAndReceiverBoth_GivenSenderOutgoingAndReceiverIncoming() {
+        io.vertx.core.json.JsonObject from = mockManualGroupWithDirection( "1", Direction.OUTGOING );
+        io.vertx.core.json.JsonObject to   = mockManualGroupWithDirection( "2", Direction.INCOMING );
+
+        Assert.assertEquals(Direction.BOTH, this.service.computeDirectionForAddLinkCheck(from, true));
+        Assert.assertEquals(Direction.BOTH, this.service.computeDirectionForAddLinkCheck(to,   false));
+    }
+
+    @Test
+    public void computeNewDirectionAfterAddingLink_shouldReturnSenderIncomingAndReceiverNone_GivenSenderNoneAndBroadcastReceiverNone() {
+        io.vertx.core.json.JsonObject from = mockManualGroupWithDirection( "1", Direction.NONE );
+        io.vertx.core.json.JsonObject to   = mockBroadcastGroupWithDirection( "2", Direction.NONE );
+
+        Assert.assertEquals(Direction.INCOMING, this.service.computeDirectionForAddLinkCheck(from, true));
+        Assert.assertEquals(Direction.NONE,     this.service.computeDirectionForAddLinkCheck(to,   false));
+    }
+
+    @Test
+    public void computeNewDirectionAfterAddingLink_shouldReturnSenderBothAndReceiverNone_GivenSenderOutgoingAndBroadcastReceiverNone() {
+        io.vertx.core.json.JsonObject from = mockManualGroupWithDirection( "1", Direction.OUTGOING );
+        io.vertx.core.json.JsonObject to   = mockBroadcastGroupWithDirection( "2", Direction.NONE );
+
+        Assert.assertEquals(Direction.BOTH, this.service.computeDirectionForAddLinkCheck(from, true));
+        Assert.assertEquals(Direction.NONE, this.service.computeDirectionForAddLinkCheck(to,   false));
+    }
 }
