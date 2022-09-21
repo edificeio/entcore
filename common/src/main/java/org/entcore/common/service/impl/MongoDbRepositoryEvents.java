4443ec5b3a28c107f0a85a3fc806eaceb7fd4c78
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2022-09-21 20:45:35 +0200

    fix(archive): Fix an NPE in documents exports

--- common/src/main/java/org/entcore/common/service/impl/MongoDbRepositoryEvents.java
+++ common/src/main/java/org/entcore/common/service/impl/MongoDbRepositoryEvents.java
@@ -312,11 +312,11 @@ public class MongoDbRepositoryEvents extends AbstractRepositoryEvents {
 				{
 					JsonArray results = event.body().getJsonArray("results");
 
-					for(int i = results.size(); i-->0;)
-						DocumentHelper.clearComments(results.getJsonObject(i), true);
-
 					if ("ok".equals(event.body().getString("status")) && results != null)
 					{
+						for(int i = results.size(); i-->0;)
+							DocumentHelper.clearComments(results.getJsonObject(i), true);
+
 						createExportDirectory(exportPath, locale, new Handler<String>()
 						{
 							@Override
