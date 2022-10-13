950239d8a141ccc2044683f0a416c7d447e74c6b
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2022-10-13 13:24:26 +0200

    feat(feeder): #15556, Add result handlers to queries sent through the TransactionHelper

--- feeder/src/main/java/org/entcore/feeder/utils/TransactionHelper.java
+++ feeder/src/main/java/org/entcore/feeder/utils/TransactionHelper.java
@@ -27,15 +27,19 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
+import fr.wseduc.webutils.Either;
+
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TransactionHelper {
 
 	private static final Logger log = LoggerFactory.getLogger(TransactionHelper.class);
 	private final Neo4j neo4j;
-	private volatile JsonArray statements;
+	private volatile List<Statement> statements;
 	private AtomicInteger remainingStatementNumber;
 	private final int statementNumber;
 	private Integer transactionId;
@@ -62,6 +66,48 @@ public class TransactionHelper {
 
 	}
 
+	static class Statement
+	{
+		public String query;
+		public JsonObject params;
+		public Handler<Either<String, JsonArray>> resultHandler;
+
+		public Statement(String query, JsonObject params, Handler<Either<String, JsonArray>> resultHandler)
+		{
+			this.query = query;
+			this.params = params;
+			this.resultHandler = resultHandler;
+		}
+
+		public JsonObject toJsonObject()
+		{
+			JsonObject o = new JsonObject().put("statement", this.query);
+			if(this.params != null)
+				o.put("parameters", this.params);
+			return o;
+		}
+
+		public void handleResult(JsonArray result)
+		{
+			if(this.resultHandler != null)
+				this.resultHandler.handle(new Either.Right<String, JsonArray>(result));
+		}
+
+		public void handleError(String error)
+		{
+			if(this.resultHandler != null)
+				this.resultHandler.handle(new Either.Left<String, JsonArray>(error));
+		}
+
+		public static JsonArray toNeoStatements(List<Statement> statements)
+		{
+			JsonArray neoStatements = new JsonArray();
+			for(int i = 0; i < statements.size(); i++)
+				neoStatements.add(statements.get(i).toJsonObject());
+			return neoStatements;
+		}
+	}
+
 	public TransactionHelper(Neo4j neo4j, Integer transactionId) {
 		this(neo4j, 1000, transactionId);
 	}
@@ -78,18 +124,21 @@ public class TransactionHelper {
 		this.neo4j = neo4j;
 		this.remainingStatementNumber = new AtomicInteger(statementNumber);
 		this.statementNumber = statementNumber;
-		this.statements = new fr.wseduc.webutils.collections.JsonArray();
+		this.statements = new ArrayList<Statement>();
 		this.transactionId = transactionId;
 		if (this.transactionId == null) {
-			send(new fr.wseduc.webutils.collections.JsonArray());
+			send(new ArrayList<Statement>());
 		}
 	}
 
 	public void add(String query, JsonObject params) {
+		this.add(query, params, null);
+	}
+	public void add(String query, JsonObject params, Handler<Either<String, JsonArray>> resultHandler) {
 		if (autoSend && !waitingQuery && transactionId != null &&
 				remainingStatementNumber.getAndDecrement() == 0) {
-			final JsonArray s = statements;
-			statements = new fr.wseduc.webutils.collections.JsonArray();
+			final List<Statement> s = statements;
+			this.statements = new ArrayList<Statement>();
 			send(s);
 			remainingStatementNumber = new AtomicInteger(statementNumber);
 		}
@@ -97,24 +146,20 @@ public class TransactionHelper {
 			if (log.isDebugEnabled()) {
 				log.debug("query : " + query + " - params : " + (params != null ? params.encode() : "{}"));
 			}
-			JsonObject statement = new JsonObject().put("statement", query);
-			if (params != null) {
-				statement.put("parameters", params);
-			}
-			statements.add(statement);
+			statements.add(new Statement(query, params, resultHandler));
 		}
 	}
 
-	private void send(JsonArray s) {
+	private void send(List<Statement> s) {
 		send(s, null);
 	}
 
-	private void send(JsonArray s, final Handler<Message<JsonObject>> handler) {
+	private void send(List<Statement> s, final Handler<Message<JsonObject>> handler) {
 		if (error != null) {
 			throw new IllegalStateException(error.body().getString("message"));
 		}
 		waitingQuery = true;
-		neo4j.executeTransaction(s, transactionId, false, new Handler<Message<JsonObject>>() {
+		neo4j.executeTransaction(Statement.toNeoStatements(statements), transactionId, false, new Handler<Message<JsonObject>>() {
 			@Override
 			public void handle(Message<JsonObject> message) {
 				if (handler != null) {
@@ -160,8 +205,32 @@ public class TransactionHelper {
 			commitHandler = handler;
 			return;
 		}
-		if (transactionId != null || statements.size() > 0) {
-			neo4j.executeTransaction(statements, transactionId, true, handler);
+		if (transactionId != null || statements.size() > 0)
+		{
+			List<Statement> commitStatements = statements;
+			neo4j.executeTransaction(Statement.toNeoStatements(statements), transactionId, true, new Handler<Message<JsonObject>>()
+			{
+				@Override
+				public void handle(Message<JsonObject> msg)
+				{
+					JsonObject body = msg.body();
+
+					if("ok".equals(body.getString("status")))
+					{
+						JsonArray results = body.getJsonArray("results");
+						for(int i = 0; i < commitStatements.size(); i++)
+							commitStatements.get(i).handleResult(results.getJsonArray(i));
+					}
+					else
+					{
+						String error = body.getString("message", "");
+						for(int i = 0; i < commitStatements.size(); i++)
+							commitStatements.get(i).handleError(error);
+					}
+
+					handler.handle(msg);
+				}
+			});
 			if (transactionId != null) {
 				resetTimeOutTimer.cancel();
 				resetTimeOutTimer.purge();
@@ -197,8 +266,8 @@ public class TransactionHelper {
 			flush = true;
 			flushHandler = handler;
 		} else if (transactionId != null) {
-			final JsonArray s = statements;
-			statements = new fr.wseduc.webutils.collections.JsonArray();
+			final List<Statement> s = statements;
+			statements = new ArrayList<Statement>();
 			send(s, handler);
 			remainingStatementNumber = new AtomicInteger(statementNumber);
 		}
