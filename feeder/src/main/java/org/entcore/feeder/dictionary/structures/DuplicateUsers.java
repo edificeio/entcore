f8097432546b73357a4ce5bd3868b143075c4676
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2022-10-13 13:20:47 +0200

    chore(feeder): #15556, Make the duplicate logic clearer

--- feeder/src/main/java/org/entcore/feeder/dictionary/structures/DuplicateUsers.java
+++ feeder/src/main/java/org/entcore/feeder/dictionary/structures/DuplicateUsers.java
@@ -713,10 +713,17 @@ public class DuplicateUsers {
 			score += exactMatch(firstName, cleanAttribute(fu.getString("firstName")));
 			score += exactMatch(birthDate, cleanAttribute(fu.getString("birthDate")));
 			score += exactMatch(email, cleanAttribute(fu.getString("email")));
-			if (score > 3 && ((!source.equals(fu.getString("source")) &&
-					(notDeduplicateSource.contains(source) ^ notDeduplicateSource.contains(fu.getString("source")))) ||
-					disappearanceDate != null || fu.getLong("disappearanceDate") != null)) {
-				tx.add(query, params.copy().put("dId", fu.getString("id")).put("score", score));
+
+			if(score > 3)
+			{
+				boolean isSameSource = source.equals(fu.getString("source"));
+				boolean compatibleSources = notDeduplicateSource.contains(source) ^ notDeduplicateSource.contains(fu.getString("source"));
+				boolean oneIsDisappearing = disappearanceDate != null || fu.getLong("disappearanceDate") != null;
+
+				if(oneIsDisappearing || (!isSameSource && compatibleSources))
+				{
+					tx.add(query, params.copy().put("dId", fu.getString("id")).put("score", score));
+				}
 			}
 		}
 	}
