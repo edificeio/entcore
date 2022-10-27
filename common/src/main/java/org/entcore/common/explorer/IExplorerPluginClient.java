914da8a90451a163747661826ce6c74bbf427200
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2022-10-27 10:54:30 +0200

    feat: #WB-1032, explorer share plugin

--- common/src/main/java/org/entcore/common/explorer/IExplorerPluginClient.java
+++ common/src/main/java/org/entcore/common/explorer/IExplorerPluginClient.java
@@ -2,6 +2,7 @@ package org.entcore.common.explorer;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.entcore.common.explorer.impl.ExplorerPluginClient;
 import org.entcore.common.explorer.impl.ExplorerPluginClientDefault;
@@ -24,11 +25,28 @@ public interface IExplorerPluginClient {
 
     Future<DeleteResponse> deleteById(UserInfos user, Set<String> ids);
 
+    default Future<ShareResponse> shareById(UserInfos user, String id, JsonObject shares){
+        final Set<String> ids = new HashSet<>();
+        ids.add(id);
+        return shareByIds(user, ids, shares);
+    }
+
+    Future<ShareResponse> shareByIds(UserInfos user, Set<String> ids, JsonObject shares);
+
     Future<JsonObject> getMetrics(UserInfos user);
 
     class DeleteResponse {
         public final List<String> deleted = new ArrayList<>();
         public final List<String> notDeleted = new ArrayList<>();
+    }
+    class ShareResponse {
+        public final int nbShared;
+        public final Map<String, JsonArray> notifyTimelineMap;
+
+        public ShareResponse(int nbShared, JsonObject notifyTimelineMap) {
+            this.notifyTimelineMap = (Map<String, JsonArray>)(Object)notifyTimelineMap.getMap();
+            this.nbShared = nbShared;
+        }
     }
 
     class IndexResponse {
