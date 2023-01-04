3afc2945a66ffc932591c85024b8dd9c3154cac8
dboissin <damien.boissin@web-education.net>
2023-01-04 18:44:08 +0100

    fix(common): #59115 change pg client for email and remove transaction when useless

--- common/src/main/java/org/entcore/common/email/impl/PostgresEmailHelperDefault.java
+++ common/src/main/java/org/entcore/common/email/impl/PostgresEmailHelperDefault.java
@@ -1,14 +1,22 @@
 package org.entcore.common.email.impl;
 
-import io.reactiverse.pgclient.*;
-import io.reactiverse.pgclient.data.Json;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.pgclient.PgConnectOptions;
+import io.vertx.pgclient.PgPool;
+import io.vertx.pgclient.SslMode;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Transaction;
+import io.vertx.sqlclient.Tuple;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -16,32 +24,37 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import fr.wseduc.mongodb.MongoDb;
+
 public class PostgresEmailHelperDefault implements PostgresEmailHelper {
     static Logger log = LoggerFactory.getLogger(org.entcore.common.email.impl.PostgresEmailHelperDefault.class);
     final PgPool pool;
     final String tableName;
     final String readTableName;
     final String attachementTableName;
+    final MongoDb mongoDb;
 
     public PostgresEmailHelperDefault(Vertx vertx, JsonObject pgConfig) {
         final SslMode sslMode = SslMode.valueOf(pgConfig.getString("ssl-mode", "DISABLE"));
-        final PgPoolOptions options = new PgPoolOptions()
+        final PgConnectOptions options = new PgConnectOptions()
                 .setPort(pgConfig.getInteger("port", 5432))
                 .setHost(pgConfig.getString("host"))
                 .setDatabase(pgConfig.getString("database"))
                 .setUser(pgConfig.getString("user"))
                 .setPassword(pgConfig.getString("password"))
-                .setIdleTimeout(pgConfig.getInteger("idle-timeout", 300)) // unit seconds
+                .setIdleTimeout(pgConfig.getInteger("idle-timeout", 1800)); // unit seconds
+        final PoolOptions poolOptions = new PoolOptions()
                 .setMaxSize(pgConfig.getInteger("pool-size", 5));
         if (!SslMode.DISABLE.equals(sslMode)) {
             options
                 .setSslMode(sslMode)
                 .setTrustAll(SslMode.ALLOW.equals(sslMode) || SslMode.PREFER.equals(sslMode) || SslMode.REQUIRE.equals(sslMode));
         }
-        this.pool = PgClient.pool(vertx, options);
+        this.pool = PgPool.pool(vertx, options, poolOptions);
         this.tableName = pgConfig.getString("tablename", "mail.mail_events");
         this.attachementTableName = pgConfig.getString("attachment-tablename", "mail.attachments_events");
         this.readTableName = pgConfig.getString("read-tablename", "mail.read_events");
+        this.mongoDb = pgConfig.getBoolean("log-emails", false) ? MongoDb.getInstance() : null;
     }
 
     private static LocalDateTime getDate(final JsonObject extraParams){
@@ -77,7 +90,7 @@ public class PostgresEmailHelperDefault implements PostgresEmailHelper {
         query.append("INSERT INTO ").append(readTableName);
         query.append("(id, date, read_at").append(separator).append(String.join(",",names)).append(") ");
         query.append("VALUES ($1, $2, $3").append(separator).append(String.join(",",placeholders)).append(") RETURNING id");
-        this.pool.preparedQuery(query.toString(), tuple, r -> {
+        this.pool.preparedQuery(query.toString()).execute(tuple, r -> {
             if (r.succeeded()) {
                 promise.complete();
             } else {
@@ -88,41 +101,49 @@ public class PostgresEmailHelperDefault implements PostgresEmailHelper {
     }
 
     public Future<Void> createWithAttachments(PostgresEmailBuilder.EmailBuilder mailB, List<PostgresEmailBuilder.AttachmentBuilder> attachmentsB) {
-        final Future<Void> promise = Future.future();
         final Map<String, Object> mail = mailB.getMail();
-        pool.begin(resTx -> {
-            if (resTx.succeeded()) {
-                final List<Future> futures = new ArrayList<>();
-                futures.add(insert(resTx.result(), tableName, mail).setHandler(r -> {
-                    if (r.failed()) {
-                        log.error("Failed to create email: ", r.cause().getMessage());
-                    }
-                }));
-                for (final PostgresEmailBuilder.AttachmentBuilder attB : attachmentsB) {
-                    final Map<String, Object> att = attB.getAttachment();
-                    att.put("mail_id", mail.get("id"));
-                    futures.add(insert(resTx.result(), attachementTableName, att).setHandler(r -> {
+        if (this.mongoDb != null) {
+            this.mongoDb.insert("logemails", mailB.toJsonObject().put("logcreated", MongoDb.now()));
+        }
+
+        if (attachmentsB != null && !attachmentsB.isEmpty()) {
+            final Future<Void> promise = Future.future();
+            pool.begin(resTx -> {
+                if (resTx.succeeded()) {
+                    final List<Future> futures = new ArrayList<>();
+                    futures.add(insert(resTx.result(), tableName, mail).setHandler(r -> {
                         if (r.failed()) {
-                            log.error("Failed to create attachment: ", r.cause().getMessage());
+                            log.error("Failed to create email: ", r.cause().getMessage());
                         }
                     }));
-                }
-                CompositeFuture.all(futures).setHandler(res -> {
-                    if (res.succeeded()) {
-                        resTx.result().commit(promise);
-                    } else {
-                        resTx.result().rollback();
-                        promise.fail(res.cause());
+                    for (final PostgresEmailBuilder.AttachmentBuilder attB : attachmentsB) {
+                        final Map<String, Object> att = attB.getAttachment();
+                        att.put("mail_id", mail.get("id"));
+                        futures.add(insert(resTx.result(), attachementTableName, att).setHandler(r -> {
+                            if (r.failed()) {
+                                log.error("Failed to create attachment: ", r.cause().getMessage());
+                            }
+                        }));
                     }
-                });
-            } else {
-                promise.fail(resTx.cause());
-            }
-        });
-        return promise;
+                    CompositeFuture.all(futures).setHandler(res -> {
+                        if (res.succeeded()) {
+                            resTx.result().commit(promise);
+                        } else {
+                            resTx.result().rollback();
+                            promise.fail(res.cause());
+                        }
+                    });
+                } else {
+                    promise.fail(resTx.cause());
+                }
+            });
+            return promise;
+        } else {
+            return insert(null, tableName, mail);
+        }
     }
 
-    protected Future<Void> insert(PgTransaction transaction, String tableName, final Map<String, Object> params) {
+    protected Future<Void> insert(Transaction transaction, String tableName, final Map<String, Object> params) {
         final Future<Void> future = Future.future();
         final StringBuilder query = new StringBuilder();
         final StringBuilder values = new StringBuilder();
@@ -140,20 +161,26 @@ public class PostgresEmailHelperDefault implements PostgresEmailHelper {
             values.append(separator).append("$").append(index++);
             final Object value = params.get(key);
             if (value instanceof JsonObject || value instanceof JsonArray) {
-                tuple.addValue(Json.create(value));
+                tuple.addValue((value));
             } else {
                 tuple.addValue(value);
             }
             separator = ", ";
         }
         query.append(") VALUES (").append(values).append(") RETURNING id");
-        transaction.preparedQuery(query.toString(), tuple, r -> {
+        final Handler<AsyncResult<RowSet<Row>>> h = r -> {
             if (r.succeeded()) {
                 future.complete();
             } else {
                 future.fail(r.cause());
             }
-        });
+        };
+        if (transaction != null) {
+            transaction.preparedQuery(query.toString()).execute(tuple, h);
+        } else {
+            this.pool.preparedQuery(query.toString()).execute(tuple, h);
+        }
         return future;
     }
+
 }
