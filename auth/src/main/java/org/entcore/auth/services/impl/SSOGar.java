c95fa742e73fcb16e900a6db1e21cb8fc2de1bed
dbreyton <david.breyton@gmail.com>
2023-02-17 13:00:35 +0100

    feat(auth): implement the SaaS multi-tenant by id instead of by host in SSOGar

--- auth/src/main/java/org/entcore/auth/services/impl/SSOGar.java
+++ auth/src/main/java/org/entcore/auth/services/impl/SSOGar.java
@@ -2,57 +2,49 @@ package org.entcore.auth.services.impl;
 
 import fr.wseduc.webutils.Either;
 import io.vertx.core.Handler;
-import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.eventbus.Message;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.entcore.common.neo4j.Neo4j;
+import org.entcore.common.neo4j.Neo4jResult;
 import org.opensaml.saml2.core.Assertion;
-
-import java.util.List;
-
-import static fr.wseduc.webutils.Utils.handlerToAsyncHandler;
-import static fr.wseduc.webutils.http.response.DefaultResponseHandler.defaultResponseHandler;
 
 
 public class SSOGar extends AbstractSSOProvider {
+    private static final Logger log = LoggerFactory.getLogger(SSOGar.class);
 
     @Override
     public void generate(EventBus eb, String userId, String host, String serviceProviderEntityId, Handler<Either<String, JsonArray>> handler) {
+        String query = "match (s:Structure)-[:DEPENDS]-(:ProfileGroup)-[:IN]-(u:User {id:{userId}}) " +
+                "where has(s.exports) AND s.exports <> [] unwind s.exports as exp with exp where exp starts with 'GAR-' " +
+                "return distinct replace(exp, 'GAR-', '') as entId limit 1 ";
 
-        JsonObject sendTOMediacentre = new JsonObject().put("action", "getConfig");
+        Neo4j.getInstance().execute(query, new JsonObject().put("userId", userId),
+                Neo4jResult.validUniqueResultHandler(evt -> {
+                    if (evt.isLeft()) {
+                        handler.handle(new Either.Left(evt.left().getValue()));
+                        return;
+                    }
 
-
-        eb.send("openent.mediacentre", sendTOMediacentre, handlerToAsyncHandler(new Handler<Message<JsonObject>>() {
-            @Override
-            public void handle(Message<JsonObject> message) {
-                if (("error").equals(message.body().getString("status")) ) {
-                    handler.handle(new Either.Left<>(message.body().toString()));
-                } else {
-                    JsonObject configMediacentre = message.body().getJsonObject("message");
                     JsonArray jsonArrayResult = new JsonArray();
-
-                    if (configMediacentre.getValue("id-ent") instanceof String) {
-                        jsonArrayResult
-                                .add(new JsonObject().put("idEnt", configMediacentre.getString("id-ent")));
-                    } else {
-                        jsonArrayResult
-                                .add(new JsonObject().put("idEnt",
-                                        configMediacentre.getJsonObject("id-ent", new JsonObject()).getString(host)));
+                    JsonObject entIdJO = evt.right().getValue();
+                    //empty result
+                    if(!entIdJO.containsKey("entId")) {
+                        log.error("[SSOGar@generate] Empty GAR project Id for user : " + userId);
                     }
 
                     jsonArrayResult
+                            .add(new JsonObject().put("idEnt", entIdJO.getString("entId")));
+                    jsonArrayResult
                             .add(new JsonObject().put("GARPersonIdentifiant", userId));
                     handler.handle(new Either.Right<String, JsonArray>(jsonArrayResult));
-                }
-            }
-        }));
+                }));
     }
 
     @Override
     public void execute(Assertion assertion, Handler<Either<String, Object>> handler) {
         handler.handle(new Either.Left<String, Object>("execute function ot avalable on SSO Gar Implementation"));
     }
-
-
 }
