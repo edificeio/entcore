82a5dbf84ec7cd016534acd7f2b69f27f624c417
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2023-02-21 17:29:50 +0100

    feat(auth): #WB-1580, support international phone number input

--- auth/src/main/resources/public/ts/validate-mail/app.ts
+++ auth/src/main/resources/public/ts/validate-mail/app.ts
@@ -4,10 +4,12 @@ import { OdeModules, conf } from 'ode-ngjs-front';
 import { AppController } from "./controller";
 import * as App from './directives/validate-mail.directive';
 import * as NavbarLegacy from './directives/navbar-legacy.directive';
+import * as IntlPhoneInput from './directives/intl-phone-input.directive';
 
 angular.module("app", [OdeModules.getBase(), OdeModules.getI18n(), OdeModules.getUi()])
 .controller("appCtrl", ['$scope', AppController])
 .directive("validateMail", App.DirectiveFactory)
 .directive("navbarLegacy", NavbarLegacy.DirectiveFactory)
+.directive("intlPhoneInput", IntlPhoneInput.DirectiveFactory)
 ;
 conf().Platform.apps.initialize(APP.PORTAL, true);
