c502f9eb05e91b71e38abd3e02a48a10700ccc1b
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2023-03-02 15:31:34 +0100

    feat: #WB2-536, catch and log pg error

--- common/src/main/java/org/entcore/common/postgres/PostgresClientBusConsumer.java
+++ common/src/main/java/org/entcore/common/postgres/PostgresClientBusConsumer.java
@@ -8,7 +8,6 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import io.vertx.pgclient.pubsub.PgSubscriber;
 import io.vertx.sqlclient.*;
 
 import java.util.ArrayList;
@@ -32,75 +31,82 @@ public class PostgresClientBusConsumer implements IPostgresClient {
         this.pgClient = inner;
         this.consumer = vertx.eventBus().localConsumer(PostgresClientBusHelper.getAddress(suffix));
         this.consumer.handler(message -> {
-            final JsonObject payload = message.body();
-            if(isQuery(payload)){
-                final String query = jsonToQuery(payload);
-                final Tuple tuple = jsonToQueryTuple(payload);
-                this.preparedQuery(query, tuple).onComplete(res->{
-                    if(res.succeeded()){
-                        final JsonObject result = resultToJson(res.result());
-                        message.reply(result);
-                    }else{
-                        message.fail(500, res.cause().getMessage());
-                        log.error("Query failed: ", res.cause());
-                    }
-                });
-            }else if( isTransaction(payload)){
-                this.transaction().onComplete(res->{
-                    if(res.succeeded()){
-                        final IPostgresTransaction transaction = res.result();
-                        final JsonArray params = jsonToTransactionParams(payload);
-                        final List<JsonObject> results = new ArrayList<>(params.size());
-                        final List<Future> futureResults = new ArrayList<>();
-                        for(int i = 0 ; i < params.size(); i++){
-                            final JsonObject param = params.getJsonObject(i);
-                            if(isQuery(param)){
-                                final String query = jsonToQuery(param);
-                                final Tuple tuple = jsonToQueryTuple(param);
-                                final int index = i;
-                                futureResults.add(transaction.addPreparedQuery(query, tuple).onComplete(queryRes -> {
-                                    if(queryRes.succeeded()){
-                                        final JsonObject result = resultToJson(queryRes.result());
-                                        results.add(index, result);
-                                    }
-                                }));
-                            }else if(isNotify(param)){
-                                final String channel = jsonToNotifyChannel(param);
-                                final String mess = jsonToNotifyMessage(param);
-                                final int index = i;
-                                futureResults.add(transaction.notify(channel, mess).onComplete(notifyRes -> {
-                                    if(notifyRes.succeeded()){
-                                        final JsonObject result = resultNotifyToJson();
-                                        results.add(index, result);
-                                    }
-                                }));
-                            }else{
-                                futureResults.add(Future.succeededFuture());
-                                log.warn("Transaction parsed query failed: ", param);
-                            }
+            try {
+                final JsonObject payload = message.body();
+                if (isQuery(payload)) {
+                    final String query = jsonToQuery(payload);
+                    final Tuple tuple = jsonToQueryTuple(payload);
+                    this.preparedQuery(query, tuple).onComplete(res -> {
+                        if (res.succeeded()) {
+                            final JsonObject result = resultToJson(res.result());
+                            message.reply(result);
+                        } else {
+                            message.fail(500, res.cause().getMessage());
+                            log.error("Query failed: ", res.cause());
                         }
-                        transaction.commit().onComplete(resCommit -> {
-                            if(resCommit.succeeded()){
-                                //wait until all finish
-                                CompositeFuture.all(futureResults).onSuccess(e->{
-                                    final JsonObject jsonTr = transactionToJson(new JsonArray(results));
-                                    message.reply(jsonTr);
-                                }).onFailure(e->{
-                                    log.error("Transaction commit failed: ", e);
-                                });
-                            }else{
-                                message.fail(500, resCommit.cause().getMessage());
-                                log.error("Transaction commit failed: ", resCommit.cause());
+                    });
+                } else if (isTransaction(payload)) {
+                    this.transaction().onComplete(res -> {
+                        if (res.succeeded()) {
+                            final IPostgresTransaction transaction = res.result();
+                            final JsonArray params = jsonToTransactionParams(payload);
+                            final List<JsonObject> results = new ArrayList<>(params.size());
+                            final List<Future> futureResults = new ArrayList<>();
+                            for (int i = 0; i < params.size(); i++) {
+                                final JsonObject param = params.getJsonObject(i);
+                                if (isQuery(param)) {
+                                    final String query = jsonToQuery(param);
+                                    final Tuple tuple = jsonToQueryTuple(param);
+                                    final int index = i;
+                                    futureResults.add(transaction.addPreparedQuery(query, tuple).onComplete(queryRes -> {
+                                        if (queryRes.succeeded()) {
+                                            final JsonObject result = resultToJson(queryRes.result());
+                                            results.add(index, result);
+                                        } else {
+                                            log.error("Query failed: " + query, queryRes.cause());
+                                        }
+                                    }));
+                                } else if (isNotify(param)) {
+                                    final String channel = jsonToNotifyChannel(param);
+                                    final String mess = jsonToNotifyMessage(param);
+                                    final int index = i;
+                                    futureResults.add(transaction.notify(channel, mess).onComplete(notifyRes -> {
+                                        if (notifyRes.succeeded()) {
+                                            final JsonObject result = resultNotifyToJson();
+                                            results.add(index, result);
+                                        }
+                                    }));
+                                } else {
+                                    futureResults.add(Future.succeededFuture());
+                                    log.warn("Transaction parsed query failed: ", param);
+                                }
                             }
-                        });
-                    }else{
-                        message.fail(500, res.cause().getMessage());
-                        log.error("Transaction failed: ", res.cause());
-                    }
-                });
-            }else{
-                log.error("Could not parse query: "+payload);
-                message.fail(500, "parse failed");
+                            transaction.commit().onComplete(resCommit -> {
+                                if (resCommit.succeeded()) {
+                                    //wait until all finish
+                                    CompositeFuture.all(futureResults).onSuccess(e -> {
+                                        final JsonObject jsonTr = transactionToJson(new JsonArray(results));
+                                        message.reply(jsonTr);
+                                    }).onFailure(e -> {
+                                        log.error("Transaction commit failed: ", e);
+                                    });
+                                } else {
+                                    message.fail(500, resCommit.cause().getMessage());
+                                    log.error("Transaction commit failed: ", resCommit.cause());
+                                }
+                            });
+                        } else {
+                            message.fail(500, res.cause().getMessage());
+                            log.error("Transaction failed: ", res.cause());
+                        }
+                    });
+                } else {
+                    log.error("Could not parse query: " + payload);
+                    message.fail(500, "parse failed");
+                }
+            }catch(final Exception e){
+                log.error("Could not exec query: ", e);
+                message.fail(500, e.getMessage());
             }
         });
     }
