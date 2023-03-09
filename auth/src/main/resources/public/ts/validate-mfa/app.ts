67aecf4bd42b93b5eb3116d1b91796ddf0e73607
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2023-03-09 15:51:59 +0100

    feat(auth): #WB-1677, add a navbar on MFA when in fullscreen

--- auth/src/main/resources/public/ts/validate-mfa/app.ts
+++ auth/src/main/resources/public/ts/validate-mfa/app.ts
@@ -3,9 +3,11 @@ import { APP } from "ode-ts-client";
 import { OdeModules, conf } from 'ode-ngjs-front';
 import { AppController } from "./controller";
 import * as App from './directives/validate-mfa.directive';
+import * as NavbarLegacy from '../validate-mail/directives/navbar-legacy.directive';
 
 angular.module("app", [OdeModules.getBase(), OdeModules.getI18n(), OdeModules.getUi()])
 .controller("appCtrl", ['$scope', AppController])
+.directive("navbarLegacy", NavbarLegacy.DirectiveFactory)
 .directive("validateMfa", App.DirectiveFactory)
 ;
 conf().Platform.apps.initialize(APP.PORTAL, true);
