eba3d2e12158d68a5ba31cb829b49d33e10098c3
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2023-03-28 11:08:19 +0200

    fix(directory): #WB-1675, add a 10px margin to avoid scrollbars on some browsers

--- directory/src/main/resources/public/ts/directives/adaptiveHeight.ts
+++ directory/src/main/resources/public/ts/directives/adaptiveHeight.ts
@@ -17,24 +17,28 @@ export const adaptiveHeight = ng.directive('adaptiveHeight', [() => {
         link: (scope: AdaptiveHeightScope, element, attributes) => {
             if(typeof ResizeObserver !== "undefined") {
                 const observer:ResizeObserver = new ResizeObserver( entries => {
+                    let height:number;
                     for (const entry of entries) {
                         if (entry.contentBoxSize) {
                             // Content-box value, if available
-                            element.css({"height": Math.min(entry.contentBoxSize[0].blockSize,800)+'px'});
+                            height = Math.min(entry.contentBoxSize[0].blockSize, 800);
                             break;
                         }
                         if (entry.contentRect) {
                             // Content-rect value, if available
-                            element.css({"height": Math.min(entry.contentRect.height+10,800)+'px'});
+                            height = Math.min(entry.contentRect.height, 800);
                             break;
                         }
                         if (entry.devicePixelContentBoxSize) {
                             // Best value
-                            element.css({"height": Math.min(entry.devicePixelContentBoxSize[0].blockSize,800)+'px'});
+                            height = Math.min(entry.devicePixelContentBoxSize[0].blockSize, 800);
                             break;
                         }
                         // Default value, if size is not well known
-                        element.css({"height": Math.min(entry.target.scrollHeight+110||1000,800)+'px'});
+                        height = Math.min(entry.target.scrollHeight+100||1000, 800);
+                    }
+                    if( height ) {
+                        element.css({"height": (height+10)+'px'}); // add a 10px safety margin
                     }
                 });
 
