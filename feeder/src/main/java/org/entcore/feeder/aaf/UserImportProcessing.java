09640cead405fcf86585ffbf16ea2644b2d21511
Valentin PEIRO <valentin.peiro@opendigitaleducation.com>
2023-06-20 18:37:22 +0200

    fix(feeder): Fix an import

--- feeder/src/main/java/org/entcore/feeder/aaf/UserImportProcessing.java
+++ feeder/src/main/java/org/entcore/feeder/aaf/UserImportProcessing.java
@@ -24,6 +24,7 @@ import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
 
 import java.util.Set;
 
