352e9287ce6bd43ad634f4bf757214132cb7cf62
Romu-C <romuald.coulaud@gmail.com>
2023-10-23 15:04:00 +0200

    Feat: #WB-634 Add readonly view for ADML for diffusion list

--- directory/src/main/resources/public/ts/behaviours.ts
+++ directory/src/main/resources/public/ts/behaviours.ts
@@ -1,93 +1,127 @@
-import { Behaviours, http, idiom as lang, _, ui } from 'entcore';
+import { Behaviours, http, idiom as lang, _, ui } from "entcore";
 
-console.log('directory behaviours loaded');
+console.log("directory behaviours loaded");
 
-Behaviours.register('directory', {
-	rights:{
-		workflow: {
-			externalNotifications: "org.entcore.timeline.controllers.TimelineController|mixinConfig",
-			historyView: "org.entcore.timeline.controllers.TimelineController|historyView",
-			showMoodMotto: "org.entcore.directory.controllers.UserBookController|userBookMottoMood",
-			switchTheme: "org.entcore.directory.controllers.UserBookController|userBookSwitchTheme",
-            generateMergeKey: "org.entcore.directory.controllers.UserController|generateMergeKey",
-			mergeByKey: "org.entcore.directory.controllers.UserController|mergeByKey",
-			allowSharebookmarks: "org.entcore.directory.controllers.ShareBookmarkController|allowSharebookmarks",
-			allowLoginUpdate: "org.entcore.directory.controllers.UserController|allowLoginUpdate",
-			allowClassAdminAddUsers: "org.entcore.directory.controllers.DirectoryController|allowClassAdminAddUsers",
-			allowClassAdminResetPassword: "org.entcore.directory.controllers.DirectoryController|allowClassAdminResetPassword",
-			allowClassAdminBlockUsers: "org.entcore.directory.controllers.DirectoryController|allowClassAdminBlockUsers",
-			allowClassAdminDeleteUsers: "org.entcore.directory.controllers.DirectoryController|allowClassAdminDeleteUsers",
-			allowClassAdminUnlinkUsers: "org.entcore.directory.controllers.DirectoryController|allowClassAdminUnlinkUsers",
-			allowClassAdminCSVImport: "org.entcore.directory.controllers.DirectoryController|allowClassAdminCSVImport"
-		}
-	},
-	sniplets: {
-		facebook: {
-			title: 'sniplet.facebook.title',
-			description: 'sniplet.facebook.desc',
-			controller: {
-				initSource: function(){
-					this.source = {
-						groups: []
-					};
-					this.search = {
-						text: '',
-						groups: [],
-						structures: [],
-						structure: null
-					};
+Behaviours.register("directory", {
+  rights: {
+    workflow: {
+      externalNotifications:
+        "org.entcore.timeline.controllers.TimelineController|mixinConfig",
+      historyView:
+        "org.entcore.timeline.controllers.TimelineController|historyView",
+      showMoodMotto:
+        "org.entcore.directory.controllers.UserBookController|userBookMottoMood",
+      switchTheme:
+        "org.entcore.directory.controllers.UserBookController|userBookSwitchTheme",
+      generateMergeKey:
+        "org.entcore.directory.controllers.UserController|generateMergeKey",
+      mergeByKey: "org.entcore.directory.controllers.UserController|mergeByKey",
+      allowSharebookmarks:
+        "org.entcore.directory.controllers.ShareBookmarkController|allowSharebookmarks",
+      allowLoginUpdate:
+        "org.entcore.directory.controllers.UserController|allowLoginUpdate",
+      allowClassAdminAddUsers:
+        "org.entcore.directory.controllers.DirectoryController|allowClassAdminAddUsers",
+      allowClassAdminResetPassword:
+        "org.entcore.directory.controllers.DirectoryController|allowClassAdminResetPassword",
+      allowClassAdminBlockUsers:
+        "org.entcore.directory.controllers.DirectoryController|allowClassAdminBlockUsers",
+      allowClassAdminDeleteUsers:
+        "org.entcore.directory.controllers.DirectoryController|allowClassAdminDeleteUsers",
+      allowClassAdminUnlinkUsers:
+        "org.entcore.directory.controllers.DirectoryController|allowClassAdminUnlinkUsers",
+      allowClassAdminCSVImport:
+        "org.entcore.directory.controllers.DirectoryController|allowClassAdminCSVImport",
+    },
+  },
+  sniplets: {
+    facebook: {
+      title: "sniplet.facebook.title",
+      description: "sniplet.facebook.desc",
+      controller: {
+        initSource: function () {
+          this.source = {
+            groups: [],
+          };
+          this.search = {
+            text: "",
+            groups: [],
+            structures: [],
+            structure: null,
+          };
 
-					http().get('/userbook/structures').done(function(structures){
-						this.search.structures = structures;
-						this.$apply('search');
-					}.bind(this));
-				},
-				viewUserInfos: function(userId){
-					window.open('/userbook/annuaire#/' + userId, '_blank');
-				},
-				removeGroup: function(index, group){
-					this.source.groups.splice(index, 1);
-					this.search.groups.push(group);
-				},
-				addGroup: function(group){
-					this.source.groups.push(group);
-					var index = this.search.groups.indexOf(group);
-					this.search.groups.splice(index, 1);
-				},
-				loadGroups: function(){
-					var that = this
-					http().get('/userbook/structure/' + this.search.structure.id).done(function(structure){
-						this.search.groups = structure.profileGroups.concat(structure.manualGroups);
-						_.map(this.search.groups, function(group){ group.translatedName = that.groupTranslation(group.name) })
-						this.$apply('search');
-					}.bind(this));
-				},
-				init: function(){
-					this.source.groups.forEach(function(group){
-						http().get('/userbook/visible/users/' + group.id).done(function(users){
-							group.users = users;
-							this.$apply('source');
-						}.bind(this));
-					}.bind(this))
-				},
-				applySource: function(){
-					this.setSnipletSource(this.source);
-				},
-				colorFromType: function(type){
-					return ui.profileColors.match(type);
-				},
-				groupTranslation: function(groupName){
-					var splittedName = groupName.split('-')
-					return splittedName.length > 1 ?
-						lang.translate(groupName.substring(0, groupName.lastIndexOf('-'))) + '-' + lang.translate(groupName.split('-')[splittedName.length - 1]) :
-						groupName
-				},
-				getType: function(type){
-					if(type instanceof Array)
-						 return type[0]
-					return type
-				}
-			}
-		}
-	}
+          http()
+            .get("/userbook/structures")
+            .done(
+              function (structures) {
+                this.search.structures = structures;
+                this.$apply("search");
+              }.bind(this)
+            );
+        },
+        viewUserInfos: function (userId) {
+          window.open("/userbook/annuaire#/" + userId, "_blank");
+        },
+        removeGroup: function (index, group) {
+          this.source.groups.splice(index, 1);
+          this.search.groups.push(group);
+        },
+        addGroup: function (group) {
+          this.source.groups.push(group);
+          var index = this.search.groups.indexOf(group);
+          this.search.groups.splice(index, 1);
+        },
+        loadGroups: function () {
+          var that = this;
+          http()
+            .get("/userbook/structure/" + this.search.structure.id)
+            .done(
+              function (structure) {
+                this.search.groups = structure.profileGroups.concat(
+                  structure.manualGroups
+                );
+                _.map(this.search.groups, function (group) {
+                  group.translatedName = that.groupTranslation(group.name);
+                });
+                this.$apply("search");
+              }.bind(this)
+            );
+        },
+        init: function () {
+          this.source.groups.forEach(
+            function (group) {
+              http()
+                .get("/userbook/visible/users/" + group.id)
+                .done(
+                  function (users) {
+                    group.users = users;
+                    this.$apply("source");
+                  }.bind(this)
+                );
+            }.bind(this)
+          );
+        },
+        applySource: function () {
+          this.setSnipletSource(this.source);
+        },
+        colorFromType: function (type) {
+          return ui.profileColors.match(type);
+        },
+        groupTranslation: function (groupName) {
+          var splittedName = groupName.split("-");
+          return splittedName.length > 1
+            ? lang.translate(
+                groupName.substring(0, groupName.lastIndexOf("-"))
+              ) +
+                "-" +
+                lang.translate(groupName.split("-")[splittedName.length - 1])
+            : groupName;
+        },
+        getType: function (type) {
+          if (type instanceof Array) return type[0];
+          return type;
+        },
+      },
+    },
+  },
 });
