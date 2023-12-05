cba93fcbb29c81129b0f999789357e3acaf5bb91
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2023-12-05 10:51:59 +0100

    doc: js doc

--- tests/src/test/scenarios/libs/api.js
+++ tests/src/test/scenarios/libs/api.js
@@ -1,13 +1,24 @@
 const axios = require('axios');
 const {ROOT_URL, ADMC_EMAIL, ADMC_PASSWORD, DEFAULT_PASSWORD} = require('./env.js')
 
+/**
+ * @param {*} world The current scenario object
+ * @returns information about the currently logged in user or null
+ */
 function getCurrentOneSessionId(world) {
     if(world.sessions) {
         return world.sessions[world.sessions['current']]
     }
     return null;
 }
-
+/**
+ * Execute a GET request.
+ * Note that the response is not necessarily OK as the status is not validated.
+ * @param {string} uri Path of the url
+ * @param {*} world The current scenario object
+ * @param {boolean} unauthenticated true if the call has to be anonymous, false otherwise
+ * @returns raw axios's response 
+ */
 async function get(uri, world, unauthenticated) {
     const headers = { }
     if(!unauthenticated) {
@@ -21,7 +32,15 @@ async function get(uri, world, unauthenticated) {
         validateStatus: false
     })
 }
-
+/**
+ * POST a json body to the specified URI.
+ * Note that the response is not necessarily OK as the status is not validated.
+ * @param {string} uri Path of the url
+ * @param {*} data The body content of the request as a JSON object
+ * @param {*} world The current scenario object
+ * @param {boolean} unauthenticated true if the call has to be anonymous, false otherwise
+ * @returns raw axios's response 
+ */
 async function post(uri, data, world, unauthenticated) {
     const headers = { "Content-Type": "application/json" }
     if(!unauthenticated) {
@@ -36,6 +55,15 @@ async function post(uri, data, world, unauthenticated) {
         validateStatus: false
     })
 }
+/**
+ * POST a form encoded body to the specified URI.
+ * Note that the response is not necessarily OK as the status is not validated.
+ * @param {string} uri Path of the url
+ * @param {*} data The body content of the request as a JSON object
+ * @param {*} world The current scenario object
+ * @param {boolean} unauthenticated true if the call has to be anonymous, false otherwise
+ * @returns raw axios's response 
+ */
 async function postForm(uri, data, unauthenticated, world) {
     const headers = { "Content-Type": "application/x-www-form-urlencoded" }
     if(!unauthenticated) {
@@ -53,6 +81,11 @@ async function postForm(uri, data, unauthenticated, world) {
     })
 }
 
+/**
+ * @param {string} cookieName Name of the cookie to extract
+ * @param {axios.Response} axiosResponse Raw response from which to extract the cookie value
+ * @returns Value of the cookie or null if none was found
+ */
 function getCookieByName(cookieName, axiosResponse) {
     const cookie = axiosResponse.headers['set-cookie']
     .find(cookie => cookie.includes(cookieName))
