dcc4077c93eb2584d3c2b5ea25a90c74d387b886
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-02-14 15:47:51 +0100

    feat: #WB-2569, purge deleted users

--- audience/src/main/java/org/entcore/audience/services/impl/AudienceRepositoryEvents.java
+++ audience/src/main/java/org/entcore/audience/services/impl/AudienceRepositoryEvents.java
@@ -1,46 +1,32 @@
 package org.entcore.audience.services.impl;
 
-import io.vertx.core.Handler;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import org.apache.commons.lang3.StringUtils;
+import org.entcore.audience.services.AudienceService;
 import org.entcore.common.user.RepositoryEvents;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- * 
+ * Handle clean up of audience data when users are deleted or merged.
  */
 public class AudienceRepositoryEvents implements RepositoryEvents {
   private static final Logger log = LoggerFactory.getLogger(AudienceRepositoryEvents.class);
 
-  @Override
-  public void deleteUsers(JsonArray users, Handler<List<ResourceChanges>> handler) {
-    final Set<String> userIds = getUserIds(users);
-    log.info("Calling purge of users : " + userIds);
-    // TODO delete entries in reactions
-    throw new UnsupportedOperationException("implement.deleteUsers");
+  private final AudienceService audienceService;
+
+  public AudienceRepositoryEvents(final AudienceService audienceService) {
+    this.audienceService = audienceService;
   }
 
-  @Override
-  public void mergeUsers(String keepedUserId, String deletedUserId) {
-    // TODO replace in reactions and views deletedUserId by  keepUserId
-    throw new UnsupportedOperationException("implement.mergeUsers");
-  }
 
   @Override
-  public void removeShareGroups(JsonArray oldGroups) {
-    // TODO remove users' reaction on concerned resources
-    throw new UnsupportedOperationException("implement.removeShareGroups");
-  }
-
-  private Set<String> getUserIds(JsonArray users) {
+  public void deleteUsers(final JsonArray users) {
     final Set<String> userIds;
     if(users == null || users.isEmpty()) {
       userIds = Collections.emptySet();
@@ -49,7 +35,19 @@ public class AudienceRepositoryEvents implements RepositoryEvents {
           .filter(StringUtils::isNotEmpty)
           .collect(Collectors.toSet());
     }
-    return userIds;
+    log.info("Calling purge of users : " + userIds);
+    audienceService.deleteUsers(userIds)
+        .onSuccess(e -> log.info("Users have been successfully deleted from audience : " + userIds))
+        .onFailure(th -> log.warn("An error occurred while deleting users from audience : " + userIds, th));
   }
+
+  @Override
+  public void mergeUsers(String keptdUserId, String deletedUserId) {
+    log.info("Merge users " + deletedUserId + " -> " + keptdUserId);
+    audienceService.mergeUsers(keptdUserId, deletedUserId)
+        .onSuccess(e -> log.info("Users have been successfully merged"))
+        .onFailure(th -> log.warn("An error occurred while merging users from audience", th));
+  }
+
 
 }
