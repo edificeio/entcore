f1f1440fe5cae8a1f58f41471122e619ce75109f
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-02-14 11:58:13 +0100

    feat: #WB-2569, purge deleted users
    feat: #WB-2569, purge deleted users (wip)
    
    feat: #WB-2569, purge deleted users (wip)

--- audience/src/main/java/org/entcore/audience/Audience.java
+++ audience/src/main/java/org/entcore/audience/Audience.java
@@ -1,6 +1,8 @@
 package org.entcore.audience;
 
 import org.entcore.audience.controllers.AudienceController;
+import org.entcore.audience.services.AudienceService;
+import org.entcore.audience.services.impl.AudienceRepositoryEvents;
 import org.entcore.common.http.BaseServer;
 
 public class Audience extends BaseServer {
@@ -9,5 +11,7 @@ public class Audience extends BaseServer {
   public void start() throws Exception {
     super.start();
     addController(new AudienceController(vertx, config()));
+    final AudienceService audienceService = null; // TODO create an instance
+    setRepositoryEvents(new AudienceRepositoryEvents(audienceService));
   }
 }
