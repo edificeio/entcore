d85b366bcfd1c95fa11216c923b2f652720cdb02
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-02-14 11:58:13 +0100

    feat: #WB-2569, purge deleted users
    feat: #WB-2569, purge deleted users (wip)
    
    feat: #WB-2569, purge deleted users (wip)

---
+++ audience/src/main/java/org/entcore/audience/services/AudienceService.java
@@ -0,0 +1,22 @@
+package org.entcore.audience.services;
+
+import io.vertx.core.Future;
+
+import java.util.Set;
+
+public interface AudienceService {
+  /**
+   * Purge audience data related to the supplied users.
+   * @param userIds Ids of the users who have been deleted
+   * @return a Future that completes when the deletion is done
+   */
+  Future<Void> deleteUsers(final Set<String> userIds);
+
+  /**
+   * Replace all references to {@code deletedUserId} by references to {@code keptdUserId}
+   * @param keptdUserId Id of the user to keep
+   * @param deletedUserId Id of the user who has been merged into the other one
+   * @return a Future that completes when the deletion is done
+   */
+  Future<Void> mergeUsers(final String keptdUserId, final String deletedUserId);
+}
