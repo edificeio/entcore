91cb23130a197e2ed9aede38c2e0229d914703c9
mariusestaque <marius.estaque@opendigitaleducation.com>
2024-02-19 08:46:06 +0100

    fix: #WB-2526, audience apis for views

---
+++ common/src/main/java/org/entcore/common/sql/ISql.java
@@ -0,0 +1,35 @@
+package org.entcore.common.sql;
+
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+public interface ISql {
+
+  void prepared(String query, JsonArray values, Handler<Message<JsonObject>> handler);
+
+  void prepared(String query, JsonArray values, DeliveryOptions deliveryOptions, Handler<Message<JsonObject>> handler);
+
+  void raw(String query, Handler<Message<JsonObject>> handler);
+
+  void insert(String table, JsonObject params, Handler<Message<JsonObject>> handler);
+
+  void insert(String table, JsonObject params, String returning, Handler<Message<JsonObject>> handler);
+
+  void insert(String table, JsonArray fields, JsonArray values, Handler<Message<JsonObject>> handler);
+
+  void insert(String table, JsonArray fields, JsonArray values, String returning,
+              Handler<Message<JsonObject>> handler);
+
+  void upsert(String table, JsonArray fields, JsonArray values, JsonArray conflictFields,
+              JsonArray updateFields, String returning,
+              Handler<Message<JsonObject>> handler);
+
+  void select(String table, JsonArray fields, Handler<Message<JsonObject>> handler);
+
+  void transaction(JsonArray statements, Handler<Message<JsonObject>> handler);
+
+  void transaction(JsonArray statements, DeliveryOptions deliveryOptions, Handler<Message<JsonObject>> handler);
+}
