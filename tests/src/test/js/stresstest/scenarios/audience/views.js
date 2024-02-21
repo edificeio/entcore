48b58f5644698e9921c5d9c0cc25e37a6be8147f
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-02-21 18:23:51 +0100

    test: #WB-2571, init data for audience stress tests

--- tests/src/test/js/stresstest/scenarios/audience/views.js
+++ tests/src/test/js/stresstest/scenarios/audience/views.js
@@ -2,51 +2,95 @@ import http from 'k6/http';
 import { check } from 'k6';
 import chai, { describe } from 'https://jslib.k6.io/k6chaijs/4.3.4.2/index.js';
 import {authenticateWeb, getHeaders} from '../../utils/user.utils.js';
-import { Session } from '../../utils/authentication.utils.js';
-import { SharedArray } from 'k6/data';
+import { getSchoolByName, getUsersOfSchool } from '../../utils/structure.utils.js';
+import {initViewsData} from './views-init.js'
 
 const rootUrl = __ENV.ROOT_URL;
-const dataRootPath = __ENV.DATA_ROOT_PATH;
-const nbRounds = __ENV.DATA_NB_ROUNDS || 100;
 const duration = __ENV.DURATION || '60s';
 const vus = parseInt(__ENV.VUS || '1');
+const nbUsers = parseInt(__ENV.NB_USERS || '10');
+const schoolName = __ENV.DATA_SCHOOL_NAME || "Tests Audience"
+const dataRootPath = __ENV.DATA_ROOT_PATH;
 
 chai.config.logFailures = true;
 
+
 export const options = {
+  setupTimeout: '1h',
   thresholds: {
     checks: ['rate == 1.00'],
   },
   scenarios: {
-    view_files: {
-     exec: 'viewPosts',
-     executor: "per-vu-iterations",
-     maxDuration: duration,
+    registerViews: {
+     exec: 'registerViews',
+     executor: "constant-vus",
+     duration: duration,
+     vus: vus
+    },
+    getViewsCounts: {
+     exec: 'getViewsCounts',
+     executor: "constant-vus",
+     duration: duration,
      vus: vus
     }
   }
 };
-const userData = new SharedArray('userData', function () {
-  return JSON.parse(open(`${dataRootPath}/audience/users.json`))
-});
-let webSession;
+
+const teacherData = open(`${dataRootPath}/audience/enseignants.csv`, 'b')
 
 export function setup() {
-    webSession = new Session(null, null, -1);
+  initViewsData(schoolName, teacherData)
+  const session = authenticateWeb(__ENV.ADMC_LOGIN, __ENV.ADMC_PASSWORD);
+  const school = getSchoolByName(schoolName, session)
+  const users = getUsersOfSchool(school, session)
+  let ids = []
+  for(let i = 0; i < Math.min(users.length, nbUsers); i++) {
+    const user = users[i]
+    const userSession = authenticateWeb(user.login, 'password')
+    ids = ids.concat(getAllPosts(userSession))
+  }
+  return {users, postIds: ids};
 }
 
-export function viewPosts() {
-    const user = userData[Math.floor(Math.random() * userData.length)]
-    const session = authenticateWeb(user.login, user.password);
-    const postIds = getAllPosts(session);
-    describe("[WEB] - Check can view allowed posts", () => {
-        for (let i = 0; i < nbRounds; i ++) {
-          const response = viewPost(postIds[i], session);
-          check(response, {
+
+export function registerViews(data) {
+    const {users, postIds} = data
+    const user = users[Math.floor(Math.random() * users.length)]
+    const session = authenticateWeb(user.login, 'password');
+    describe("[WEB] - Check can increment views", () => {
+        for(let postId of postIds) {
+          check(viewPost(postId, session), {
             "should get an OK response from views incrementation": r => r.status == 200
-          })
+          });
         }
     });
+}
+export function getViewsCounts(data) {
+  const {users, postIds} = data
+  const user = users[Math.floor(Math.random() * users.length)]
+  const session = authenticateWeb(user.login, 'password');
+  describe("[WEB] - Check can view counts", () => {
+    for(let postId of postIds) {
+      check(viewDetails(postId, session), {
+        "should get an OK response from views details": r => r.status == 200
+      })
+      check(viewSummary(postId, session), {
+        "should get an OK response from views summary": r => r.status == 200
+      })
+    }
+  });
+}
+
+function viewPost(postId, session) {
+  return http.post(`${rootUrl}/audience/views/blog/post/${postId}`, {}, {headers: getHeaders(session)});
+}
+
+function viewSummary(postId, session) {
+  return http.get(`${rootUrl}/audience/views/count/blog/post?resourceIds=${postId}`, {}, {headers: getHeaders(session)});
+}
+
+function viewDetails(postId, session) {
+  return http.get(`${rootUrl}/audience/views/details/blog/post/${postId}`, {}, {headers: getHeaders(session)});
 }
 
 function getAllPosts(session) {
@@ -61,8 +105,4 @@ function getAllPosts(session) {
     JSON.parse(fetchedPosts.body).map(post => postIds.push(post._id))
   });
   return postIds;
-}
-
-function viewPost(postId, session) {
-  return http.get(`${rootUrl}/audience/views/blog/post/${postId}`, {headers: getHeaders(session)});
 }
