da5237045229dd5de166027702de6639a425ed35
Evane Abner <abner.leyogho-evane@edifice.io>
2024-05-14 16:09:48 +0200

    chore (Auth) : upadate refacto slo

--- auth/src/main/java/org/entcore/auth/services/OpenIdDataHandler.java
+++ auth/src/main/java/org/entcore/auth/services/OpenIdDataHandler.java
@@ -1,15 +1,11 @@
 package org.entcore.auth.services;
 
-import java.util.List;
 
 import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 import jp.eisbahn.oauth2.server.async.Handler;
 
 public interface OpenIdDataHandler {
-       void getAuthorizationsBySessionId(String sessionId, Handler<List<JsonObject>> handler);
-
-       void getTokensByAuthId(String authId, Handler<List<JsonObject>> handler);
 
        void deleteTokensByAuthId(String authId);
 
