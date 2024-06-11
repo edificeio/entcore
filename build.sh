210bc824ee55bd71fa611b905f8e0791b2b7d0bf
mariusestaque <marius.estaque@opendigitaleducation.com>
2024-06-11 13:57:54 +0200

    fix: typo in skipTests maven option

--- build.sh
+++ build.sh
@@ -182,7 +182,7 @@ publish() {
     *)         export nexusRepository='releases' ;;
   esac
 
-  docker compose run --rm  maven mvn -DrepositoryId=ode-$nexusRepository -DskiptTests --settings /var/maven/.m2/settings.xml deploy
+  docker compose run --rm  maven mvn -DrepositoryId=ode-$nexusRepository -DskipTests --settings /var/maven/.m2/settings.xml deploy
 }
 
 for param in "$@"
