6e88cc88310c665fe3d17800d26663138e0a46eb
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2024-08-16 15:17:16 +0200

    feat(admin, common, directory): WB-2970, chantier Champ fonction
    Squashed commit of the following:
    
    commit e7c4a008e63bd9cf032a45901e407b344aa7b1ef
    Author: Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
    Date:   Fri Aug 16 15:06:51 2024 +0200
    
        fix: feedback from beta tests
    
    commit cbe776612f4b853e21a11d8839d792b94badb5b5
    Author: jcbe-ode <67910608+jcbe-ode@users.noreply.github.com>
    Date:   Fri Aug 16 11:12:39 2024 +0200
    
        chore: Update gradle.properties
    
    commit 80298071919ca2ecf2eec8bc1d6e31c9536ae541
    Author: jcbe-ode <67910608+jcbe-ode@users.noreply.github.com>
    Date:   Fri Aug 16 10:10:08 2024 +0200
    
        feat(admin): WB-3179, WB-3181, WB-3182, WB-3183, user positions frontend #504
    
        * feat: #WB-3179 #WB-3184 manage position in a structure
    
        * feat: #WB-3181 Add user position section in user detail page
    
        * feat(admin): #WB-3182, allow selecting positions during creation of a new user
    
        * feat(directory): #WB-3178, show UserPositions in directory
    
        * fix(directory): #WB-3190, adapt frontend service to backend implementation
    
        * wip
    
        ---------
    
        Co-authored-by: Romu-C <romuald.coulaud@gmail.com>
    
    commit f7fe16548986a7ed96492974e592966339dab62f
    Author: mariusode <125455829+mariusode@users.noreply.github.com>
    Date:   Wed Aug 14 17:43:34 2024 +0200
    
        feat: WB-3184, CRUD user positions
    
    commit 0b1999225e87ce490ffd17ffd90fc3cb5a5e6388
    Author: Junior BERNARD <junior.bernard@opendigitaleducation.com>
    Date:   Tue Jul 9 09:18:38 2024 +0200
    
        chore: changes from code review
    
    commit 95c9a0cdcbaf4c759e309967dc80e4f6937a37b5
    Author: Junior BERNARD <junior.bernard@opendigitaleducation.com>
    Date:   Mon Jul 8 11:12:31 2024 +0200
    
        test: add tests on attributing positions to a user
    
    commit 4ee58a8e36708ce25b8618d9fbfd6a9e170f2e5d
    Author: Junior BERNARD <junior.bernard@opendigitaleducation.com>
    Date:   Fri Jul 5 09:42:14 2024 +0200
    
        feat: test positions
    
    commit a6fa9859fc79a17ecdf7e9fc71b4cc6cff73d671
    Author: Junior BERNARD <junior.bernard@opendigitaleducation.com>
    Date:   Wed Jul 3 18:21:03 2024 +0200
    
        feat: test positions
    
    commit 7ed9c79a0ad30a3870792ffd09d5a4a62c59f74f
    Author: Junior BERNARD <junior.bernard@opendigitaleducation.com>
    Date:   Wed Jul 3 18:15:46 2024 +0200
    
        feat: test positions

---
+++ common/src/main/java/org/entcore/common/user/position/UserPositionService.java
@@ -0,0 +1,53 @@
+package org.entcore.common.user.position;
+
+import io.vertx.core.Future;
+import org.entcore.common.user.UserInfos;
+
+import java.util.Set;
+
+public interface UserPositionService {
+	/**
+	 * Retrieve user positions linked to the structures managed by the current admin
+	 * @param prefix if present, filters the result : keeps only the user positions whose name match the prefix pattern
+	 * @param structureId if present, filters the result : keeps the user positions linked to the structure
+	 * @param adminInfos the current admin infos
+	 * @return the retrieved user positions, filtered by prefix or structure
+	 */
+	Future<Set<UserPosition>> getUserPositions(String prefix, String structureId, UserInfos adminInfos);
+
+	/**
+	 * Retrieve a user position by its id
+	 * @param userPositionId the id of the user position to retrieve
+	 * @param adminInfos the current admin infos
+	 * @return the user position
+	 */
+	Future<UserPosition> getUserPosition(String userPositionId, UserInfos adminInfos);
+
+	/**
+	 * create a user position and links it to a structure
+	 * @param positionName the name of the user position being created
+	 * @param structureId the id of the structure it must be attached to
+	 * @param source the type of source of the user position being created
+	 * @param adminInfos the current admin infos
+	 * @return the newly created user position
+	 */
+	Future<UserPosition> createUserPosition(String positionName, String structureId, UserPositionSource source, UserInfos adminInfos);
+
+	/**
+	 * Rename the user position
+	 * @param positionName the new user position name
+	 * @param positionId the id of the user position to rename
+	 * @param adminInfos the current admin infos
+	 * @return the renamed user position
+	 */
+	Future<UserPosition> renameUserPosition(String positionName, String positionId, UserInfos adminInfos);
+
+	/**
+	 * Delete a user position in a specific structure
+	 * @param positionId the id of the position to delete
+	 * @param adminInfos the current admin infos
+	 * @return void
+	 */
+	Future<Void> deleteUserPosition(String positionId, UserInfos adminInfos);
+
+}
---
+++ common/src/main/java/org/entcore/common/user/position/UserPositionSource.java
@@ -0,0 +1,7 @@
+package org.entcore.common.user.position;
+
+public enum UserPositionSource {
+	MANUAL,
+	CSV,
+	AAF
+}
