15650810227eac456130448e758b1b60d2e3fe8c
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2024-08-19 11:38:15 +0200

    fix(directory): WB-3189, better management of users without any UserPosition defined

--- directory/src/main/java/org/entcore/directory/services/impl/DefaultUserService.java
+++ directory/src/main/java/org/entcore/directory/services/impl/DefaultUserService.java
@@ -323,7 +323,7 @@ public class DefaultUserService implements UserService {
 				"OPTIONAL MATCH u-[:IN]->(fgroup: FunctionalGroup) WITH COLLECT(distinct {id: fgroup.id, name: fgroup.name}) as admGroups, parents, children, functions, u, structureNodes " +
 				"OPTIONAL MATCH u-[:ADMINISTRATIVE_ATTACHMENT]->(admStruct: Structure) WITH COLLECT(distinct {id: admStruct.id}) as admStruct, admGroups, parents, children, functions, u, structureNodes " +
 				"OPTIONAL MATCH u-[r:TEACHES]->(s:Subject) WITH COLLECT(distinct s.code) as subjectCodes, admStruct, admGroups, parents, children, functions, u, structureNodes " +
-				"OPTIONAL MATCH u-[h:HAS_POSITION]->(p:UserPosition)-[:IN]->(struct:Structure) WITH COLLECT(distinct {id: p.id, name: p.name, source: p.source, structureId: struct.id}) as userPositions, subjectCodes, admStruct, admGroups, parents, children, functions, u, structureNodes ";
+				"OPTIONAL MATCH u-[h:HAS_POSITION]->(p:UserPosition)-[:IN]->(struct:Structure) WITH CASE WHEN p IS NOT NULL THEN COLLECT(distinct {id: p.id, name: p.name, source: p.source, structureId: struct.id}) ELSE [] END as userPositions, subjectCodes, admStruct, admGroups, parents, children, functions, u, structureNodes ";
 
 		if (getManualGroups)
 			query += "OPTIONAL MATCH u-[:IN]->(mgroup: ManualGroup) WITH COLLECT(distinct {id: mgroup.id, name: mgroup.name}) as manualGroups, userPositions, subjectCodes, admStruct, admGroups, parents, children, functions, u, structureNodes ";
