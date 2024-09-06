98e3d2db3e0a043df751917710ff3cda49de4119
RomuC <romuald.coulaud@gmail.com>
2024-09-06 14:34:20 +0200

    fix error when no positions

--- directory/src/main/resources/public/ts/controllers/directory.ts
+++ directory/src/main/resources/public/ts/controllers/directory.ts
@@ -266,7 +266,7 @@ export const directoryController = ng.controller('DirectoryController',['$scope'
 				}
 				return { label: lang.translate(element), type: element, available: test };
 			}),
-			positions: $scope.criteria.positions.map((element) => {
+			positions: $scope.criteria.positions?.map((element) => {
 				return {label: element.name, type: element.id};
 			}),
 			types: $scope.criteria.groupTypes.map((element) => {
