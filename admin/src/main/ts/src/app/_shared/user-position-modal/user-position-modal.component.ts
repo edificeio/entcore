cdae7c4d0b0334fbad32db8ae6948930bc8d39ab
Romu-C <romuald.coulaud@gmail.com>
2024-09-17 15:24:18 +0200

    fix(admin): #WB-3336 reenable save action

--- admin/src/main/ts/src/app/_shared/user-position-modal/user-position-modal.component.ts
+++ admin/src/main/ts/src/app/_shared/user-position-modal/user-position-modal.component.ts
@@ -27,23 +27,31 @@ export class UserPositionModalComponent extends OdeComponent implements OnInit {
   private _userPosition: UserPosition;
   get userPosition(): UserPosition {
     return this._userPosition;
-  };
+  }
   @Input() set userPosition(value: UserPosition) {
     this._userPosition = value;
     this.editableName = value?.name;
-    this.saving = false;
   }
-  @Input() show: boolean = true;
+  private _show: boolean = true;
+  @Input() get show(): boolean {
+    return this._show;
+  }
+  set show(value: boolean) {
+    this._show = value;
+    if (value) {
+      this.saving = false;
+    }
+  }
 
   @Output() onClose: EventEmitter<UserPosition> = new EventEmitter();
-  
+
   public editableName: string = "";
   public saving: boolean = false;
 
   get isUpdateModal(): boolean {
     return !!this.userPosition?.id;
   }
-  
+
   constructor(
     injector: Injector,
     private ns: NotifyService,
@@ -55,68 +63,88 @@ export class UserPositionModalComponent extends OdeComponent implements OnInit {
 
   ngOnInit(): void {
     this.saving = false;
-    
+
     if (!this.userPosition) {
-      this.userPosition = {name:"", source:"MANUAL"};
+      this.userPosition = { name: "", source: "MANUAL" };
     }
   }
 
   async save(): Promise<void> {
+    if (this.saving) return;
     this.saving = true;
     if (this.isUpdateModal) {
       this.userPosition = await this.spinner
-        .perform('portal-content', this.userPositionServices.updateUserPosition(
-          {...this.userPosition, name: this.editableName}
-        ))
-        .then( updated => {
+        .perform(
+          "portal-content",
+          this.userPositionServices.updateUserPosition({
+            ...this.userPosition,
+            name: this.editableName,
+          })
+        )
+        .then((updated) => {
           this.ns.success(
             "notify.user-position.update.success.content",
             "notify.user-position.success.title"
           );
           return updated;
         })
-        .catch(err => {
+        .catch((err) => {
           this.ns.error(
-              {
-                  key: 'notify.user-position.update.error.content',
-                  parameters: {
-                    position: this.userPosition.name
-                  }
-              }, 'notify.user-position.update.error.title', err);
+            {
+              key: "notify.user-position.update.error.content",
+              parameters: {
+                position: this.userPosition.name,
+              },
+            },
+            "notify.user-position.update.error.title",
+            err
+          );
           return this.userPosition;
         });
     } else {
       this.userPosition = await this.spinner
-        .perform('portal-content', this.userPositionServices.createUserPosition({
-          name: this.editableName,
-          structureId: this.structureId,
-        }))
-        .then(created => {
+        .perform(
+          "portal-content",
+          this.userPositionServices.createUserPosition({
+            name: this.editableName,
+            structureId: this.structureId,
+          })
+        )
+        .then((created) => {
           this.ns.success(
             "notify.user-position.create.success.content",
             "notify.user-position.success.title"
           );
           return created;
         })
-        .catch(err => {
-          debugger
-          if (err.response.status === 409 && err.response?.data?.existingPositionId) {
+        .catch((err) => {
+          debugger;
+          if (
+            err.response.status === 409 &&
+            err.response?.data?.existingPositionId
+          ) {
             this.ns.error(
-                {
-                    key: 'notify.user-position.create.error.duplicate.content',
-                    parameters: {
-                        position: this.editableName
-                    }
-                }, 'notify.user-position.create.error.title', err);
+              {
+                key: "notify.user-position.create.error.duplicate.content",
+                parameters: {
+                  position: this.editableName,
+                },
+              },
+              "notify.user-position.create.error.title",
+              err
+            );
           } else {
             this.ns.error(
-                {
-                    key: 'notify.user-position.create.error.content',
-                    parameters: {
-                        position: this.editableName
-                    }
-                }, 'notify.user-position.create.error.title', err);
-            }
+              {
+                key: "notify.user-position.create.error.content",
+                parameters: {
+                  position: this.editableName,
+                },
+              },
+              "notify.user-position.create.error.title",
+              err
+            );
+          }
           return undefined;
         });
     }
@@ -124,9 +152,8 @@ export class UserPositionModalComponent extends OdeComponent implements OnInit {
     this.userPosition = undefined;
     this.editableName = undefined;
     this.show = false;
-    this.saving = false;
   }
-  
+
   cancel() {
     this.onClose.emit();
     this.userPosition = undefined;
