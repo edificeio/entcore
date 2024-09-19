d5694520e43895f26bceefe47125280bbd26900c
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2024-09-19 15:36:57 +0200

    test: #WB-3339, tests for user positions during AAF imports

---
+++ tests/src/test/js/it/scenarios/position/aaf-import-after.js
@@ -0,0 +1,108 @@
+import { check } from "k6";
+import chai, { describe } from "https://jslib.k6.io/k6chaijs/4.3.4.2/index.js";
+import {
+  authenticateWeb,
+  triggerImport,
+  getSchoolByName,
+  getUsersOfSchool,
+  getUserProfileOrFail,
+  getPositionsOfStructure,
+  attributePositions,
+  getOrCreatePosition
+} from "https://raw.githubusercontent.com/edificeio/edifice-k6-commons/develop/dist/index.js";
+import { sleep } from 'k6';
+
+chai.config.logFailures = true;
+
+export const options = {
+  setupTimeout: "1h",
+  maxRedirects: 0,
+  thresholds: {
+    checks: ["rate == 1.00"],
+  },
+  scenarios: {
+    importUserWithFunctionsInAAFStep2: {
+      exec: 'testImportUserWithFunctionsInAAFStep2',
+      executor: "per-vu-iterations",
+      vus: 1,
+      maxDuration: "60s",
+      gracefulStop: '5s',
+    },
+  },
+};
+
+
+/**
+ * Ensure that after an AAF import :
+ * - users are attached to their rightful user positions
+ * - user positions that are not present anymore in the file are removed from the structure
+ */
+export function testImportUserWithFunctionsInAAFStep2() {
+  describe("[Position-AAFImport]", () => {
+    const allPositions = [
+      /*`DIRECTION / CHEF D'ETABLISSEMENT ADJOINT`,*/ //this one has disappeared
+      `ENSEIGNEMENT DEVANT ELEVES / ENSEIGNER EN SEGPA OU EREA`, // <-- already existed
+      // Below are the new positions
+      `PRESTATIONS / ENTRETIEN`,
+      `ARTICHAUT / PATATES`,
+      `LEGUMES / HARICOTS`,
+      `LEGUMES / PETITS POIS`
+    ];
+    const session = authenticateWeb(__ENV.ADMC_LOGIN, __ENV.ADMC_PASSWORD)
+    const importedStructure = getSchoolByName("CLG-CLG INTEGRATION-PARIS")
+    const users = getUsersOfSchool(importedStructure, session);
+    
+
+    const manualPosition = getOrCreatePosition(`IT Position - Manual`, importedStructure, session)
+    let user = getUserByLastName(users, 'AAAA');
+    attributePositions(user, [manualPosition], session)
+    
+
+    console.log("Launching import....")
+    triggerImport(session)
+    console.log("....waiting for import to be done....")
+    sleep(3)
+    console.log("....stopped waiting")
+    
+    const structurePositions = getPositionsOfStructure(importedStructure, session)
+    describe("Imported structure", () => {
+      check(structurePositions, {
+        'manual user position remains after an AAF import' : positions => positions.filter(p => p.source !== 'AAF').length === 1,
+        'structure has only the user positions specified in AAF and the manual one': actualPositions => allPositions.length === (actualPositions.length - 1),
+        'structure has all the user positions specified in AAF': actualPositions => {
+          const actualNames = new Set();
+          for(let p of actualPositions) {
+            actualNames.add(p.name);
+          }
+          return allPositions.filter(expectedName => !actualNames.has(expectedName)).length === 0;
+        }
+      })
+    })
+    const expectedValues = [
+      ['AAAA', 'User with 2 AAF positions and 1 manual', [`PRESTATIONS / ENTRETIEN`, `ENSEIGNEMENT DEVANT ELEVES / ENSEIGNER EN SEGPA OU EREA`, `IT Position - Manual`]],
+      ['BBBB', 'User who didn\'t have positions before', [`ENSEIGNEMENT DEVANT ELEVES / ENSEIGNER EN SEGPA OU EREA`]],
+      ['CCCC', 'New user with multiple positions', [`ARTICHAUT / PATATES`, `LEGUMES / HARICOTS`, `LEGUMES / PETITS POIS`]]
+    ]
+    for(let expectedValue of expectedValues) {
+      const [lastName, label, expectedPositionNames] = expectedValue;
+      describe(label, () => {
+        const user = getUserByLastName(users, lastName);
+        const userPositions = getUserProfileOrFail(user.id, session).userPositions || []
+        check(userPositions, {
+          'user has the number of expected positions': ups => ups.length === expectedPositionNames.length,
+          'user has the expected positions': ups => {
+            const actualNames = new Set();
+            for(let p of ups) {
+              actualNames.add(p.name);
+            }
+            return expectedPositionNames.filter(expectedName => !actualNames.has(expectedName)).length === 0;
+          }
+        })
+      })
+    }
+})
+};
+
+function getUserByLastName(users, lastName) {
+  return (users || []).filter(u => u.lastName === lastName)[0]
+}
