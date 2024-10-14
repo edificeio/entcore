d841d531e68355aedcbdfd0b010f93911f3281e6
Jean-Christophe <jean-christophe.benoit@opendigitaleducation.com>
2024-10-14 17:28:21 +0200

    feat: allow access to i18n of other modules through vertx bus

--- common/src/main/java/org/entcore/common/http/BaseServer.java
+++ common/src/main/java/org/entcore/common/http/BaseServer.java
@@ -44,6 +44,7 @@ import org.entcore.common.elasticsearch.ElasticSearch;
 import org.entcore.common.events.EventStoreFactory;
 import org.entcore.common.explorer.ExplorerPluginFactory;
 import org.entcore.common.http.filter.*;
+import org.entcore.common.http.i18n.I18nHandler;
 import org.entcore.common.http.response.SecurityHookRender;
 import org.entcore.common.http.response.OverrideThemeHookRender;
 import org.entcore.common.neo4j.Neo4j;
@@ -75,6 +76,7 @@ public abstract class BaseServer extends Server {
 	private ResourcesProvider resourceProvider = null;
 	private RepositoryHandler repositoryHandler;
 	private SearchingHandler searchingHandler;
+	private I18nHandler i18nHandler;
 	private String schema;
 	private String contentSecurityPolicy;
 	private AccessLogger accessLogger;
@@ -106,6 +108,7 @@ public abstract class BaseServer extends Server {
 
 		repositoryHandler = new RepositoryHandler(getEventBus(vertx));
 		searchingHandler = new SearchingHandler(getEventBus(vertx));
+		i18nHandler = new I18nHandler();
 
 		Config.getInstance().setConfig(config);
 
@@ -142,6 +145,7 @@ public abstract class BaseServer extends Server {
 		}
 		vertx.eventBus().localConsumer("user.repository", repositoryHandler);
 		vertx.eventBus().localConsumer("search.searching", this.searchingHandler);
+		vertx.eventBus().localConsumer(moduleName.toLowerCase()+".i18n", this.i18nHandler);
 
 		loadI18nAssetsFiles();
 
