635cfde22d47c5e881a18afc668d05c651aed393
Zakaria BANI <zakaria.bani@edifice.io>
2024-10-14 17:27:06 +0200

    feat: #MOZO-184, display the author and the date of creation of a manual group in the admin console

--- admin/src/main/ts/src/app/groups/groups.service.ts
+++ admin/src/main/ts/src/app/groups/groups.service.ts
@@ -1,17 +1,17 @@
-import {Injectable} from '@angular/core';
-import {GroupModel} from '../core/store/models/group.model';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
-import {GroupsStore} from './groups.store';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { GroupModel } from '../core/store/models/group.model';
 import { StructureModel } from '../core/store/models/structure.model';
+import { GroupsStore } from './groups.store';
 
 export type GroupUpdatePayload = {
-    name?: string;
-    autolinkTargetAllStructs?: boolean;
-    autolinkTargetStructs?: Array<string>;
-    autolinkUsersFromGroups?: Array<string>;
-}
+  name?: string;
+  autolinkTargetAllStructs?: boolean;
+  autolinkTargetStructs?: Array<string>;
+  autolinkUsersFromGroups?: Array<string>;
+};
 
 @Injectable()
 export class GroupsService {
@@ -31,18 +31,22 @@ export class GroupsService {
         );
     }
 
-    public update(groupId: string, groupUpdatePayload: GroupUpdatePayload): Observable<void> {
-        return this.httpClient.put<void>(`/directory/group/${groupId}`, groupUpdatePayload)
+    public update(groupId: string, groupUpdatePayload: GroupUpdatePayload): Observable<GroupModel> {
+        return this.httpClient.put<GroupModel>(`/directory/group/${groupId}`, groupUpdatePayload)
         .pipe(
-            tap(() => {
+            tap((group) => {
                 const sGroup: GroupModel = this.groupsStore.structure.groups.data.find(g => g.id === groupId);
                 if (sGroup) {
                     sGroup.name = groupUpdatePayload.name;
+                    sGroup.modifiedAt = group.modifiedAt;
+                    sGroup.modifiedByName = group.modifiedByName;
                     sGroup.autolinkTargetAllStructs = groupUpdatePayload.autolinkTargetAllStructs;
                     sGroup.autolinkTargetStructs = groupUpdatePayload.autolinkTargetStructs;
                     sGroup.autolinkUsersFromGroups = groupUpdatePayload.autolinkUsersFromGroups;
                 }
                 this.groupsStore.group.name = groupUpdatePayload.name;
+                this.groupsStore.group.modifiedAt = group.modifiedAt;
+                this.groupsStore.group.modifiedByName = group.modifiedByName;
                 this.groupsStore.group.autolinkTargetAllStructs = groupUpdatePayload.autolinkTargetAllStructs;
                 this.groupsStore.group.autolinkTargetStructs = groupUpdatePayload.autolinkTargetStructs;
                 this.groupsStore.group.autolinkUsersFromGroups = groupUpdatePayload.autolinkUsersFromGroups;
