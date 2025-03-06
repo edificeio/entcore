308c9c789061922d3bcdf224d6313fab953b8453
RomuC <romuald.coulaud@gmail.com>
2025-03-06 14:15:05 +0100

    feat(class-admin): #WB-3882 international number in user info page (#605)

--- directory/src/main/resources/public/ts/app.ts
+++ directory/src/main/resources/public/ts/app.ts
@@ -1,52 +1,53 @@
-import { routes, ng } from 'entcore';
-import { accountController } from './controllers/account';
-import { classAdminController } from './admin/controller';
-import { directoryController } from './controllers/directory';
-import { adaptiveHeight } from './directives/adaptiveHeight';
+import { ng, routes } from "entcore";
+import { classAdminController } from "./admin/controller";
+import { accountController } from "./controllers/account";
+import { directoryController } from "./controllers/directory";
+import { adaptiveHeight } from "./directives/adaptiveHeight";
+import { intlPhoneInputDirective } from "./directives/intlPhoneInput";
 
 routes.define(function ($routeProvider) {
-    if (window.location.href.indexOf('mon-compte') !== -1) {
-        $routeProvider
-            .when('/edit-user/:id', {
-                action: 'editUser'
-            })
-            .when('/edit-user-infos/:id', {
-                action: 'editUserInfos'
-            })
-            .when('/edit-me', {
-                action: 'editMe'
-            })
-            .when('/themes', {
-                action: 'themes'
-            })
-            .otherwise({
-                redirectTo: 'edit-me'
-            });
-    }
-    else {
-        $routeProvider
-            .when('/search', {
-                action: 'directory'
-            })
-            .when('/myClass', {
-                action: 'myClass'
-            })
-            .when("/user-view/:userId", {
-                action: "viewUser"
-            })
-            .when('/:userId', {
-                action: 'viewUser'
-            })
-            .when('/group-view/:groupId', {
-                action: 'viewGroup'
-            })
-            .otherwise({
-                redirectTo: '/myClass'
-            });
-    }
+  if (window.location.href.indexOf("mon-compte") !== -1) {
+    $routeProvider
+      .when("/edit-user/:id", {
+        action: "editUser",
+      })
+      .when("/edit-user-infos/:id", {
+        action: "editUserInfos",
+      })
+      .when("/edit-me", {
+        action: "editMe",
+      })
+      .when("/themes", {
+        action: "themes",
+      })
+      .otherwise({
+        redirectTo: "edit-me",
+      });
+  } else {
+    $routeProvider
+      .when("/search", {
+        action: "directory",
+      })
+      .when("/myClass", {
+        action: "myClass",
+      })
+      .when("/user-view/:userId", {
+        action: "viewUser",
+      })
+      .when("/:userId", {
+        action: "viewUser",
+      })
+      .when("/group-view/:groupId", {
+        action: "viewGroup",
+      })
+      .otherwise({
+        redirectTo: "/myClass",
+      });
+  }
 });
 
 ng.controllers.push(accountController);
 ng.controllers.push(classAdminController);
 ng.controllers.push(directoryController);
 ng.directives.push(adaptiveHeight);
+ng.directives.push(intlPhoneInputDirective);
