f1a338ec4a1b339332edcd7703bf75ad1e069bd8
Théo Vogel <contact@theovogel.fr>
2025-03-19 15:48:32 +0100

    feat(app-registry):INTEG-812 add controller for ptit observatoire widget

--- app-registry/src/main/java/org/entcore/registry/AppRegistry.java
+++ app-registry/src/main/java/org/entcore/registry/AppRegistry.java
@@ -19,8 +19,8 @@
 
 package org.entcore.registry;
 
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
 import org.entcore.common.appregistry.AppRegistryEventsHandler;
 import org.entcore.common.http.BaseServer;
 import org.entcore.registry.controllers.*;
@@ -39,6 +39,9 @@ public class AppRegistry extends BaseServer {
 		JsonObject eduMalinConf = config.getJsonObject("edumalin-widget-config");
 		if(eduMalinConf != null)
 			addController(new EdumalinWidgetController());
+		JsonObject ptitObservatoireConf = config.getJsonObject("ptit-observatoire-widget-config");
+		if (ptitObservatoireConf != null)
+			addController(new PtitObservatoireController());
 		setDefaultResourceFilter(new AppRegistryFilter());
 		new AppRegistryEventsHandler(vertx, new NopAppRegistryEventService());
 		vertx.eventBus().publish("app-registry.loaded", new JsonObject());
