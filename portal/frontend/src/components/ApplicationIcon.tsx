b8c4e2889ff7080cd995d8178457870c0c85b283
evanthonyly <anthony.ly@edifice.io>
2025-06-05 17:47:42 +0200

    fix: empty string if no name

--- portal/frontend/src/components/ApplicationIcon.tsx
+++ portal/frontend/src/components/ApplicationIcon.tsx
@@ -6,7 +6,9 @@ import { getIconClass } from '~/utils/icon-class-name';
 export function ApplicationIcon({ data }: { data: Application }) {
   const { t } = useTranslation('common');
   const iconClassName = getIconClass(data);
-  const appName = data.prefix ? t(data.prefix.substring(1)) : data.displayName;
+  const appName = data.prefix
+    ? t(data.prefix.substring(1))
+    : data.displayName || '';
   return (
     <div style={{ width: 107, height: 127, textAlign: 'center' }}>
       <a
