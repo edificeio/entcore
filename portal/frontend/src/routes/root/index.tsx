f44d938f7865289c54c020e596f1b3a153a53353
evanthonyly <anthony.ly@edifice.io>
2025-06-05 17:21:21 +0200

    fix: common on translate (#758)

--- portal/frontend/src/routes/root/index.tsx
+++ portal/frontend/src/routes/root/index.tsx
@@ -1,6 +1,8 @@
-import { Layout, LoadingScreen, useEdificeClient } from '@edifice.io/react';
+import { Layout, LoadingScreen, useEdificeClient, useEdificeTheme } from '@edifice.io/react';
 import { matchPath } from 'react-router-dom';
 import { basename } from '..';
+import { useEffect } from 'react';
+import { MyAppLayout } from '~/layouts/MyAppsLayout';
 
 
 /** Check old format URL and redirect if needed */
@@ -25,10 +27,26 @@ export const loader = async () => {
 
 export const Root = () => {
   const { init } = useEdificeClient();
+  const { theme } = useEdificeTheme();
+
+  // Load theme for icons
+  useEffect(() => {
+    if (!theme) return;
+    const themeMode = theme.is1d ? 'one' : 'neo';
+    const url = `/assets/themes/ode-bootstrap-${themeMode}/skins/default/theme.css`;
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url;
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, [theme]);
 
   if (!init) return <LoadingScreen position={false} />;
 
-  return init ? <Layout>my app</Layout> : null;
+  return init ? <Layout><MyAppLayout /></Layout> : null;
 };
 
 export default Root;
