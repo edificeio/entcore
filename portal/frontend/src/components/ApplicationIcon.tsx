90c99c4f43c052717e641bb23eb5e300eb22218d
evanthonyly <anthony.ly@edifice.io>
2025-06-06 15:59:45 +0200

    chore: display name of services app (#764)

--- portal/frontend/src/components/ApplicationIcon.tsx
+++ portal/frontend/src/components/ApplicationIcon.tsx
@@ -1,22 +1,29 @@
-import { Application } from "~/models/application";
-import './application-icon.css';
+import { Application } from '~/models/application';
 import { useTranslation } from 'react-i18next';
 import { getIconClass } from '~/utils/icon-class-name';
+import './application-icon.css';
+import { getAppName } from '~/utils/get-app-name';
 
 export function ApplicationIcon({ data }: { data: Application }) {
   const { t } = useTranslation('common');
+  const isImage = data.icon.includes('/');
   const iconClassName = getIconClass(data);
-  const appName = data.prefix
-    ? t(data.prefix.substring(1))
-    : t(data.name) || data.displayName;
+  const appName = getAppName(data, t);
 
   return (
     <div style={{ width: 107, height: 127, textAlign: 'center' }}>
       <a
+        className="application-icon rounded bg-primary mb-8 d-flex align-items-center justify-content-center mx-auto"
         href={data.address}
-        className="application-icon rounded bg-primary mb-8"
+        rel={data.isExternal ? 'noopener noreferrer' : undefined}
+        style={{ width: 64, height: 64 }}
+        target={data.isExternal ? '_blank' : undefined}
       >
-        <i className={iconClassName} />
+        {isImage ? (
+          <img src={data.icon} alt={appName} />
+        ) : (
+          <i className={iconClassName} />
+        )}
       </a>
       <h1
         className="small text-gray-900"
--- portal/frontend/src/services/queries/applications.ts
+++ portal/frontend/src/services/queries/applications.ts
@@ -1,10 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { applicationsService } from "../api";
+import mockData from '~/mocks/mockApplications.json';
 
 export const useApplications = () => {
   const query = useQuery({
     queryKey: ['applications'],
-    queryFn: () => applicationsService.getApplications(),
+    queryFn: async () => {
+      if (import.meta.env.DEV) {
+        return mockData;
+      }
+      return applicationsService.getApplications();
+    },
   });
 
   const displayedApps = query.data?.apps.filter((app) => app.display);
