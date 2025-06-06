449d58e79fdba07527206c4df7c4642ecfa094ca
evanthonyly <anthony.ly@edifice.io>
2025-06-06 12:13:12 +0200

    chore: display connecteurs (#761)

--- portal/frontend/src/features/application-list/ApplicationList.tsx
+++ portal/frontend/src/features/application-list/ApplicationList.tsx
@@ -1,19 +1,51 @@
-import { ApplicationIcon } from "~/components/ApplicationIcon"
-import { useApplications } from "~/services"
+import { useTranslation } from 'react-i18next';
+import { ApplicationIcon } from '~/components/ApplicationIcon';
+import { useApplications } from '~/services';
 
 export function ApplicationList() {
   const { applications, isLoading, isError } = useApplications();
+  const { t } = useTranslation('common');
 
   if (isLoading) return <div>Chargement des applications...</div>;
   if (isError) return <div>Erreur lors du chargement des applications.</div>;
+
+  const sortedApps = [...(applications || [])].sort((a, b) =>
+    t(a.prefix.substring(1)).localeCompare(t(b.prefix.substring(1))),
+  );
+
+  const internalApps = sortedApps.filter((app) => !app.isExternal);
+  const externalApps = sortedApps.filter((app) => app.isExternal);
+
   return (
-    <div
-      className="d-flex flex-wrap gap-16 justify-content-center mx-auto"
-      style={{ maxWidth: 1091 }}
-    >
-      {applications?.map((app) => (
-        <ApplicationIcon key={app.name} data={app} />
-      ))}
-    </div>
+    <>
+      <div
+        className="d-flex flex-wrap gap-16 justify-content-center mx-auto"
+        style={{ maxWidth: 1091 }}
+      >
+        {internalApps.map((app) => (
+          <ApplicationIcon key={app.name} data={app} />
+        ))}
+      </div>
+      {(externalApps.length && (
+        <div className="d-flex flex-wrap gap-16 flex-column">
+          <div className="w-full bg-gray-400" style={{ height: 1 }}></div>
+          <h2
+            className="small text-center my-8"
+            style={{ fontFamily: 'Arimo' }}
+          >
+            Autres services connectés
+          </h2>
+          <div
+            className="d-flex flex-wrap gap-32 justify-content-center mx-auto"
+            style={{ maxWidth: 1091 }}
+          >
+            {internalApps.map((app) => (
+              <ApplicationIcon key={app.name} data={app} />
+            ))}
+          </div>
+        </div>
+      )) ||
+        null}
+    </>
   );
 }
--- portal/frontend/src/layouts/MyAppsLayout.tsx
+++ portal/frontend/src/layouts/MyAppsLayout.tsx
@@ -3,10 +3,12 @@ import { useTranslation } from 'react-i18next';
 
 export const MyAppLayout = () => {
   const { t } = useTranslation('common');
-  return <div className="d-flex flex-column gap-24 px-8 py-24">
-    <header>
-      <h1 className="m-0">{t("navbar.applications")}</h1>
-    </header>
-    <ApplicationList />
-  </div>
+  return (
+    <div className="d-flex flex-column gap-24 px-8 py-24">
+      <header>
+        <h1 className="m-0 h3 text-info">{t('navbar.applications')}</h1>
+      </header>
+      <ApplicationList />
+    </div>
+  );
 }
