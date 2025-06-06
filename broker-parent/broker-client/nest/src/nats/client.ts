0e936525bf475e5bd1b8604a012effdacf753097
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-06-06 10:19:45 +0200

    feat: #RBACK-67, nats client for quarkus apps

--- broker-parent/broker-client/nest/src/nats/client.ts
+++ broker-parent/broker-client/nest/src/nats/client.ts
@@ -2,63 +2,63 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
-import type { UpdateGroupResponseDTO } from './types';
+import type { AddGroupMemberRequestDTO } from './types';
 
 import type { AddGroupMemberResponseDTO } from './types';
 
 import type { AppRegistrationRequestDTO } from './types';
 
-import type { FindSessionRequestDTO } from './types';
-
 import type { AppRegistrationResponseDTO } from './types';
-
-import type { AddGroupMemberRequestDTO } from './types';
-
-import type { FindGroupByExternalIdResponseDTO } from './types';
-
-import type { ListenOnlyDTO } from './types';
-
-import type { FetchTranslationsResponseDTO } from './types';
-
-import type { RemoveGroupSharesRequestDTO } from './types';
-
-import type { DeleteGroupRequestDTO } from './types';
-
-import type { UpsertGroupSharesRequestDTO } from './types';
-
-import type { UpdateGroupRequestDTO } from './types';
-
-import type { RemoveGroupMemberRequestDTO } from './types';
-
-import type { RemoveGroupMemberResponseDTO } from './types';
-
-import type { GetUserDisplayNamesRequestDTO } from './types';
-
-import type { RemoveGroupSharesResponseDTO } from './types';
-
-import type { ListenAndAnswerDTO } from './types';
-
-import type { FetchTranslationsRequestDTO } from './types';
-
-import type { UpsertGroupSharesResponseDTO } from './types';
-
-import type { FindGroupByExternalIdRequestDTO } from './types';
-
-import type { RegisterTranslationFilesRequestDTO } from './types';
-
-import type { GetUserDisplayNamesResponseDTO } from './types';
-
-import type { FindSessionResponseDTO } from './types';
 
 import type { CreateGroupRequestDTO } from './types';
 
 import type { CreateGroupResponseDTO } from './types';
 
-import type { RegisterTranslationFilesResponseDTO } from './types';
+import type { DeleteGroupRequestDTO } from './types';
 
 import type { DeleteGroupResponseDTO } from './types';
 
 import type { DummyResponseDTO } from './types';
+
+import type { FetchTranslationsRequestDTO } from './types';
+
+import type { FetchTranslationsResponseDTO } from './types';
+
+import type { FindGroupByExternalIdRequestDTO } from './types';
+
+import type { FindGroupByExternalIdResponseDTO } from './types';
+
+import type { FindSessionRequestDTO } from './types';
+
+import type { FindSessionResponseDTO } from './types';
+
+import type { GetUserDisplayNamesRequestDTO } from './types';
+
+import type { GetUserDisplayNamesResponseDTO } from './types';
+
+import type { ListenAndAnswerDTO } from './types';
+
+import type { ListenOnlyDTO } from './types';
+
+import type { RegisterTranslationFilesRequestDTO } from './types';
+
+import type { RegisterTranslationFilesResponseDTO } from './types';
+
+import type { RemoveGroupMemberRequestDTO } from './types';
+
+import type { RemoveGroupMemberResponseDTO } from './types';
+
+import type { RemoveGroupSharesRequestDTO } from './types';
+
+import type { RemoveGroupSharesResponseDTO } from './types';
+
+import type { UpdateGroupRequestDTO } from './types';
+
+import type { UpdateGroupResponseDTO } from './types';
+
+import type { UpsertGroupSharesRequestDTO } from './types';
+
+import type { UpsertGroupSharesResponseDTO } from './types';
 
 
 @Injectable()
@@ -72,69 +72,14 @@ export class EntNatsServiceClient {
 
   
   
-  async listenOnlyExample(request: ListenOnlyDTO) {
-    const eventAddress = "ent.test.listen";
-    this.natsClient.emit(eventAddress, request);
-  }
-  
-  
-  
-  async listenAndAnswerExample(request: ListenAndAnswerDTO): Promise<DummyResponseDTO> {
-    const eventAddress = "ent.test.listen.reply";
+  async addGroupMember(request: AddGroupMemberRequestDTO): Promise<AddGroupMemberResponseDTO> {
+    const eventAddress = "directory.group.member.add";
     console.debug("Sending request to NATS subject, " + eventAddress);
     const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
     if(!reply) {
       throw new Error('No reply received');
     }
-    return JSON.parse(reply) as DummyResponseDTO;
-  }
-  
-  
-  
-  async fetchTranslations(request: FetchTranslationsRequestDTO, application: string): Promise<FetchTranslationsResponseDTO> {
-    const eventAddress = "i18n." + application + ".fetch";
-    console.debug("Sending request to NATS subject, " + eventAddress);
-    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
-    if(!reply) {
-      throw new Error('No reply received');
-    }
-    return JSON.parse(reply) as FetchTranslationsResponseDTO;
-  }
-  
-  
-  
-  async registerI18nFiles(request: RegisterTranslationFilesRequestDTO, application: string): Promise<RegisterTranslationFilesResponseDTO> {
-    const eventAddress = "i18n." + application + ".register";
-    console.debug("Sending request to NATS subject, " + eventAddress);
-    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
-    if(!reply) {
-      throw new Error('No reply received');
-    }
-    return JSON.parse(reply) as RegisterTranslationFilesResponseDTO;
-  }
-  
-  
-  
-  async upsertGroupShares(request: UpsertGroupSharesRequestDTO, application: string): Promise<UpsertGroupSharesResponseDTO> {
-    const eventAddress = "share.group.upsert." + application + "";
-    console.debug("Sending request to NATS subject, " + eventAddress);
-    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
-    if(!reply) {
-      throw new Error('No reply received');
-    }
-    return JSON.parse(reply) as UpsertGroupSharesResponseDTO;
-  }
-  
-  
-  
-  async removeGroupShares(request: RemoveGroupSharesRequestDTO, application: string): Promise<RemoveGroupSharesResponseDTO> {
-    const eventAddress = "share.group.remove." + application + "";
-    console.debug("Sending request to NATS subject, " + eventAddress);
-    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
-    if(!reply) {
-      throw new Error('No reply received');
-    }
-    return JSON.parse(reply) as RemoveGroupSharesResponseDTO;
+    return JSON.parse(reply) as AddGroupMemberResponseDTO;
   }
   
   
@@ -151,18 +96,6 @@ export class EntNatsServiceClient {
   
   
   
-  async updateManualGroup(request: UpdateGroupRequestDTO): Promise<UpdateGroupResponseDTO> {
-    const eventAddress = "directory.group.manual.update";
-    console.debug("Sending request to NATS subject, " + eventAddress);
-    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
-    if(!reply) {
-      throw new Error('No reply received');
-    }
-    return JSON.parse(reply) as UpdateGroupResponseDTO;
-  }
-  
-  
-  
   async deleteManualGroup(request: DeleteGroupRequestDTO): Promise<DeleteGroupResponseDTO> {
     const eventAddress = "directory.group.manual.delete";
     console.debug("Sending request to NATS subject, " + eventAddress);
@@ -175,26 +108,14 @@ export class EntNatsServiceClient {
   
   
   
-  async addGroupMember(request: AddGroupMemberRequestDTO): Promise<AddGroupMemberResponseDTO> {
-    const eventAddress = "directory.group.member.add";
+  async fetchTranslations(request: FetchTranslationsRequestDTO, application: string): Promise<FetchTranslationsResponseDTO> {
+    const eventAddress = "i18n." + application + ".fetch";
     console.debug("Sending request to NATS subject, " + eventAddress);
     const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
     if(!reply) {
       throw new Error('No reply received');
     }
-    return JSON.parse(reply) as AddGroupMemberResponseDTO;
-  }
-  
-  
-  
-  async removeGroupMember(request: RemoveGroupMemberRequestDTO): Promise<RemoveGroupMemberResponseDTO> {
-    const eventAddress = "directory.group.member.delete";
-    console.debug("Sending request to NATS subject, " + eventAddress);
-    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
-    if(!reply) {
-      throw new Error('No reply received');
-    }
-    return JSON.parse(reply) as RemoveGroupMemberResponseDTO;
+    return JSON.parse(reply) as FetchTranslationsResponseDTO;
   }
   
   
@@ -211,6 +132,18 @@ export class EntNatsServiceClient {
   
   
   
+  async findSession(request: FindSessionRequestDTO): Promise<FindSessionResponseDTO> {
+    const eventAddress = "session.find";
+    console.debug("Sending request to NATS subject, " + eventAddress);
+    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
+    if(!reply) {
+      throw new Error('No reply received');
+    }
+    return JSON.parse(reply) as FindSessionResponseDTO;
+  }
+  
+  
+  
   async getUserDisplayNames(request: GetUserDisplayNamesRequestDTO): Promise<GetUserDisplayNamesResponseDTO> {
     const eventAddress = "directory.users.get.displaynames";
     console.debug("Sending request to NATS subject, " + eventAddress);
@@ -223,14 +156,21 @@ export class EntNatsServiceClient {
   
   
   
-  async findSession(request: FindSessionRequestDTO): Promise<FindSessionResponseDTO> {
-    const eventAddress = "session.find";
+  async listenOnlyExample(request: ListenOnlyDTO) {
+    const eventAddress = "ent.test.listen";
+    this.natsClient.emit(eventAddress, request);
+  }
+  
+  
+  
+  async listenOnlyExample(request: ListenAndAnswerDTO): Promise<DummyResponseDTO> {
+    const eventAddress = "ent.test.listen.reply";
     console.debug("Sending request to NATS subject, " + eventAddress);
     const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
     if(!reply) {
       throw new Error('No reply received');
     }
-    return JSON.parse(reply) as FindSessionResponseDTO;
+    return JSON.parse(reply) as DummyResponseDTO;
   }
   
   
@@ -247,6 +187,42 @@ export class EntNatsServiceClient {
   
   
   
+  async registerI18nFiles(request: RegisterTranslationFilesRequestDTO, application: string): Promise<RegisterTranslationFilesResponseDTO> {
+    const eventAddress = "i18n." + application + ".register";
+    console.debug("Sending request to NATS subject, " + eventAddress);
+    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
+    if(!reply) {
+      throw new Error('No reply received');
+    }
+    return JSON.parse(reply) as RegisterTranslationFilesResponseDTO;
+  }
+  
+  
+  
+  async removeGroupMember(request: RemoveGroupMemberRequestDTO): Promise<RemoveGroupMemberResponseDTO> {
+    const eventAddress = "directory.group.member.delete";
+    console.debug("Sending request to NATS subject, " + eventAddress);
+    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
+    if(!reply) {
+      throw new Error('No reply received');
+    }
+    return JSON.parse(reply) as RemoveGroupMemberResponseDTO;
+  }
+  
+  
+  
+  async removeGroupShares(request: RemoveGroupSharesRequestDTO, application: string): Promise<RemoveGroupSharesResponseDTO> {
+    const eventAddress = "share.group.remove." + application + "";
+    console.debug("Sending request to NATS subject, " + eventAddress);
+    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
+    if(!reply) {
+      throw new Error('No reply received');
+    }
+    return JSON.parse(reply) as RemoveGroupSharesResponseDTO;
+  }
+  
+  
+  
   async testApplication(request: AppRegistrationRequestDTO, application: string): Promise<AppRegistrationResponseDTO> {
     const eventAddress = "ent." + application + ".test";
     console.debug("Sending request to NATS subject, " + eventAddress);
@@ -255,6 +231,30 @@ export class EntNatsServiceClient {
       throw new Error('No reply received');
     }
     return JSON.parse(reply) as AppRegistrationResponseDTO;
+  }
+  
+  
+  
+  async updateManualGroup(request: UpdateGroupRequestDTO): Promise<UpdateGroupResponseDTO> {
+    const eventAddress = "directory.group.manual.update";
+    console.debug("Sending request to NATS subject, " + eventAddress);
+    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
+    if(!reply) {
+      throw new Error('No reply received');
+    }
+    return JSON.parse(reply) as UpdateGroupResponseDTO;
+  }
+  
+  
+  
+  async upsertGroupShares(request: UpsertGroupSharesRequestDTO, application: string): Promise<UpsertGroupSharesResponseDTO> {
+    const eventAddress = "share.group.upsert." + application + "";
+    console.debug("Sending request to NATS subject, " + eventAddress);
+    const reply = await firstValueFrom(this.natsClient.send(eventAddress, request));
+    if(!reply) {
+      throw new Error('No reply received');
+    }
+    return JSON.parse(reply) as UpsertGroupSharesResponseDTO;
   }
   
   
--- broker-parent/broker-client/quarkus/src/main/java/org/entcore/broker/EntNatsServiceClient.java
+++ broker-parent/broker-client/quarkus/src/main/java/org/entcore/broker/EntNatsServiceClient.java
@@ -22,89 +22,14 @@ public class EntNatsServiceClient {
   }
   
   
-  public void listenOnlyExample(org.entcore.broker.api.dto.ListenOnlyDTO request) {
-    String subject = "ent.test.listen";
-    Log.debug("Sending request to NATS subject: " + subject);
-    try {
-      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
-      
-      this.connection.publish(subject, payload);
-      return;
-      
-    } catch (Exception e) {
-      Log.error("Failed to send request to NATS", e);
-      throw new RuntimeException("Failed to send request to NATS", e);
-    }
-  }
-  
-  public org.entcore.broker.api.dto.DummyResponseDTO listenAndAnswerExample(org.entcore.broker.api.dto.ListenAndAnswerDTO request) {
-    String subject = "ent.test.listen.reply";
+  public org.entcore.broker.api.dto.directory.AddGroupMemberResponseDTO addGroupMember(org.entcore.broker.api.dto.directory.AddGroupMemberRequestDTO request) {
+    String subject = "directory.group.member.add";
     Log.debug("Sending request to NATS subject: " + subject);
     try {
       final byte[] payload = this.objectMapper.writeValueAsBytes(request);
       
       byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.DummyResponseDTO.class);
-      
-    } catch (Exception e) {
-      Log.error("Failed to send request to NATS", e);
-      throw new RuntimeException("Failed to send request to NATS", e);
-    }
-  }
-  
-  public org.entcore.broker.api.dto.i18n.FetchTranslationsResponseDTO fetchTranslations(org.entcore.broker.api.dto.i18n.FetchTranslationsRequestDTO request, final String application) {
-    String subject = "i18n." + application + ".fetch";
-    Log.debug("Sending request to NATS subject: " + subject);
-    try {
-      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
-      
-      byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.i18n.FetchTranslationsResponseDTO.class);
-      
-    } catch (Exception e) {
-      Log.error("Failed to send request to NATS", e);
-      throw new RuntimeException("Failed to send request to NATS", e);
-    }
-  }
-  
-  public org.entcore.broker.api.dto.i18n.RegisterTranslationFilesResponseDTO registerI18nFiles(org.entcore.broker.api.dto.i18n.RegisterTranslationFilesRequestDTO request, final String application) {
-    String subject = "i18n." + application + ".register";
-    Log.debug("Sending request to NATS subject: " + subject);
-    try {
-      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
-      
-      byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.i18n.RegisterTranslationFilesResponseDTO.class);
-      
-    } catch (Exception e) {
-      Log.error("Failed to send request to NATS", e);
-      throw new RuntimeException("Failed to send request to NATS", e);
-    }
-  }
-  
-  public org.entcore.broker.api.dto.shares.UpsertGroupSharesResponseDTO upsertGroupShares(org.entcore.broker.api.dto.shares.UpsertGroupSharesRequestDTO request, final String application) {
-    String subject = "share.group.upsert." + application + "";
-    Log.debug("Sending request to NATS subject: " + subject);
-    try {
-      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
-      
-      byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.shares.UpsertGroupSharesResponseDTO.class);
-      
-    } catch (Exception e) {
-      Log.error("Failed to send request to NATS", e);
-      throw new RuntimeException("Failed to send request to NATS", e);
-    }
-  }
-  
-  public org.entcore.broker.api.dto.shares.RemoveGroupSharesResponseDTO removeGroupShares(org.entcore.broker.api.dto.shares.RemoveGroupSharesRequestDTO request, final String application) {
-    String subject = "share.group.remove." + application + "";
-    Log.debug("Sending request to NATS subject: " + subject);
-    try {
-      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
-      
-      byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.shares.RemoveGroupSharesResponseDTO.class);
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.directory.AddGroupMemberResponseDTO.class);
       
     } catch (Exception e) {
       Log.error("Failed to send request to NATS", e);
@@ -127,21 +52,6 @@ public class EntNatsServiceClient {
     }
   }
   
-  public org.entcore.broker.api.dto.directory.UpdateGroupResponseDTO updateManualGroup(org.entcore.broker.api.dto.directory.UpdateGroupRequestDTO request) {
-    String subject = "directory.group.manual.update";
-    Log.debug("Sending request to NATS subject: " + subject);
-    try {
-      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
-      
-      byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.directory.UpdateGroupResponseDTO.class);
-      
-    } catch (Exception e) {
-      Log.error("Failed to send request to NATS", e);
-      throw new RuntimeException("Failed to send request to NATS", e);
-    }
-  }
-  
   public org.entcore.broker.api.dto.directory.DeleteGroupResponseDTO deleteManualGroup(org.entcore.broker.api.dto.directory.DeleteGroupRequestDTO request) {
     String subject = "directory.group.manual.delete";
     Log.debug("Sending request to NATS subject: " + subject);
@@ -157,29 +67,14 @@ public class EntNatsServiceClient {
     }
   }
   
-  public org.entcore.broker.api.dto.directory.AddGroupMemberResponseDTO addGroupMember(org.entcore.broker.api.dto.directory.AddGroupMemberRequestDTO request) {
-    String subject = "directory.group.member.add";
+  public org.entcore.broker.api.dto.i18n.FetchTranslationsResponseDTO fetchTranslations(org.entcore.broker.api.dto.i18n.FetchTranslationsRequestDTO request, final String application) {
+    String subject = "i18n." + application + ".fetch";
     Log.debug("Sending request to NATS subject: " + subject);
     try {
       final byte[] payload = this.objectMapper.writeValueAsBytes(request);
       
       byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.directory.AddGroupMemberResponseDTO.class);
-      
-    } catch (Exception e) {
-      Log.error("Failed to send request to NATS", e);
-      throw new RuntimeException("Failed to send request to NATS", e);
-    }
-  }
-  
-  public org.entcore.broker.api.dto.directory.RemoveGroupMemberResponseDTO removeGroupMember(org.entcore.broker.api.dto.directory.RemoveGroupMemberRequestDTO request) {
-    String subject = "directory.group.member.delete";
-    Log.debug("Sending request to NATS subject: " + subject);
-    try {
-      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
-      
-      byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.directory.RemoveGroupMemberResponseDTO.class);
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.i18n.FetchTranslationsResponseDTO.class);
       
     } catch (Exception e) {
       Log.error("Failed to send request to NATS", e);
@@ -202,6 +97,21 @@ public class EntNatsServiceClient {
     }
   }
   
+  public org.entcore.broker.api.dto.session.FindSessionResponseDTO findSession(org.entcore.broker.api.dto.session.FindSessionRequestDTO request) {
+    String subject = "session.find";
+    Log.debug("Sending request to NATS subject: " + subject);
+    try {
+      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
+      
+      byte[] response = connection.request(subject, payload).get().getData();
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.session.FindSessionResponseDTO.class);
+      
+    } catch (Exception e) {
+      Log.error("Failed to send request to NATS", e);
+      throw new RuntimeException("Failed to send request to NATS", e);
+    }
+  }
+  
   public org.entcore.broker.api.dto.directory.GetUserDisplayNamesResponseDTO getUserDisplayNames(org.entcore.broker.api.dto.directory.GetUserDisplayNamesRequestDTO request) {
     String subject = "directory.users.get.displaynames";
     Log.debug("Sending request to NATS subject: " + subject);
@@ -217,14 +127,29 @@ public class EntNatsServiceClient {
     }
   }
   
-  public org.entcore.broker.api.dto.session.FindSessionResponseDTO findSession(org.entcore.broker.api.dto.session.FindSessionRequestDTO request) {
-    String subject = "session.find";
+  public void listenOnlyExample(org.entcore.broker.api.dto.ListenOnlyDTO request) {
+    String subject = "ent.test.listen";
+    Log.debug("Sending request to NATS subject: " + subject);
+    try {
+      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
+      
+      this.connection.publish(subject, payload);
+      return;
+      
+    } catch (Exception e) {
+      Log.error("Failed to send request to NATS", e);
+      throw new RuntimeException("Failed to send request to NATS", e);
+    }
+  }
+  
+  public org.entcore.broker.api.dto.DummyResponseDTO listenOnlyExample(org.entcore.broker.api.dto.ListenAndAnswerDTO request) {
+    String subject = "ent.test.listen.reply";
     Log.debug("Sending request to NATS subject: " + subject);
     try {
       final byte[] payload = this.objectMapper.writeValueAsBytes(request);
       
       byte[] response = connection.request(subject, payload).get().getData();
-      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.session.FindSessionResponseDTO.class);
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.DummyResponseDTO.class);
       
     } catch (Exception e) {
       Log.error("Failed to send request to NATS", e);
@@ -247,6 +172,51 @@ public class EntNatsServiceClient {
     }
   }
   
+  public org.entcore.broker.api.dto.i18n.RegisterTranslationFilesResponseDTO registerI18nFiles(org.entcore.broker.api.dto.i18n.RegisterTranslationFilesRequestDTO request, final String application) {
+    String subject = "i18n." + application + ".register";
+    Log.debug("Sending request to NATS subject: " + subject);
+    try {
+      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
+      
+      byte[] response = connection.request(subject, payload).get().getData();
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.i18n.RegisterTranslationFilesResponseDTO.class);
+      
+    } catch (Exception e) {
+      Log.error("Failed to send request to NATS", e);
+      throw new RuntimeException("Failed to send request to NATS", e);
+    }
+  }
+  
+  public org.entcore.broker.api.dto.directory.RemoveGroupMemberResponseDTO removeGroupMember(org.entcore.broker.api.dto.directory.RemoveGroupMemberRequestDTO request) {
+    String subject = "directory.group.member.delete";
+    Log.debug("Sending request to NATS subject: " + subject);
+    try {
+      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
+      
+      byte[] response = connection.request(subject, payload).get().getData();
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.directory.RemoveGroupMemberResponseDTO.class);
+      
+    } catch (Exception e) {
+      Log.error("Failed to send request to NATS", e);
+      throw new RuntimeException("Failed to send request to NATS", e);
+    }
+  }
+  
+  public org.entcore.broker.api.dto.shares.RemoveGroupSharesResponseDTO removeGroupShares(org.entcore.broker.api.dto.shares.RemoveGroupSharesRequestDTO request, final String application) {
+    String subject = "share.group.remove." + application + "";
+    Log.debug("Sending request to NATS subject: " + subject);
+    try {
+      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
+      
+      byte[] response = connection.request(subject, payload).get().getData();
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.shares.RemoveGroupSharesResponseDTO.class);
+      
+    } catch (Exception e) {
+      Log.error("Failed to send request to NATS", e);
+      throw new RuntimeException("Failed to send request to NATS", e);
+    }
+  }
+  
   public org.entcore.broker.api.appregistry.AppRegistrationResponseDTO testApplication(org.entcore.broker.api.appregistry.AppRegistrationRequestDTO request, final String application) {
     String subject = "ent." + application + ".test";
     Log.debug("Sending request to NATS subject: " + subject);
@@ -255,6 +225,36 @@ public class EntNatsServiceClient {
       
       byte[] response = connection.request(subject, payload).get().getData();
       return this.objectMapper.readValue(response, org.entcore.broker.api.appregistry.AppRegistrationResponseDTO.class);
+      
+    } catch (Exception e) {
+      Log.error("Failed to send request to NATS", e);
+      throw new RuntimeException("Failed to send request to NATS", e);
+    }
+  }
+  
+  public org.entcore.broker.api.dto.directory.UpdateGroupResponseDTO updateManualGroup(org.entcore.broker.api.dto.directory.UpdateGroupRequestDTO request) {
+    String subject = "directory.group.manual.update";
+    Log.debug("Sending request to NATS subject: " + subject);
+    try {
+      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
+      
+      byte[] response = connection.request(subject, payload).get().getData();
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.directory.UpdateGroupResponseDTO.class);
+      
+    } catch (Exception e) {
+      Log.error("Failed to send request to NATS", e);
+      throw new RuntimeException("Failed to send request to NATS", e);
+    }
+  }
+  
+  public org.entcore.broker.api.dto.shares.UpsertGroupSharesResponseDTO upsertGroupShares(org.entcore.broker.api.dto.shares.UpsertGroupSharesRequestDTO request, final String application) {
+    String subject = "share.group.upsert." + application + "";
+    Log.debug("Sending request to NATS subject: " + subject);
+    try {
+      final byte[] payload = this.objectMapper.writeValueAsBytes(request);
+      
+      byte[] response = connection.request(subject, payload).get().getData();
+      return this.objectMapper.readValue(response, org.entcore.broker.api.dto.shares.UpsertGroupSharesResponseDTO.class);
       
     } catch (Exception e) {
       Log.error("Failed to send request to NATS", e);
