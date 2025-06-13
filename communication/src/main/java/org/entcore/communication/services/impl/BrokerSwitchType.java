8a4f95e03f153a38c2e60819d2d0b9189dac3fd8
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-06-13 16:39:01 +0200

    feat(broker): #RBACK-83, implement a service switch to read/write from the new communication rules service

---
+++ communication/src/main/java/org/entcore/communication/services/impl/BrokerSwitchType.java
@@ -0,0 +1,8 @@
+package org.entcore.communication.services.impl;
+
+public enum BrokerSwitchType {
+  READ_LEGACY_WRITE_LEGACY,
+  READ_LEGACY_WRITE_BOTH,
+  READ_NEW_WRITE_NEW,
+  READ_NEW_WRITE_BOTH,
+}
