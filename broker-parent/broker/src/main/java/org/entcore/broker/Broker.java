7b15b3b6f009746386ad733934c50a88e864b03a
Junior BERNARD <junior.bernard@opendigitaleducation.com>
2025-06-13 16:39:01 +0200

    feat(broker): #RBACK-83, implement a service switch to read/write from the new communication rules service

--- broker-parent/broker/src/main/java/org/entcore/broker/Broker.java
+++ broker-parent/broker/src/main/java/org/entcore/broker/Broker.java
@@ -1,41 +1,81 @@
 package org.entcore.broker;
 
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import org.entcore.broker.client.BrokerClient;
 import org.entcore.broker.client.BrokerClientFactory;
 import org.entcore.broker.controllers.BrokerController;
 import org.entcore.common.http.BaseServer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Broker extends BaseServer {
 
   private BrokerClient brokerClient;
+  private MessageConsumer<JsonObject> migrationConsumer;
+
 
   @Override
   public void start(final Promise<Void> startPromise) throws Exception {
     super.start(startPromise);
     addController(new BrokerController());
     brokerClient = BrokerClientFactory.getClient(vertx);
-    brokerClient.start().onComplete(ar -> {
-      if (ar.succeeded()) {
-        log.info("Broker client started successfully.");
-        startPromise.tryComplete();
-      } else {
-        log.error("Failed to start broker client.", ar.cause());
-        startPromise.tryFail(ar.cause());
-      }
+    brokerClient.start().onSuccess(ar -> {
+      log.info("Broker client started successfully.");
+      this.migrationConsumer = vertx.eventBus().consumer("broker.proxy.legacy.migration", this::onMigrationMessage);
+      startPromise.tryComplete();
+    }).onFailure(ar -> {
+      log.error("Failed to start broker client.", ar);
+      startPromise.tryFail(ar);
     });
   }
 
-  @Override
-  public void stop(Promise<Void> stopPromise) throws Exception {
-    log.info("Stopping Broker...");
-    super.stop(stopPromise);
-    brokerClient.close().onSuccess(e -> {
-      log.info("Broker client stopped successfully.");
-      stopPromise.tryComplete();
-    }).onFailure(err -> {
-      log.error("Failed to stop broker client.", err);
-      stopPromise.tryFail(err);
+  private void onMigrationMessage(final Message<JsonObject> message) {
+    final JsonObject payload = message.body();
+    final String subject = "broker.proxy.legacy.migration." + payload.getString("service") + "." + payload.getString("action");
+    brokerClient.request(subject, payload.toString()).onSuccess(downstreamServiceResponse -> {
+      message.reply(parseProxyfiedMigrationResponse(downstreamServiceResponse));
+    }).onFailure(th -> {
+      log.error("An error occurred while processing migration message: " + payload, th);
+      message.fail(500, th.getMessage());
     });
+  }
+
+  private Object parseProxyfiedMigrationResponse(Object downstreamServiceResponse) {
+    final String rawResponse;
+    if (downstreamServiceResponse instanceof byte[]) {
+      final byte[] responseBytes = (byte[]) downstreamServiceResponse;
+      rawResponse = new String(responseBytes);
+    } else if (downstreamServiceResponse instanceof String) {
+      rawResponse = ((String) downstreamServiceResponse);
+    } else {
+      return JsonObject.mapFrom(downstreamServiceResponse);
+    }
+    if (rawResponse.charAt(0) == '{') {
+      return new JsonObject(rawResponse);
+    } else {
+      return new JsonArray(rawResponse);
+    }
+  }
+
+  @Override
+  public void stop(Promise<Void> stopPromise) {
+    log.info("Stopping Broker...");
+    final List<Future<?>> futures = new ArrayList<>();
+    futures.add(brokerClient.close());
+    if (migrationConsumer != null) {
+      futures.add(migrationConsumer.unregister());
+    }
+    Future.all(futures)
+      .onSuccess(e -> stopPromise.tryComplete())
+      .onFailure(err -> {
+        log.error("Failed to unregister consumers", err);
+        stopPromise.tryFail(err);
+      });
   }
 }
--- communication/src/main/java/org/entcore/communication/Communication.java
+++ communication/src/main/java/org/entcore/communication/Communication.java
@@ -20,14 +20,26 @@
 package org.entcore.communication;
 
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.entcore.common.http.BaseServer;
 import org.entcore.common.notification.TimelineHelper;
 import org.entcore.communication.controllers.CommunicationController;
 import org.entcore.communication.filters.CommunicationFilter;
+import org.entcore.communication.services.CommunicationService;
+import org.entcore.communication.services.impl.BrokerSwitchCommunicationService;
+import org.entcore.communication.services.impl.BrokerSwitchType;
 import org.entcore.communication.services.impl.DefaultCommunicationService;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Communication extends BaseServer {
+
+	private Logger log = LoggerFactory.getLogger(Communication.class);
 
 	@Override
 	public void start(final Promise<Void> startPromise) throws Exception {
@@ -35,10 +47,32 @@ public class Communication extends BaseServer {
     TimelineHelper helper = new TimelineHelper(vertx, vertx.eventBus(), config);
 		CommunicationController communicationController = new CommunicationController();
 
-		communicationController.setCommunicationService(new DefaultCommunicationService(helper, config.getJsonArray("discoverVisibleExpectedProfile", new JsonArray())));
+		final DefaultCommunicationService communicationService = new DefaultCommunicationService(helper, config.getJsonArray("discoverVisibleExpectedProfile", new JsonArray()));
+		final CommunicationService brokerSwitchCommunicationService = getSwitchservice(communicationService, vertx, config);
+		communicationController.setCommunicationService(brokerSwitchCommunicationService);
 
 		addController(communicationController);
 		setDefaultResourceFilter(new CommunicationFilter());
+	}
+
+	private CommunicationService getSwitchservice(final CommunicationService communicationService,
+																														final Vertx vertx,
+																														final JsonObject config) {
+		final JsonObject brokerConfig = config.getJsonObject("migration", new JsonObject());
+		if (brokerConfig.getBoolean("enabled", false)) {
+			log.info("Using service switch for communication service");
+			final BrokerSwitchType switchType = BrokerSwitchType.valueOf(
+					brokerConfig.getString("switch-type", BrokerSwitchType.READ_LEGACY_WRITE_LEGACY.name()));
+			log.info("Switch type: " + switchType);
+			final Set<String> availableReadActions = brokerConfig.getJsonArray("available-read-actions", new JsonArray())
+				.stream()
+				.map(o -> (String)o)
+				.collect(Collectors.toSet());
+			return new BrokerSwitchCommunicationService(communicationService, switchType, availableReadActions, vertx.eventBus());
+		} else {
+			log.info("Legacy communication service only");
+			return communicationService;
+		}
 	}
 
 }
