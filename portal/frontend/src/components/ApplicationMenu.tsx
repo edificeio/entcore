ff706a408ad8139c4248af0a07bf7888fa1a217a
evanthonyly <anthony.ly@edifice.io>
2025-06-16 11:18:23 +0200

    chore: link and icon on app  menu (#784)

--- portal/frontend/src/components/ApplicationMenu.tsx
+++ portal/frontend/src/components/ApplicationMenu.tsx
@@ -1,88 +1,48 @@
-import { Dropdown, useEdificeClient, useLibraryUrl } from '@edifice.io/react';
-import {
-  IconInfoCircle,
-  IconExternalLink,
-  IconStar,
-  IconStarFull,
-  IconLibrary,
-} from '@edifice.io/react/icons';
-import { useTranslation } from 'react-i18next';
-import { Application } from '~/models/application';
+import { Dropdown, useLibraryUrl } from "@edifice.io/react";
+import { IconInfoCircle, IconExternalLink, IconStar, IconLibrary } from "@edifice.io/react/icons";
+import { useTranslation } from "react-i18next";
+import { Application } from "~/models/application";
+import { openInNewTab } from "~/utils/open-in-new-tab";
 
-export function ApplicationMenu({
-  data,
-  onToggleFavorite,
-  onActionDone,
-}: {
-  data: Application;
-  onToggleFavorite: () => void;
-  onActionDone: () => void;
-}) {
+export function ApplicationMenu({ data }: { data: Application }) {
   const { t } = useTranslation('common');
   const libraryUrl = useLibraryUrl(data.displayName);
-  const { currentLanguage } = useEdificeClient();
-  const helpUrl =
-    currentLanguage && data.help?.[currentLanguage]
-      ? data.help[currentLanguage]
-      : (data.help?.['en'] ?? undefined);
-  const dataIdFavorite = data.isFavorite
-    ? 'btn-remove-favorite'
-    : 'btn-add-favorite';
-
-  const openInNewTab = (url: string, e?: React.MouseEvent) => {
-    e?.preventDefault();
-    onActionDone();
-    window.open(url, '_blank', 'noopener,noreferrer');
-  };
 
   const dropdownItems = [
     <Dropdown.Item
-      data-id="btn-open"
       key="open"
-      onClick={(e?: React.MouseEvent) => {
-        e?.preventDefault();
-        onActionDone();
-        if (data.category === 'connector' || data.isExternal) {
-          openInNewTab(data.address);
-        } else {
-          window.open(data.address, '_self');
-        }
-      }}
+      onClick={
+        data.category === 'connector'
+          ? openInNewTab(data.address)
+          : () => window.open(data.address)
+      }
       icon={<IconExternalLink />}
     >
       {t('my.apps.open.application')}
     </Dropdown.Item>,
+
     <Dropdown.Item
-      data-id={dataIdFavorite}
       key="favorite"
-      onClick={(e?: React.MouseEvent) => {
-        e?.preventDefault();
-        onActionDone();
-        onToggleFavorite();
-      }}
-      icon={data.isFavorite ? <IconStar /> : <IconStarFull />}
+      onClick={() => console.log(data.isFavorite ? 'remove favorite' : 'add favorite')}
+      icon={<IconStar />}
     >
-      {data.isFavorite
-        ? t('my.apps.remove.favorite')
-        : t('my.apps.add.favorite')}
+      {data.isFavorite ? t('my.apps.remove.favorite') : t('my.apps.add.favorite')}
     </Dropdown.Item>,
 
-    data.libraries && libraryUrl && (
+    data.libraries && (
       <Dropdown.Item
-        data-id="btn-libraries"
         key="examples"
-        onClick={(e) => openInNewTab(libraryUrl, e)}
+        onClick={openInNewTab(libraryUrl)}
         icon={<IconLibrary />}
       >
         {t('my.apps.examples')}
       </Dropdown.Item>
     ),
 
-    helpUrl && (
+    data.help && ( 
       <Dropdown.Item
-        data-id="btn-help"
         key="help"
-        onClick={(e) => openInNewTab(helpUrl, e)}
+        onClick={openInNewTab(data.help?.fr)}
         icon={<IconInfoCircle />}
       >
         {t('my.apps.infos')}
