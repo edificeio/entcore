92e52068121423d2eec49f20ffb96315095bcbe6
evanthonyly <anthony.ly@edifice.io>
2025-06-17 15:29:13 +0200

    chore: user apps preferences services

--- portal/frontend/src/services/api/index.ts
+++ portal/frontend/src/services/api/index.ts
@@ -1,3 +1,5 @@
 import { createApplicationsService } from './applicationsService';
+import { createPreferencesService } from './preferencesService';
 
 export const applicationsService = createApplicationsService();
+export const preferencesService = createPreferencesService();
