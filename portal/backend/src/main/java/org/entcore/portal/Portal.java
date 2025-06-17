9bd2bc625d25b8236a11e3c600846540e62ee32b
dboissin <damien.boissin@web-education.net>
2025-06-17 18:09:11 +0200

    fix(portal): move widgets registration from infra starter to portal

--- portal/backend/src/main/java/org/entcore/portal/Portal.java
+++ portal/backend/src/main/java/org/entcore/portal/Portal.java
@@ -19,8 +19,17 @@
 
 package org.entcore.portal;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Promise;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.file.FileProps;
 import io.vertx.core.json.JsonObject;
+
+import static fr.wseduc.webutils.Utils.handlerToAsyncHandler;
+
+import java.io.File;
+import java.util.List;
 
 import org.entcore.broker.api.utils.AddressParameter;
 import org.entcore.broker.api.utils.BrokerProxyUtils;
@@ -48,10 +57,60 @@ public class Portal extends BaseServer {
         final CacheService cacheService = CacheService.create(vertx);
         BrokerProxyUtils.addBrokerProxy(new I18nBrokerListenerImpl(vertx, assetPath, cacheService), vertx, parameter);
         BrokerProxyUtils.addBrokerProxy(new EventBrokerListenerImpl(), vertx);
-        addController(new PortalController());
-        startPromise.tryComplete();
-      })
-      .onFailure(ex -> log.error("Error when start Infra server super classes", ex));
+        addController(new PortalController(skins));
+			registerGlobalWidgets(config.getString("widgets-path", config.getString("assets-path", ".") + "/assets/widgets"));
+			startPromise.tryComplete();
+		})
+		.onFailure(ex -> log.error("Error when start Infra server super classes", ex));
+	}
+
+	private void registerWidget(final String widgetPath){
+		final String widgetName = new File(widgetPath).getName();
+		JsonObject widget = new JsonObject()
+				.put("name", widgetName)
+				.put("js", "/assets/widgets/"+widgetName+"/"+widgetName+".js")
+				.put("path", "/assets/widgets/"+widgetName+"/"+widgetName+".html");
+
+		if(vertx.fileSystem().existsBlocking(widgetPath + "/i18n")){
+			widget.put("i18n", "/assets/widgets/"+widgetName+"/i18n");
+		}
+
+		JsonObject message = new JsonObject()
+				.put("widget", widget);
+		vertx.eventBus().request("wse.app.registry.widgets", message, handlerToAsyncHandler(new Handler<Message<JsonObject>>() {
+			public void handle(Message<JsonObject> event) {
+				if("error".equals(event.body().getString("status"))){
+					log.error("Error while registering widget "+widgetName+". "+event.body().getJsonArray("errors"));
+					return;
+				}
+				log.info("Successfully registered widget "+widgetName);
+			}
+		}));
+	}
+
+	private void registerGlobalWidgets(String widgetsPath) {
+		vertx.fileSystem().readDir(widgetsPath, new Handler<AsyncResult<List<String>>>() {
+			public void handle(AsyncResult<List<String>> asyn) {
+				if(asyn.failed()){
+					log.error("Error while registering global widgets.", asyn.cause());
+					return;
+				}
+				final List<String> paths = asyn.result();
+				for(final String path: paths){
+					vertx.fileSystem().props(path, new Handler<AsyncResult<FileProps>>() {
+						public void handle(AsyncResult<FileProps> asyn) {
+							if(asyn.failed()){
+								log.error("Error while registering global widget " + path, asyn.cause());
+								return;
+							}
+							if(asyn.result().isDirectory()){
+								registerWidget(path);
+							}
+						}
+					});
+				}
+			}
+		});
 	}
 
 }
