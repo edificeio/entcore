883114ae1cab6e91c5f7e5985e7c98925b1c3ca6
evanthonyly <anthony.ly@edifice.io>
2025-06-18 15:42:12 +0200

    chore: data-id for plan test (#792)
    * chore: display help english fallback or none
    
    * chore: add data-id for test plan
    
    * chore: add scroll on tabs responsive

--- portal/frontend/src/features/application-list/ApplicationListGrid.tsx
+++ portal/frontend/src/features/application-list/ApplicationListGrid.tsx
@@ -1,13 +1,25 @@
 import { ApplicationWrapper } from "~/components/ApplicationWrapper";
 import { Application } from "~/models/application";
 
-export function ApplicationListGrid({ applications }: { applications: Application[] }) {
-  return <div
-    className="d-flex flex-wrap gap-16 justify-content-center mx-auto"
-    style={{ maxWidth: 1091 }}
-  >
-    {applications.map((application) => (
-      <ApplicationWrapper key={application.name} data={application} />
-    ))}
-  </div>
+export function ApplicationListGrid({
+  applications,
+  isConnectors,
+}: {
+  applications: Application[];
+  isConnectors?: boolean;
+}) {
+  const dataId = isConnectors
+    ? 'applications-list-connectors'
+    : 'applications-list';
+  return (
+    <div
+      data-id={dataId}
+      className="d-flex flex-wrap gap-16 justify-content-center mx-auto"
+      style={{ maxWidth: 1091 }}
+    >
+      {applications.map((application) => (
+        <ApplicationWrapper key={application.name} data={application} />
+      ))}
+    </div>
+  );
 }
