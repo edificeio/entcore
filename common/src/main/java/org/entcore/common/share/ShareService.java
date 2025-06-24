9086b5cd5cea979375947d41302241752ac64653
Vincent Billard <vincent.billard@edifice.io>
2025-06-24 15:08:46 +0200

    add javadoc

--- common/src/main/java/org/entcore/common/share/ShareService.java
+++ common/src/main/java/org/entcore/common/share/ShareService.java
@@ -33,6 +33,10 @@ import java.util.Set;
 
 public interface ShareService {
 
+	/**
+	 * Parameter use in userUtils to add a filter on the visible query on group or ids.
+	 * This constant is used as a key in additionalParams to pass the list of ids to check
+	 */
 	String EXPECTED_IDS_USERS_GROUPS = "expectedIdsOfUsersAndGroups";
 
 	void inheritShareInfos(String userId, String resourceId, String acceptLanguage, String search,
