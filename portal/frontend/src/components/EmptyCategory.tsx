892cab37e05fdea0cae0e05fe60e9b5e743ce88a
evanthonyly <anthony.ly@edifice.io>
2025-06-26 11:55:32 +0200

    chore: search behavior

--- portal/frontend/src/components/EmptyCategory.tsx
+++ portal/frontend/src/components/EmptyCategory.tsx
@@ -14,6 +14,16 @@ export function EmptyCategory({ category }: { category: Category}) {
 		/>
 	}
 
+	if (category === 'search') {
+    return (
+      <EmptyScreen
+        imageSrc={emptyFavoritesImage}
+        text={t('my.apps.empty.favorite.text')}
+        title={t('my.apps.empty.favorite.title')}
+      />
+    );
+  }
+
 	return <EmptyScreen
 		imageSrc="https://edificeio.github.io/edifice-frontend-framework/assets/illu-search-DLlTIc41.svg"
 		text={t('my.apps.empty.apps.text')}
