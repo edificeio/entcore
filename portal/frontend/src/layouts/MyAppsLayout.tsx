ecd960740a699a8b9e47c27c5f33edcf9767a82a
evanthonyly <anthony.ly@edifice.io>
2025-07-01 15:19:21 +0200

    chore: search on my apps (#823)
    * chore: search behavior
    
    * chore: search behavior and empty state

--- portal/frontend/src/layouts/MyAppsLayout.tsx
+++ portal/frontend/src/layouts/MyAppsLayout.tsx
@@ -4,6 +4,8 @@ import { ToolbarCategories } from '~/components/ToolbarCategories';
 import { useHydrateUserPreferences } from '~/hooks/useHydrateUserPreferences';
 import './my-apps.css';
 import { useApplications } from '~/services';
+import { SearchBar } from '@edifice.io/react';
+import { useMemo, useState } from 'react';
 
 export const MyAppLayout = ({ theme }: { theme: string }) => {
   const { t } = useTranslation('common');
@@ -11,18 +13,46 @@ export const MyAppLayout = ({ theme }: { theme: string }) => {
 
   const { isHydrated } = useHydrateUserPreferences();
   const { applications, isLoading, isError } = useApplications();
+  const [search, setSearch] = useState('');
 
   const isReady =
     isHydrated && !isLoading && !isError && applications !== undefined;
+
+  const normalizeLoose = (str: string) =>
+    str
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .toLocaleLowerCase();
+
+  const filteredApps = useMemo(() => {
+    if (!applications) return [];
+
+    const searchNorm = normalizeLoose(search);
+
+    return applications.filter((app) =>
+      normalizeLoose(app.appName).includes(searchNorm),
+    );
+  }, [applications, search]);
+
   if (!isReady) return <div>skeleton</div>;
 
   return (
     <div className={classLayout}>
-      <header>
+      <header className="d-flex justify-content-between align-items-center my-apps-header">
         <h1 className="m-0 h3 text-info">{t('navbar.applications')}</h1>
+        <SearchBar
+          isVariant
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder={t('my.apps.search')}
+          className="my-apps-search"
+          size="md"
+        />
       </header>
-      <ToolbarCategories />
-      <ApplicationList applications={applications} />
+      {!search.length && <ToolbarCategories />}
+      <ApplicationList
+        applications={filteredApps}
+        isSearch={search.length ? true : false}
+      />
     </div>
   );
 };
--- portal/frontend/src/models/category.ts
+++ portal/frontend/src/models/category.ts
@@ -5,4 +5,5 @@ export type Category =
   | 'pedagogy'
   | 'organisation'
   | 'connector'
+  | 'search'
   | 'none';
