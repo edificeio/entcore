f793da3e8cf3dee93ab44d5abf43377734ed27d4
evanthonyly <anthony.ly@edifice.io>
2025-07-01 16:34:31 +0200

    chore: empty illustration on assets (#826)

--- portal/frontend/src/components/EmptyCategory.tsx
+++ portal/frontend/src/components/EmptyCategory.tsx
@@ -2,31 +2,36 @@ import { EmptyScreen } from "@edifice.io/react";
 import { useTranslation } from "react-i18next";
 import { Category } from "~/models/category";
 import emptyFavoritesImage from '~/assets/illu-empty-apps-favorite.svg';
+import emptyImage from '~/assets/illu-empty-apps.svg';
 
-export function EmptyCategory({ category }: { category: Category}) {
-	const { t } = useTranslation('common');
+export function EmptyCategory({ category }: { category: Category }) {
+  const { t } = useTranslation('common');
 
-	if(category === 'favorites') {
-		return <EmptyScreen
-			imageSrc={emptyFavoritesImage}
-			text={t('my.apps.empty.favorite.text')}
-			title={t('my.apps.empty.favorite.title')}
-		/>
-	}
-
-	if (category === 'search') {
+  if (category === 'favorites') {
     return (
       <EmptyScreen
-        imageSrc="https://edificeio.github.io/edifice-frontend-framework/assets/illu-search-DLlTIc41.svg"
+        imageSrc={emptyFavoritesImage}
+        text={t('my.apps.empty.favorite.text')}
+        title={t('my.apps.empty.favorite.title')}
+      />
+    );
+  }
+
+  if (category === 'search') {
+    return (
+      <EmptyScreen
+        imageSrc={emptyImage}
         text={t('my.apps.empty.search.text')}
         title={t('my.apps.empty.search.title')}
       />
     );
   }
 
-	return <EmptyScreen
-		imageSrc="https://edificeio.github.io/edifice-frontend-framework/assets/illu-search-DLlTIc41.svg"
-		text={t('my.apps.empty.apps.text')}
-		title={t('my.apps.empty.apps.title')}
-	/>
+  return (
+    <EmptyScreen
+      imageSrc={emptyImage}
+      text={t('my.apps.empty.apps.text')}
+      title={t('my.apps.empty.apps.title')}
+    />
+  );
 }
