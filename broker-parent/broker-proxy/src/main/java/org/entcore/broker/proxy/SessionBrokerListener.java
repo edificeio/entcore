4e74f312a492fa48754d831df6830cd370c94158
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2025-09-10 11:15:06 +0200

    feat: #PEDAGO-3032, add refresh session

--- broker-parent/broker-proxy/src/main/java/org/entcore/broker/proxy/SessionBrokerListener.java
+++ broker-parent/broker-proxy/src/main/java/org/entcore/broker/proxy/SessionBrokerListener.java
@@ -4,6 +4,8 @@ import io.vertx.core.Future;
 import org.entcore.broker.api.BrokerListener;
 import org.entcore.broker.api.dto.session.FindSessionRequestDTO;
 import org.entcore.broker.api.dto.session.FindSessionResponseDTO;
+import org.entcore.broker.api.dto.session.RefreshSessionRequestDTO;
+import org.entcore.broker.api.dto.session.RefreshSessionResponseDTO;
 
 
 /**
@@ -21,4 +23,14 @@ public interface SessionBrokerListener {
    */
   @BrokerListener(subject = "session.find", proxy = true)
   Future<FindSessionResponseDTO> findSession(FindSessionRequestDTO request);
+
+  /**
+   * This method is called to refresh (recreate) a session.
+   * It takes a RefreshSessionRequestDTO as input and returns a RefreshSessionResponseDTO.
+   *
+   * @param request The request containing the session info to refresh.
+   * @return The response containing the new sessionId.
+   */
+  @BrokerListener(subject = "session.refresh", proxy = true)
+  Future<RefreshSessionResponseDTO> refreshSession(RefreshSessionRequestDTO request);
 }
