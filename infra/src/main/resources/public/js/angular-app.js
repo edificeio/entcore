d8c562cb795b500781c492985c303197167a437d
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2025-09-16 09:29:47 +0200

    fix(infra,portal): COCO-4365, remove deprecated pdf viewer

--- infra/src/main/resources/public/js/angular-app.js
+++ infra/src/main/resources/public/js/angular-app.js
@@ -5931,81 +5931,16 @@ module.directive("carousel", function () {
 module.directive("pdfViewer", function () {
   return {
     restrict: "E",
-    template:
-      "" +
-      '<div class="file-controls">' +
-      '<i class="previous" ng-click="previousPage()"></i>' +
-      '<i class="next" ng-click="nextPage()"></i>' +
-      "</div>" +
-      '<div class="pagination">' +
-      '<input type="text" ng-model="pageIndex" ng-change="openPage()" /> / [[numPages]]' +
-      "</div>",
-    link: function (scope, element, attributes) {
-      var pdf;
-      scope.pageIndex = 1;
-      scope.nextPage = function () {
-        if (scope.pageIndex < scope.numPages) {
-          scope.pageIndex++;
-          scope.openPage();
-        }
-      };
-      scope.previousPage = function () {
-        if (scope.pageIndex > 0) {
-          scope.pageIndex--;
-          scope.openPage();
-        }
-      };
-      scope.openPage = function () {
-        var pageNumber = parseInt(scope.pageIndex);
-        if (!pageNumber) {
-          return;
-        }
-        if (pageNumber < 1) {
-          pageNumber = 1;
-        }
-        if (pageNumber > scope.numPages) {
-          pageNumber = scope.numPages;
-        }
-        pdf.getPage(pageNumber).then(function (page) {
-          var viewport;
-          if (!$(canvas).hasClass("fullscreen")) {
-            viewport = page.getViewport(1);
-            var scale = element.width() / viewport.width;
-            viewport = page.getViewport(scale);
-          } else {
-            viewport = page.getViewport(2);
-          }
-
-          var context = canvas.getContext("2d");
-          canvas.height = viewport.height;
-          canvas.width = viewport.width;
-
-          var renderContext = {
-            canvasContext: context,
-            viewport: viewport,
-          };
-          page.render(renderContext);
-        });
-      };
-      scope.$parent.render = scope.openPage;
-
-      window.PDFJS = {
-        workerSrc: "/infra/public/js/viewers/pdf.js/pdf.worker.js",
-      };
-      var canvas = document.createElement("canvas");
-      $(canvas).addClass("render");
-      element.append(canvas);
-      loader.openFile({
-        url: "/infra/public/js/viewers/pdf.js/pdf.js",
-        success: function () {
-          PDFJS.getDocument(attributes.ngSrc).then(function (file) {
-            pdf = file;
-            scope.numPages = pdf.pdfInfo.numPages;
-            scope.$apply("numPages");
-            scope.openPage();
-          });
-        },
-      });
+    template: "",
+    link: function (_scope, element, attributes) {
+      if (navigator.pdfViewerEnabled) {
+        let reference = window.open(attributes.ngSrc, "pdfviewer");
+        reference && reference.focus();
+      } else {
+        let span = document.createElement("span");
+        span.innerText = lang.translate("embed.video.incompatible");
+        element.append(span);
+      }
     },
   };
 });
