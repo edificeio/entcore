27d9cff8e3e59f2c877df6e57f13d979e6ba9df0
Mattox <96073465+Mattox0@users.noreply.github.com>
2025-09-26 16:49:02 +0200

    feat(sso-flashquizz): add SSO for Flashquizz (#14)
    * feat(sso-flashquizz): add sso flashquizz

---
+++ auth/src/main/java/org/entcore/auth/services/impl/SSOFlashquizz.java
@@ -0,0 +1,31 @@
+package org.entcore.auth.services.impl;
+
+import fr.wseduc.webutils.Either;
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.opensaml.saml2.core.Assertion;
+
+public class SSOFlashquizz extends AbstractSSOProvider {
+    @Override
+    public void generate(EventBus eb, String userId, String host, String serviceProviderEntityId, Handler<Either<String, JsonArray>> handler) {
+        JsonObject request = new JsonObject()
+                .put("userId", userId)
+                .put("host", host)
+                .put("serviceProviderEntityId", serviceProviderEntityId);
+        eb.request("fr.openent.ssoflashquizz", request, reply -> {
+            if (reply.succeeded()) {
+                JsonArray response = (JsonArray) reply.result().body();
+                handler.handle(new Either.Right<String, JsonArray>(response));
+            } else {
+                handler.handle(new Either.Left<String, JsonArray>(reply.cause().getMessage()));
+            }
+        });
+    }
+
+    @Override
+    public void execute(Assertion assertion, Handler<Either<String, Object>> handler) {
+        handler.handle(new Either.Left<>("[SSOFlashquizz::execute] execute function not available on SSO Flashquizz Implementation"));
+    }
+}
