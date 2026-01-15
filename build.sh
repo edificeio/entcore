2eddbc5a09dc42fda9f745fd70349afbe3c1e7f1
Vincent Billard <vincent.billard@edifice.io>
2026-01-15 09:24:43 +0100

    fix publish stage in ci

--- build.sh
+++ build.sh
@@ -258,10 +258,10 @@ publish() {
     *)         export nexusRepository='releases' ;;
   esac
 
-  docker compose run --rm  maven mvn $MVN_OPTS -DrepositoryId=ode-$nexusRepository -DskipTests --settings /var/maven/.m2/settings.xml deploy
-  #cd broker-parent/broker-client/quarkus
-  #./build.sh publish
-  #cd -
+  docker compose run --rm  maven mvn  $MVN_OPTS -DrepositoryId=ode-$nexusRepository -DskipTests --settings /var/maven/.m2/settings.xml deploy
+  cd broker-parent/broker-client/quarkus
+  ./build.sh publish
+  cd -
   publishBrokerNpmLib
 }
 
