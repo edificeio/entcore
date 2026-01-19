66071455c983f807d7e6df9ec0b4f3074ef19d9f
moustaphahennawi <56674781+moustaphahennawi@users.noreply.github.com>
2026-01-19 14:25:44 +0100

    chore(sso): SRE-4712 create SSO Swarm

---
+++ auth/src/main/java/org/entcore/auth/services/impl/SSOSwarm.java
@@ -0,0 +1,64 @@
+package org.entcore.auth.services.impl;
+
+import fr.wseduc.webutils.Either;
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import org.entcore.common.events.EventStore;
+import org.entcore.common.events.EventStoreFactory;
+import org.entcore.common.neo4j.Neo4j;
+import org.entcore.common.neo4j.Neo4jResult;
+import org.entcore.common.user.UserUtils;
+import org.opensaml.saml2.core.Assertion;
+
+import static org.entcore.common.aggregation.MongoConstants.TRACE_TYPE_CONNECTOR;
+
+public class SSOSwarm extends AbstractSSOProvider {
+
+
+    private final EventStore eventStore = EventStoreFactory.getFactory().getEventStore(this.getClass().getSimpleName());
+    @Override
+    public void generate(EventBus eb, String userId, String host, String serviceProviderEntityId, Handler<Either<String, JsonArray>> handler) {
+        String query = "MATCH (u:User {id:{userId}})\n" +
+                "RETURN u.id AS id,\n" +
+                "u.login AS login,\n" +
+                "u.displayName AS displayName,\n" +
+                "u.email AS email,\n" +
+                "u.lastName AS lastName,\n" +
+                "u.firstName AS firstName,\n" +
+                "u.externalId AS externalId";
+
+        Neo4j.getInstance().execute(query, new JsonObject().put("userId", userId), Neo4jResult.validUniqueResultHandler(evt -> {
+            if (evt.isLeft()) {
+                handler.handle(new Either.Left(evt.left().getValue()));
+                return;
+            }
+
+            JsonArray result = new JsonArray();
+            JsonObject user = evt.right().getValue();
+            result.add(new JsonObject().put("id", user.getString("id", "id")));
+            result.add(new JsonObject().put("username", user.getString("login", "")));
+            result.add(new JsonObject().put("displayName", user.getString("displayName", "")));
+            result.add(new JsonObject().put("email", user.getString("email", "")));
+            result.add(new JsonObject().put("lastName", user.getString("lastName", "")));
+            result.add(new JsonObject().put("firstName", user.getString("firstName", "")));
+            result.add(new JsonObject().put("externalId", user.getString("externalId", "")));
+            UserUtils.getUserInfos(eb, userId, userInfos -> {
+                if(userInfos != null) {
+                    JsonObject customAttributes = new JsonObject().put("service", host).put("connector-type", "saml")
+                                    .put("saml-type", this.getClass().getSimpleName());
+                    eventStore.createAndStoreEvent(TRACE_TYPE_CONNECTOR, userInfos, customAttributes);
+                }
+
+            });
+
+            handler.handle(new Either.Right<>(result));
+        }));
+    }
+
+    @Override
+    public void execute(Assertion assertion, Handler<Either<String, Object>> handler) {
+        handler.handle(new Either.Left<String, Object>("execute function ot available on SSO Swarm Implementation"));
+    }
+}
