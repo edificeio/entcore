0422d93152e93c6f65b1fc0cbe53252fd327e76d
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-01-21 16:01:37 +0100

    ci: skip build clients except if -DbuildClients=true

--- build.sh
+++ build.sh
@@ -204,9 +204,9 @@ install () {
 }
 
 buildBroker () {
-  cd broker-parent/broker-client/quarkus
-  ./build.sh install
-  cd -
+  edifice install --all=false --clients=false --client-nest=true
+  #./build.sh install
+  #cd -
 }
 
 test () {
@@ -257,10 +257,10 @@ publish() {
     *)         export nexusRepository='releases' ;;
   esac
 
-  docker compose run --rm  maven mvn -DrepositoryId=ode-$nexusRepository -DskipTests --settings /var/maven/.m2/settings.xml deploy
-  cd broker-parent/broker-client/quarkus
-  ./build.sh publish
-  cd -
+  docker compose run --rm  maven mvn $MVN_OPTS -DrepositoryId=ode-$nexusRepository -DskipTests --settings /var/maven/.m2/settings.xml deploy
+  #cd broker-parent/broker-client/quarkus
+  #./build.sh publish
+  #cd -
   publishBrokerNpmLib
 }
 
