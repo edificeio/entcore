fb6a024e714283c20d71680e59cb2d123f5c0476
Nabil Mansouri <nabil.mansouri.3@gmail.com>
2026-01-28 09:26:28 +0100

    feat: #PEDAGO-3671, use new lool modal

--- workspace/src/main/resources/public/ts/app.ts
+++ workspace/src/main/resources/public/ts/app.ts
@@ -10,31 +10,34 @@ import { csvViewer } from './directives/csvViewer';
 import { txtViewer } from './directives/txtViewer';
 
 routes.define(function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			action: 'openOwn'
-		})
-		.when('/folder/:folderId', {
-			action: 'viewFolder'
-		})
-		.when('/shared/folder/:folderId', {
-			action: 'viewSharedFolder'
-		})
-		.when('/shared', {
-			action: 'openShared'
-		})
-		.when('/trash', {
-			action: 'openTrash'
-		})
-		.when('/apps', {
-			action: 'openApps'
-		})
-		.when('/external', {
-			action: 'openExternal'
-		})
-		.otherwise({
-			redirectTo: '/'
-		})
+  $routeProvider
+    .when("/", {
+      action: "openOwn",
+    })
+    .when("/folder/:folderId", {
+      action: "viewFolder",
+    })
+    .when("/shared/folder/:folderId", {
+      action: "viewSharedFolder",
+    })
+    .when("/shared", {
+      action: "openShared",
+    })
+    .when("/trash", {
+      action: "openTrash",
+    })
+    .when("/apps", {
+      action: "openApps",
+    })
+    .when("/external", {
+      action: "openExternal",
+    })
+    .when("/lool", {
+      action: "openLoolModal",
+    })
+    .otherwise({
+      redirectTo: "/",
+    });
 });
 
 ng.controllers.push(workspaceController);
