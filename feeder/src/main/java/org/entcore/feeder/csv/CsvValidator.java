bce3b821abde043135fd6f24ebec8118e794d7d1
pb-jo <93938819+pb-jo@users.noreply.github.com>
2026-03-05 14:59:36 +0100

    fix(feeder): #IMPULS-2069, import CSV explicit error message (#970, #1016, #1070)

--- feeder/src/main/java/org/entcore/feeder/csv/CsvValidator.java
+++ feeder/src/main/java/org/entcore/feeder/csv/CsvValidator.java
@@ -354,10 +354,19 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 					if (invalidColumns.size() > 0 ) {
 						parseErrors("invalid.column", invalidColumns, profile, handler);
 						return;
-					} else if (!columns.contains("classes") && !columns.contains("childClasses")) {
+					} 
+					// Relative profile: childClasses is required when linking students by name/first name
+					else if ( "Relative".equals(profile) && 
+							  !columns.contains("childExternalId") && 
+							  (columns.contains("childLastName") || columns.contains("childFirstName")) &&
+							  !columns.contains("classes") && 
+							  !columns.contains("childClasses")) {
+						addErrorByFile(profile, "missing.column.childClasses");
 						handler.handle(result);
 						return;
-					} else {
+					} 
+					// No error
+					else {
 						int j = 0;
 						for (String column : columns) {
 							if ("classes".equals(column) || "childClasses".equals(column)) {
@@ -375,11 +384,12 @@ public class CsvValidator extends CsvReport implements ImportValidator {
 				}
 				i++;
 			}
+			handler.handle(result);
 		} catch (Exception e) {
 			addError(profile, "csv.exception");
+			handler.handle(result);
 			log.error("csv.exception", e);
 		} finally {
-			handler.handle(result);
 			if(csvParser!=null) {
 				try {
 					csvParser.close();
