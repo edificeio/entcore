ec265bd9ded83f4a5add368fa77c95674b459851
vbillard91 <vincent.billard@edifice.io>
2026-03-25 16:19:34 +0100

    feat(directory): #COCO-5522 add new normalized endpoint to set and retreive preference for homepage or other module (#980)

---
+++ directory/src/main/java/org/entcore/directory/services/PreferenceCacheService.java
@@ -0,0 +1,25 @@
+package org.entcore.directory.services;
+
+import io.vertx.core.json.JsonObject;
+import org.entcore.common.user.UserInfos;
+import org.entcore.directory.pojo.dto.UserPreferenceDto;
+
+public interface PreferenceCacheService {
+
+    /**
+     * Update preferences in cache (managed in the session of the user). Use the full preferences from neo4j (legacy)
+     *
+     * @param userInfos
+     * @param preferences
+     */
+    void refreshPreferences(UserInfos userInfos, UserPreferenceDto preferences);
+
+    /**
+     * Add preferences defined in the dto to the cache (session)
+     *
+      * @param userInfos
+     * @param session
+     * @param preference
+     */
+    void addPreferences(UserInfos userInfos, JsonObject session, UserPreferenceDto preference);
+}
