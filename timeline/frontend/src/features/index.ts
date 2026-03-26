381db85888b587d364401133cc87e39750d60c84
Pascal S. <pascal.saussier@edifice.io>
2026-03-26 15:06:34 +0100

    feat:#COCO-5070 add boilerplate for new homepage (#979)
    * feat: add boilerplate for new homepage
    
    * chore : replace docker-compose deprecated command
    
    * chore: fix build.sh for homepage
    
    * chore: rename index.html to homepage.html
    
    * feedback from copilot
    
    ---------
    
    Co-authored-by: jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>

---
+++ timeline/frontend/src/features/index.ts
@@ -0,0 +1,7 @@
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ This is a starter file and can be deleted.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ This folder should contain all features in an app
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
