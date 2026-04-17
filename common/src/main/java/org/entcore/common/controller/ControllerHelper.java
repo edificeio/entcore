557a0a18cbdd50932f1e57ba372b69292f2637b0
vbillard91 <vincent.billard@edifice.io>
2026-04-17 15:17:35 +0200

    fix(common): #COCO-5693 add missing resource id in share notifications (#1000)

--- common/src/main/java/org/entcore/common/controller/ControllerHelper.java
+++ common/src/main/java/org/entcore/common/controller/ControllerHelper.java
@@ -337,24 +337,21 @@ public abstract class ControllerHelper extends BaseController implements Shareab
 				.put("resourceUri", pathPrefix + "/" + resource);
 		}
 		final JsonObject params = p;
-		crudService.retrieve(resource, new Handler<Either<String, JsonObject>>() {
-			@Override
-			public void handle(Either<String, JsonObject> r) {
-				if (r.isRight()) {
-					String attr = (resourceNameAttribute != null && !resourceNameAttribute.trim().isEmpty()) ?
-							resourceNameAttribute : "name";
-					params.put("resourceName", r.right().getValue().getString(attr, ""));
-					final Object preview = params.remove("preview");
-					if (preview instanceof JsonObject) {
-						notification.notifyTimeline(request, notificationName, user, recipients, null, null, params, false, (JsonObject) preview);
-					} else {
-						notification.notifyTimeline(request, notificationName, user, recipients, params);
-					}
-				} else {
-					log.error("Unable to send timeline notification : missing name on resource " + resource);
-				}
-			}
-		});
+		crudService.retrieve(resource, r -> {
+            if (r.isRight()) {
+                String attr = (resourceNameAttribute != null && !resourceNameAttribute.trim().isEmpty()) ?
+                        resourceNameAttribute : "name";
+                params.put("resourceName", r.right().getValue().getString(attr, ""));
+                final Object preview = params.remove("preview");
+                if (preview instanceof JsonObject) {
+                    notification.notifyTimeline(request, notificationName, user, recipients, resource, null, params, false, (JsonObject) preview);
+                } else {
+                    notification.notifyTimeline(request, notificationName, user, recipients, resource, params );
+                }
+            } else {
+                log.error("Unable to send timeline notification : missing name on resource " + resource);
+            }
+        });
 	}
 
 	protected void shareResource(final HttpServerRequest request, final String notificationName,
