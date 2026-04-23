d57a0ee8da681c4d80a25d7f804a158e7976d63a
Pascal S. <pascal.saussier@edifice.io>
2026-04-23 10:28:24 +0200

    feat(wayfv2): #ENABLING-713 add boilerplate for new React wayf app (#997)
    * feat: #ENABLING-713 init wayf v2 React version
    
    * feat: #ENABLING-713 add scaffolding for react wayf v2
    
    * chore: use cookie to test feature
    
    * chore: review, clean and optimization

---
+++ auth/frontend/src/config/index.ts
@@ -0,0 +1,7 @@
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ This is a starter file and can be deleted.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ This folder should contain all config and constants
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
