621f49693de40cf43562960f58deab09b5a4b0ae
Damien Romito Castro <damien.romito-castro@edifice.io>
2026-05-11 14:39:49 +0200

    export preferenceService

--- timeline/frontend/src/components/BetaSwitch/useBetaSwitch.ts
+++ timeline/frontend/src/components/BetaSwitch/useBetaSwitch.ts
@@ -1,15 +1,7 @@
-import { odeServices } from '@edifice.io/client';
 import { useToast } from '@edifice.io/react';
 import { useState } from 'react';
 import { useI18n } from '~/hooks/useI18n';
-
-type UserPrefs = { homePage: { betaEnabled: boolean } | null };
-
-function deactivateHomepage() {
-  return odeServices.http().put<UserPrefs>('/userbook/api/preferences', {
-    homePage: { betaEnabled: false },
-  });
-}
+import { preferenceService } from '~/services';
 
 export function useBetaSwitch() {
   const { common_t } = useI18n();
@@ -20,7 +12,7 @@ export function useBetaSwitch() {
     setIsSwitching(true);
 
     try {
-      await deactivateHomepage();
+      await preferenceService.deactivateHomepage();
       window.location.reload();
     } catch {
       toast.error(common_t('betaSwitch.error'));
