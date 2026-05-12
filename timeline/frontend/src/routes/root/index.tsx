0566a2996a61e65c9d88b7de541b5c03151d4acb
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-05-12 18:25:04 +0200

    fix(homepage): #IMPULS-5441, add SchoolSpaceContainer

--- timeline/frontend/src/routes/root/index.tsx
+++ timeline/frontend/src/routes/root/index.tsx
@@ -2,6 +2,7 @@ import { LoadingScreen, PageLayout, useEdificeClient } from '@edifice.io/react';
 import {
   LastInfosContainer,
   MessageFlashListContainer,
+  SchoolSpaceContainer,
 } from '@edifice.io/react/homepage';
 import { BetaSwitchContainer } from '~/components/BetaSwitch/BetaSwitchContainer';
 
@@ -19,7 +20,7 @@ export const Root = () => {
     <PageLayout scrollMode="columns" variant="fullpage">
       <PageLayout.Header />
       <PageLayout.SidebarLeft className="d-grid align-content-start bg-white py-16 gap-16">
-        {/* <SchoolSpaceContainer /> */}
+        <SchoolSpaceContainer />
         <LastInfosContainer />
       </PageLayout.SidebarLeft>
       <PageLayout.Content className="d-grid align-content-start py-16 gap-16">
