441a3bd8072e944cf446c6f061f016e9009ddd01
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-05-19 18:27:56 +0200

    test(communication): #ENABLING-726, add tests on every communication service endpoint

--- tests/src/test/js/it/scenarios/communication/_utils.ts
+++ tests/src/test/js/it/scenarios/communication/_utils.ts
@@ -1,8 +1,11 @@
 import { check } from "k6";
 import {
+  getHeaders,
     Visible,
   } from "../../../node_modules/edifice-k6-commons/dist/index.js";
+import http from "k6/http";
 
+const rootUrl = __ENV.ROOT_URL;
 
 export function checkSearchVisible(res, expectedUserFields:string[], expectedGroupFields:string[], checkName) {
   const checks = {}
@@ -20,4 +23,74 @@ export function checkSearchVisible(res, expectedUserFields:string[], expectedGro
   if(!ok) {
     console.error(checkName, res)
   }
+}
+
+export function checkTrue(res: any, name: string, actual: boolean) {
+  const ok = check(res, {
+    [`${name}`]: () => actual === true,
+  });
+  if(!ok) {
+    console.error(`Was expecting ${name} to be true`);
+  }
+  return ok;
+}
+
+export function checkFalse(res: any, name: string, actual: boolean) {
+  const ok = check(res, {
+    [`${name}`]: () => actual === false,
+  });
+  if(!ok) {
+    console.error(`Was expecting ${name} to be false`);
+  }
+  return ok;
+}
+
+export function checkEquals(res: any, name: string, expected: any, actual: any) {
+  const ok = check(res, {
+    [`${name}`]: () => expected === actual,
+  });
+  if(!ok) {
+    console.error(`Was expecting ${name} to equal ${expected} but got ${actual}`);
+  }
+  return ok;
+}
+
+export function checkNotEquals(res: any, name: string, expected: any, actual: any) {
+  const ok = check(res, {
+    [`${name}`]: () => expected !== actual,
+  });
+  if(!ok) {
+    console.error(`Was expecting ${name} to not equal ${expected}`);
+  }
+  return ok;
+}
+
+export function checkGte(res: any, name: string, expected: number, actual: number) {
+  const ok = check(res, {
+    [`${name}`]: () => actual >= expected,
+  });
+  if(!ok) {
+    console.error(`Was expecting ${name} to be greater than or equal to ${expected} but got ${actual}`);
+  }
+  return ok;
+}
+
+export function checkStatus(res: any, name: string, expectedStatus: number) {
+  return checkEquals(res, name, expectedStatus, res.status);
+}
+
+export function checkContains(res: any, name: string, array: any[], predicate: (item: any) => boolean) {
+  return checkTrue(res, name, array.some(predicate));
+}
+
+export function checkNotContains(res: any, name: string, array: any[], predicate: (item: any) => boolean) {
+  return checkFalse(res, name, array.some(predicate));
+}
+
+export function checkUsersCanCommunicate(user1Id: string, user2Id: string, expectedCanCommunicate: boolean) {
+  const res = http.get(
+    `${rootUrl}/communication/verify/${user1Id}/${user2Id}`,
+    { headers: getHeaders() },
+  );
+  return res.status === 200 && JSON.parse(<string>res.body).canCommunicate === expectedCanCommunicate;
 }
