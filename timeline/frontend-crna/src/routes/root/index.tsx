ee2d49bf9ed3281977d87fede5cacc17632776d3
moustaphahennawi <56674781+moustaphahennawi@users.noreply.github.com>
2026-05-20 18:02:45 +0200

    chore: change illustration

--- timeline/frontend-crna/src/routes/root/index.tsx
+++ timeline/frontend-crna/src/routes/root/index.tsx
@@ -6,36 +6,18 @@ import {
 } from '@edifice.io/react';
 import {
   LastInfosContainer,
-  MessageFlashList,
   MessageFlashListContainer,
-  SchoolSpace,
+  SchoolSpaceContainer,
 } from '@edifice.io/react/homepage';
 import { useState } from 'react';
 import backgroundImage from '~/assets/background.png';
 import { MediacentreWidget, WidgetMasonry } from '~/components';
-import { AppFooter } from '~/components/AppFooter';
 import { AvantagesWidget } from '~/components/AvantagesWidget/AvantagesWidget';
 import { BetaSwitchContainer } from '~/components/BetaSwitch/BetaSwitchContainer';
 import { CarnetDeBordWidget } from '~/components/CarnetDeBordWidget';
 import { WidgetErrorBoundary } from '~/components/ui/WidgetErrorBoundary';
 import { WelcomeWidget } from '~/components/WelcomeWidget';
 
-const MOCK_SCHOOLS = [
-  {
-    id: 'school-1',
-    name: 'Collège Jean Moulin',
-    UAI: '0012345A',
-    classes: [],
-    exports: [],
-  },
-  {
-    id: 'school-2',
-    name: 'Lycée Jeanne Ferry de Loisette en Royan',
-    UAI: '0098765Z',
-    classes: [],
-    exports: [],
-  },
-];
 
 /** Check old format URL and redirect if needed */
 export const loader = async () => {
@@ -44,7 +26,6 @@ export const loader = async () => {
 
 export const Root = () => {
   const { init } = useEdificeClient();
-  const [selectedSchool, setSelectedSchool] = useState(MOCK_SCHOOLS[1]);
   const [pageErrors, setPageErrors] = useState<string[]>([]);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
@@ -93,18 +74,15 @@ export const Root = () => {
             {successMessage}
           </Alert>
         )}
-        <SchoolSpace
-          schools={MOCK_SCHOOLS}
-          selectedSchool={selectedSchool}
-          onSelectedSchoolChange={(idx) => setSelectedSchool(MOCK_SCHOOLS[idx])}
-        />
+        <SchoolSpaceContainer />
+
         <WidgetErrorBoundary>
           <LastInfosContainer />
         </WidgetErrorBoundary>
       </PageLayout.SidebarLeft>
       <PageLayout.Content className="d-grid align-content-start py-16 gap-16">
         <BetaSwitchContainer />
-        <MessageFlashList
+        {/* <MessageFlashList
           messages={[
             {
               author: 'Platform Team',
@@ -119,7 +97,7 @@ export const Root = () => {
               title: 'Welcome to the new platform!',
             },
           ]}
-        />
+        /> */}
         <MessageFlashListContainer />
 
         <WidgetErrorBoundary>
@@ -135,7 +113,6 @@ export const Root = () => {
             <CarnetDeBordWidget onError={handleWidgetError} />
           </WidgetErrorBoundary>
         </WidgetMasonry>
-        <AppFooter />
       </PageLayout.Content>
     </PageLayout>
   );
