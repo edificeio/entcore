c3f2e2b7188e5a966d6a7153951b2b6d51a2b8d6
RomuC <romuald.coulaud@edifice.io>
2026-06-02 15:41:53 +0200

    feat(timeline): #IMPULS-5664 add notifications (#1025)
    * feat/IMPULS_add-notification-container
    
    * update translation + padding
    
    * update layout to have open & close behaviour.
    
    * update with notification container
    
    * fix review
    
    ---------
    
    Co-authored-by: Damien Romito Castro <damien.romito-castro@edifice.io>

--- timeline/frontend/src/routes/root/index.tsx
+++ timeline/frontend/src/routes/root/index.tsx
@@ -1,10 +1,19 @@
-import { LoadingScreen, PageLayout, useEdificeClient } from '@edifice.io/react';
+import {
+  ButtonBeta,
+  LoadingScreen,
+  PageLayout,
+  useEdificeClient,
+} from '@edifice.io/react';
 import {
   LastInfosContainer,
   MessageFlashListContainer,
+  NotificationListContainer,
   SchoolSpaceContainer,
 } from '@edifice.io/react/homepage';
+import { IconClose } from '@edifice.io/react/icons';
+import { useTranslation } from 'react-i18next';
 import { BetaSwitchContainer } from '~/components/BetaSwitch/BetaSwitchContainer';
+import { useNotificationsLayout } from './hooks/useNotificationsLayout';
 
 /** Check old format URL and redirect if needed */
 export const loader = async () => {
@@ -13,12 +22,21 @@ export const loader = async () => {
 
 export const Root = () => {
   const { init } = useEdificeClient();
+  const { isSidebarOpen, toggleNotifications, closeNotifications } =
+    useNotificationsLayout();
+  const { t } = useTranslation();
 
   if (!init) return <LoadingScreen position={false} />;
 
   return (
-    <PageLayout scrollMode="columns" variant="fullpage">
-      <PageLayout.Header />
+    <PageLayout
+      scrollMode="columns"
+      variant="fullpage"
+      noPadding={{
+        sidebarRight: true,
+      }}
+    >
+      <PageLayout.Header onNotificationsClick={toggleNotifications} />
       <PageLayout.SidebarLeft className="d-grid align-content-start bg-white py-16 gap-16">
         <SchoolSpaceContainer />
         <LastInfosContainer />
@@ -27,6 +45,31 @@ export const Root = () => {
         <BetaSwitchContainer />
         <MessageFlashListContainer />
       </PageLayout.Content>
+
+      {isSidebarOpen ? (
+        <PageLayout.SidebarRight className="position-relative">
+          <ButtonBeta
+            aria-label={t('close')}
+            color="tertiary"
+            leftIcon={<IconClose />}
+            type="button"
+            variant="ghost"
+            title={t('close')}
+            onClick={closeNotifications}
+            className="pagelayout-sidebar-close-button"
+            data-testid="pagelayout-sidebar-close-button"
+          />
+          <NotificationListContainer />
+        </PageLayout.SidebarRight>
+      ) : (
+        <PageLayout.Overlay
+          closeButton={true}
+          onClose={closeNotifications}
+          backdrop={true}
+        >
+          <NotificationListContainer />
+        </PageLayout.Overlay>
+      )}
     </PageLayout>
   );
 };
