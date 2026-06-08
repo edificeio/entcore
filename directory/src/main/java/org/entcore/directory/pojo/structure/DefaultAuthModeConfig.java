fee7cb97a57acb33b955ff6b57a07d5b47626a9b
vbillard91 <vincent.billard@edifice.io>
2026-06-08 14:20:39 +0200

    feat(directory): #IMPULS-5911 add endpoint to update and retreive default auth configuration (#1033)
    * feat(directory): #IMPULS-5911 add endpoint to update and retreive default auth configuration
    
    * fix rebase
    
    * fix rebase

---
+++ directory/src/main/java/org/entcore/directory/pojo/structure/DefaultAuthModeConfig.java
@@ -0,0 +1,48 @@
+package org.entcore.directory.pojo.structure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DefaultAuthModeConfig {
+
+    private final Map<Profile, DefaultAuthMode> defaultAuthModes = new HashMap<>();
+
+    public Map<Profile, DefaultAuthMode> getDefaultAuthModes() {
+        return defaultAuthModes;
+    }
+
+    public enum DefaultAuthMode {
+        ENT,
+        FEDERATED;
+    }
+
+    public enum Profile {
+
+        PERSONNEL("Personnel"),
+        STUDENT("Student"),
+        TEACHER("Teacher"),
+        RELATIVE("Relative"),
+        GUEST("Guest");
+
+        private final String neo4jName;
+
+        Profile(String neo4jName) {
+            this.neo4jName = neo4jName;
+        }
+
+        public static Profile fromNeo4j(String profile) {
+            for (Profile p : Profile.values()) {
+                if (p.neo4jName.equals(profile)) {
+                    return p;
+                }
+            }
+            return null;
+        }
+
+        public String getNeo4jName(){
+            return neo4jName;
+        }
+
+
+    }
+}
