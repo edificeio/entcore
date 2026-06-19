6f12bed9999cf6ce09ff6d1f6f597976db4b1a51
vbillard91 <vincent.billard@edifice.io>
2026-06-19 11:31:49 +0200

    feat(directory): #IMPULS-5922 filter by default federated users from pdf and mail export (#1044)
    * add resilience to massmail param includeFederated

--- directory/src/test/java/org/entcore/directory/services/impl/DefaultMassMailServiceTest.java
+++ directory/src/test/java/org/entcore/directory/services/impl/DefaultMassMailServiceTest.java
@@ -76,13 +76,14 @@ public class DefaultMassMailServiceTest {
 
     /**
      * <h1>Goal</h1>
-     * <p>Ensures that massmailUsers returns the users of the structure with federated flag = false when the user is
-     * not federated and federated flag = true when federated = true and federatedIDP != null.</p>
+     * <p>Ensures that massmailUsers, when "includeFederated" is set, returns the users of the structure with
+     * federated flag = false when the user is not federated and federated flag = true when federated = true and
+     * federatedIDP != null.</p>
      */
     @Test
     public void testMassmailUsersReturnsFederatedFlag(final TestContext testContext) {
         final Async async = testContext.async();
-        defaultMassMailService.massmailUsers("massmail-structure-01", new JsonObject().put("activated", "all"), superAdmin(), h -> {
+        defaultMassMailService.massmailUsers("massmail-structure-01", new JsonObject().put("activated", "all").put("includeFederated", true), superAdmin(), h -> {
             testContext.assertTrue(h.isRight(), "Failed to find users of structure " + h);
             final JsonArray users = h.right().getValue();
             final JsonObject simpleStudent = findById(users, student.getId());
@@ -97,18 +98,51 @@ public class DefaultMassMailServiceTest {
 
     /**
      * <h1>Goal</h1>
-     * <p>Ensures that massmailUsers returns the users with federated flag = true when the user is in a structure
-     * with a federated auth default.</p>
+     * <p>Ensures that massmailUsers, when "includeFederated" is set, returns the users with federated flag = true
+     * when the user is in a structure with a federated auth default.</p>
      */
     @Test
     public void testMassmailUsersOnFederatedStructure(final TestContext testContext) {
         final Async async = testContext.async();
-        defaultMassMailService.massmailUsers("massmail-structure-02", new JsonObject().put("activated", "all"), superAdmin(), h -> {
+        defaultMassMailService.massmailUsers("massmail-structure-02", new JsonObject().put("activated", "all").put("includeFederated", true), superAdmin(), h -> {
             testContext.assertTrue(h.isRight(), "Failed to find users of structure " + h);
             final JsonArray users = h.right().getValue();
             final JsonObject studentOnFederatedStructure = findById(users, studentOnStructureWithIdp.getId());
             testContext.assertNotNull(studentOnFederatedStructure, "Student of the federated structure should be in the structure");
             testContext.assertEquals(Boolean.TRUE, studentOnFederatedStructure.getBoolean("hasFederatedIdentity"));
+            async.complete();
+        });
+    }
+
+    /**
+     * <h1>Goal</h1>
+     * <p>Ensures that massmailUsers excludes federated users (federated identity) by default, i.e. when the
+     * "includeFederated" filter is not set, while still returning non-federated users.</p>
+     */
+    @Test
+    public void testMassmailUsersExcludesFederatedUsersByDefault(final TestContext testContext) {
+        final Async async = testContext.async();
+        defaultMassMailService.massmailUsers("massmail-structure-01", new JsonObject().put("activated", "all"), superAdmin(), h -> {
+            testContext.assertTrue(h.isRight(), "Failed to find users of structure " + h);
+            final JsonArray users = h.right().getValue();
+            testContext.assertNotNull(findById(users, student.getId()), "Simple student should be in the structure");
+            testContext.assertNull(findById(users, studentFederated.getId()), "Federated student should be excluded by default");
+            async.complete();
+        });
+    }
+
+    /**
+     * <h1>Goal</h1>
+     * <p>Ensures that massmailUsers excludes users belonging to a structure with a federated auth default by
+     * default, i.e. when the "includeFederated" filter is not set.</p>
+     */
+    @Test
+    public void testMassmailUsersExcludesFederatedStructureUsersByDefault(final TestContext testContext) {
+        final Async async = testContext.async();
+        defaultMassMailService.massmailUsers("massmail-structure-02", new JsonObject().put("activated", "all"), superAdmin(), h -> {
+            testContext.assertTrue(h.isRight(), "Failed to find users of structure " + h);
+            final JsonArray users = h.right().getValue();
+            testContext.assertNull(findById(users, studentOnStructureWithIdp.getId()), "Student of the federated structure should be excluded by default");
             async.complete();
         });
     }
