e926acf3892e88758c63325c6bb66cbf4709f263
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-08-24 17:47:34 +0200

    feat(homepage): #IMPULS-6022, customization form save

--- timeline/frontend/src/services/api/customizeService.ts
+++ timeline/frontend/src/services/api/customizeService.ts
@@ -4,4 +4,9 @@ export const customizeService = {
   listLanguages: () => odeServices.http().get<string[]>('/languages'),
 
   listFonts: () => odeServices.http().get<IThemeDesc[]>('/themes'),
+
+  saveLanguagePreference: (lang: string) =>
+    odeServices.conf().savePreference('language', {
+      'default-domain': lang,
+    }),
 };
