2a87e793f2ebfa2e024ff7256e2b86c1e54084f6
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-08-25 09:21:02 +0200

    wip

--- timeline/frontend/src/components/CustomizeForm/ChoiceButton.tsx
+++ timeline/frontend/src/components/CustomizeForm/ChoiceButton.tsx
@@ -1,21 +1,26 @@
 import { ButtonBeta, ButtonBetaProps } from '@edifice.io/react';
+import clsx from 'clsx';
+import './ChoiceButton.css';
 
 export type ChoiceButtonProps = ButtonBetaProps & { isSelected: boolean };
 
 export const ChoiceButton = ({
   children,
+  className: baseClassName,
   isSelected,
-  onClick: handleClick,
+  ...buttonBetaProps
 }: ChoiceButtonProps) => {
+  const className = clsx('choice-button', baseClassName, {
+    isSelected: isSelected,
+  });
   return (
-    <>
-      <ButtonBeta
-        variant={isSelected ? 'filled' : 'outline'}
-        aria-pressed={isSelected}
-        onClick={handleClick}
-      >
-        {children}
-      </ButtonBeta>
-    </>
+    <ButtonBeta
+      variant={isSelected ? 'filled' : 'outline'}
+      aria-pressed={isSelected}
+      className={className}
+      {...buttonBetaProps}
+    >
+      {children}
+    </ButtonBeta>
   );
 };
