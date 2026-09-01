8dcd10dc58e085ffc3f773c999d9164f85f05848
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-09-01 10:19:52 +0200

    wip: backgrounds frontend

--- timeline/frontend/src/services/queries/customize.ts
+++ timeline/frontend/src/services/queries/customize.ts
@@ -1,5 +1,6 @@
 import { useToast } from '@edifice.io/react';
 import { queryOptions, useMutation, useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 import { useI18n } from '~/hooks/useI18n';
 import { customizeService } from '../api/customizeService';
 
@@ -10,6 +11,7 @@ export const customizeQueryKeys = {
   all: () => ['customize'],
   languages: () => [...customizeQueryKeys.all(), 'languages'],
   fonts: () => [...customizeQueryKeys.all(), 'fonts'],
+  backgrounds: () => [...customizeQueryKeys.all(), 'backgrounds'],
 };
 
 /**
@@ -36,22 +38,61 @@ export const customizeQueryOptions = {
       staleTime: Infinity,
     });
   },
+  /**
+   * @returns Query options for fetching the list of available backgrounds. The query is cached indefinitely since it is not expected to change.
+   */
+  getBackgrounds() {
+    return queryOptions({
+      queryKey: customizeQueryKeys.backgrounds(),
+      queryFn: () => customizeService.listBackgrounds(),
+      staleTime: Infinity,
+    });
+  },
 };
 
-export const useLanguages = () =>
-  useQuery(customizeQueryOptions.getLanguages());
-
-export const useFonts = () => useQuery(customizeQueryOptions.getFonts());
-
-export const useSaveCustomization = () => {
+export const useCustomization = () => {
   const { t } = useI18n();
   const toast = useToast();
 
-  return useMutation({
+  const { data: languages, isError: isLanguagesError } = useQuery(
+    customizeQueryOptions.getLanguages(),
+  );
+  const { data: fonts, isError: isFontsError } = useQuery(
+    customizeQueryOptions.getFonts(),
+  );
+  const { data: backgrounds, isError: isBackgroundsError } = useQuery(
+    customizeQueryOptions.getBackgrounds(),
+  );
+
+  const saveMutation = useMutation({
     mutationFn: async ({ lang }: { lang: string; font: string }) =>
       await Promise.all([customizeService.saveLanguagePreference(lang)]),
     onSuccess: () => {
       toast.success(t('homepage.customize.form.save.success'));
     },
+    onError: () => {
+      toast.error(t('homepage.customize.form.save.error'));
+    },
   });
+
+  useEffect(() => {
+    if (isLanguagesError)
+      toast.error(
+        t('homepage.customize.form.load.error', { code: 'languages' }),
+      );
+    if (isFontsError)
+      toast.error(t('homepage.customize.form.load.error', { code: 'fonts' }));
+    if (isBackgroundsError)
+      toast.error(
+        t('homepage.customize.form.load.error', { code: 'backgrounds' }),
+      );
+  }, [isLanguagesError, isFontsError, isBackgroundsError]);
+
+  return {
+    languages,
+    fonts,
+    backgrounds,
+    isError: isLanguagesError || isFontsError || isBackgroundsError,
+    saveMutation,
+  };
 };
