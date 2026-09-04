fa3b86d0d1af54b8e0e9199624c3ef32663f9f74
jcbe-ode <jean-christophe.benoit@opendigitaleducation.com>
2026-09-04 09:55:15 +0200

    revert preferenceService

--- timeline/frontend/src/services/api/preferenceService.ts
+++ timeline/frontend/src/services/api/preferenceService.ts
@@ -2,20 +2,12 @@ import { odeServices } from '@edifice.io/client';
 
 type UserPrefs = {
   homePage: { betaEnabled: boolean } | null;
-  background: string | null;
 };
 
 export const preferenceService = {
   deactivateHomepage: () => {
     return odeServices.http().put<UserPrefs>('/userbook/api/preferences', {
       homePage: { betaEnabled: false },
-    });
-  },
-
-  saveCustomization: (language: string, background: string) => {
-    return odeServices.http().put('/userbook/api/preferences', {
-      language: { 'default-domain': language },
-      background,
     });
   },
 };
